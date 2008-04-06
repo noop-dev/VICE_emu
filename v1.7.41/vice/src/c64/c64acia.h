f79e8982443aec37e88ce62019a4292d5157014e
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 10:24:56 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.7.41'.

---
+++ v1.7.41/vice/src/c64/c64acia.h
@@ -0,0 +1,34 @@
+/*
+ *
+ * This file is part of Commodore 64 emulator.
+ * See README for copyright notice
+ *
+ * ACIA 6551 register number declarations.
+ * Interrupt signals are defined in interrupt.h.
+ *
+ * Written by
+ *    Andre Fachat <a.fachat@physik.tu-chemnitz.de>
+ *
+ */
+
+#ifndef _C64ACIA_H
+#define _C64ACIA_H
+
+#include "types.h"
+
+struct snapshot_s;
+
+extern void acia1_init(void);
+extern void acia1_reset(void);
+extern BYTE REGPARM1 acia1_read(ADDRESS a);
+extern BYTE REGPARM1 acia1_peek(ADDRESS a);
+extern void REGPARM2 acia1_store(ADDRESS a, BYTE b);
+
+extern int acia1_cmdline_options_init(void);
+extern int acia1_resources_init(void);
+
+extern int acia1_write_snapshot_module(struct snapshot_s *p);
+extern int acia1_read_snapshot_module(struct snapshot_s *p);
+
+#endif
+
