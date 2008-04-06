5b40b1401ff03bf4644e53706170bbbea0666395
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 16:16:20 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.14.38'.

---
+++ tags/v1.14/v1.14.38/vice/src/c64/c64io.h
@@ -0,0 +1,41 @@
+/*
+ * c64io.h -- C64 io handling.
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _C64IO_H
+#define _C64IO_H
+ 
+#include "types.h"
+
+extern BYTE REGPARM1 io1_read(WORD addr);
+extern void REGPARM2 io1_store(WORD addr, BYTE value);
+extern BYTE REGPARM1 io2_read(WORD addr);
+extern void REGPARM2 io2_store(WORD addr, BYTE value);
+
+struct mem_ioreg_list_s;
+extern void c64io_ioreg_add_list(struct mem_ioreg_list_s **mem_ioreg_list);
+
+#endif
+
