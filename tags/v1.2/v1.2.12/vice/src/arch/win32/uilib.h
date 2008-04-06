eaf230a4d07a76129588daa23092a4987e5de0ae
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 08:56:56 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.2.12'.

---
+++ tags/v1.2/v1.2.12/vice/src/arch/win32/uilib.h
@@ -0,0 +1,47 @@
+/*
+ * uilib.h - Common UI elements for the Windows user interface.
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *  Manfred Spraul <manfreds@colorfullife.com>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _UILIB_H
+#define _UILIB_H
+
+#include <windows.h>
+
+extern char *ui_select_file(const char *title, const char *filter,
+                            char*(*content_read_function)(const char*),
+                            HWND hwnd);
+extern void ui_set_res_num(char *res, int value, int num);
+
+extern char *read_disk_image_contents(const char *name);
+extern char *read_tape_image_contents(const char *name);
+extern char *read_disk_or_tape_image_contents(const char *name);
+
+extern void ui_show_text(HWND hParent, const char *szCaption,
+                         const char *szHeader, const char *szText);
+
+#endif
+
