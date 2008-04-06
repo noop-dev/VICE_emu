9ef4aee5070fc32aabd951f9c7c142666055e950
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 16:25:23 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.15.3'.

---
+++ tags/v1.15/v1.15.3/vice/src/c64/c64-cmdline-options.c
@@ -0,0 +1,79 @@
+/*
+ * c64-cmdline-options.c
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "c64-cmdline-options.h"
+#include "cmdline.h"
+#include "machine.h"
+
+
+static const cmdline_option_t cmdline_options[] =
+{
+    { "-pal", SET_RESOURCE, 0, NULL, NULL, "MachineVideoStandard",
+      (void *)MACHINE_SYNC_PAL, NULL, "Use PAL sync factor" },
+    { "-ntsc", SET_RESOURCE, 0, NULL, NULL, "MachineVideoStandard",
+      (void *)MACHINE_SYNC_NTSC, NULL, "Use NTSC sync factor" },
+    { "-ntscold", SET_RESOURCE, 0, NULL, NULL, "MachineVideoStandard",
+      (void *)MACHINE_SYNC_NTSCOLD,
+      NULL, "Use old NTSC sync factor" },
+    { "-kernal", SET_RESOURCE, 1, NULL, NULL, "KernalName", NULL,
+      "<name>", "Specify name of Kernal ROM image" },
+    { "-basic", SET_RESOURCE, 1, NULL, NULL, "BasicName", NULL,
+      "<name>", "Specify name of BASIC ROM image" },
+    { "-chargen", SET_RESOURCE, 1, NULL, NULL, "ChargenName", NULL,
+      "<name>", "Specify name of character generator ROM image" },
+    { "-emuid", SET_RESOURCE, 0, NULL, NULL, "EmuID", (void *)1,
+      NULL, "Enable emulator identification" },
+    { "+emuid", SET_RESOURCE, 0, NULL, NULL, "EmuID", (void *)0,
+      NULL, "Disable emulator identification" },
+    { "-kernalrev", SET_RESOURCE, 1, NULL, NULL, "KernalRev", NULL,
+      "<revision>", "Patch the Kernal ROM to the specified <revision>" },
+#ifdef HAVE_RS232
+    { "-acia1", SET_RESOURCE, 0, NULL, NULL, "Acia1Enable", (void *)1,
+      NULL, "Enable the $DE** ACIA RS232 interface emulation" },
+    { "+acia1", SET_RESOURCE, 0, NULL, NULL, "Acia1Enable", (void *)0,
+      NULL, "Disable the $DE** ACIA RS232 interface emulation" },
+#endif
+#ifdef COMMON_KBD
+    { "-keymap", SET_RESOURCE, 1, NULL, NULL, "KeymapIndex", NULL,
+      "<number>", N_("Specify index of keymap file (0=symbol, 1=positional)") },
+    { "-symkeymap", SET_RESOURCE, 1, NULL, NULL, "KeymapSymFile", NULL,
+      "<name>", N_("Specify name of symbolic keymap file") },
+    { "-poskeymap", SET_RESOURCE, 1, NULL, NULL, "KeymapPosFile", NULL,
+      "<name>", N_("Specify name of positional keymap file") },
+#endif
+    { NULL }
+};
+
+int c64_cmdline_options_init(void)
+{
+    return cmdline_register_options(cmdline_options);
+}
+
