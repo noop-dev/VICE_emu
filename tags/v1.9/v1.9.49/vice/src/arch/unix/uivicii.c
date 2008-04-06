d89319f4428e9a930d27f6dee7755558d3e9b501
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 11:53:38 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.9.49'.

---
+++ tags/v1.9/v1.9.49/vice/src/arch/unix/uivicii.c
@@ -0,0 +1,126 @@
+/*
+ * uivicii.c
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "c64ui.h"
+#include "resources.h"
+#include "uipalette.h"
+#include "uimenu.h"
+#include "uivicii.h"
+#include "utils.h"
+#include "uipalemu.h"
+
+
+UI_MENU_DEFINE_STRING_RADIO(PaletteFile)
+
+static ui_menu_entry_t palette_submenu[] = {
+    { N_("*Default"),
+      (ui_callback_t)radio_PaletteFile, (ui_callback_data_t)"default", NULL },
+    { "*C64S",
+      (ui_callback_t)radio_PaletteFile, (ui_callback_data_t)"c64s", NULL },
+    { N_("*CCS64"),
+      (ui_callback_t)radio_PaletteFile, (ui_callback_data_t)"ccs64", NULL },
+    { N_("*Frodo"),
+      (ui_callback_t)radio_PaletteFile, (ui_callback_data_t)"frodo", NULL },
+    { N_("*GoDot"),
+      (ui_callback_t)radio_PaletteFile, (ui_callback_data_t)"godot", NULL },
+    { "*PC64",
+      (ui_callback_t)radio_PaletteFile, (ui_callback_data_t)"pc64", NULL },
+    { "--" },
+    { N_("Load custom"),
+      (ui_callback_t)ui_load_palette,
+      (ui_callback_data_t)"PaletteFile", NULL },
+    { NULL }
+};
+
+UI_MENU_DEFINE_TOGGLE(VICIIDoubleSize)
+UI_MENU_DEFINE_TOGGLE(VICIIDoubleScan)
+UI_MENU_DEFINE_TOGGLE(VICIIVideoCache)
+UI_MENU_DEFINE_TOGGLE(VICIICheckSsColl)
+UI_MENU_DEFINE_TOGGLE(VICIICheckSbColl)
+UI_MENU_DEFINE_TOGGLE(VICIINewLuminances)
+UI_MENU_DEFINE_TOGGLE(ExternalPalette)
+#ifdef USE_XF86_EXTENSIONS
+UI_MENU_DEFINE_TOGGLE(VICIIFullscreen)
+UI_MENU_DEFINE_STRING_RADIO(VICIIFullscreenDevice)
+
+static ui_menu_entry_t set_fullscreen_device_submenu[] = {
+#ifdef USE_XF86_VIDMODE_EXT
+    { "*Vidmode", (ui_callback_t)radio_VICIIFullscreenDevice,
+      (ui_callback_data_t)"Vidmode", NULL },
+#endif
+#ifdef USE_XF86_DGA2_EXTENSIONS
+    { "*DGA2", (ui_callback_t)radio_VICIIFullscreenDevice,
+      (ui_callback_data_t)"DGA2", NULL },
+#endif
+    { NULL }
+};
+
+#endif
+
+ui_menu_entry_t vic_submenu[] = {
+    { N_("*Double size"),
+      (ui_callback_t)toggle_VICIIDoubleSize, NULL, NULL },
+    { N_("*Double scan"),
+      (ui_callback_t)toggle_VICIIDoubleScan, NULL, NULL },
+    { N_("*Video cache"),
+      (ui_callback_t)toggle_VICIIVideoCache, NULL, NULL },
+    { "--" },
+#ifdef USE_XF86_EXTENSIONS
+    { N_("*Enable fullscreen"),
+      (ui_callback_t)toggle_VICIIFullscreen, NULL, NULL, XK_d, UI_HOTMOD_META },
+    { N_("Fullscreen device"),
+      NULL, NULL, set_fullscreen_device_submenu },
+    { "--" },
+#ifdef USE_XF86_VIDMODE_EXT
+#endif
+#ifdef USE_XF86_DGA2_EXTENSIONS
+#endif
+#endif
+    { N_("Video standard"),
+      NULL, NULL, set_video_standard_submenu },
+    { "--" },
+    { N_("*Sprite-sprite collisions"),
+      (ui_callback_t)toggle_VICIICheckSsColl, NULL, NULL },
+    { N_("*Sprite-background collisions"),
+      (ui_callback_t)toggle_VICIICheckSbColl, NULL, NULL },
+    { "--" },
+    { N_("*New Luminances"),
+      (ui_callback_t)toggle_VICIINewLuminances, NULL, NULL },
+    { N_("*External color set"),
+      (ui_callback_t)toggle_ExternalPalette, NULL, NULL },
+    { N_("Color set"),
+      NULL, NULL, palette_submenu },
+    { "--" },
+    { N_("PAL Emulation"),
+      NULL, NULL, PALMode_submenu },
+    { NULL }
+};
+
