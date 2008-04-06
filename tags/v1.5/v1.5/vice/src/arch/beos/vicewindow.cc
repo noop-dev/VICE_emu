f8669e9dbc8b87f11db54636b4ccaf15bf8d22ac
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 09:17:43 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.5'.

---
+++ tags/v1.5/v1.5/vice/src/arch/beos/vicewindow.cc
@@ -0,0 +1,254 @@
+/*
+ * vicewindow.cc - Implementation of the BeVICE's window
+ *
+ * Written by
+ *  Andreas Matthies <andreas.matthies@arcormail.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+
+#include <Alert.h>
+#include <Application.h>
+#include <FilePanel.h>
+#include <MenuItem.h>
+
+#include "vicemenu.h"
+
+extern "C" {
+#include "attach.h"
+#include "constants.h"
+#include "datasette.h"
+#include "drive.h"
+#include "info.h"
+#include "interrupt.h"
+#include "kbd.h"
+#include "log.h"
+#include "main.h"
+#include "keyboard.h"
+#include "machine.h"
+#include "maincpu.h"
+#include "main_exit.h"
+#include "resources.h"
+#include "sound.h"
+#include "statusbar.h"
+#include "tape.h"
+#include "ui.h"
+#include "ui_file.h"
+#include "utils.h"
+#include "vicewindow.h"
+#include "vsync.h"
+}
+
+/* FIXME: some stuff we need from the ui module */
+extern ViceWindow 			*windowlist[];
+extern int 					window_count;
+extern ui_res_value_list   	*machine_specific_values;
+extern ui_menu_toggle      	*machine_specific_toggles;
+
+
+void ViceWindow::Update_Menus(
+		ui_menu_toggle *toggle_list,
+		ui_res_value_list *value_list)
+{
+    int i,j;
+    int value;
+    int result;
+	BMenuItem *item;
+	
+	/* the general toggle items */
+	for (i = 0; toggle_list[i].name != NULL; i++) {
+        resources_get_value(toggle_list[i].name, (resource_value_t *) &value);
+        if (item = menubar->FindItem(toggle_list[i].item_id))
+        	item->SetMarked(value ? true : false);
+    }
+    /* the machine specific toggle items */
+    if (machine_specific_toggles) {
+        for (i = 0; machine_specific_toggles[i].name != NULL; i++) {
+            resources_get_value(machine_specific_toggles[i].name, (resource_value_t *) &value);
+    	    if (item = menubar->FindItem(machine_specific_toggles[i].item_id))
+	        	item->SetMarked(value ? true : false);
+        }
+    }
+
+	/* the general multiple-value-items */
+    for (i = 0; value_list[i].name != NULL; i++) {
+        result=resources_get_value(value_list[i].name,
+                                   (resource_value_t *) &value);
+        if (result==0) {
+            for (j = 0; value_list[i].vals[j].item_id != 0; j++) {
+                if (value == value_list[i].vals[j].value) {
+                	/* the corresponding menu is supposed to be in RadioMode */
+                    if (item = menubar->FindItem(value_list[i].vals[j].item_id))
+                    	item->SetMarked(true);
+                }
+            }
+        }
+    }
+	/* the machine specific multiple-value-items */
+    if (machine_specific_values){
+        for (i = 0; machine_specific_values[i].name != NULL; i++) {
+            result=resources_get_value(machine_specific_values[i].name,
+                                       (resource_value_t *) &value);
+            if (result==0) {
+                for (j = 0; machine_specific_values[i].vals[j].item_id != 0; j++) {
+                    if (value == machine_specific_values[i].vals[j].value) {
+                        if (item = menubar->FindItem(machine_specific_values[i].vals[j].item_id))
+                        	item->SetMarked(true);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+/* the view for the emulators bitmap, we need a special Draw() */
+class ViceView : public BView {
+	public:
+		ViceView(BRect rect);
+		virtual void Draw(BRect rect);
+};
+
+ViceView::ViceView(BRect rect) 
+	: BView(rect,"view",B_FOLLOW_LEFT|B_FOLLOW_TOP, B_WILL_DRAW)
+{
+}
+
+void ViceView::Draw(BRect rect) {
+	ViceWindow *wnd = (ViceWindow *)Window();
+	DrawBitmap(wnd->bitmap,rect,rect);
+}
+
+
+ViceWindow::ViceWindow(BRect frame, char const *title) 
+		: BWindow(frame, title,
+		B_TITLED_WINDOW,
+		B_NOT_ZOOMABLE | B_NOT_RESIZABLE) {
+
+	BMenu *menu, *submenu;
+	BMenuItem *item;
+	BRect r;
+
+	/* create the menubar */
+	menubar = menu_create(machine_class);
+	AddChild(menubar);
+
+	/* create the File Panel */
+	filepanel = new BFilePanel(B_OPEN_PANEL, new BMessenger(this), NULL,
+				B_FILE_NODE, false);
+
+	/* create the Save Panel */
+	savepanel = new BFilePanel(B_SAVE_PANEL, new BMessenger(this), NULL,
+				B_FILE_NODE, false);
+		
+	/* the bitmap and the corresponding view */
+	bitmap = new BBitmap(frame,B_CMAP8,false,true);
+	r = menubar->Frame();
+	view = new ViceView(BRect(frame.left,frame.top+r.Height()+1,
+						frame.right,frame.bottom+r.Height()+2));
+	AddChild(view);
+
+	/* the statusbar is created in Resize() */
+	statusbar = NULL;
+
+	/* register the window */
+	windowlist[window_count++] = this;
+
+	/* finally display the window */
+	Resize(frame.Width(), frame.Height());
+	Show();
+}
+
+ViceWindow::~ViceWindow() {
+	RemoveChild(menubar);
+	delete menubar;
+	RemoveChild(view);
+	delete view;
+	RemoveChild(statusbar);
+	delete statusbar;
+	delete filepanel;
+	delete savepanel;
+}
+
+
+bool ViceWindow::QuitRequested() {
+	/* send an exit request to ui's event loop but dont't close the window here */
+	BMessage msg;
+	msg.what = MENU_EXIT_REQUESTED;
+	ui_add_event(&msg);
+	return false;
+}
+
+void ViceWindow::MessageReceived(BMessage *message) {
+	/* FIXME: sometimes the menubar holds the focus so we have to delete it */ 
+	if (CurrentFocus()) {
+		menubar->MakeFocus(false);
+	}
+		
+	ui_add_event(message);
+	switch(message->what) {
+		default:
+			BWindow::MessageReceived(message);
+			break;
+	}
+}
+
+void ViceWindow::Resize(unsigned int width, unsigned int height) {
+	BRect oldsize;
+	BRect statusbar_frame;
+	float new_windowheight;
+	
+	if (BWindow::Lock()) {
+		view->ResizeTo(width, height);
+		if (statusbar) {
+			RemoveChild(statusbar);
+			delete statusbar;
+			statusbar = NULL;
+		}
+		statusbar_frame.top = view->Frame().bottom+1;
+		statusbar_frame.bottom = view->Frame().bottom+41;
+		statusbar_frame.left = 0;
+		statusbar_frame.right = view->Frame().right;
+		statusbar = new ViceStatusbar(statusbar_frame);
+		AddChild(statusbar);
+		ui_statusbar_update();
+		new_windowheight = 	menubar->Frame().Height()+
+			view->Frame().Height()+
+			statusbar->Frame().Height();
+		BWindow::ResizeTo(width, new_windowheight);
+		BWindow::Unlock();
+	}
+	/* create new bitmap if size has changed */
+	/* notice, that BRect sizes are simply differences of coordinates */
+	oldsize = bitmap->Bounds();
+	if ((oldsize.Width()+1) != width || (oldsize.Height()+1) != height) {
+		delete bitmap;
+		bitmap = new BBitmap(BRect(0,0,width-1,height-1),B_CMAP8,false,true);
+	}
+}
+
+void ViceWindow::DrawBitmap(void) {
+	if	(BWindow::Lock()) {
+		view->DrawBitmap(bitmap);
+		view->Sync();
+		BWindow::Unlock();
+	} 
+}
---
+++ tags/v1.5/v1.5/vice/src/pet/petmem.h
@@ -0,0 +1,60 @@
+/*
+ * petmem.h - PET memory handling.
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  André Fachat <fachat@physik.tu-chemnitz.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _PETMEM_H
+#define _PETMEM_H
+
+#include "types.h"
+
+#define PET_RAM_SIZE		0x8000
+#define PET_ROM_SIZE		0x8000
+#define PET_CHARGEN_ROM_SIZE	0x2000
+
+#define	PET_KERNAL1_CHECKSUM	3236
+#define	PET_KERNAL2_CHECKSUM	31896
+#define	PET_KERNAL4_CHECKSUM	53017
+
+#define	PET_EDIT1G_CHECKSUM	51858
+#define	PET_EDIT2G_CHECKSUM	64959
+#define	PET_EDIT2B_CHECKSUM	1514
+#define	PET_EDIT4G40_CHECKSUM	14162
+#define	PET_EDIT4B40_CHECKSUM	27250
+#define	PET_EDIT4B80_CHECKSUM	21166
+
+extern int pet_mem_init_resources(void);
+extern int pet_mem_init_cmdline_options(void);
+
+extern void petmem_reset(void);
+extern int superpet_diag(void);
+
+extern int petmem_dump(FILE *fp);
+extern int petmem_undump(FILE *fp);
+
+extern void set_screen(void);
+
+#endif
+
