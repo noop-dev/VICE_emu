bddcfff7e0297be63f1ddfcdc73bacfe32b60a44
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 07:46:23 +0000

    This commit was manufactured by cvs2svn to create tag 'v0.14.2.35'.

---
+++ v0.14.2.35/vice/acconfig.h
@@ -0,0 +1,26 @@
+/* Define to your X11 display depth.  */
+#undef X_DISPLAY_DEPTH
+
+/* Define if you want to use Rober W. McMullen's TextField widget.  */
+#undef ENABLE_TEXTFIELD
+
+/* The number of bytes in an unsigned long.  */
+#undef SIZEOF_UNSIGNED_LONG
+
+/* The number of bytes in an unsigned int.  */
+#undef SIZEOF_UNSIGNED_INT
+
+/* The number of bytes in an unsigned short.  */
+#undef SIZEOF_UNSIGNED_SHORT
+
+/* Does libXext have XShmPutImage?  */
+#undef HAVE_XSHMPUTIMAGE
+
+/* Are we using the `readline' library or one replacement?  */
+#undef HAVE_READLINE
+
+/* Package name.  */
+#define PACKAGE ""
+
+/* Version number.  */
+#define VERSION ""
