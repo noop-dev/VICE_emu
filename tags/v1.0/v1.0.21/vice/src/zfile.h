3f8b128cea1e8052199c74f15362fd01b7d8502d
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 08:25:17 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.0.21'.

---
+++ tags/v1.0/v1.0.21/vice/src/zfile.h
@@ -0,0 +1,65 @@
+/*
+ * zfile.h - Transparent handling of compressed files.
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _ZFILE_H
+#define _ZFILE_H
+
+#ifdef STDC_HEADERS
+#ifndef __riscos
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#endif
+#include <stdio.h>
+#endif
+
+#if defined(WIN32) || defined(WINCE) || defined(__riscos)
+#ifndef _MODE_T_
+typedef int mode_t;
+#endif
+#endif
+
+#ifdef OS2
+#include <string.h>
+#endif
+
+/* actions to be done when a zfile is closed */
+typedef enum {
+	ZFILE_KEEP,		/* nothing, keep original file (default) */
+	ZFILE_REQUEST,		/* request the user what to do */
+	ZFILE_DEL		/* remove original file */
+} zfile_action_t;
+
+file_desc_t zopen(const char *name, mode_t mode, int flags);
+int zclose(file_desc_t fd);
+FILE *zfopen(const char *name, const char *mode);
+int zfclose(FILE *stream);
+int zclose_all(void);
+
+int zfile_close_action(const char *filename, zfile_action_t action,
+				const char *request_string);
+
+#endif /* _ZFILE_H */
