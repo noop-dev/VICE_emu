d7a04be68a9ab9e281914eab0536d7336b0d5eb8
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 07:47:26 +0000

    This commit was manufactured by cvs2svn to create tag 'v0.14.2.43'.

---
+++ v0.14.2.43/vice/src/c64/rsuser.h
@@ -0,0 +1,55 @@
+
+
+/*
+ * pruser.h - Printer device for userport.
+ *
+ * Written by
+ *  André Fachat        (a.fachat@physik.tu-chemnitz.de)
+ *
+ * Patches by
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _RSUSER_H_
+#define _RSUSER_H_
+
+#define	RTS_OUT		0x02
+#define	DTR_OUT		0x04
+
+#define	DCD_IN		0x10
+#define	CTS_IN		0x40
+#define	DSR_IN		0x80
+
+void rsuser_init(void);
+
+int rsuser_init_resources(void);
+int rsuser_init_cmdline_options(void);
+
+int rsuser_enabled;
+
+void userport_serial_write_sr(BYTE);
+void userport_serial_write_ctrl(int);
+BYTE userport_serial_read_ctrl();
+
+int int_rsuser(long offset);
+
+#endif
+
