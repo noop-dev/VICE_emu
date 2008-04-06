ff836802a400761d6c06c92b729a224c22964ddd
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 08:41:08 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.1.7'.

---
+++ tags/v1.1/v1.1.7/vice/src/c64/psid.h
@@ -0,0 +1,38 @@
+/*
+ * psid.h - PSID file handling.
+ *
+ * Written by
+ *  Dag Lem <resid@nimrod.no>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _PSID_H
+#define _PSID_H
+
+#include "types.h"
+
+int psid_init_resources(void);
+int psid_init_cmdline_options(void);
+int psid_load_file(const char* filename);
+void psid_init_tune(void);
+void psid_init_driver(void);
+
+#endif
