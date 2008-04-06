d4ca843835ff4de222ec1f99f8470d66387e29c5
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 08:51:20 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.2'.

---
+++ tags/v1.2/v1.2/vice/src/arch/os2/types.h
@@ -0,0 +1,121 @@
+/*
+ * types.h - Generic type definitions for VICE/2.
+ *
+ * Written by
+ *  Thomas Bretz (tbretz@gsi.de)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _VICE_TYPES_H
+#define _VICE_TYPES_H
+
+#include <limits.h>
+
+#undef VERSION
+
+#define INCL_TYPES
+#define INCL_WINSYS
+#define INCL_WININPUT
+#define INCL_GPIBITMAPS
+#define INCL_WINPALETTE
+#define INCL_DOSPROCESS
+#define INCL_WINFRAMEMGR
+#define INCL_WINWINDOWMGR
+#define INCL_WINRECTANGLES
+#include <os2.h>
+#define INCL_OS2MM
+#include <os2me.h>
+
+#undef VERSION
+#define VERSION VICE2VERSION
+
+#ifdef __IBMC__
+ #define inline _Inline  // This means only a possible inline. See doku.
+ #define getcwd _getcwd
+ #define R_OK   4
+ #define W_OK   2
+ typedef int pid_t;
+ #ifndef __EXTENDED__
+ typedef long off_t;
+ #endif
+ #define STDOUT_FILENO (0xFFFF & fileno(stdout))
+ #define STDERR_FILENO (0xFFFF & fileno(stderr))
+ #define _O_BINARY O_BINARY
+ #define _O_TRUNC  O_TRUNC
+ #define _O_WRONLY O_WRONLY
+ #define _O_CREAT  O_CREAT
+ #define _P_WAIT   P_WAIT
+#endif
+
+#ifdef __EMX__
+  #define vfork fork
+#endif
+
+typedef signed char SIGNED_CHAR;
+
+typedef unsigned short   WORD;
+typedef   signed short  SWORD;
+typedef   signed int   SDWORD;
+typedef unsigned long   DWORD;
+
+#undef ADDRESS
+typedef WORD ADDRESS;
+
+typedef DWORD CLOCK;
+/* Maximum value of a CLOCK.  */
+#define CLOCK_MAX (~((CLOCK)0))
+
+#if X_DISPLAY_DEPTH == 16
+typedef WORD		  		PIXEL;
+#elif X_DISPLAY_DEPTH == 24
+typedef DWORD				PIXEL;
+#elif X_DISPLAY_DEPTH == 8 || X_DISPLAY_DEPTH == 0
+//#ifndef OS2
+#undef PIXEL
+typedef BYTE		PIXEL;
+//#endif
+#else
+#error Unsupported display depth!
+#endif
+
+#if X_DISPLAY_DEPTH == 8 || X_DISPLAY_DEPTH == 0
+typedef WORD		PIXEL2;
+typedef DWORD		PIXEL4;
+#else
+typedef struct { PIXEL a, b; }		PIXEL2;
+typedef struct { PIXEL a, b, c, d; }	PIXEL4;
+#endif
+
+#if defined(__GNUC__) && defined(__i386__) && !defined(NO_REGPARM) && !defined(OS2)
+#define REGPARM1 __attribute__((regparm(1)))
+#define REGPARM2 __attribute__((regparm(2)))
+#define REGPARM3 __attribute__((regparm(3)))
+#else
+#define REGPARM1
+#define REGPARM2
+#define REGPARM3
+#endif
+
+/* Use ANSI IO on RISC OS */
+typedef int file_desc_t;
+#define ILLEGAL_FILE_DESC      -1
+
+#endif  /* _VICE_TYPES_H */
---
+++ tags/v1.2/v1.2/vice/src/sounddrv/soundwav.c
@@ -0,0 +1,142 @@
+/*
+ * soundwav.c - Implementation of the RIFF/WAV dump sound device
+ *
+ * Written by
+ *  Teemu Rantanen <tvr@cs.hut.fi>
+ *  Dag Lem <resid@nimrod.no>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "sound.h"
+#include "types.h"
+#include "utils.h"
+
+static FILE *wav_fd = NULL;
+static int samples = 0;
+
+static int wav_init(warn_t *w, const char *param, int *speed,
+		   int *fragsize, int *fragnr, double bufsize)
+{
+    /* RIFF/WAV header. */
+    BYTE header[45] =
+      "RIFFxxxxWAVEfmt \020\0\0\0\001\0\001\0xxxxxxxx\002\0\020\0dataxxxx";
+    DWORD samples_per_sec = *speed;
+    DWORD bytes_per_sec = *speed*2;
+    int i;
+
+    if (!param)
+	param = "vicesnd.wav";
+    wav_fd = fopen(param, "w");
+    if (!wav_fd)
+	return 1;
+
+    /* Reset number of samples. */
+    samples = 0;
+
+    /* Sampling rate and bytes per second stored as little endian numbers. */
+    for (i = 0; i < 4; i++) {
+      header[24 + i] = samples_per_sec & 0xff;
+      header[28 + i] = bytes_per_sec & 0xff;
+      samples_per_sec >>= 8;
+      bytes_per_sec >>= 8;
+    }
+
+    if (fwrite(header, 1, 44, wav_fd) != 44)
+	return 1;
+
+    return 0;
+}
+
+static int wav_write(warn_t *w, SWORD *pbuf, size_t nr)
+{
+    int	i;
+
+    /* Swap bytes on big endian machines. */
+#ifdef WORDS_BIGENDIAN
+    for (i = 0; i < nr; i++) {
+      pbuf[i] = (((WORD)pbuf[i] & 0xff) << 8) | ((WORD)pbuf[i] >> 8);
+    }
+#endif
+
+    i = fwrite(pbuf, sizeof(SWORD), nr, wav_fd);
+    if (i != nr)
+	return 1;
+
+    /* Swap the bytes back just in case. */
+#ifdef WORDS_BIGENDIAN
+    for (i = 0; i < nr; i++) {
+      pbuf[i] = (((WORD)pbuf[i] & 0xff) << 8) | ((WORD)pbuf[i] >> 8);
+    }
+#endif
+
+    /* Accumulate number of samples. */
+    samples += nr;
+
+    return 0;
+}
+
+static void wav_close(warn_t *w)
+{
+    BYTE rlen[4];
+    BYTE dlen[4];
+    DWORD rifflen = samples*2 + 36;
+    DWORD datalen = samples*2;
+    int i;
+
+    /* RIFF length and WAVE data length stored as little endian numbers. */
+    for (i = 0; i < 4; i++) {
+      rlen[i] = rifflen & 0xff;
+      dlen[i] = datalen & 0xff;
+      rifflen >>= 8;
+      datalen >>= 8;
+    }
+
+    fseek(wav_fd, 4, SEEK_SET);
+    fwrite(rlen, 1, 4, wav_fd);
+
+    fseek(wav_fd, 32, SEEK_CUR);
+    fwrite(dlen, 1, 4, wav_fd);
+
+    fclose(wav_fd);
+    wav_fd = NULL;
+}
+
+static sound_device_t wav_device =
+{
+    "wav",
+    wav_init,
+    wav_write,
+    NULL,
+    NULL,
+    NULL,
+    wav_close,
+    NULL,
+    NULL
+};
+
+int sound_init_wav_device(void)
+{
+    return sound_register_device(&wav_device);
+}
