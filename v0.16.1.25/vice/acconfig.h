230eabd6a0ba282a2a2b62d66a90dc6f04f3ad63
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 08:08:49 +0000

    This commit was manufactured by cvs2svn to create tag 'v0.16.1.25'.

---
+++ v0.16.1.25/vice/acconfig.h
@@ -0,0 +1,60 @@
+/* Define to your X11 display depth.  */
+#undef X_DISPLAY_DEPTH
+
+/* Define if you want to use Rober W. McMullen's TextField widget.  */
+#undef ENABLE_TEXTFIELD
+
+/* The number of bytes in an unsigned long.  */
+#undef SIZEOF_UNSIGNED_LONG
+
+/* The number of bytes in an unsigned int.  */
+#undef SIZEOF_UNSIGNED_INT
+
+/* The number of bytes in an unsigned short.  */
+#undef SIZEOF_UNSIGNED_SHORT
+
+/* Does libXext have XShmPutImage?  */
+#undef HAVE_XSHMPUTIMAGE
+
+/* Is libXpm available?  */
+#undef HAVE_LIBXPM
+
+/* Do we want to use XFree86 VIDMODE extensions?  */
+#undef USE_VIDMODE_EXTENSION
+
+/* Are we using the `readline' library or one replacement?  */
+#undef HAVE_READLINE
+
+/* Do we want to enable RS232 support via ACIA emulation?  */
+#undef HAVE_RS232
+
+/* Do we want to enable Printer support?  */
+#undef HAVE_PRINTER
+
+/* Do we want to enable the ReSID code support?  */
+#undef HAVE_RESID
+
+/* Do you want to use MIDAS Sound System instead of Allegro for sound
+   playback?  */
+#undef USE_MIDAS_SOUND
+
+/* Do we have to emulate a joystick?  */
+#undef HAS_JOYSTICK
+
+/* Is digital joystick support avaiable?  */
+#undef HAS_DIGITAL_JOYSTICK
+
+/* Is a Linux compatible joystick support avaiable?  */
+#undef LINUX_JOYSTICK
+
+/* Is a BSD compatible joystick support avaiable?  */
+#undef BSD_JOYSTICK
+
+/* Package name.  */
+#define PACKAGE ""
+
+/* Version number.  */
+#define VERSION ""
+
+/* Define if this version is unstable.  */
+#define UNSTABLE
