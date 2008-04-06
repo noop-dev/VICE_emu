34a05734dbcfd72ffca18c3e607333f760bc7b39
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 08:47:02 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.1.23'.

---
+++ v1.1.23/vice/src/c128/c128mmu.c
@@ -0,0 +1,252 @@
+/*
+ * c128mmu.c
+ *
+ * Written by
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include "c128mem.h"
+#include "c128mmu.h"
+#include "c64mem.h"
+#include "cmdline.h"
+#include "interrupt.h"
+#include "kbd.h"
+#include "log.h"
+#include "mem.h"
+#include "resources.h"
+#include "types.h"
+#include "vdc.h"
+#include "vicii.h"
+#include "z80.h"
+#include "z80mem.h"
+
+/* #define MMU_DEBUG */
+
+/* MMU register.  */
+static BYTE mmu[11];
+
+/* Memory configuration.  */
+static int chargen_in;
+static int basic_lo_in;
+static int basic_hi_in;
+static int kernal_in;
+static int editor_in;
+static int io_in;
+
+/* State of the 40/80 column key.  */
+static int mmu_column4080_key = 1;
+
+/* Logging goes here.  */
+static log_t mmu_log = LOG_ERR;
+
+/* ------------------------------------------------------------------------- */
+
+static int set_column4080_key(resource_value_t v)
+{
+    mmu_column4080_key = (int)v;
+
+#ifdef HAS_SINGLE_CANVAS
+    vdc_set_set_canvas_refresh(mmu_column4080_key ? 0 : 1);
+    vic_ii_set_set_canvas_refresh(mmu_column4080_key ? 1 : 0);
+#endif
+    return 0;
+}
+
+static resource_t resources[] = {
+    { "40/80ColumnKey", RES_INTEGER, (resource_value_t) 1,
+      (resource_value_t *) &mmu_column4080_key, set_column4080_key },
+    { NULL }
+};
+
+int mmu_init_resources(void)
+{
+    return resources_register(resources);
+}
+
+static cmdline_option_t cmdline_options[] = {
+    { "-40col", SET_RESOURCE, 0, NULL, NULL, "40/80ColumnKey",
+      (resource_value_t) 1,
+      NULL, "Activate 40 column mode" },
+    { "-80col", SET_RESOURCE, 0, NULL, NULL, "40/80ColumnKey",
+      (resource_value_t) 0,
+      NULL, "Activate 80 comumn mode" },
+    { NULL }
+};
+
+int mmu_init_cmdline_options(void)
+{
+    return cmdline_register_options(cmdline_options);
+}
+
+/* ------------------------------------------------------------------------- */
+
+static void mmu_toggle_column4080_key(void)
+{
+    mmu_column4080_key = !mmu_column4080_key;
+    resources_set_value("40/80ColumnKey",
+                        (resource_value_t)mmu_column4080_key);
+    log_message(mmu_log, "40/80 column key %s.",
+                (mmu_column4080_key) ? "released" : "pressed");
+}
+
+static void mmu_switch_cpu(int value)
+{
+    if (value) {
+        log_message(mmu_log, "Switching to 8502 CPU.");
+        z80_trigger_dma();
+    } else {
+        log_message(mmu_log, "Switching to Z80 CPU.");
+        maincpu_trigger_dma();
+    }
+}
+
+static void mmu_set_ram_bank(BYTE value)
+{
+    /* (We handle only 128K here.)  */
+    ram_bank = ram + (((long) value & 0x40) << 10);
+#ifdef MMU_DEBUG
+    log_message(mmu_log, "Set RAM bank %i.", (value & 0x40) ? 1 : 0);
+#endif
+}
+
+/* ------------------------------------------------------------------------- */
+
+BYTE REGPARM1 mmu_read(ADDRESS addr)
+{
+    addr &= 0xff;
+
+#ifdef MMU_DEBUG
+    log_message(mmu_log, "MMU READ $%x.", addr);
+#endif
+
+    if (addr < 0xb) {
+        if (addr == 5) {
+            /* 0x80 = 40/80 key released.  */
+            return (mmu[5] & 0x7f) | (mmu_column4080_key ? 0x80 : 0);
+        } else {
+            return mmu[addr];
+        }
+    } else {
+        return 0xf;
+    }
+}
+
+void REGPARM2 mmu_store(ADDRESS address, BYTE value)
+{
+    address &= 0xff;
+
+#ifdef MMU_DEBUG
+    log_message(mmu_log, "MMU STORE $%x <- #$%x.", address, value);
+#endif
+
+    if (address < 0xb) {
+        BYTE oldvalue;
+
+        oldvalue = mmu[address];
+        mmu[address] = value;
+
+        switch (address) {
+          case 0:
+            /* Configuration register (CR).  */
+            io_in = !(value & 0x1);
+            basic_lo_in = !(value & 0x2);
+            basic_hi_in = !(value & 0xc);
+            kernal_in = chargen_in = editor_in = !(value & 0x30);
+            mmu_set_ram_bank(value);
+            break;
+          case 5:
+            value = (value & 0x7f) | 0x30;
+            if ((value & 1) ^ (oldvalue & 1))
+                mmu_switch_cpu(value & 1);
+            break;
+          case 6:
+            /* RAM configuration register (RCR).  */
+            mem_set_ram_config(value);
+            break;
+          case 7:
+          case 8:
+          case 9:
+          case 10:
+            page_zero = (ram + (mmu[0x8] & 0x1 ? 0x10000 : 0x00000)
+                         + (mmu[0x7] << 8));
+            page_one = (ram + (mmu[0xa] & 0x1 ? 0x10000 : 0x00000)
+                        + (mmu[0x9] << 8));
+#ifdef MMU_DEBUG
+            log_message(mmu_log, "PAGE ZERO %05x PAGE ONE %05x",
+                (mmu[0x8] & 0x1 ? 0x10000 : 0x00000) + (mmu[0x7] << 8),
+                (mmu[0xa] & 0x1 ? 0x10000 : 0x00000) + (mmu[0x9] << 8));
+#endif
+            break;
+        }
+
+        mem_update_config(((basic_lo_in) ? 1 : 0) | ((basic_hi_in) ? 2 : 0)
+                          | ((kernal_in) ? 4 : 0) | ((mmu[0] & 0x40) ? 8 : 0)
+                          | ((io_in) ? 16 : 0));
+        z80mem_update_config(((io_in) ? 1 : 0) | ((mmu[0] & 0x40) ? 2 : 0));
+    }
+}
+
+/* $FF00 - $FFFF: RAM or Kernal, with MMU at $FF00 - $FF04.  */
+BYTE REGPARM1 mmu_ffxx_read(ADDRESS addr)
+{
+    if (addr == 0xff00)
+        return mmu[0];
+    else if (kernal_in)
+        return read_kernal(addr);
+    else
+        return read_top_shared(addr);
+}
+
+void REGPARM2 mmu_ffxx_store(ADDRESS addr, BYTE value)
+{
+    if (addr == 0xff00)
+        mmu_store(0, value);
+    else if (addr <= 0xff04)
+        mmu_store(0, mmu[addr & 0xf]);
+    else
+        store_top_shared(addr, value);
+}
+
+/* ------------------------------------------------------------------------- */
+
+void mmu_init(void)
+{
+    if (mmu_log == LOG_ERR)
+        mmu_log = log_open("MMU");
+
+    set_column4080_key((resource_value_t)mmu_column4080_key);
+
+    mmu[5] = 0;
+}
+
+void mmu_reset(void)
+{
+    /* FIXME?  Is this the real configuration?  */
+    basic_lo_in = basic_hi_in = kernal_in = editor_in = 1;
+    io_in = 1;
+    chargen_in = 0;
+
+    kbd_register_column4080_key(mmu_toggle_column4080_key);
+}
+
---
+++ v1.1.23/vice/src/drive/fdc.c
@@ -0,0 +1,636 @@
+
+/*
+ * fdc.c - 1001/8x50 FDC emulation
+ *
+ * Written by
+ *  Andre' Fachat <fachat@physik.tu-chemnitz.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <string.h>
+
+#include "alarm.h"
+#include "diskimage.h"
+#include "drive.h"
+#include "drivecpu.h"
+#include "fdc.h"
+#include "log.h"
+#include "types.h"
+#include "vdrive-bam.h"
+#include "vdrive.h"
+
+#undef FDC_DEBUG
+
+static void clk_overflow_callback(int fnum, CLOCK sub, void *data);
+static int int_fdc(int fnum, long offset);
+
+static void clk_overflow_callback0(CLOCK sub, void *data)
+{
+    clk_overflow_callback(0, sub, data);
+}
+
+static void clk_overflow_callback1(CLOCK sub, void *data)
+{
+    clk_overflow_callback(1, sub, data);
+}
+
+static int int_fdc0(CLOCK offset)
+{
+    return int_fdc(0, offset);
+}
+
+static int int_fdc1(CLOCK offset)
+{
+    return int_fdc(1, offset);
+}
+
+/************************************************************************/
+
+#define	NUM_FDC	2
+
+static log_t fdc_log = LOG_ERR;
+
+typedef struct fdc_t {
+    int	fdc_state;
+    alarm_t	fdc_alarm;
+    CLOCK	alarm_clk;
+    BYTE 	*buffer;
+    BYTE 	*iprom;
+    int		drive_type;
+    int		last_track;
+    int		last_sector;
+    int		last_wps;	/* save write protect switch */
+    disk_image_t *image;
+} fdc_t;
+	
+static fdc_t fdc[NUM_FDC];
+
+void fdc_reset(int fnum, int drive_type)
+{
+#ifdef FDC_DEBUG
+    log_message(fdc_log, "fdc_reset: enabled=%d\n",enabled);
+#endif
+
+    switch (drive_type) {
+    case DRIVE_TYPE_1001:
+    case DRIVE_TYPE_8050:
+    case DRIVE_TYPE_8250:
+	fdc[fnum].drive_type = drive_type;
+	fdc[fnum].fdc_state = FDC_RESET0;
+	alarm_set(&fdc[fnum].fdc_alarm, drive_clk[fnum] + 20);
+	break;
+    default:
+	fdc[fnum].drive_type = DRIVE_TYPE_NONE;
+	alarm_unset(&fdc[fnum].fdc_alarm);
+	fdc[fnum].fdc_state = FDC_UNUSED;
+	break;
+    }
+}
+
+static BYTE fdc_do_job(int fnum, int buf,
+				int drv, BYTE job, BYTE *header)
+{
+#ifdef FDC_DEBUG
+static BYTE fdc_do_job_(int fnum, int buf,
+				int drv, BYTE job, BYTE *header);
+    BYTE retval = fdc_do_job_(fnum, buf, drv, job, header);
+    log_message(fdc_log, "  fdc_do_job (%02x) -> %02x\n",
+	job, retval);
+    return retval;
+}
+static BYTE fdc_do_job_(int fnum, int buf,
+				int drv, BYTE job, BYTE *header)
+{
+#endif
+    int rc, dnr;
+    int i;
+    unsigned int track, sector;
+    BYTE *base;
+    BYTE sector_data[256];
+    BYTE disk_id[2];
+
+    track = header[2];
+    sector = header[3];
+
+    if (DRIVE_IS_DUAL(fdc[fnum].drive_type)) {
+	/* dual disk drive */
+        dnr = drv;
+    } else {
+	/* single disk drive */
+        if (drv) {
+	    return FDC_ERR_SYNC;
+	}
+        dnr = fnum;
+    }
+
+    rc = 0;
+    base = &(fdc[fnum].buffer[(buf + 1) << 8]);
+
+#ifdef FDC_DEBUG
+    log_message(fdc_log, "do job %02x, buffer %d ($%04x): d%d t%d s%d, "
+		"image=%p, type=%04d",
+		job, buf, (buf + 1) << 8, dnr, track, sector,
+		fdc[dnr].image,
+		fdc[dnr].image ? fdc[dnr].image->type : 0);
+#endif
+
+    if (fdc[dnr].image == NULL
+           || (fdc[dnr].image->type != DISK_IMAGE_TYPE_D80
+		&& fdc[dnr].image->type != DISK_IMAGE_TYPE_D82)
+	) {
+	return FDC_ERR_SYNC;
+    }
+
+    vdrive_bam_get_disk_id(dnr + 8, disk_id);
+
+    switch (job) {
+    case 0x80:		/* read */
+	if (header[0] != disk_id[0] || header[1] != disk_id[1]) {
+	    rc = FDC_ERR_ID;
+	    break;
+	}
+	rc = disk_image_read_sector(fdc[dnr].image, sector_data, track, sector);
+        if (rc < 0) {
+            log_error(drive[dnr].log,
+                  "Cannot read T:%d S:%d from disk image.",
+                  track, sector);
+            rc = FDC_ERR_DRIVE;
+        } else {
+	    memcpy(base, sector_data, 256);
+	    rc = FDC_ERR_OK;
+	}
+	break;
+    case 0x90:		/* write */
+	if (header[0] != disk_id[0] || header[1] != disk_id[1]) {
+	    rc = FDC_ERR_ID;
+	    break;
+	}
+	if (fdc[dnr].image->read_only) {
+	    rc = FDC_ERR_WPROT;
+	    break;
+	}
+	memcpy(sector_data, base, 256);
+        rc = disk_image_write_sector(fdc[dnr].image, sector_data, track,
+                                     sector);
+        if (rc < 0) {
+            log_error(drive[dnr].log,
+                  "Could not update T:%d S:%d on disk image.",
+                  track, sector);
+            rc = FDC_ERR_DRIVE;
+        } else {
+            rc = FDC_ERR_OK;
+        }
+        break;
+    case 0xA0:		/* verify */
+	if (header[0] != disk_id[0] || header[1] != disk_id[1]) {
+	    rc = FDC_ERR_ID;
+	    break;
+	}
+        rc = disk_image_read_sector(fdc[dnr].image, sector_data, track,
+                                    sector);
+        if (rc < 0) {
+            log_error(drive[dnr].log,
+                  "Cannot read T:%d S:%d from disk image.",
+                  track, sector);
+            rc = FDC_ERR_DRIVE;
+        } else {
+            rc = FDC_ERR_OK;
+            for (i = 0; i < 256; i++) {
+                if (fnum) {
+                    if (sector_data[i] != base[i]) {
+			rc = FDC_ERR_VERIFY;
+		    }
+                } else {
+                    if (sector_data[i] != base[i]) {
+			rc = FDC_ERR_VERIFY;
+		    }
+                }
+	    }
+        }
+        break;
+    case 0xB0:		/* seek */
+	header[0] = disk_id[0];
+	header[1] = disk_id[1];
+	header[2] = fdc[dnr].last_track;
+	header[3] = 1;
+	rc = FDC_ERR_OK;
+	break;
+    case 0xC0:		/* bump (to track 0) */
+	rc = FDC_ERR_OK;
+	break;
+    case 0xD0:		/* jump to buffer :-( */
+	rc = FDC_ERR_DRIVE;
+	break;
+    case 0xE0:		/* execute when drive/head ready */
+	/* we have to check for standard format code that is copied
+	   to buffers 0-3 */
+	if (!memcmp(fdc[fnum].iprom, &fdc[fnum].buffer[0x100], 0x300)) {
+	    int ntracks, nsectors = 0;
+	    /* detected format code */
+#ifdef FDC_DEBUG
+	    log_message(fdc_log, "format code: \n");
+	    log_message(fdc_log, "     track for zones side 0: %d %d %d %d\n",
+		fdc[fnum].buffer[0xb0], fdc[fnum].buffer[0xb1],
+		fdc[fnum].buffer[0xb2], fdc[fnum].buffer[0xb3]);
+	    log_message(fdc_log, "     track for zones side 1: %d %d %d %d\n",
+		fdc[fnum].buffer[0xb4], fdc[fnum].buffer[0xb5],
+		fdc[fnum].buffer[0xb6], fdc[fnum].buffer[0xb7]);
+	    log_message(fdc_log, "     secs per track: %d %d %d %d\n",
+		fdc[fnum].buffer[0x99], fdc[fnum].buffer[0x9a],
+		fdc[fnum].buffer[0x9b], fdc[fnum].buffer[0x9c]);
+	    log_message(fdc_log, "     vars: 870=%d 873=%d 875=%d\n",
+		fdc[fnum].buffer[0x470], fdc[fnum].buffer[0x473],
+		fdc[fnum].buffer[0x475]);
+	    log_message(fdc_log, "     track=%d, sector=%d\n",
+		track, sector);
+	    log_message(fdc_log, "     id=%02x,%02x (%c%c)\n",
+		header[0],header[1], header[0],header[1]);
+	    log_message(fdc_log, "     sides=%d\n",
+		fdc[fnum].buffer[0xac]);
+#endif
+	    if (fdc[dnr].image->read_only) {
+	        rc = FDC_ERR_WPROT;
+	        break;
+	    }
+	    ntracks = (fdc[fnum].buffer[0xac] > 1) ? 154 : 77;
+
+	    memset(sector_data, 0, 256);
+
+	    for (rc = 0, track = 1; rc == 0 && track <= ntracks; track ++) {
+		if (track < 78) {
+		    for (i=3; i >= 0; i--) {
+			if (track < fdc[fnum].buffer[0xb0 + i]) {
+			    nsectors = fdc[fnum].buffer[0x99 + i];
+			    break;
+			}
+		    }
+		} else {
+		    for (i=3; i >= 0; i--) {
+			if (track < fdc[fnum].buffer[0xb4 + i]) {
+			    nsectors = fdc[fnum].buffer[0x99 + i];
+			    break;
+			}
+		    }
+		}
+		for (sector = 0; sector < nsectors; sector ++) {
+                    rc = disk_image_write_sector(fdc[dnr].image, sector_data,
+                                                 track, sector);
+                    if (rc < 0) {
+                        log_error(drive[dnr].log,
+                  		"Could not update T:%d S:%d on disk image.",
+                  		track, sector);
+            		rc = FDC_ERR_DCHECK;
+			break;
+		    }
+		}
+	    }
+
+            vdrive_bam_set_disk_id(dnr + 8, header);
+
+	    if (!rc)
+	        rc = FDC_ERR_OK;
+	} else {
+	    rc = FDC_ERR_DRIVE;
+	}
+	break;
+    case 0xF0:
+	if (header[0] != disk_id[0] || header[1] != disk_id[1]) {
+	    rc = FDC_ERR_ID;
+	    break;
+	}
+	/* try to read block header from disk */
+	rc = FDC_ERR_OK;
+	break;
+    }
+
+    drive[dnr].current_half_track = 2 * track;
+    fdc[dnr].last_track = track;
+    fdc[dnr].last_sector = sector;
+
+    return (BYTE) rc;
+}
+
+static int int_fdc(int fnum, long offset)
+{
+    CLOCK rclk = drive_clk[fnum] - offset;
+    int i, j;
+    int drv_wps;
+
+#ifdef FDC_DEBUG
+    if (fdc[fnum].fdc_state < FDC_RUN) {
+	static int old_state[NUM_FDC] = { -1, -1 };
+	if (fdc[fnum].fdc_state != old_state[fnum])
+	    log_message(fdc_log, "int_fdc%d %d: state=%d\n",
+					fnum, rclk, fdc[fnum].fdc_state);
+	old_state[fnum] = fdc[fnum].fdc_state;
+    }
+#endif
+
+    switch(fdc[fnum].fdc_state) {
+    case FDC_RESET0:
+        drive[fnum].current_half_track = 2 * 38;
+	fdc[fnum].buffer[0] = 2;
+	fdc[fnum].fdc_state++;
+	fdc[fnum].alarm_clk = rclk + 2000;
+	alarm_set(&fdc[fnum].fdc_alarm, fdc[fnum].alarm_clk);
+	break;
+    case FDC_RESET1:
+	if (fdc[fnum].buffer[0] == 0) {
+	    fdc[fnum].buffer[0] = 1;
+	    fdc[fnum].fdc_state++;
+	}
+	fdc[fnum].alarm_clk = rclk + 2000;
+	alarm_set(&fdc[fnum].fdc_alarm, fdc[fnum].alarm_clk);
+	break;
+    case FDC_RESET2:
+	if (fdc[fnum].buffer[0] == 0) {
+	    /* emulate routine written to buffer RAM */
+	    fdc[fnum].buffer[1] = 0x0e;
+	    fdc[fnum].buffer[2] = 0x2d;	
+	    /* number of sides on disk drive */
+	    fdc[fnum].buffer[0xac] =
+		(fdc[fnum].drive_type == DRIVE_TYPE_8050) ? 1 : 2;
+	    /* 0 = 4040 (2A), 1 = 8x80 (2C) drive type */
+	    fdc[fnum].buffer[0xea] = 1;	
+	    fdc[fnum].buffer[0xee] = 5;	/* 3 for 4040, 5 for 8x50 */
+	    fdc[fnum].buffer[0] = 3;	/* 5 for 4040, 3 for 8x50 */
+	    fdc[fnum].fdc_state = FDC_RUN;
+	    fdc[fnum].alarm_clk = rclk + 10000;
+	} else {
+	    fdc[fnum].alarm_clk = rclk + 2000;
+	}
+	alarm_set(&fdc[fnum].fdc_alarm, fdc[fnum].alarm_clk);
+	break;
+    case FDC_RUN:
+	/* check write protect switch */
+	drv_wps = drive_read_viad2_prb(&drive[fnum]) & 0x10;
+	if (drv_wps != fdc[fnum].last_wps) {
+	    fdc[fnum].buffer[0xA6 + fnum] = 1;
+	    fdc[fnum].last_wps = drv_wps;
+	}
+	if (fnum == 0 && DRIVE_IS_DUAL(fdc[0].drive_type)) {
+	    drv_wps = drive_read_viad2_prb(&drive[1]) & 0x10;
+	    if (drv_wps != fdc[1].last_wps) {
+	        fdc[1].buffer[0xA6 + 1] = 1;
+	        fdc[1].last_wps = drv_wps;
+	    }
+	}
+
+	/* check buffers */
+	for (i=14; i >= 0; i--) {
+	    /* job there? */
+	    if (fdc[fnum].buffer[i + 3] > 127) {
+		/* pointer to buffer/block header:
+		    +0 = ID1
+		    +1 = ID2
+		    +2 = Track
+		    +3 = Sector
+		*/
+		j = (i << 3) + 0x21;
+#ifdef FDC_DEBUG
+		log_message(fdc_log, "D/Buf %d/%x: Job code %02x t:%02d s:%02d",
+			fnum, i, fdc[fnum].buffer[i+3],
+			fdc[fnum].buffer[j+2],fdc[fnum].buffer[j+3]);
+#endif
+		fdc[fnum].buffer[i + 3] =
+			fdc_do_job(fnum, 			/* FDC# */
+				i,				/* buffer# */
+				fdc[fnum].buffer[i+3] & 1,	/* drive */
+				(int)(fdc[fnum].buffer[i+3] & 0xfe),
+                                                                /* job code */
+				&(fdc[fnum].buffer[j]) 		/* header */
+			);
+	    }
+	}
+	/* check "move head", by half tracks I guess... */
+	for (i = 0; i < 2; i++) {
+	    if (fdc[fnum].buffer[i + 0xa1]) {
+#ifdef FDC_DEBUG
+		log_message(fdc_log, "D %d: move head %d",
+		    fnum, fdc[fnum].buffer[i + 0xa1]);
+#endif
+		fdc[fnum].buffer[i + 0xa1] = 0;
+	    }
+	}
+	fdc[fnum].alarm_clk = rclk + 30000;
+	alarm_set(&fdc[fnum].fdc_alarm, fdc[fnum].alarm_clk);
+	/* job loop */
+	break;
+    }
+
+    return 0;
+}
+
+static void clk_overflow_callback(int fnum, CLOCK sub, void *data)
+{
+    if (fdc[fnum].fdc_state != FDC_UNUSED) {
+	if (fdc[fnum].alarm_clk > sub) {
+	    fdc[fnum].alarm_clk -= sub;
+	} else {
+	    fdc[fnum].alarm_clk = 0;
+	}
+    }
+}
+
+void fdc_init(int fnum, BYTE *buffermem, BYTE *ipromp)
+{
+    fdc[fnum].buffer = buffermem;
+    fdc[fnum].iprom = ipromp;
+
+    if (fdc_log == LOG_ERR)
+        fdc_log = log_open("fdc");
+
+    if (fnum == 0) {
+        alarm_init(&fdc[fnum].fdc_alarm, &drive0_alarm_context,
+               "fdc0", int_fdc0);
+        clk_guard_add_callback(&drive0_clk_guard, clk_overflow_callback0, NULL);
+    } else
+    if (fnum == 1) {
+        alarm_init(&fdc[fnum].fdc_alarm, &drive1_alarm_context,
+               "fdc1", int_fdc1);
+        clk_guard_add_callback(&drive1_clk_guard, clk_overflow_callback1, NULL);
+    }
+}
+
+/************************************************************************/
+
+int fdc_attach_image(disk_image_t *image, int unit)
+{
+    if (unit != 8 && unit != 9)
+        return -1;
+
+    switch(image->type) {
+      case DISK_IMAGE_TYPE_D80:
+        log_message(fdc_log, "Unit %d: D80 disk image attached: %s",
+                    unit, image->name);
+        break;
+      case DISK_IMAGE_TYPE_D82:
+        log_message(fdc_log, "Unit %d: D82 disk image attached: %s",
+                    unit, image->name);
+        break;
+      default:
+        return -1;
+    }
+
+    fdc[unit - 8].image = image;
+    return 0;
+}
+
+int fdc_detach_image(disk_image_t *image, int unit)
+{
+    if (unit != 8 && unit != 9)
+        return -1;
+
+    switch(image->type) {
+      case DISK_IMAGE_TYPE_D80:
+        log_message(fdc_log, "Unit %d: D80 disk image detached: %s",
+                    unit, image->name);
+        break;
+      case DISK_IMAGE_TYPE_D82:
+        log_message(fdc_log, "Unit %d: D82 disk image detached: %s",
+                    unit, image->name);
+        break;
+      default:
+        return -1;
+    }
+
+    fdc[unit - 8].image = NULL;
+    return 0;
+}
+
+/************************************************************************/
+
+/* The dump format has a module header and the data generated by the
+ * chip...
+ *
+ * The version of this dump description is 0/0
+ */
+
+#define FDC_DUMP_VER_MAJOR      0
+#define FDC_DUMP_VER_MINOR      0
+
+/*
+ * The dump data:
+ *
+ * UBYTE        STATE		FDC state
+ * DWORD        CLK		clk ticks till next fdc invocation
+ * UBYTE	NDRV		number of drives (1 or 2)
+ * UBYTE        LTRACK0		last track
+ * UBYTE        LSECTOR0	last sector
+ * UBYTE        LTRACK1		last track (if ndrv == 2)
+ * UBYTE        LSECTOR1	last sector (if ndrv == 2)
+ *
+ */
+
+
+int fdc_write_snapshot_module(snapshot_t *p, int fnum)
+{
+    snapshot_module_t *m;
+    char name[100];
+
+    if (fdc[fnum].fdc_state == FDC_UNUSED) {
+	return 0;
+    }
+
+    sprintf(name, "FDC%i", fnum);
+
+    m = snapshot_module_create(p, name,
+                              FDC_DUMP_VER_MAJOR, FDC_DUMP_VER_MINOR);
+    if (m == NULL)
+        return -1;
+
+    snapshot_module_write_byte(m, fdc[fnum].fdc_state);
+
+    /* clk till next invocation */
+    snapshot_module_write_dword(m,
+                                (DWORD)(fdc[fnum].alarm_clk - drive_clk[fnum]));
+
+    /* number of drives - so far 1 only */
+    snapshot_module_write_byte(m, 1);
+
+    /* last accessed track/sector */
+    snapshot_module_write_byte(m, ((BYTE)(fdc[fnum].last_track)));
+    snapshot_module_write_byte(m, ((BYTE)(fdc[fnum].last_sector)));
+
+    snapshot_module_close(m);
+
+    return 0;
+}
+
+int fdc_read_snapshot_module(snapshot_t *p, int fnum)
+{
+    BYTE vmajor, vminor;
+    BYTE byte, ndrv;
+    DWORD dword;
+    snapshot_module_t *m;
+    char name[100];
+
+    sprintf(name, "FDC%d", fnum);
+
+    m = snapshot_module_open(p, name, &vmajor, &vminor);
+    if (m == NULL) {
+	log_message(fdc_log, "Could not find snapshot module %s", name);
+        return -1;
+    }
+
+    if (vmajor != FDC_DUMP_VER_MAJOR) {
+        log_error(fdc_log,
+                  "Snapshot module version (%d.%d) newer than %d.%d.",
+                  vmajor, vminor, FDC_DUMP_VER_MAJOR, FDC_DUMP_VER_MINOR);
+        snapshot_module_close(m);
+        return -1;
+    }
+
+    snapshot_module_read_byte(m, &byte);
+    if (byte > FDC_LAST_STATE) {
+        snapshot_module_close(m);
+	return -1;
+    }
+    fdc[fnum].fdc_state = byte;
+
+    /* clk till next invocation */
+    snapshot_module_read_dword(m, &dword);
+    fdc[fnum].alarm_clk = drive_clk[fnum] + dword;
+    alarm_set(&fdc[fnum].fdc_alarm, fdc[fnum].alarm_clk);
+
+    /* number of drives - so far 1 only */
+    snapshot_module_read_byte(m, &ndrv);
+
+    /* last accessed track/sector */
+    snapshot_module_read_byte(m, &byte);
+    fdc[fnum].last_track = byte;
+    snapshot_module_read_byte(m, &byte);
+    fdc[fnum].last_sector = byte;
+
+    if (ndrv > 1) {
+	/* ignore drv 0 values */
+        snapshot_module_read_byte(m, &byte);
+        snapshot_module_read_byte(m, &byte);
+    }
+
+    if (snapshot_module_close(m) < 0)
+        return -1;
+
+    return 0;
+}
+
