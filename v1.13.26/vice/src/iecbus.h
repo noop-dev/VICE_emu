8e55eadedd2b43d6d8de747bf0a275eb3f887bd8
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 15:00:04 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.13.26'.

---
+++ v1.13.26/vice/src/iecbus.h
@@ -0,0 +1,53 @@
+/*
+ * iecbus.h - IEC bus handling.
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _IECBUS_H
+#define _IECBUS_H
+
+#include "types.h"
+
+#define IECBUS_NUM 16
+
+typedef struct iecbus_s {
+    BYTE drv_bus[IECBUS_NUM];
+    BYTE drv_data[IECBUS_NUM];
+    BYTE drv_port;
+    BYTE cpu_bus;
+    BYTE cpu_port;
+    BYTE iec_fast_1541;
+} iecbus_t;
+
+extern iecbus_t iecbus;
+
+extern iecbus_t *iecbus_drive_port(void);
+
+extern void iecbus_init(void);
+extern void iecbus_calculate_callback_index(void);
+
+extern void (*iecbus_callback_write)(BYTE, CLOCK);
+
+#endif
+
