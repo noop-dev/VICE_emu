dfdcc598371458070a314f30053c65303b00bf07
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 08:43:21 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.1.12'.

---
+++ v1.1.12/vice/src/arch/unix/archdep.h
@@ -0,0 +1,64 @@
+/*
+ * archdep.h - Miscellaneous system-specific stuff.
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _ARCHDEP_H
+#define _ARCHDEP_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "vice.h"
+#include "archapi.h"
+
+/* Filesystem dependant operators.  */
+#define FSDEVICE_DEFAULT_DIR   "."
+#define FSDEV_DIR_SEP_STR      "/"
+#define FSDEV_DIR_SEP_CHR      '/'
+#define FSDEV_EXT_SEP_STR      "."
+#define FSDEV_EXT_SEP_CHR      '.'
+
+/* Path separator.  */
+#define FINDPATH_SEPARATOR_CHAR         ':'
+#define FINDPATH_SEPARATOR_STRING       ":"
+
+/* Modes for fopen().  */
+#define MODE_READ              "r"
+#define MODE_READ_TEXT         "r"
+#define MODE_READ_WRITE        "r+"
+#define MODE_WRITE             "w"
+#define MODE_WRITE_TEXT        "w"
+#define MODE_APPEND            "w+"
+#define MODE_APPEND_READ_WRITE "a+"
+
+/* Define the default system directory (where the ROMs are).  */
+#define LIBDIR          PREFIX "/lib/vice"
+#define DOCDIR          LIBDIR "/doc"
+#define VICEUSERDIR     ".vice"
+
+extern const char *archdep_home_path(void);
+
+#endif
+
