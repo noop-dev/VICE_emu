1373570160d20be00fab8a7a0c4ff439c429fc65
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 13:54:23 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.11.45'.

---
+++ tags/v1.11/v1.11.45/vice/src/arch/win32/uisound.c
@@ -0,0 +1,211 @@
+/*
+ * uisound.c - Sound device configuration user interface for Win32 platforms.
+ *
+ * Written by
+ *  Tibor Biczo <crown@mail.matav.hu>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include <string.h>
+#include <windows.h>
+
+#include "res.h"
+#include "resources.h"
+#include "sound.h"
+#include "system.h"
+#include "ui.h"
+#include "utils.h"
+#include "winmain.h"
+
+
+static int ui_sound_freq[] = {
+    8000,
+    11025,
+    22050,
+    44100
+};
+
+static int ui_sound_buffer[] = {
+    100,
+    150,
+    200,
+    250,
+    300,
+    350
+};
+
+static int ui_sound_adjusting[] = {
+    SOUND_ADJUST_FLEXIBLE,
+    SOUND_ADJUST_ADJUSTING,
+    SOUND_ADJUST_EXACT
+};
+
+static void init_sound_dialog(HWND hwnd)
+{
+    HWND snd_hwnd;
+    int res_value;
+    char *devicename;
+
+    snd_hwnd = GetDlgItem(hwnd, IDC_SOUND_FREQ);
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("8000 Hz"));
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("11025 Hz"));
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("22050 Hz"));
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("44100 Hz"));
+    resources_get_value("SoundSampleRate", (resource_value_t *)&res_value);
+    switch (res_value) {
+      case 8000:
+        res_value = 0;
+        break;
+      case 11025:
+        res_value = 1;
+        break;
+      case 22050:
+        res_value = 2;
+        break;
+      case 44100:
+      default:
+        res_value = 3;
+        break;
+    }
+    SendMessage(snd_hwnd, CB_SETCURSEL, (WPARAM)res_value, 0);
+
+    snd_hwnd=GetDlgItem(hwnd, IDC_SOUND_BUFFER);
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("100 msec"));
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("150 msec"));
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("200 msec"));
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("250 msec"));
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("300 msec"));
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("350 msec"));
+    resources_get_value("SoundBufferSize", (resource_value_t *)&res_value);
+    switch (res_value) {
+      case 100:
+        res_value = 0;
+        break;
+      case 150:
+        res_value = 1;
+        break;
+      case 200:
+        res_value = 2;
+        break;
+      case 250:
+        res_value = 3;
+        break;
+      case 300:
+        res_value = 4;
+        break;
+       case 350:
+       default:
+        res_value = 5;
+        break;
+    }
+    SendMessage(snd_hwnd, CB_SETCURSEL, (WPARAM)res_value, 0);
+
+    snd_hwnd=GetDlgItem(hwnd, IDC_SOUND_OVERSAMPLE);
+    SendMessage(snd_hwnd, CB_ADDSTRING,0, (LPARAM)TEXT("None"));
+    SendMessage(snd_hwnd, CB_ADDSTRING,0, (LPARAM)TEXT("2x"));
+    SendMessage(snd_hwnd, CB_ADDSTRING,0, (LPARAM)TEXT("4x"));
+    SendMessage(snd_hwnd, CB_ADDSTRING,0, (LPARAM)TEXT("8x"));
+    resources_get_value("SoundOversample", (resource_value_t *)&res_value);
+    SendMessage(snd_hwnd, CB_SETCURSEL, (WPARAM)res_value, 0);
+
+    snd_hwnd=GetDlgItem(hwnd, IDC_SOUND_SYNCH);
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("Flexible"));
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("Adjusting"));
+    SendMessage(snd_hwnd, CB_ADDSTRING, 0, (LPARAM)TEXT("Exact"));
+    resources_get_value("SoundSpeedAdjustment", (resource_value_t *)&res_value);
+    switch (res_value) {
+      case SOUND_ADJUST_FLEXIBLE:
+      default:
+        res_value=0;
+        break;
+      case SOUND_ADJUST_ADJUSTING:
+        res_value=1;
+        break;
+      case SOUND_ADJUST_EXACT:
+        res_value=2;
+        break;
+    }
+    SendMessage(snd_hwnd, CB_SETCURSEL, (WPARAM)res_value, 0);
+
+    resources_get_value("SoundDeviceName", (resource_value_t *)&devicename);
+    if (devicename && !strcasecmp("wmm", devicename))
+        res_value = IDC_SOUND_WMM;
+    else
+        res_value = IDC_SOUND_DIRECTX;
+        
+    CheckRadioButton(hwnd, IDC_SOUND_DIRECTX, IDC_SOUND_WMM, res_value);
+
+}
+
+static BOOL CALLBACK dialog_proc(HWND hwnd, UINT msg, WPARAM wparam,
+                                 LPARAM lparam)
+{
+    int command;
+
+    switch (msg) {
+      case WM_INITDIALOG:
+        system_init_dialog(hwnd);
+        init_sound_dialog(hwnd);
+        return TRUE;
+      case WM_COMMAND:
+        command=LOWORD(wparam);
+        switch (command) {
+          case IDC_SOUND_DIRECTX:
+            resources_set_value("SoundDeviceName",(resource_value_t)"dx");
+            ui_display_statustext("Sound driver: DirectX");
+            break;
+          case IDC_SOUND_WMM:
+            resources_set_value("SoundDeviceName",(resource_value_t)"wmm");
+            ui_display_statustext("Sound driver: WMM");
+            break;
+          case IDOK:
+            resources_set_value("SoundSampleRate",
+                                (resource_value_t)ui_sound_freq[SendMessage(
+                                GetDlgItem(hwnd,IDC_SOUND_FREQ),
+                                CB_GETCURSEL,0,0)]);
+            resources_set_value("SoundBufferSize",
+                                (resource_value_t)ui_sound_buffer[SendMessage(
+                                GetDlgItem(hwnd,IDC_SOUND_BUFFER),
+                                CB_GETCURSEL,0,0)]);
+            resources_set_value("SoundOversample",
+                                (resource_value_t)SendMessage(
+                                GetDlgItem(hwnd,IDC_SOUND_OVERSAMPLE),
+                                CB_GETCURSEL,0,0));
+            resources_set_value("SoundSpeedAdjustment",
+                                (resource_value_t)ui_sound_adjusting[SendMessage(
+                                GetDlgItem(hwnd, IDC_SOUND_SYNCH),
+                                CB_GETCURSEL, 0, 0)]);
+
+          case IDCANCEL:
+            EndDialog(hwnd,0);
+            return TRUE;
+        }
+        break;
+    }
+    return FALSE;
+}
+
+void ui_sound_settings_dialog(HWND hwnd)
+{
+    DialogBox(winmain_instance, MAKEINTRESOURCE(IDD_SOUND_SETTINGS_DIALOG),
+              hwnd, dialog_proc);
+}
+
---
+++ tags/v1.11/v1.11.45/vice/src/drive/drive-resources.c
@@ -0,0 +1,230 @@
+/*
+ * drive-resources.c - Hardware-level Commodore disk drive emulation,
+ *                     resource module.
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "drive-resources.h"
+#include "drive.h"
+#include "drivecpu.h"
+#include "driverom.h"
+#include "iecdrive.h"
+#include "log.h"
+#include "machine-drive.h"
+#include "resources.h"
+#include "vdrive-bam.h"
+
+
+/* Is true drive emulation switched on?  */
+static int drive_true_emulation;
+
+
+static int set_drive_true_emulation(resource_value_t v, void *param)
+{
+    drive_true_emulation = (int)v;
+
+    if ((int)v) {
+        if (drive[0].type != DRIVE_TYPE_NONE) {
+            drive[0].enable = 1;
+            drive_cpu_reset_clk(&drive0_context);
+        }
+        if (drive[1].type != DRIVE_TYPE_NONE) {
+            drive[1].enable = 1;
+            drive_cpu_reset_clk(&drive1_context);
+        }
+        drive_enable(0);
+        drive_enable(1);
+        iec_calculate_callback_index();
+    } else {
+        drive_disable(0);
+        drive_disable(1);
+
+        /* update BAM after true drive emulation having probably
+           changed the BAM on disk (14May1999) */
+        if (drive[0].image != NULL) {
+            vdrive_bam_reread_bam(8);
+        }
+        if (drive[1].image != NULL) {
+            vdrive_bam_reread_bam(9);
+        }
+    }
+    return 0;
+}
+
+int drive0_resources_type(resource_value_t v, void *param)
+{
+    unsigned int type;
+    int busses;
+
+    type = (unsigned int)v;
+    busses = iec_available_busses();
+
+    /* if bus for drive type is not allowed, set to default value for bus */
+    if (!drive_match_bus(type, 0, busses)) {
+        if (busses & IEC_BUS_IEC) {
+            type = DRIVE_TYPE_1541;
+        } else
+        if (busses & IEC_BUS_IEEE) {
+            type = DRIVE_TYPE_2031;
+        } else
+            type = DRIVE_TYPE_NONE;
+    }
+
+    if (DRIVE_IS_DUAL(type)) {
+        /* dual disk drives disable second emulated unit */
+        log_warning(drive[0].log,
+                    "Dual disk drive disables second emulated drive");
+
+        drive1_resources_type((resource_value_t)DRIVE_TYPE_NONE, NULL);
+    }
+
+    switch (type) {
+      case DRIVE_TYPE_1541:
+      case DRIVE_TYPE_1541II:
+      case DRIVE_TYPE_1551:
+      case DRIVE_TYPE_1571:
+      case DRIVE_TYPE_1581:
+      case DRIVE_TYPE_2031:
+      case DRIVE_TYPE_1001:
+      case DRIVE_TYPE_2040:
+      case DRIVE_TYPE_3040:
+      case DRIVE_TYPE_4040:
+      case DRIVE_TYPE_8050:
+      case DRIVE_TYPE_8250:
+        if (drive[0].type != type) {
+            drive[0].current_half_track = 2 * 18;
+            if ((type == DRIVE_TYPE_1001)
+                || (type == DRIVE_TYPE_8050)
+                || (type == DRIVE_TYPE_8250)) {
+                drive[0].current_half_track = 2 * 38;
+            }
+        }
+        drive[0].type = type;
+        if (drive_true_emulation) {
+            drive[0].enable = 1;
+            drive_enable(0);
+            iec_calculate_callback_index();
+        }
+        drive_set_disk_drive_type(type, 0);
+        drive_rom_initialize_traps(0);
+        machine_drive_idling_method(0);
+        return 0;
+      case DRIVE_TYPE_NONE:
+        drive[0].type = type;
+        drive_disable(0);
+        return 0;
+      default:
+        return -1;
+    }
+}
+
+int drive1_resources_type(resource_value_t v, void *param)
+{
+    unsigned int type;
+    int busses;
+
+    type = (unsigned int)v;
+    busses = iec_available_busses();
+
+    /* if bus for drive type is not allowed, set to default value for bus */
+    if (!drive_match_bus(type, 1, busses)) {
+        if (busses & IEC_BUS_IEC) {
+            type = DRIVE_TYPE_1541;
+        } else
+        if (busses & IEC_BUS_IEEE) {
+            type = DRIVE_TYPE_2031;
+        } else
+            type = DRIVE_TYPE_NONE;
+    }
+
+    if (drive[0].enable && DRIVE_IS_DUAL(drive[0].type)) {
+        /* dual disk drives disable second emulated unit */
+
+        log_warning(drive[1].log,
+                    "Dual disk drive disables second emulated drive");
+
+        type = DRIVE_TYPE_NONE;
+    }
+
+    switch (type) {
+      case DRIVE_TYPE_1541:
+      case DRIVE_TYPE_1541II:
+      case DRIVE_TYPE_1551:
+      case DRIVE_TYPE_1571:
+      case DRIVE_TYPE_1581:
+      case DRIVE_TYPE_2031:
+      case DRIVE_TYPE_1001:
+      case DRIVE_TYPE_2040:
+      case DRIVE_TYPE_3040:
+      case DRIVE_TYPE_4040:
+      case DRIVE_TYPE_8050:
+      case DRIVE_TYPE_8250:
+        if (drive[1].type != type) {
+            drive[1].current_half_track = 2 * 18;
+            if ((type == DRIVE_TYPE_1001)
+                || (type == DRIVE_TYPE_8050)
+                || (type == DRIVE_TYPE_8250)) {
+                drive[1].current_half_track = 2 * 38;
+            }
+        }
+        drive[1].type = type;
+        if (drive_true_emulation) {
+            drive[1].enable = 1;
+            drive_enable(1);
+            iec_calculate_callback_index();
+        }
+        drive_set_disk_drive_type(type, 1);
+        drive_rom_initialize_traps(1);
+        machine_drive_idling_method(1);
+        return 0;
+      case DRIVE_TYPE_NONE:
+        drive[1].type = type;
+        drive_disable(1);
+        return 0;
+      default:
+        return -1;
+    }
+}
+
+static const resource_t resources[] = {
+    { "DriveTrueEmulation", RES_INTEGER, (resource_value_t)1,
+      (resource_value_t *)&drive_true_emulation,
+      set_drive_true_emulation, NULL },
+    { NULL }
+};
+
+int drive_resources_init(void)
+{
+    return machine_drive_resources_init() | resources_register(resources);
+}
+
+void drive_resources_shutdown(void)
+{
+    machine_drive_resources_shutdown();
+}
+
