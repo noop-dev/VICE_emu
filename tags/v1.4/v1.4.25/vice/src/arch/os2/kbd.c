fdb414486141cf7356d196410377b885fddcf5e2
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 09:17:35 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.4.25'.

---
+++ tags/v1.4/v1.4.25/vice/src/arch/os2/kbd.c
@@ -0,0 +1,93 @@
+/*
+ * kbd.c - Keyboard emulation. (OS/2)
+ *
+ * Written by
+ *  Thomas Bretz <tbretz@gsi.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <os2.h>  // CCHMAXPATH
+
+#include <string.h>
+
+#include "kbd.h"
+#include "parse.h"
+#include "utils.h"
+#include "cmdline.h"
+#include "resources.h"
+
+BYTE joystick_value[3];
+
+static int keymap_index;
+static char *keymapfile;//[CCHMAXPATH];
+
+/* ------------------------------------------------------------------------ */
+
+int kbd_init(int num, ...)
+{
+    keyconvmap.map[0] = xmalloc(256*sizeof(keyconv));
+    keyconvmap.map[1] = xmalloc(256*sizeof(keyconv));
+
+    load_keymap_file("os2.vkm");
+    if (strcmp(keymapfile, "os2.vkm"))
+        load_keymap_file(keymapfile);
+    return 0;
+}
+
+static int set_keymap_index(resource_value_t v)
+{
+    keymap_index = (int)v;
+
+    return 0;
+}
+
+static int set_keymap_file(resource_value_t v)
+{
+    string_set(&keymapfile, (const char*) v);
+
+    return 0;
+}
+
+
+static resource_t resources[] = {
+    { "KeymapFile", RES_STRING, (resource_value_t) "os2.vkm",
+      (resource_value_t *) &keymapfile, set_keymap_file },
+    { NULL }
+};
+
+int kbd_init_resources(void)
+{
+    return resources_register(resources);
+}
+
+static cmdline_option_t cmdline_options[] = {
+     { "-keymap", SET_RESOURCE, 1, NULL, NULL, "KeymapFile", NULL,
+      "<name>", "Specify name of keymap file" },
+    { NULL },
+};
+
+int kbd_init_cmdline_options(void)
+{
+    return cmdline_register_options(cmdline_options);
+}
+
---
+++ tags/v1.4/v1.4.25/vice/src/fixpoint.h
@@ -0,0 +1,68 @@
+/*
+ * fixpoint.h - Fixed point routines.
+ *
+ * Written by
+ *  Andreas Dehmel <dehmel@forwiss.tu-muenchen.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _FIXPOINT_H
+#define _FIXPOINT_H
+
+#define INLINE_FIXPOINT_FUNCTIONS
+
+#ifdef FIXPOINT_ARITHMETIC
+#define FIXPOINT_PREC   12
+typedef int vreal_t;
+#define REAL_VALUE(x)((vreal_t)((x) * (1<<FIXPOINT_PREC)))
+#define REAL_MULT(x,y)fixpoint_mult(x,y)
+#define REAL_TO_INT(x)((int)((x)>>FIXPOINT_PREC))
+
+/* inline these functions or not? */
+#ifdef INLINE_FIXPOINT_FUNCTIONS
+#include "fixpoint.c"
+#else  /* INLINE_FIXPOINT_FUNCTIONS */
+extern vreal_t fixpoint_mult(vreal_t x, vreal_t y);
+#endif /* INLINE_FIXPOINT_FUNCTIONS */
+
+#else  /* FIXPOINT_ARITHMETIC */
+
+typedef float vreal_t;
+#define REAL_VALUE(x)   (x)
+#define REAL_MULT(x,y)  (x*y)
+#define REAL_TO_INT(x)  ((int)(x))
+
+#endif /* FIXPOINT_ARITHMETIC */
+
+/* Some platforms (e.g. RISC OS) throw floating exceptions when attempting
+   a float-to-int cast where the floating point value is outside the range of
+   an int. Use these macros when casting potentially large FP-values. */
+
+#if defined(__riscos) 
+#define BIG_FLOAT_TO_INT(f)     fmod(f, 2147483648.0)
+#define BIG_FLOAT_TO_UINT(f)    fmod(f, 4294967296.0)
+#else
+#define BIG_FLOAT_TO_INT(f)     (f)
+#define BIG_FLOAT_TO_UINT(f)    (f)
+#endif
+
+#endif
+
