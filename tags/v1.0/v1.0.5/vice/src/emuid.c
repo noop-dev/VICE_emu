aa2a79b687dda82e31c06d0c1cc84a31f583ef12
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 08:20:31 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.0.5'.

---
+++ tags/v1.0/v1.0.5/vice/src/emuid.c
@@ -0,0 +1,41 @@
+/*
+ * emuid.c -- Emulator ID for use in the different emulators.
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+#include "types.h"
+
+/* Emulation identification string.  */
+BYTE emulator_id[] = {
+    0x56, 0x49, 0x43, 0x45, 0x20, 0x30, 0x2e, 0x31,     /* VICE 0.1 */
+    0x35, 0x20, 0x28, 0x43, 0x29, 0x20, 0x45, 0x2e,     /* 5.(c) E. */
+    0x50, 0x45, 0x52, 0x41, 0x5a, 0x5a, 0x4f, 0x4c,     /* PERAZZOL */
+    0x49, 0x2f, 0x41, 0x2e, 0x46, 0x41, 0x43, 0x48,     /* I/A.FACH */
+    0x41, 0x54, 0x2f, 0x54, 0x2e, 0x52, 0x41, 0x4e,     /* AT/T.RAN */
+    0x54, 0x41, 0x4e, 0x45, 0x4e, 0x2f, 0x44, 0x2e,     /* TANEN/D. */
+    0x53, 0x4c, 0x41, 0x44, 0x49, 0x43, 0x2f, 0x41,     /* SLADIC/A */
+    0x2e, 0x42, 0x4f, 0x4f, 0x53, 0x45, 0x2f, 0x4a,     /* .BOOSE/J */
+    0x2e, 0x56, 0x41, 0x4c, 0x54, 0x41, 0x2f, 0x4a,     /* .VALTA/J */
+    0x2e, 0x53, 0x4f, 0x4e, 0x4e, 0x49, 0x4e, 0x45,     /* .SONNINE */
+    0x4e, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,     /* N....... */
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x15, 0x56, 0x55      /* ......VU */
+};
