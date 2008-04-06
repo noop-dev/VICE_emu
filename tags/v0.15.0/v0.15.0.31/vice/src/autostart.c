6fad4b79876fe0e5a84971c9a0a9df52b8f76451
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 07:58:25 +0000

    This commit was manufactured by cvs2svn to create tag 'v0.15.0.31'.

---
+++ tags/v0.15.0/v0.15.0.31/vice/src/autostart.c
@@ -0,0 +1,478 @@
+/*
+ * autostart.c - automatic image loading and starting
+ *
+ * Written by
+ *  Teemu Rantanen      (tvr@cs.hut.fi)
+ *  Ettore Perazzoli    (ettore@comm2000.it)
+ *
+ * Patches by
+ *  André Fachat        (a.fachat@physik.tu-chemnitz.de)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#ifdef STDC_HEADERS
+#include <stdio.h>
+#include <stdlib.h>
+#endif
+
+#include "autostart.h"
+
+#include "attach.h"
+#include "interrupt.h"
+#include "kbdbuf.h"
+#include "mem.h"
+#include "resources.h"
+#include "serial.h"
+#include "tape.h"
+#include "ui.h"
+#include "utils.h"
+#include "vdrive.h"
+#include "vmachine.h"
+#include "machine.h"
+#include "snapshot.h"
+#include "warn.h"
+
+
+/* Kernal addresses.  Set by `autostart_init()'.  */
+
+static int blnsw;		/* Cursor Blink enable: 0 = Flash Cursor */
+static int pnt;			/* Pointer: Current Screen Line Address */
+static int pntr;		/* Cursor Column on Current Line */
+static int lnmx;		/* Physical Screen Line Length */
+
+/* Current state of the autostart routine.  */
+static enum {
+    AUTOSTART_NONE,
+    AUTOSTART_ERROR,
+    AUTOSTART_HASTAPE,
+    AUTOSTART_LOADINGTAPE,
+    AUTOSTART_HASDISK,
+    AUTOSTART_LOADINGDISK,
+    AUTOSTART_HASSNAPSHOT,
+    AUTOSTART_DONE
+} autostartmode = AUTOSTART_NONE;
+
+/* Warnings.  */
+static warn_t *pwarn = NULL;
+
+/* Flag: was true 1541 emulation turned on when we started booting the disk
+   image?  */
+static int orig_true1541_state = 0;
+
+/* PETSCII name of the program to load. NULL if default */
+static BYTE *autostart_program_name = NULL;
+
+/* Minimum number of cycles before we feed BASIC with commands.  */
+static CLOCK min_cycles;
+
+/* Flag: Do we want to switch true 1541 emulation on/off during autostart?  */
+static int handle_true1541;
+
+/* Flag: autostart is initialized.  */
+static int autostart_enabled = 0;
+
+/* ------------------------------------------------------------------------- */
+
+/* Deallocate program name if we have one */
+static void deallocate_program_name(void)
+{
+    if (autostart_program_name) {
+	free(autostart_program_name);
+	autostart_program_name = NULL;
+    }
+}
+
+static enum { YES, NO, NOT_YET } check(const char *s)
+{
+    int screen_addr = mem_read(pnt) | (mem_read(pnt + 1) << 8);
+    int line_length = lnmx < 0 ? -lnmx : mem_read(lnmx) + 1;
+    int cursor_column = mem_read(pntr);
+    int addr, i;
+
+    if (!kbd_buf_is_empty() || cursor_column != 0 || mem_read(blnsw) != 0)
+	return NOT_YET;
+
+    addr = screen_addr - line_length;
+    for (i = 0; s[i] != '\0'; i++) {
+	if (mem_read((ADDRESS) (addr + i)) != s[i] % 64) {
+	    if (mem_read((ADDRESS) (addr + i)) != (BYTE) 32)
+		return NO;
+	    return NOT_YET;
+	}
+    }
+
+    return YES;
+}
+
+static void set_true1541_mode(int on)
+{
+    resources_set_value("DriveTrueEmulation", (resource_value_t) on);
+    ui_update_menus();
+}
+
+static int get_true1541_state(void)
+{
+    int value;
+
+    if (resources_get_value("DriveTrueEmulation",
+                            (resource_value_t *) & value) < 0)
+	return 0;
+
+    return value;
+}
+
+/* ------------------------------------------------------------------------- */
+
+static void load_snapshot_trap(ADDRESS unused_addr, void *unused_data)
+{
+    if (autostart_program_name
+        && machine_read_snapshot(autostart_program_name) < 0)
+        ui_error("Cannot load snapshot file\n");
+    ui_update_menus();
+}
+
+/* ------------------------------------------------------------------------- */
+
+/* Initialize autostart.  */
+int autostart_init(CLOCK _min_cycles, int _handle_true1541,
+		   int _blnsw, int _pnt, int _pntr, int _lnmx)
+{
+    blnsw = _blnsw;
+    pnt = _pnt;
+    pntr = _pntr;
+    lnmx = _lnmx;
+
+    if (!pwarn) {
+	pwarn = warn_init("AUTOSTART", 32);
+	if (!pwarn)
+	    return -1;
+    }
+    min_cycles = _min_cycles;
+    handle_true1541 = _handle_true1541;
+
+    if (_min_cycles)
+	autostart_enabled = 1;
+    else
+	autostart_enabled = 0;
+
+    return 0;
+}
+
+void autostart_disable(void)
+{
+    if (!autostart_enabled)
+	return;
+
+    autostartmode = AUTOSTART_ERROR;
+    deallocate_program_name();
+    warn(pwarn, -1, "disabling autostart");
+}
+
+/* ------------------------------------------------------------------------- */
+
+/* This function is called by the `serialreceivebyte()' trap as soon as EOF
+   is reached.  */
+static void disk_eof_callback(void)
+{
+    if (handle_true1541) {
+        if (orig_true1541_state)
+            warn(pwarn, -1, "switching true 1541 on");
+        set_true1541_mode(orig_true1541_state);
+    }
+
+    warn(pwarn, -1, "starting program");
+
+    autostartmode = AUTOSTART_DONE;
+
+    serial_set_eof_callback(NULL);
+}
+
+/* This function is called by the `serialattention()' trap before
+   returning.  */
+static void disk_attention_callback(void)
+{
+    kbd_buf_feed("RUN\r");
+
+    serial_set_attention_callback(NULL);
+
+    /* Next step is waiting for end of loading, to turn true 1541 emulation
+       on.  */
+    serial_set_eof_callback(disk_eof_callback);
+}
+
+
+/* Execute the actions for the current `autostartmode', advancing to the next
+   mode if necessary.  */
+void autostart_advance(void)
+{
+    char *tmp;
+
+    if (clk < min_cycles || !autostart_enabled)
+	return;
+
+    switch (autostartmode) {
+      case AUTOSTART_HASTAPE:
+        switch (check("READY.")) {
+          case YES:
+            warn(pwarn, -1, "loading tape");
+            if (autostart_program_name) {
+                tmp = concat("LOAD\"", autostart_program_name,
+                             "\"\r", NULL);
+                kbd_buf_feed(tmp);
+                free(tmp);
+            } else
+                kbd_buf_feed("LOAD\r");
+            autostartmode = AUTOSTART_LOADINGTAPE;
+            deallocate_program_name();
+            break;
+          case NO:
+            autostart_disable();
+            break;
+          case NOT_YET:
+            break;
+        }
+        break;
+      case AUTOSTART_LOADINGTAPE:
+        switch (check("READY.")) {
+          case YES:
+            warn(pwarn, -1, "starting program");
+            kbd_buf_feed("RUN\r");
+            autostartmode = AUTOSTART_DONE;
+            break;
+          case NO:
+            autostart_disable();
+            break;
+          case NOT_YET:
+            break;
+        }
+        break;
+      case AUTOSTART_HASDISK:
+        switch (check("READY.")) {
+          case YES:
+            {
+                int no_traps;
+
+                warn(pwarn, -1, "loading disk");
+                orig_true1541_state = get_true1541_state();
+                if (handle_true1541) {
+                    resources_get_value("NoTraps",
+					(resource_value_t *) & no_traps);
+                    if (!no_traps) {
+                        if (orig_true1541_state)
+                            warn(pwarn, -1,
+                                 "switching true 1541 emulation off");
+                        set_true1541_mode(0);
+                    } else {
+                        if (!orig_true1541_state)
+                            warn(pwarn, -1,
+                                 "switching true 1541 emulation on");
+                        set_true1541_mode(1);
+                    }
+                } else
+                    no_traps = 0;
+
+                if (autostart_program_name) {
+                    tmp = malloc(strlen(autostart_program_name) + 20);
+                    sprintf(tmp, "LOAD\"%s\",8,1\r",
+                            autostart_program_name);
+                    kbd_buf_feed(tmp);
+                    free(tmp);
+                } else
+                    kbd_buf_feed("LOAD\"*\",8,1\r");
+
+                if (no_traps) {
+                    kbd_buf_feed("RUN\r");
+                    autostartmode = AUTOSTART_DONE;
+                } else {
+                    autostartmode = AUTOSTART_LOADINGDISK;
+                    deallocate_program_name();
+                    serial_set_attention_callback(disk_attention_callback);
+                }
+                break;
+            }
+          case NO:
+            autostart_disable();
+            break;
+          case NOT_YET:
+            break;
+        }
+        break;
+      case AUTOSTART_HASSNAPSHOT:
+        warn(pwarn, -1, "loading snapshot");
+	maincpu_trigger_trap(load_snapshot_trap,(void*)0);
+        autostartmode = AUTOSTART_DONE;
+	break;
+      default:
+        return;
+    }
+
+    if (autostartmode == AUTOSTART_ERROR && handle_true1541) {
+	warn(pwarn, -1, "now turning true 1541 emulation %s",
+	     orig_true1541_state ? "on" : "off");
+	set_true1541_mode(orig_true1541_state);
+    }
+}
+
+static int autostart_ignore_reset = 0;
+
+/* Clean memory and reboot for autostart.  */
+static void reboot_for_autostart(const char *program_name)
+{
+    if (!autostart_enabled)
+	return;
+
+    warn(pwarn, -1, "rebooting...");
+    mem_powerup();
+    autostart_ignore_reset = 1;
+    maincpu_trigger_reset();
+    deallocate_program_name();
+    if (program_name)
+	autostart_program_name = stralloc(program_name);
+}
+
+/* ------------------------------------------------------------------------- */
+
+/* Autostart snapshot file `file_name'.  */
+int autostart_snapshot(const char *file_name, const char *program_name)
+{
+    BYTE vmajor, vminor;
+    snapshot_t *snap;
+
+    if (file_name == NULL || !autostart_enabled)
+	return -1;
+
+    deallocate_program_name();	/* not needed at all */
+
+    if (!(snap = snapshot_open(file_name, &vmajor, &vminor, machine_name)) ) {
+	warn(pwarn, -1, "cannot attach file '%s' (as a snapshot)", file_name);
+	autostartmode = AUTOSTART_ERROR;
+	return -1;
+    }
+    snapshot_close(snap);
+    warn(pwarn, -1, "attached file `%s' as a snapshot file", file_name);
+    autostartmode = AUTOSTART_HASSNAPSHOT;
+    reboot_for_autostart(file_name);	/* use for snapshot */
+
+    return 0;
+}
+
+/* Autostart tape image `file_name'.  */
+int autostart_tape(const char *file_name, const char *program_name)
+{
+    if (file_name == NULL || !autostart_enabled)
+	return -1;
+
+    if (serial_select_file(DT_TAPE, 1, file_name) < 0) {
+	warn(pwarn, -1, "cannot attach file '%s' (as a tape)", file_name);
+	autostartmode = AUTOSTART_ERROR;
+	deallocate_program_name();
+	return -1;
+    }
+    warn(pwarn, -1, "attached file `%s' as a tape image", file_name);
+    autostartmode = AUTOSTART_HASTAPE;
+    reboot_for_autostart(program_name);
+
+    return 0;
+}
+
+/* Autostart disk image `file_name'.  */
+int autostart_disk(const char *file_name, const char *program_name)
+{
+    if (file_name == NULL || !autostart_enabled)
+	return -1;
+
+    if (file_system_attach_disk(8, file_name) < 0) {
+	warn(pwarn, -1, "cannot attach file `%s' as a disk image", file_name);
+	file_system_detach_disk(8);
+	autostartmode = AUTOSTART_ERROR;
+	deallocate_program_name();
+	return -1;
+    }
+    warn(pwarn, -1, "attached file `%s' as a disk image to device #8",
+	 file_name);
+    autostartmode = AUTOSTART_HASDISK;
+    reboot_for_autostart(program_name);
+
+    return 0;
+}
+
+/* Autostart `file_name', trying to auto-detect its type.  */
+int autostart_autodetect(const char *file_name, const char *program_name)
+{
+    if (file_name == NULL)
+	return -1;
+
+    if (!autostart_enabled) {
+	fprintf(errfile, "Couldn't autostart - unknown kernal!");
+	return -1;
+    }
+    if (autostart_disk(file_name, program_name) == 0)
+	warn(pwarn, -1, "`%s' detected as a disk image", file_name);
+    else if (autostart_tape(file_name, program_name) == 0)
+	warn(pwarn, -1, "`%s' detected as a tape image", file_name);
+    else if (autostart_snapshot(file_name, program_name) == 0)
+	warn(pwarn, -1, "`%s' detected as a snapshot file", file_name);
+    else {
+	warn(pwarn, -1, "type of file `%s' unrecognized", file_name);
+	return -1;
+    }
+
+    return 0;
+}
+
+/* Autostart the image attached to device `num'.  */
+int autostart_device(int num)
+{
+    if (!autostart_enabled)
+	return -1;
+
+    switch (num) {
+      case 8:
+        autostartmode = AUTOSTART_HASDISK;
+        break;
+      case 1:
+        autostartmode = AUTOSTART_HASTAPE;
+        break;
+      default:
+        return -1;
+    }
+
+    reboot_for_autostart(NULL);
+    return 0;
+}
+
+/* Disable autostart on reset.  */
+void autostart_reset(void)
+{
+    if (!autostart_enabled)
+	return;
+
+    if (!autostart_ignore_reset
+        && autostartmode != AUTOSTART_NONE
+        && autostartmode != AUTOSTART_ERROR) {
+	warn(pwarn, -1, "disabling autostart");
+	autostartmode = AUTOSTART_NONE;
+	deallocate_program_name();
+    }
+    autostart_ignore_reset = 0;
+}
---
+++ tags/v0.15.0/v0.15.0.31/vice/src/c64/c64.c
@@ -0,0 +1,500 @@
+/*
+ * c64.c
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *  Teemu Rantanen (tvr@cs.hut.fi)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#ifdef STDC_HEADERS
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef __riscos
+#include <unistd.h>
+#endif
+#endif
+
+#include "machine.h"
+
+#include "attach.h"
+#include "autostart.h"
+#include "c64.h"
+#include "c64cia.h"
+#include "c64mem.h"
+#include "c64tpi.h"
+#include "c64ui.h"
+#include "cartridge.h"
+#include "drive.h"
+#include "interrupt.h"
+#include "kbd.h"
+#include "kbdbuf.h"
+#include "maincpu.h"
+#include "mon.h"
+#include "patchrom.h"
+#include "reu.h"
+#include "serial.h"
+#include "sid.h"
+#include "snapshot.h"
+#include "tapeunit.h"
+#include "traps.h"
+#include "utils.h"
+#include "vicii.h"
+#include "vmachine.h"
+#include "vsync.h"
+#include "resources.h"
+
+#if defined __MSDOS__ || defined WIN32
+#include "c64kbd.h"
+#endif
+
+#ifdef HAVE_RS232
+#include "c64acia.h"
+#include "c64rsuser.h"
+#include "rs232.h"
+#include "rsuser.h"
+#endif
+
+#ifdef HAVE_PRINTER
+#include "print.h"
+#include "prdevice.h"
+#include "pruser.h"
+#endif
+
+#ifdef HAVE_MOUSE
+#include "mouse.h"
+#endif
+
+static void vsync_hook(void);
+
+/* ------------------------------------------------------------------------- */
+
+
+/* Serial traps.  */
+static trap_t c64_serial_traps[] = {
+    {
+        "SerialListen",
+        0xED24,
+        0xEDAB,
+        {0x20, 0x97, 0xEE},
+        serialattention
+    },
+    {
+        "SerialSaListen",
+        0xED36,
+        0xEDAB,
+        {0x78, 0x20, 0x8E},
+        serialattention
+    },
+    {
+        "SerialSendByte",
+        0xED40,
+        0xEDAB,
+        {0x78, 0x20, 0x97},
+        serialsendbyte
+    },
+    {
+        "SerialReceiveByte",
+        0xEE13,
+        0xEDAB,
+        {0x78, 0xA9, 0x00},
+        serialreceivebyte
+    },
+    {
+        "SerialReady",
+        0xEEA9,
+        0xEDAB,
+        {0xAD, 0x00, 0xDD},
+        trap_serial_ready
+    },
+
+    {
+        NULL,
+        0,
+        0,
+        {0, 0, 0},
+        NULL
+    }
+};
+
+/* Tape traps.  */
+static trap_t c64_tape_traps[] = {
+    {
+        "FindHeader",
+        0xF72F,
+        0xF732,
+        {0x20, 0x41, 0xF8},
+        findheader
+    },
+    {
+        "WriteHeader",
+        0xF7BE,
+        0xF7C1,
+        {0x20, 0x6B, 0xF8},
+        writeheader
+    },
+    {
+        "TapeReceive",
+        0xF8A1,
+        0xFC93,
+        {0x20, 0xBD, 0xFC},
+        tapereceive
+    },
+    {
+        NULL,
+        0,
+        0,
+        {0, 0, 0},
+        NULL
+    }
+};
+
+/* ------------------------------------------------------------------------ */
+
+/* C64-specific resource initialization.  This is called before initializing
+   the machine itself with `machine_init()'.  */
+int machine_init_resources(void)
+{
+    if (traps_init_resources() < 0
+        || vsync_init_resources() < 0
+        || video_init_resources() < 0
+        || c64_mem_init_resources() < 0
+        || vic_ii_init_resources() < 0
+        || sound_init_resources() < 0
+        || sid_init_resources() < 0
+#ifdef HAVE_RS232
+        || acia1_init_resources() < 0
+        || rs232_init_resources() < 0
+        || rsuser_init_resources() < 0
+#endif
+#ifdef HAVE_PRINTER
+        || print_init_resources() < 0
+        || prdevice_init_resources() < 0
+        || pruser_init_resources() < 0
+#endif
+#ifdef HAVE_MOUSE
+        || mouse_init_resources() < 0
+#endif
+        || kbd_init_resources() < 0
+        || drive_init_resources() < 0
+	|| cartridge_init_resources() < 0)
+        return -1;
+
+    return 0;
+}
+
+/* C64-specific command-line option initialization.  */
+int machine_init_cmdline_options(void)
+{
+    if (traps_init_cmdline_options() < 0
+        || vsync_init_cmdline_options() < 0
+        || video_init_cmdline_options() < 0
+        || c64_mem_init_cmdline_options() < 0
+        || vic_ii_init_cmdline_options() < 0
+        || sound_init_cmdline_options() < 0
+        || sid_init_cmdline_options() < 0
+#ifdef HAVE_RS232
+        || acia1_init_cmdline_options() < 0
+        || rs232_init_cmdline_options() < 0
+        || rsuser_init_cmdline_options() < 0
+#endif
+#ifdef HAVE_PRINTER
+        || print_init_cmdline_options() < 0
+        || prdevice_init_cmdline_options() < 0
+        || pruser_init_cmdline_options() < 0
+#endif
+#ifdef HAVE_MOUSE
+        || mouse_init_cmdline_options() < 0
+#endif
+        || kbd_init_cmdline_options() < 0
+        || drive_init_cmdline_options() < 0)
+        return -1;
+
+    return 0;
+}
+
+/* C64-specific initialization.  */
+int machine_init(void)
+{
+    if (mem_load() < 0)
+        return -1;
+
+    fprintf(logfile, "\nInitializing Serial Bus...\n");
+
+    /* Setup trap handling.  */
+    traps_init();
+
+    /* Initialize serial traps.  */
+    serial_init(c64_serial_traps);
+
+    /* Initialize drives, and attach true 1541 emulation hooks to
+       drive 8 (which is the only true 1541-capable device).  */
+    file_system_set_hooks(8, drive_attach_floppy, drive_detach_floppy);
+    file_system_set_hooks(9, drive_attach_floppy, drive_detach_floppy);
+    file_system_init();
+
+#ifdef HAVE_RS232
+    /* Initialize RS232 handler.  */
+    rs232_init();
+    c64_rsuser_init();
+#endif
+
+#ifdef HAVE_PRINTER
+    /* Initialize print devices.  */
+    print_init();
+#endif
+
+    /* Initialize the tape emulation.  */
+    tape_init(0xb2, 0x90, 0x93, 0x29f, 0, 0xc1, 0xae, c64_tape_traps,
+              0x277, 0xc6);
+
+    /* Fire up the hardware-level 1541 emulation.  */
+    drive_init(C64_PAL_CYCLES_PER_SEC, C64_NTSC_CYCLES_PER_SEC);
+
+    /* Initialize autostart.  */
+    autostart_init(3 * C64_PAL_RFSH_PER_SEC * C64_PAL_CYCLES_PER_RFSH, 1,
+                   0xcc, 0xd1, 0xd3, 0xd5);
+
+    /* Initialize the VIC-II emulation.  */
+    if (vic_ii_init() == NULL)
+        return -1;
+    vic_ii_enable_extended_keyboard_rows(0);
+    cia1_enable_extended_keyboard_rows(0);
+
+    /* Initialize the keyboard.  */
+#if !defined __MSDOS__ && !defined WIN32
+    if (kbd_init() < 0)
+        return -1;
+#else
+    if (c64_kbd_init() < 0)
+        return -1;
+#endif
+
+    /* Initialize the monitor.  */
+    monitor_init(&maincpu_monitor_interface, &drive0_monitor_interface, &drive1_monitor_interface);
+
+    /* Initialize vsync and register our hook function.  */
+    vsync_init(C64_PAL_RFSH_PER_SEC, C64_PAL_CYCLES_PER_SEC, vsync_hook);
+
+    /* Initialize sound.  Notice that this does not really open the audio
+       device yet.  */
+    sound_init(C64_PAL_CYCLES_PER_SEC, C64_PAL_CYCLES_PER_RFSH);
+
+    /* Initialize keyboard buffer.  */
+    kbd_buf_init(631, 198, 10, C64_PAL_CYCLES_PER_RFSH * C64_PAL_RFSH_PER_SEC);
+
+    /* Initialize the C64-specific part of the UI.  */
+    c64_ui_init();
+
+#ifdef HAVE_MOUSE
+    /* Initialize mouse support (if present).  */
+    mouse_init();
+#endif
+
+    return 0;
+}
+
+/* C64-specific reset sequence.  */
+void machine_reset(void)
+{
+    maincpu_int_status.alarm_handler[A_RASTERDRAW] = int_rasterdraw;
+    maincpu_int_status.alarm_handler[A_RASTERFETCH] = int_rasterfetch;
+    maincpu_int_status.alarm_handler[A_RASTER] = int_raster;
+    maincpu_int_status.alarm_handler[A_CIA1TOD] = int_cia1tod;
+    maincpu_int_status.alarm_handler[A_CIA1TA] = int_cia1ta;
+    maincpu_int_status.alarm_handler[A_CIA1TB] = int_cia1tb;
+    maincpu_int_status.alarm_handler[A_CIA2TOD] = int_cia2tod;
+    maincpu_int_status.alarm_handler[A_CIA2TA] = int_cia2ta;
+    maincpu_int_status.alarm_handler[A_CIA2TB] = int_cia2tb;
+
+#ifdef HAVE_RS232
+    maincpu_int_status.alarm_handler[A_ACIA1] = int_acia1;
+    maincpu_int_status.alarm_handler[A_RSUSER] = int_rsuser;
+#endif
+
+    reset_cia1();
+    reset_cia2();
+    sid_reset();
+    reset_tpi();
+
+#ifdef HAVE_RS232
+    reset_acia1();
+
+    rs232_reset();
+    rsuser_reset();
+#endif
+
+#ifdef HAVE_PRINTER
+    print_reset();
+#endif
+
+    /* reset_reu(); *//* FIXME */
+
+    /* The VIC-II must be the *last* to be reset.  */
+    reset_vic_ii();
+
+    autostart_reset();
+
+    drive_reset();
+}
+
+void machine_powerup(void)
+{
+    mem_powerup();
+    maincpu_trigger_reset();
+}
+
+void machine_shutdown(void)
+{
+    /* Detach all devices.  */
+    serial_remove(-1);
+}
+
+void machine_handle_pending_alarms(int num_write_cycles)
+{
+     vic_ii_handle_pending_alarms(num_write_cycles);
+}
+
+/* ------------------------------------------------------------------------- */
+
+/* This hook is called at the end of every frame.  */
+static void vsync_hook(void)
+{
+    CLOCK sub;
+
+    drive_vsync_hook();
+
+    autostart_advance();
+
+    /* We have to make sure the number of cycles subtracted is multiple of
+       `C64_PAL_CYCLES_PER_RFSH' here, or the VIC-II emulation could go
+       nuts.  */
+    sub = maincpu_prevent_clk_overflow(C64_PAL_CYCLES_PER_RFSH);
+    if (sub > 0) {
+        vic_ii_prevent_clk_overflow(sub);
+#ifdef HAVE_RS232
+        rsuser_prevent_clk_overflow(sub);
+#endif
+        cia1_prevent_clk_overflow(sub);
+        cia2_prevent_clk_overflow(sub);
+        sound_prevent_clk_overflow(sub);
+        vsync_prevent_clk_overflow(sub);
+    }
+    /* The 1541 has to deal both with our overflowing and its own one, so it
+       is called even when there is no overflowing in the main CPU.  */
+    /* FIXME: Do we have to check drive_enabled here?  */
+    drive_prevent_clk_overflow(sub, 0);
+    drive_prevent_clk_overflow(sub, 1);
+
+#ifdef HAS_JOYSTICK
+    joystick();
+#endif
+}
+
+int machine_set_restore_key(int v)
+{
+    maincpu_set_nmi(I_RESTORE, v ? 1 : 0);
+    return 1;
+}
+
+/* ------------------------------------------------------------------------- */
+
+long machine_get_cycles_per_second(void)
+{
+    return C64_PAL_CYCLES_PER_SEC;
+}
+
+/* ------------------------------------------------------------------------- */
+
+#define SNAP_MAJOR 1
+#define SNAP_MINOR 0
+
+const char machine_name[] = "C64";
+
+int machine_class = VICE_MACHINE_C64;
+
+int machine_write_snapshot(const char *name, int save_roms, int save_disks)
+{
+    snapshot_t *s;
+
+    s = snapshot_create(name, SNAP_MAJOR, SNAP_MINOR, machine_name);
+    if (s == NULL)
+        return -1;
+
+    /* Execute drive CPUs to get in sync with the main CPU.  */
+    drive_cpu_execute();
+
+    if (maincpu_write_snapshot_module(s) < 0
+        || mem_write_snapshot_module(s, save_roms) < 0
+        || cia1_write_snapshot_module(s) < 0
+        || cia2_write_snapshot_module(s) < 0
+        || sid_write_snapshot_module(s) < 0
+        || drive_write_snapshot_module(s, save_disks, save_roms) < 0
+        || vic_ii_write_snapshot_module(s) < 0) {
+        snapshot_close(s);
+        unlink(name);
+        return -1;
+    }
+
+    snapshot_close(s);
+    return 0;
+}
+
+int machine_read_snapshot(const char *name)
+{
+    snapshot_t *s;
+    BYTE minor, major;
+
+    s = snapshot_open(name, &major, &minor, machine_name);
+    if (s == NULL)
+        return -1;
+
+#if 0
+    if (major != SNAP_MAJOR || minor != SNAP_MINOR) {
+        fprintf(logfile, "Snapshot version (%d.%d) not valid: expecting %d.%d.\n",
+               major, minor, SNAP_MAJOR, SNAP_MINOR);
+        goto fail;
+    }
+#endif
+
+    vic_ii_prepare_for_snapshot();
+
+    if (maincpu_read_snapshot_module(s) < 0
+        || mem_read_snapshot_module(s) < 0
+        || cia1_read_snapshot_module(s) < 0
+        || cia2_read_snapshot_module(s) < 0
+        || sid_read_snapshot_module(s) < 0
+        || drive_read_snapshot_module(s) < 0
+        || vic_ii_read_snapshot_module(s) < 0)
+        goto fail;
+
+    snapshot_close(s);
+
+    return 0;
+
+fail:
+    if (s != NULL)
+        snapshot_close(s);
+    maincpu_trigger_reset();
+    return -1;
+}
