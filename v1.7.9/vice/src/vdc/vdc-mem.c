f161b09a2abd0ae9e79c9cab384687cbcb7750ef
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 10:03:43 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.7.9'.

---
+++ v1.7.9/vice/src/vdc/vdc-mem.c
@@ -0,0 +1,366 @@
+/*
+ * vdc-mem.c - Memory interface for the MOS 8563 (VDC) emulation.
+ *
+ * Written by
+ *  Markus Brenner   <markus@brenner.de>
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "maincpu.h"
+#include "types.h"
+#include "vdc-mem.h"
+#include "vdc.h"
+#include "vdctypes.h"
+#include "vicii.h"
+
+/* #define REG_DEBUG */
+
+static void vdc_write_data(void)
+{
+    int ptr;
+
+    /* Update address.  */
+    ptr = (vdc.regs[18] << 8) + vdc.regs[19];
+
+    /* Write data byte to update address. */
+    vdc.ram[ptr & vdc.vdc_address_mask] = vdc.regs[31];
+    ptr += 1;
+    vdc.regs[18] = (ptr >> 8) & 0xff;
+    vdc.regs[19] = ptr & 0xff;
+}
+
+
+static void vdc_perform_fillcopy(void)
+{
+    int ptr, ptr2;
+    int i;
+    int blklen;
+
+    /* Word count, # of bytes to copy */
+    blklen = vdc.regs[30] ? vdc.regs[30] : 256;
+
+    /* Update address.  */
+    ptr = (vdc.regs[18] << 8) + vdc.regs[19];
+
+    if (vdc.regs[24] & 0x80) { /* COPY flag */
+        /* Block start address.  */
+        ptr2 = (vdc.regs[32] << 8) + vdc.regs[33];
+        for (i = 0; i < blklen; i++) {
+            vdc.ram[(ptr + i) & vdc.vdc_address_mask]
+                = vdc.ram[(ptr2 + i) & vdc.vdc_address_mask];
+        }
+        ptr2 += blklen;
+        vdc.regs[31] = vdc.ram[(ptr2 - 1) & vdc.vdc_address_mask];
+        vdc.regs[32] = (ptr2 >> 8) & 0xff;
+        vdc.regs[33] = ptr2 & 0xff;
+    } else {
+#ifdef REG_DEBUG
+        log_message(vdc.log, "Fill mem %04x, len %03x, data %02x",
+                    ptr, blklen, vdc.regs[31]);
+#endif
+        for (i = 0; i < blklen; i++)
+            vdc.ram[(ptr + i) & vdc.vdc_address_mask] = vdc.regs[31];
+    }
+
+    ptr = ptr + blklen;
+    vdc.regs[18] = (ptr >> 8) & 0xff;
+    vdc.regs[19] = ptr & 0xff;
+}
+
+
+/* VDC interface functions. */
+
+/* Store a value in a VDC register. */
+void REGPARM2 vdc_store(ADDRESS addr, BYTE value)
+{
+    vic_ii_handle_pending_alarms(maincpu_num_write_cycles());
+
+    /* $d600 sets the internal vdc address pointer */
+    if ((addr & 1) == 0)
+    {
+#ifdef REG_DEBUG
+        /*log_message(vdc.log, "STORE $D600 %02x", value);*/
+#endif
+        vdc.update_reg = value & 0x3f;
+        return;
+    }
+
+    /* $d601 sets the vdc register indexed by the update register pointer */
+    vdc.regs[vdc.update_reg] = value;
+
+#ifdef REG_DEBUG
+    log_message(vdc.log, "REG %02i VAL %02x", vdc.update_reg, value);
+#endif
+
+    switch (vdc.update_reg)
+    {
+      case 0:                   /* R00  Horizontal total (characters + 1) */
+/*      new_vdc_cycles_per_line = vdc[0] + 1; */
+#ifdef REG_DEBUG
+    log_message(vdc.log, "REG 0 unsupported!");
+#endif
+        break;
+
+      case 1:                   /* R01  Horizontal characters displayed */
+        if (vdc.regs[1] >= 8 && vdc.regs[1] <= VDC_SCREEN_MAX_TEXTCOLS) {
+            if (vdc.screen_text_cols != vdc.regs[1]) {
+                vdc.force_cache_flush = 1;
+                vdc.force_repaint = 1;
+                vdc.screen_text_cols = vdc.regs[1];
+            }
+        }
+#ifdef REG_DEBUG
+        log_message(vdc.log, "Horizontal Displayed %i", vdc.regs[1]);
+#endif
+        break;
+
+      case 2:                   /* R02  Horizontal Sync Position */
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 2 unsupported!");
+#endif
+        break;
+
+      case 3:                   /* R03  Horizontal/Vertical Sync widths */
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 3 unsupported!");
+#endif
+        break;
+
+      case 4:                   /* R04  Vertical total (character) rows */
+/*
+        new_vdc_vertical_total = vdc[4] + 1;
+*/
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 4 unsupported!");
+#endif
+        break;
+
+      case 5:                   /* R05  Vertical total line adjust */
+/*        new_vdc_vertical_adjust = vdc[5] & 0x1f; */
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 5 unsupported!");
+#endif
+        break;
+
+      case 6:                   /* R06  Number of display lines on screen */
+/*        new_vdc_screen_textlines = vdc[6] & 0x7f; */
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 6 unsupported!");
+#endif
+        break;
+
+      case 7:                   /* R07  Vertical sync position */
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 7 unsupported!");
+#endif
+        break;
+
+      case 8:                   /* R08  unused: Interlace and Skew */
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 8 unsupported!");
+#endif
+        break;
+
+      case 9:                   /* R09  Rasters between two display lines */
+/*      new_screen_charheight = vdc_min(8, vdc[9] + 1); */
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 9 unsupported!");
+#endif
+        break;
+
+      case 10:                  /* R10  Cursor Mode, Start Scan */
+        switch (value & 0x60) {
+          case 0x00:
+            vdc.cursor_visible = 1;
+            vdc.cursor_frequency = 0;
+            break;
+          case 0x20:
+            vdc.cursor_visible = 0;
+            vdc.cursor_frequency = 0;
+            break;
+          case 0x40:
+            vdc.cursor_visible = 1;
+            vdc.cursor_frequency = 16;
+            break;
+          case 0x60:
+            vdc.cursor_visible = 1;
+            vdc.cursor_frequency = 32;
+            break;
+        }
+        break;
+
+      case 11:                  /* R11  Cursor */
+        break;
+
+      case 12:                  /* R12  Display Start Address hi */
+      case 13:                  /* R13  Display Start Address lo */
+        vdc.screen_adr = ((vdc.regs[12] << 8) | vdc.regs[13])
+                         & vdc.vdc_address_mask;
+#ifdef REG_DEBUG
+        log_message(vdc.log,"Update screen_adr: %x.", vdc.screen_adr);
+#endif
+        break;
+
+      case 14:
+      case 15:                  /* R14-5 Cursor location HI/LO */
+        vdc.crsrpos = ((vdc.regs[14] << 8) | vdc.regs[15])
+                      & vdc.vdc_address_mask;;
+        break;
+
+      case 16:                  /* R16/17 Light Pen hi/lo */
+      case 17:
+        break;
+
+      case 18:                  /* R18/19 Update Address hi/lo */
+      case 19:
+        vdc.update_adr = ((vdc.regs[18] << 8) | vdc.regs[19])
+                         & vdc.vdc_address_mask;
+        break;
+
+      case 20:                  /* R20/21 Attribute Start Address hi/lo */
+      case 21:
+        vdc.attribute_adr = ((vdc.regs[20] << 8) | vdc.regs[21])
+                            & vdc.vdc_address_mask;
+#ifdef REG_DEBUG
+        log_message(vdc.log,"Update attribute_adr: %x.", vdc.attribute_adr);
+#endif
+        break;
+
+      case 24:
+        if (value & 0x20)
+          vdc.text_blink_frequency = 32;
+        else
+          vdc.text_blink_frequency = 16;
+#ifdef REG_DEBUG
+        log_message(vdc.log, "Blink frequency: %s.",
+                    (vdc.regs[24] & 0x20) ? "1/32" : "1/16");
+        log_message(vdc.log, "Screen mode: %s.",
+                    (vdc.regs[24] & 0x40) ? "reverse" : "normal");
+#endif
+        break;
+
+      case 25:
+        vdc.raster.video_mode = (vdc.regs[25] & 0x80)
+                                ? VDC_BITMAP_MODE : VDC_TEXT_MODE;
+#ifdef REG_DEBUG
+        log_message(vdc.log, "Video mode: %s.",
+                    (vdc.regs[25] & 0x80) ? "bitmap" : "text");
+        log_message(vdc.log, "Color source: %s.",
+                    (vdc.regs[25] & 0x40) ? "attribute space" : "register 26");
+        if (vdc.regs[25] & 0x20)
+            log_message(vdc.log, "Semi-Graphics Mode unsupported!");
+        if (vdc.regs[25] & 0x10)
+            log_message(vdc.log, "Double-Pixel Mode unsupported!");
+#endif
+        break;
+
+      case 26:
+#ifdef REG_DEBUG
+        log_message(vdc.log, "Color register %x.", vdc.regs[26]);
+#endif
+        break;
+
+      case 27:                  /* R27  Row/Adrs. Increment */
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 27 unsupported!");
+#endif
+
+      case 28:
+        vdc.chargen_adr = ((vdc.regs[28] << 8) & 0xe000)
+                          & vdc.vdc_address_mask;
+#ifdef REG_DEBUG
+        log_message(vdc.log, "Update chargen_adr: %x.", vdc.chargen_adr);
+#endif
+        break;
+
+      case 30:                  /* Word Count */
+        vdc_perform_fillcopy();
+        break;
+
+      case 31:                  /* Data */
+        vdc_write_data();
+        break;
+
+      case 32:                  /* R32/33 Block Start Address hi/lo */
+      case 33:
+        break;
+
+      case 34:                  /* R34  Display Enable Begin */
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 34 unsupported!");
+#endif
+        break;
+
+      case 35:                  /* R35  Display Enable End */
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 35 unsupported!");
+#endif
+        break;
+
+      case 36:                  /* R36  DRAM Refresh Rate */
+#ifdef REG_DEBUG
+        log_message(vdc.log, "REG 36 unsupported!");
+#endif
+        break;
+    }
+}
+
+
+BYTE REGPARM1 vdc_read(ADDRESS addr)
+{
+    vic_ii_handle_pending_alarms(0);
+
+    if (addr & 1) {
+        /*log_message(vdc.log, "read: addr = %x", addr);*/
+
+        if (vdc.update_reg == 31) {
+            BYTE retval;
+            int ptr;
+
+            retval = vdc.ram[((vdc.regs[18] << 8) + vdc.regs[19])
+                     & vdc.vdc_address_mask];
+            ptr = (1 + vdc.regs[19] + (vdc.regs[18] << 8))
+                  & vdc.vdc_address_mask;
+            vdc.regs[18] = (ptr >> 8) & 0xff;
+            vdc.regs[19] = ptr & 0xff;
+            return retval;
+        }
+
+        if (vdc.update_reg < 37)
+            return vdc.regs[vdc.update_reg];
+
+        return 0xff;
+    } else {
+        /* Emulate vblank bit.  */
+        if (vdc.raster.current_line > vdc.last_displayed_line)
+            return 0xbf;
+
+        return 0x9f;
+    }
+}
+
---
+++ v1.7.9/vice/src/vic20/vic.c
@@ -0,0 +1,562 @@
+/*
+ * vic.c - A line-based VIC-I emulation (under construction).
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Andreas Matthies <andreas.matthies@gmx.net>
+ *
+ * 16/24bpp support added by
+ *  Steven Tieu <stieu@physics.ubc.ca>
+ *  Teemu Rantanen <tvr@cs.hut.fi>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+/* Warning: this emulation is very incomplete and buggy.  */
+
+#include "vice.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "clkguard.h"
+#include "log.h"
+#include "mem.h"
+#include "machine.h"
+#include "maincpu.h"
+#include "palette.h"
+#include "raster-modes.h"
+#include "resources.h"
+#include "screenshot.h"
+#include "snapshot.h"
+#include "types.h"
+#include "utils.h"
+#include "vic.h"
+#include "vic-cmdline-options.h"
+#include "vic-draw.h"
+#include "vic-mem.h"
+#include "vic-resources.h"
+#include "vic-snapshot.h"
+#include "vsync.h"
+#ifdef USE_XF86_EXTENSIONS
+#include "fullscreen.h"
+#endif
+
+/* FIXME: PAL/NTSC constants should be moved from drive.h */
+#define DRIVE_SYNC_PAL     -1
+#define DRIVE_SYNC_NTSC    -2
+#define DRIVE_SYNC_NTSCOLD -3
+
+vic_t vic;
+
+static void vic_exposure_handler (unsigned int width, unsigned int height);
+
+static void vic_exposure_handler (unsigned int width, unsigned int height)
+{
+  raster_resize_viewport (&vic.raster, width, height);
+
+  /* FIXME: Needed?  Maybe this should be triggered by
+     `raster_resize_viewport()' automatically.  */
+  raster_force_repaint (&vic.raster);
+}
+
+
+void vic_change_timing(void)
+{
+    resource_value_t mode;
+
+    resources_get_value("VideoStandard", &mode);
+
+    switch ((int)mode) {
+      case DRIVE_SYNC_NTSC:
+        clk_guard_set_clk_base (&maincpu_clk_guard, VIC20_NTSC_CYCLES_PER_RFSH);
+        vic.screen_height = VIC20_NTSC_SCREEN_LINES;
+        vic.screen_width = VIC_NTSC_SCREEN_WIDTH;
+        vic.display_width = VIC_NTSC_DISPLAY_WIDTH;
+        vic.first_displayed_line = VIC20_NTSC_FIRST_DISPLAYED_LINE;
+        vic.last_displayed_line = VIC20_NTSC_LAST_DISPLAYED_LINE;
+        vic.cycles_per_line = VIC20_NTSC_CYCLES_PER_LINE;
+        vic.cycle_offset = VIC20_NTSC_CYCLE_OFFSET;
+        break;
+      case DRIVE_SYNC_PAL:
+      default:
+        clk_guard_set_clk_base (&maincpu_clk_guard, VIC20_PAL_CYCLES_PER_RFSH);
+        vic.screen_height = VIC20_PAL_SCREEN_LINES;
+        vic.screen_width = VIC_PAL_SCREEN_WIDTH;
+        vic.display_width = VIC_PAL_DISPLAY_WIDTH;
+        vic.first_displayed_line = VIC20_PAL_FIRST_DISPLAYED_LINE;
+        vic.last_displayed_line = VIC20_PAL_LAST_DISPLAYED_LINE;
+        vic.cycles_per_line = VIC20_PAL_CYCLES_PER_LINE;
+        vic.cycle_offset = VIC20_PAL_CYCLE_OFFSET;
+        break;
+    }
+}
+
+static void vic_set_geometry(void)
+{
+    unsigned int width, height;
+
+    raster_set_geometry (&vic.raster,
+                         vic.screen_width, vic.screen_height,
+                         22*8, 23*8,    /* handled dynamically  */
+                         22, 23,        /* handled dynamically  */
+                         12*4, 38*2 - vic.first_displayed_line, /* handled dynamically  */
+                         1,
+                         vic.first_displayed_line,
+                         vic.last_displayed_line,
+                         vic.screen_width + VIC_SCREEN_MAX_TEXT_COLS * 8);
+
+    width = vic.display_width * VIC_PIXEL_WIDTH;
+    height = vic.last_displayed_line - vic.first_displayed_line + 1;
+
+#ifdef USE_XF86_EXTENSIONS
+    if (fullscreen_is_enabled ? vic_resources.fullscreen_double_size_enabled : vic_resources.double_size_enabled) {
+#else
+    if (vic_resources.double_size_enabled) {
+#endif
+        width *= 2;
+        height *= 2;
+        raster_set_pixel_size (&vic.raster, VIC_PIXEL_WIDTH * 2, 2);
+    } else
+        raster_set_pixel_size (&vic.raster, VIC_PIXEL_WIDTH, 1);
+
+    raster_resize_viewport (&vic.raster, width, height);
+
+#ifdef __MSDOS__
+  video_ack_vga_mode();
+#endif
+
+}
+
+
+
+/* Notice: The screen origin X register has a 4-pixel granularity, so our
+   write accesses are always aligned. */
+
+void vic_raster_draw_alarm_handler (CLOCK offset)
+{
+    static int pending_mem_offset;
+    static int possible_mem_offset;
+    int blank_this_line;
+
+    /* check if first visible line is reached */
+    if (vic.area == 0 && vic.raster.current_line >= vic.raster.display_ystart)
+        vic.area = 1;
+
+    /* remember if this line stays blank */
+    blank_this_line = vic.raster.blank_this_line;
+    if (blank_this_line)
+        vic.raster.display_ystop++;
+
+    /* check if row step is pending */
+    if (vic.row_increase_line == vic.raster.ycounter
+        || 2 * vic.row_increase_line == vic.raster.ycounter)
+    {
+        vic.row_counter++;
+        
+        vic.raster.ycounter = 0;
+        
+        vic.raster.display_ystop = vic.raster.current_line 
+            + (vic.text_lines - vic.row_counter) * vic.char_height;
+
+        /* if XPOS is 0 VIC displays one more rasterline */
+        if (vic.raster.display_xstart == 0)
+            vic.raster.display_ystop++;
+
+        pending_mem_offset = possible_mem_offset;
+    }
+
+    /*  update memptr */
+    if (vic.area == 1)
+        vic.memptr += pending_mem_offset;
+    
+    pending_mem_offset = 0;
+
+    /* max offset for next row */
+    possible_mem_offset = vic.text_cols;
+    
+    /* emulate the line */
+    raster_emulate_line (&vic.raster);
+
+    /* xstart may have changed; recalculate xstop */
+    vic.raster.display_xstop = vic.raster.display_xstart + vic.text_cols * 8;
+	if (vic.raster.display_xstop >= vic.screen_width)
+		vic.raster.display_xstop = vic.screen_width - 1;
+
+    /* increment ycounter and set offset for memptr */
+    if (vic.area == 1 && !blank_this_line)
+    {
+        vic.raster.ycounter++;
+
+        if (vic.row_offset != 0 || vic.raster.ycounter == vic.row_increase_line)
+        {
+            /* this only happens if char_height changes between 8 and 16 within line 7 */
+            pending_mem_offset = 
+                (vic.row_offset > 0 ? vic.row_offset : possible_mem_offset);
+            vic.row_offset = 0;
+        }
+
+        if (vic.raster.current_line >= vic.raster.display_ystop)
+            vic.area = 2;
+    }
+
+    /* handle start of frame */
+    if (vic.raster.current_line == 0)
+    {
+        raster_skip_frame (&vic.raster, vsync_do_vsync(vic.raster.skip_frame));
+        vic.raster.blank_enabled = 1;
+        vic.row_counter = 0;
+        vic.raster.ycounter = 0;
+        vic.memptr = 0;
+        vic.area = 0;
+
+        if (vic.pending_ystart >= 0)
+        {
+            vic.raster.display_ystart = vic.pending_ystart;
+            vic.raster.geometry.gfx_position.y = 
+                vic.pending_ystart - vic.first_displayed_line;
+            vic.raster.display_ystop = 
+                vic.raster.display_ystart + vic.text_lines * vic.char_height;
+
+            vic.pending_ystart = -1;
+        }
+
+        if (vic.pending_text_lines >= 0)
+        {
+            vic.text_lines = vic.pending_text_lines;
+            vic.raster.display_ystop = 
+                (vic.raster.display_ystart + vic.text_lines * vic.char_height);
+            vic.raster.geometry.gfx_size.height = vic.pending_text_lines * 8;
+            vic.raster.geometry.text_size.height = vic.pending_text_lines;
+
+            vic.pending_text_lines = -1;
+        }
+
+        vic.raster.blank = 0;
+    }
+
+    /* Set the next draw event.  */
+    vic.last_emulate_line_clk += vic.cycles_per_line;
+    vic.draw_clk = vic.last_emulate_line_clk + vic.cycles_per_line;
+    alarm_set (&vic.raster_draw_alarm, vic.draw_clk);
+}
+
+static int init_raster(void)
+{
+    raster_t *raster;
+    char *title;
+
+    raster = &vic.raster;
+
+    if (raster_init(raster, VIC_NUM_VMODES, VIC_NUM_SPRITES) < 0)
+        return -1;
+
+    raster_modes_set_idle_mode (raster->modes, VIC_IDLE_MODE);
+    raster_set_exposure_handler (raster, (void*)vic_exposure_handler);
+    raster_enable_cache (raster, vic_resources.video_cache_enabled);
+#ifdef USE_XF86_EXTENSIONS
+    raster_enable_double_scan (raster, fullscreen_is_enabled ? vic_resources.fullscreen_double_scan_enabled : vic_resources.double_scan_enabled);
+#else
+    raster_enable_double_scan (raster, vic_resources.double_scan_enabled);
+#endif
+    raster_set_canvas_refresh(raster, 1);
+
+    vic_set_geometry();
+    
+    if (vic_load_palette (vic_resources.palette_file_name) < 0) {
+        log_error (vic.log, "Cannot load palette.");
+        return -1;
+    }
+
+    title = concat ("VICE: ", machine_name, " emulator", NULL);
+    raster_set_title (raster, title);
+    free(title);
+
+    if (raster_realize(raster) < 0)
+        return -1;
+
+    raster->display_ystart = vic.first_displayed_line;
+    raster->display_ystop = vic.first_displayed_line + 1;
+    raster->display_xstart = 0;
+    raster->display_xstop = 1;
+
+    return 0;
+}
+
+
+static void clk_overflow_callback(CLOCK sub, void *data)
+{
+    if (vic.last_emulate_line_clk > (CLOCK)0)
+        vic.last_emulate_line_clk -= sub;
+}
+
+
+/* Initialization. */
+raster_t *vic_init(void)
+{
+  vic.log = log_open("VIC");
+
+  alarm_init(&vic.raster_draw_alarm, maincpu_alarm_context,
+             "VicIRasterDraw", vic_raster_draw_alarm_handler);
+
+  clk_guard_add_callback(&maincpu_clk_guard, clk_overflow_callback, NULL);
+
+  vic_change_timing();
+
+  if (init_raster() < 0)
+    return NULL;
+
+  vic.auxiliary_color = 0;
+  vic.mc_border_color = 0;
+
+  vic.color_ptr = ram;
+  vic.screen_ptr = ram;
+  vic.chargen_ptr = chargen_rom + 0x400;
+
+  /* FIXME */
+  vic.char_height = 8;
+  vic.row_increase_line = 8;
+  vic.text_cols = 22;
+  vic.text_lines = 23;
+
+  vic_reset ();
+
+  vic_draw_init ();
+#ifdef USE_XF86_EXTENSIONS
+  vic_draw_set_double_size (fullscreen_is_enabled ? vic_resources.fullscreen_double_size_enabled : vic_resources.double_size_enabled);
+#else
+  vic_draw_set_double_size (vic_resources.double_size_enabled);
+#endif
+
+  vic_update_memory_ptrs ();
+
+  vic.initialized = 1;
+
+  return &vic.raster;
+}
+
+struct canvas_s *vic_get_canvas(void)
+{
+  return vic.raster.viewport.canvas;
+}
+
+/* Reset the VIC-I chip. */
+void vic_reset (void)
+{
+  vic_change_timing();
+
+  raster_reset (&vic.raster);
+
+  vic_set_geometry();
+
+  vic.last_emulate_line_clk = 0;
+  vic.draw_clk = vic.cycles_per_line;
+  alarm_set (&vic.raster_draw_alarm, vic.draw_clk);
+
+  vic.row_counter = 0;
+  vic.memptr = 0;
+  vic.pending_ystart = -1;
+  vic.pending_text_lines = -1;
+  vic.row_offset = -1;
+  vic.area = 0;
+
+}
+
+/* WARNING: This does not change the resource value.  External modules are
+   expected to set the resource value to change the VIC-II palette instead of
+   calling this function directly.  */
+int vic_load_palette (const char *name)
+{
+  static const char *color_names[] =
+    {
+      "Black", "White", "Red", "Cyan", "Purple", "Green", "Blue",
+      "Yellow", "Orange", "Light Orange", "Pink", "Light Cyan",
+      "Light Purple", "Light Green", "Light Blue", "Light Yellow"
+    };
+  palette_t *palette;
+
+  palette = palette_create (VIC_NUM_COLORS, color_names);
+  if (palette == NULL)
+    return -1;
+
+  if (palette_load (name, palette) < 0)
+    {
+      log_message (vic.log, "Cannot load palette file `%s'.", name);
+      return -1;
+    }
+
+  return raster_set_palette (&vic.raster, palette);
+}
+
+/* This hook is called whenever the screen parameters (eg. window size) are
+   changed.  */
+void vic_resize (void)
+{
+  if (!vic.initialized)
+    return;
+
+#ifdef USE_XF86_EXTENSIONS
+    if (fullscreen_is_enabled ? vic_resources.fullscreen_double_size_enabled : vic_resources.double_size_enabled)
+#else
+    if (vic_resources.double_size_enabled)
+#endif
+
+    {
+      if (vic.raster.viewport.pixel_size.width == VIC_PIXEL_WIDTH
+          && vic.raster.viewport.canvas != NULL) {
+          raster_set_pixel_size (&vic.raster, VIC_PIXEL_WIDTH * 2, 2);
+        raster_resize_viewport (&vic.raster,
+                                vic.raster.viewport.width * 2,
+                                vic.raster.viewport.height * 2);
+      } else {
+          raster_set_pixel_size (&vic.raster, VIC_PIXEL_WIDTH * 2, 2);
+      }
+
+      vic_draw_set_double_size (1);
+    }
+  else
+    {
+      if (vic.raster.viewport.pixel_size.width == VIC_PIXEL_WIDTH * 2
+          && vic.raster.viewport.canvas != NULL) {
+          raster_set_pixel_size (&vic.raster, VIC_PIXEL_WIDTH, 1);
+        raster_resize_viewport (&vic.raster,
+                                vic.raster.viewport.width / 2,
+                                vic.raster.viewport.height / 2);
+      } else {
+          raster_set_pixel_size (&vic.raster, VIC_PIXEL_WIDTH, 1);
+      }
+
+      vic_draw_set_double_size (0);
+    }
+}
+
+/* Set the memory pointers according to the values stored in the VIC
+   registers. */
+void vic_update_memory_ptrs (void)
+{
+  static BYTE *old_chargen_ptr = NULL;
+  static BYTE *old_color_ptr = NULL;
+  static BYTE *old_screen_ptr = NULL;
+
+  ADDRESS char_addr;
+  int tmp;
+  
+  BYTE *new_chargen_ptr;
+  BYTE *new_color_ptr;
+  BYTE *new_screen_ptr;
+
+  tmp = vic.regs[0x5] & 0xf;
+  char_addr = (tmp & 0x8) ? 0x0000 : 0x8000;
+  char_addr += (tmp & 0x7) * 0x400;
+
+  if (char_addr >= 0x8000 && char_addr < 0x9000)
+    {
+      new_chargen_ptr = chargen_rom + 0x400 + (char_addr & 0xfff);
+      VIC_DEBUG_REGISTER (("Character memory at $%04X "
+                           "(character ROM + $%04X).",
+                           char_addr,
+                           char_addr & 0xfff));
+    }
+  else
+    {
+      if (char_addr == 0x1c00)
+        new_chargen_ptr = chargen_rom;    /* handle wraparound */
+      else
+        new_chargen_ptr = ram + char_addr;
+      VIC_DEBUG_REGISTER (("Character memory at $%04X.", char_addr));
+    }
+
+  new_color_ptr = ram + 0x9400 + (vic.regs[0x2] & 0x80 ? 0x200 : 0x0);
+  new_screen_ptr = ram + (((vic.regs[0x2] & 0x80) << 2)
+                          | ((vic.regs[0x5] & 0x70) << 6));
+
+  VIC_DEBUG_REGISTER (("Color memory at $%04X.", vic.color_ptr - ram));
+  VIC_DEBUG_REGISTER (("Screen memory at $%04X.", vic.screen_ptr - ram));
+
+  if (new_chargen_ptr != old_chargen_ptr)
+  {
+    raster_add_ptr_change_foreground (&vic.raster,
+          VIC_RASTER_CHAR(VIC_RASTER_CYCLE(clk) + 2),
+          (void*)&vic.chargen_ptr,
+          new_chargen_ptr);
+
+    old_chargen_ptr = new_chargen_ptr;
+  }
+
+  if (new_color_ptr != old_color_ptr)
+  {
+    raster_add_ptr_change_foreground (&vic.raster,
+          VIC_RASTER_CHAR(VIC_RASTER_CYCLE(clk) + 3),
+          (void*)&vic.color_ptr,
+          new_color_ptr);
+
+    old_color_ptr = new_color_ptr;
+  }
+  if (new_screen_ptr != old_screen_ptr)
+  {
+    raster_add_ptr_change_foreground (&vic.raster,
+          VIC_RASTER_CHAR(VIC_RASTER_CYCLE(clk) + 3),
+          (void*)&vic.screen_ptr,
+          new_screen_ptr);
+
+    old_screen_ptr = new_screen_ptr;
+  }
+}
+
+int vic_init_resources (void)
+{
+  return vic_resources_init ();
+}
+
+int vic_init_cmdline_options (void)
+{
+  return vic_cmdline_options_init ();
+}
+
+int vic_write_snapshot_module (snapshot_t *s)
+{
+  return vic_snapshot_write_module (s);
+}
+
+int vic_read_snapshot_module (snapshot_t *s)
+{
+  return vic_snapshot_read_module (s);
+}
+
+void vic_free (void)
+{
+  raster_free(&vic.raster);
+}
+
+int vic_screenshot(screenshot_t *screenshot)
+{
+    return raster_screenshot(&vic.raster, screenshot);
+}
+
+void vic_video_refresh(void)
+{
+#ifdef USE_XF86_EXTENSIONS
+  vic_resize ();
+  raster_enable_double_scan (&vic.raster,
+			     fullscreen_is_enabled ?
+			     vic_resources.fullscreen_double_scan_enabled :
+			     vic_resources.double_scan_enabled);
+#endif
+}
