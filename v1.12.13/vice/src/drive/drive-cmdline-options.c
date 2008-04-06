1e4e3a0d537ad26b7119ad1c0db6f1eefecea794
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 14:11:21 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.12.13'.

---
+++ v1.12.13/vice/src/drive/drive-cmdline-options.c
@@ -0,0 +1,61 @@
+/*
+ * drive-cmdline-options.c - Hardware-level Commodore disk drive emulation,
+ *                           command line options module.
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "cmdline.h"
+#include "drive-cmdline-options.h"
+#include "drive.h"
+#include "machine-drive.h"
+
+
+static const cmdline_option_t cmdline_options[] = {
+    { "-truedrive", SET_RESOURCE, 0, NULL, NULL, "DriveTrueEmulation",
+      (void *)1, NULL, "Enable hardware-level emulation of disk drives" },
+    { "+truedrive", SET_RESOURCE, 0, NULL, NULL, "DriveTrueEmulation",
+      (void *)0, NULL, "Disable hardware-level emulation of disk drives" },
+    { "-drive8type", SET_RESOURCE, 1, NULL, NULL, "Drive8Type",
+      (void *)DRIVE_TYPE_1541, "<type>", "Set drive type (0: no drive)" },
+    { "-drive9type", SET_RESOURCE, 1, NULL, NULL, "Drive9Type",
+      (void *)DRIVE_TYPE_NONE, "<type>", "Set drive type (0: no drive)" },
+    { "-drive8extend", SET_RESOURCE, 1, NULL, NULL, "Drive8ExtendImagePolicy",
+      (void *)DRIVE_EXTEND_NEVER, "<method>",
+      "Set drive 8 40 track extension policy (0: never, 1: ask, 2: on access)"},
+    { "-drive9extend", SET_RESOURCE, 1, NULL, NULL, "Drive9ExtendImagePolicy",
+      (void *)DRIVE_EXTEND_NEVER, "<method>",
+      "Set drive 9 40 track extension policy (0: never, 1: ask, 2: on access)"},
+    { NULL }
+};
+
+int drive_cmdline_options_init(void)
+{
+    return machine_drive_cmdline_options_init()
+        | cmdline_register_options(cmdline_options);
+}
+
---
+++ v1.12.13/vice/src/drive/ieee/ieee-resources.c
@@ -0,0 +1,113 @@
+/*
+ * ieee-resources.c
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "driverom.h"
+#include "ieee-resources.h"
+#include "ieeerom.h"
+#include "lib.h"
+#include "resources.h"
+#include "utils.h"
+
+
+static char *dos_rom_name_2031 = NULL;
+static char *dos_rom_name_1001 = NULL;
+static char *dos_rom_name_2040 = NULL;
+static char *dos_rom_name_3040 = NULL;
+static char *dos_rom_name_4040 = NULL;
+
+
+static int set_dos_rom_name_2040(resource_value_t v, void *param)
+{
+    if (util_string_set(&dos_rom_name_2040, (const char *)v))
+        return 0;
+
+    return ieeerom_load_2040();
+}
+
+static int set_dos_rom_name_3040(resource_value_t v, void *param)
+{
+    if (util_string_set(&dos_rom_name_3040, (const char *)v))
+        return 0;
+
+    return ieeerom_load_3040();
+}
+
+static int set_dos_rom_name_4040(resource_value_t v, void *param)
+{
+    if (util_string_set(&dos_rom_name_4040, (const char *)v))
+        return 0;
+
+    return ieeerom_load_4040();
+}
+
+static int set_dos_rom_name_1001(resource_value_t v, void *param)
+{
+    if (util_string_set(&dos_rom_name_1001, (const char *)v))
+        return 0;
+
+    return ieeerom_load_1001();
+}
+
+static int set_dos_rom_name_2031(resource_value_t v, void *param)
+{
+    if (util_string_set(&dos_rom_name_2031, (const char *)v))
+        return 0;
+
+    return ieeerom_load_2031();
+}
+
+static const resource_t resources[] = {
+    { "DosName2031", RES_STRING, (resource_value_t)"dos2031",
+      (void *)&dos_rom_name_2031, set_dos_rom_name_2031, NULL },
+    { "DosName2040", RES_STRING, (resource_value_t)"dos2040",
+      (void *)&dos_rom_name_2040, set_dos_rom_name_2040, NULL },
+    { "DosName3040", RES_STRING, (resource_value_t)"dos3040",
+      (void *)&dos_rom_name_3040, set_dos_rom_name_3040, NULL },
+    { "DosName4040", RES_STRING, (resource_value_t)"dos4040",
+      (void *)&dos_rom_name_4040, set_dos_rom_name_4040, NULL },
+    { "DosName1001", RES_STRING, (resource_value_t)"dos1001",
+      (void *)&dos_rom_name_1001, set_dos_rom_name_1001, NULL },
+    { NULL }
+};
+
+int ieee_resources_init(void)
+{
+    return resources_register(resources);
+}
+
+void ieee_resources_shutdown(void)
+{
+    lib_free(dos_rom_name_2031);
+    lib_free(dos_rom_name_1001);
+    lib_free(dos_rom_name_2040);
+    lib_free(dos_rom_name_3040);
+    lib_free(dos_rom_name_4040);
+}
+
