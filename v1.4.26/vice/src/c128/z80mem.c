924589fcdaa7c1f5b751c0697e7bd48f3d1c082f
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 09:17:36 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.4.26'.

---
+++ v1.4.26/vice/src/c128/z80mem.c
@@ -0,0 +1,415 @@
+/*
+ * z80mem.c
+ *
+ * Written by
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "c128mem.h"
+#include "c128mmu.h"
+#include "c64cia.h"
+#include "cmdline.h"
+#include "log.h"
+#include "mem.h"
+#include "resources.h"
+#include "sid.h"
+#include "sysfile.h"
+#include "types.h"
+#include "utils.h"
+#include "vdc-mem.h"
+#include "vdc.h"
+#include "vicii-mem.h"
+#include "vicii.h"
+#include "z80mem.h"
+
+/* Z80 boot BIOS.  */
+static BYTE z80bios_rom[0x1000];
+
+/* Name of the character ROM.  */
+static char *z80bios_rom_name = NULL;
+
+/* Logging.  */
+static log_t z80mem_log = LOG_ERR;
+
+/* Adjust this pointer when the MMU changes banks.  */
+static BYTE **bank_base;
+static int *bank_limit = NULL;
+unsigned int z80_old_reg_pc;
+
+/* Pointers to the currently used memory read and write tables.  */
+read_func_ptr_t *_z80mem_read_tab_ptr;
+store_func_ptr_t *_z80mem_write_tab_ptr;
+BYTE **_z80mem_read_base_tab_ptr;
+int *z80mem_read_limit_tab_ptr;
+
+#define NUM_CONFIGS 4
+
+/* Memory read and write tables.  */
+static store_func_ptr_t mem_write_tab[NUM_CONFIGS][0x101];
+static read_func_ptr_t mem_read_tab[NUM_CONFIGS][0x101];
+static BYTE *mem_read_base_tab[NUM_CONFIGS][0x101];
+static int mem_read_limit_tab[NUM_CONFIGS][0x101];
+
+store_func_ptr_t io_write_tab[0x101];
+read_func_ptr_t io_read_tab[0x101];
+
+static int z80_rom_loaded = 0;
+
+#define IS_NULL(s)  (s == NULL || *s == '\0')
+
+static int z80mem_load_bios(void)
+{
+    if (!z80_rom_loaded)
+        return 0;
+
+    if (!IS_NULL(z80bios_rom_name)) {
+        if (sysfile_load(z80bios_rom_name,
+            z80bios_rom, 4096, 4096) < 0) {
+            log_error(z80mem_log, "Couldn't load Z80 BIOS ROM `%s'.",
+                  z80bios_rom_name);
+            return -1;
+        }
+    }
+    return 0;
+}
+
+static int set_z80bios_rom_name(resource_value_t v)
+{
+    const char *name = (const char *) v;
+
+    if (z80bios_rom_name != NULL && name != NULL
+        && strcmp(name, z80bios_rom_name) == 0)
+        return 0;
+
+    string_set(&z80bios_rom_name, name);
+
+    return z80mem_load_bios();
+}
+
+static resource_t resources[] = {
+    { "Z80BiosName", RES_STRING, (resource_value_t) "z80bios",
+     (resource_value_t *) &z80bios_rom_name, set_z80bios_rom_name },
+    { NULL }
+};
+
+int z80mem_init_resources(void)
+{
+    return resources_register(resources);
+}
+
+static cmdline_option_t cmdline_options[] =
+{
+    { "-z80bios", SET_RESOURCE, 1, NULL, NULL, "Z80BiosName", NULL,
+      "<name>", "Specify name of Z80 BIOS ROM image" },
+    { NULL }
+};
+
+int z80mem_init_cmdline_options(void)
+{
+    return cmdline_register_options(cmdline_options);
+}
+
+/* ------------------------------------------------------------------------- */
+
+/* Generic memory access.  */
+static void REGPARM2 z80mem_store(ADDRESS addr, BYTE value)
+{
+    _z80mem_write_tab_ptr[addr >> 8](addr, value);
+}
+
+static BYTE REGPARM1 z80mem_read(ADDRESS addr)
+{
+    return _z80mem_read_tab_ptr[addr >> 8](addr);
+}
+
+static BYTE REGPARM1 read_ram(ADDRESS addr)
+{
+    return ram_bank[addr];
+}
+
+static void REGPARM2 store_ram(ADDRESS addr, BYTE value)
+{
+    ram_bank[addr] = value;
+}
+
+static BYTE REGPARM1 read_ram0(ADDRESS addr)
+{
+    return ram[addr];
+}
+
+static void REGPARM2 store_ram0(ADDRESS addr, BYTE value)
+{
+    ram[addr] = value;
+}
+
+BYTE REGPARM1 read_bios(ADDRESS addr)
+{
+    return z80bios_rom[addr & 0x0fff];
+}
+
+void REGPARM2 store_bios(ADDRESS addr, BYTE value)
+{
+    z80bios_rom[addr] = value;
+}
+
+static BYTE REGPARM1 read_one(ADDRESS addr)
+{
+    return page_one[addr - 0x100];
+}
+
+static void REGPARM2 store_one(ADDRESS addr, BYTE value)
+{
+    page_one[addr - 0x100] = value;
+}
+
+static BYTE REGPARM1 read_unconnected_io(ADDRESS addr)
+{
+    log_message(z80mem_log, "Read from unconnected IO %04x", addr);
+    return 0;
+}
+
+static void REGPARM2 store_unconnected_io(ADDRESS addr, BYTE value)
+{
+    log_message(z80mem_log, "Store to unconnected IO %04x %02x", addr, value);
+}
+
+#ifdef _MSC_VER
+#pragma optimize("",off);
+#endif
+
+void z80mem_initialize(void)
+{
+    int i, j;
+
+    /* Memory addess space.  */
+
+    for (j = 0; j < NUM_CONFIGS; j++) {
+        for (i = 0; i <= 0x100; i++) {
+            mem_read_base_tab[j][i] = NULL;
+            mem_read_limit_tab[j][i] = -1;
+        }
+    }
+
+    mem_read_tab[0][0] = read_bios;
+    mem_write_tab[0][0] = store_zero;
+    mem_read_tab[1][0] = read_bios;
+    mem_write_tab[1][0] = store_zero;
+    mem_read_tab[2][0] = read_zero;
+    mem_write_tab[2][0] = store_zero;
+    mem_read_tab[3][0] = read_zero;
+    mem_write_tab[3][0] = store_zero;
+
+    mem_read_tab[0][1] = read_bios;
+    mem_write_tab[0][1] = store_one;
+    mem_read_tab[1][1] = read_bios;
+    mem_write_tab[1][1] = store_one;
+    mem_read_tab[2][1] = read_one;
+    mem_write_tab[2][1] = store_one;
+    mem_read_tab[3][1] = read_one;
+    mem_write_tab[3][1] = store_one;
+
+    for (i = 2; i < 0x10; i++) {
+        mem_read_tab[0][i] = read_bios;
+        mem_write_tab[0][i] = store_ram;
+        mem_read_tab[1][i] = read_bios;
+        mem_write_tab[1][i] = store_ram;
+        mem_read_tab[2][i] = read_ram;
+        mem_write_tab[2][i] = store_ram;
+        mem_read_tab[3][i] = read_ram;
+        mem_write_tab[3][i] = store_ram;
+    }
+
+    for (i = 0x10; i <= 0x13; i++) {
+        mem_read_tab[0][i] = read_ram;
+        mem_write_tab[0][i] = store_ram;
+    }
+    for (i = 0x10; i <= 0x13; i++) {
+        mem_read_tab[1][i] = colorram_read;
+        mem_write_tab[1][i] = colorram_store;
+    }
+    for (i = 0x10; i <= 0x13; i++) {
+        mem_read_tab[2][i] = read_ram;
+        mem_write_tab[2][i] = store_ram;
+    }
+    for (i = 0x10; i <= 0x13; i++) {
+        mem_read_tab[3][i] = colorram_read;
+        mem_write_tab[3][i] = colorram_store;
+    }
+
+    for (j = 0; j < NUM_CONFIGS; j++) {
+        for (i = 0x14; i <= 0xbf; i++) {
+            mem_read_tab[j][i] = read_ram;
+            mem_write_tab[j][i] = store_ram;
+        }
+    }
+
+    for (i = 0xc0; i <= 0xcf; i++) {
+        mem_read_tab[0][i] = read_ram;
+        mem_write_tab[0][i] = store_ram;
+        mem_read_tab[1][i] = read_ram;
+        mem_write_tab[1][i] = store_ram;
+        mem_read_tab[2][i] = read_top_shared;
+        mem_write_tab[2][i] = store_top_shared;
+        mem_read_tab[3][i] = read_top_shared;
+        mem_write_tab[3][i] = store_top_shared;
+    }
+
+
+    for (i = 0xd0; i <= 0xdf; i++) {
+        mem_read_tab[0][i] = read_ram;
+        mem_write_tab[0][i] = store_ram;
+        mem_read_tab[1][i] = read_ram;
+        mem_write_tab[1][i] = store_ram;
+        mem_read_tab[2][i] = read_top_shared;
+        mem_write_tab[2][i] = store_top_shared;
+        mem_read_tab[3][i] = read_top_shared;
+        mem_write_tab[3][i] = store_top_shared;
+    }
+
+    for (i = 0xe0; i <= 0xfe; i++) {
+        mem_read_tab[0][i] = read_ram;
+        mem_write_tab[0][i] = store_ram;
+        mem_read_tab[1][i] = read_ram;
+        mem_write_tab[1][i] = store_ram;
+        mem_read_tab[2][i] = read_top_shared;
+        mem_write_tab[2][i] = store_top_shared;
+        mem_read_tab[3][i] = read_top_shared;
+        mem_write_tab[3][i] = store_top_shared;
+    }
+
+    for (j = 0; j < NUM_CONFIGS; j++) {
+        mem_read_tab[j][0xff] = mmu_ffxx_read;
+        mem_write_tab[j][0xff] = mmu_ffxx_store;
+
+        mem_read_tab[j][0x100] = mem_read_tab[j][0x0];
+        mem_write_tab[j][0x100] = mem_write_tab[j][0x0];
+    }
+
+    _z80mem_read_tab_ptr = mem_read_tab[0];
+    _z80mem_write_tab_ptr = mem_write_tab[0];
+    _z80mem_read_base_tab_ptr = mem_read_base_tab[0];
+    z80mem_read_limit_tab_ptr = mem_read_limit_tab[0];
+
+    /* IO address space.  */
+
+    /* At least we know what happens.  */
+    for (i = 0; i <= 0x100; i++) {
+        io_read_tab[i] = read_unconnected_io;
+        io_write_tab[i] = store_unconnected_io;
+    }
+
+    io_read_tab[0x10] = colorram_read;
+    io_write_tab[0x10] = colorram_store;
+    io_read_tab[0x11] = colorram_read;
+    io_write_tab[0x11] = colorram_store;
+    io_read_tab[0x12] = colorram_read;
+    io_write_tab[0x12] = colorram_store;
+    io_read_tab[0x13] = colorram_read;
+    io_write_tab[0x13] = colorram_store;
+
+    io_read_tab[0xd0] = vic_read;
+    io_write_tab[0xd0] = vic_store;
+    io_read_tab[0xd1] = vic_read;
+    io_write_tab[0xd1] = vic_store;
+    io_read_tab[0xd2] = vic_read;
+    io_write_tab[0xd2] = vic_store;
+    io_read_tab[0xd3] = vic_read;
+    io_write_tab[0xd3] = vic_store;
+
+    io_read_tab[0xd4] = sid_read;
+    io_write_tab[0xd4] = sid_store;
+
+    io_read_tab[0xd5] = mmu_read;
+    io_write_tab[0xd5] = mmu_store;
+
+    io_read_tab[0xd6] = vdc_read;
+    io_write_tab[0xd6] = vdc_store;
+    io_read_tab[0xd7] = d7xx_read;
+    io_write_tab[0xd7] = d7xx_store;
+
+    io_read_tab[0xd8] = colorram_read;
+    io_write_tab[0xd8] = colorram_store;
+    io_read_tab[0xd9] = colorram_read;
+    io_write_tab[0xd9] = colorram_store;
+    io_read_tab[0xda] = colorram_read;
+    io_write_tab[0xda] = colorram_store;
+    io_read_tab[0xdb] = colorram_read;
+    io_write_tab[0xdb] = colorram_store;
+
+    io_read_tab[0xdc] = cia1_read;
+    io_write_tab[0xdc] = cia1_store;
+    io_read_tab[0xdd] = cia2_read;
+    io_write_tab[0xdd] = cia2_store;
+
+    io_read_tab[0xde] = io1_read;
+    io_write_tab[0xde] = io1_store;
+    io_read_tab[0xdf] = io2_read;
+    io_write_tab[0xdf] = io2_store;
+}
+
+#ifdef _MSC_VER
+#pragma optimize("",on);
+#endif
+
+void z80mem_set_bank_pointer(BYTE **base, int *limit)
+{
+    bank_base = base;
+    bank_limit = limit;
+}
+
+void z80mem_update_config(int config)
+{
+    _z80mem_read_tab_ptr = mem_read_tab[config];
+    _z80mem_write_tab_ptr = mem_write_tab[config];
+    _z80mem_read_base_tab_ptr = mem_read_base_tab[config];
+    z80mem_read_limit_tab_ptr = mem_read_limit_tab[config];
+/*
+    if (bank_limit != NULL) {
+        *bank_base = _z80mem_read_base_tab_ptr[z80_old_reg_pc >> 8];
+        if (*bank_base != 0)
+            *bank_base = _z80mem_read_base_tab_ptr[z80_old_reg_pc >> 8]
+                         - (z80_old_reg_pc & 0xff00);
+        *bank_limit = z80mem_read_limit_tab_ptr[z80_old_reg_pc >> 8];
+    }
+*/
+}
+
+int z80mem_load(void)
+{
+    if (z80mem_log == LOG_ERR)
+        z80mem_log = log_open("Z80MEM");
+
+    z80mem_initialize();
+
+    z80_rom_loaded = 1;
+
+    if (z80mem_load_bios() < 0)
+        return -1;
+
+    return 0;
+}
+
---
+++ v1.4.26/vice/src/vic20/vic.h
@@ -0,0 +1,176 @@
+/*
+ * vic.h - A VIC-I emulation (under construction)
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *
+ * 16/24bpp support added by
+ *  Steven Tieu <stieu@physics.ubc.ca>
+ *  Teemu Rantanen <tvr@cs.hut.fi>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _VIC_H
+#define _VIC_H
+
+#include "vice.h"
+
+#include "alarm.h"
+#include "log.h"
+#include "raster.h"
+#include "types.h"
+#include "vic-mem.h"
+#include "vic20.h"
+#include "video.h"
+
+#define VIC_SCREEN_WIDTH                233
+#define VIC_SCREEN_HEIGHT               312
+
+#define VIC_SCREEN_MAX_TEXT_COLS        31
+#define VIC_SCREEN_MAX_TEXT_LINES       30
+
+#define VIC_FIRST_DISPLAYED_LINE        28
+#define VIC_LAST_DISPLAYED_LINE         293
+
+#define VIC_NUM_SPRITES 0
+
+#define VIC_NUM_COLORS 16
+
+/* On MS-DOS, do not duplicate pixels.  Otherwise, we would always need at
+   least 466 horizontal pixels to contain the whole screen.  */
+/* But this is no problem as 320*200 does not fit anyhow.  */
+#ifndef OS2
+#define VIC_DUPLICATES_PIXELS
+#endif
+
+#ifdef VIC_DUPLICATES_PIXELS
+typedef PIXEL2 VIC_PIXEL;
+#define VIC_PIXEL(n)    RASTER_PIXEL2 (&vic.raster, n)
+typedef PIXEL4 VIC_PIXEL2;
+#define VIC_PIXEL2(n)   RASTER_PIXEL4 (&vic.raster, n)
+#define VIC_PIXEL_WIDTH 2
+#else
+typedef PIXEL VIC_PIXEL;
+#define VIC_PIXEL(n)    RASTER_PIXEL (&vic.raster, n)
+typedef PIXEL2 VIC_PIXEL2;
+#define VIC_PIXEL2(n)   RASTER_PIXEL2 (&vic.raster, n)
+#define VIC_PIXEL_WIDTH 1
+#endif
+
+#define VIC_CYCLES_PER_LINE VIC20_PAL_CYCLES_PER_LINE
+
+/* Cycle # within the current line.  */
+#define VIC_RASTER_CYCLE(clk) ((int)((clk) % VIC_CYCLES_PER_LINE))
+
+/* Current vertical position of the raster.  Unlike `rasterline', which is
+   only accurate if a pending `A_RASTERDRAW' event has been served, this is
+   guarranteed to be always correct.  It is a bit slow, though.  */
+#define VIC_RASTER_Y(clk)     ((int)((clk) / VIC_CYCLES_PER_LINE)   \
+                               % VIC_SCREEN_HEIGHT)
+
+/* Video mode definitions. */
+
+enum _vic_video_mode
+  {
+    VIC_STANDARD_MODE,
+    VIC_REVERSE_MODE,
+    VIC_NUM_VMODES
+  };
+typedef enum _vic_video_mode vic_video_mode_t;
+
+#define VIC_IDLE_MODE VIC_STANDARD_MODE
+
+/* On MS-DOS, we do not need 2x drawing functions.  This is mainly to save
+   memory and (little) speed.  */
+#if !defined(__MSDOS__) && !defined(__riscos) && !defined(OS2)
+#define VIC_NEED_2X 1
+#endif
+
+struct snapshot_s;
+struct screenshot_s;
+struct palette_s;
+
+struct vic_s
+  {
+    int initialized;
+
+    log_t log;
+
+    raster_t raster;
+
+    struct palette_s *palette;
+
+    BYTE regs[64];
+
+    alarm_t raster_draw_alarm;
+
+    BYTE auxiliary_color;
+    BYTE *color_ptr;
+    BYTE *screen_ptr;
+    BYTE *chargen_ptr; /* = chargen_rom + 0x400; */
+
+    unsigned int char_height;   /* = 8 */
+    unsigned int text_cols;     /* = 22 */
+    unsigned int text_lines;    /* = 23 */
+
+    unsigned int memptr;
+  };
+typedef struct vic_s vic_t;
+
+extern vic_t vic;
+
+extern struct raster_s *vic_init(void);
+extern struct canvas_s *vic_get_canvas(void);
+extern void vic_reset(void);
+
+extern int vic_init_resources(void);
+extern int vic_init_cmdline_options(void);
+
+extern int vic_write_snapshot_module(struct snapshot_s *s);
+extern int vic_read_snapshot_module(struct snapshot_s *s);
+
+extern int vic_screenshot(struct screenshot_s *screenshot);
+
+/* Private function calls, used by the other VIC modules.  FIXME:
+   Prepend names with `_'?  */
+extern void vic_update_memory_ptrs(void);
+extern int vic_load_palette(const char *name);
+extern void vic_resize(void);
+
+/* Debugging options.  */
+
+/* #define VIC_RASTER_DEBUG */
+/* #define VIC_REGISTERS_DEBUG */
+
+#ifdef VIC_RASTER_DEBUG
+#define VIC_DEBUG_RASTER(x) log_debug x
+#else
+#define VIC_DEBUG_RASTER(x)
+#endif
+
+#ifdef VIC_REGISTERS_DEBUG
+#define VIC_DEBUG_REGISTER(x) log_debug x
+#else
+#define VIC_DEBUG_REGISTER(x)
+#endif
+
+#endif /* _VIC_H */
+
