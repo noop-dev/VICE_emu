4fc9e17069fa5766a26387331bcdbbd90ec7b858
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 08:13:37 +0000

    This commit was manufactured by cvs2svn to create tag 'v0.16.1.44'.

---
+++ tags/v0.16.1/v0.16.1.44/vice/src/arch/win32/uiattach.c
@@ -0,0 +1,235 @@
+/*
+ * uiattach.c - Implementation of the device manager dialog box.
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <windows.h>
+
+#include "attach.h"
+#include "autostart.h"
+#include "res.h"
+#include "resources.h"
+#include "serial.h"
+#include "ui.h"
+#include "uiattach.h"
+#include "uilib.h"
+#include "winmain.h"
+
+static void enable_controls_for_disk_device_type(HWND hwnd, int type)
+{
+    EnableWindow(GetDlgItem(hwnd, IDC_DISKIMAGE),
+                 type == IDC_SELECTDISK);
+    EnableWindow(GetDlgItem(hwnd, IDC_BROWSEDISK),
+                 type == IDC_SELECTDISK);
+    EnableWindow(GetDlgItem(hwnd, IDC_AUTOSTART),
+                 type == IDC_SELECTDISK);
+    EnableWindow(GetDlgItem(hwnd, IDC_DIR),
+                 type == IDC_SELECTDIR);
+    EnableWindow(GetDlgItem(hwnd, IDC_BROWSEDIR),
+                 type == IDC_SELECTDIR);
+    EnableWindow(GetDlgItem(hwnd, IDC_TOGGLE_READP00),
+                 type == IDC_SELECTDIR);
+    EnableWindow(GetDlgItem(hwnd, IDC_TOGGLE_WRITEP00),
+                 type == IDC_SELECTDIR);
+    EnableWindow(GetDlgItem(hwnd, IDC_TOGGLE_HIDENONP00),
+                 type == IDC_SELECTDIR);
+}
+
+static void init_dialog(HWND hwnd, int num)
+{
+    const char *disk_image, *dir;
+    int n;
+    char tmp[256];
+
+    disk_image = serial_get_file_name(num);
+    SetDlgItemText(hwnd, IDC_DISKIMAGE, disk_image != NULL ? disk_image : "");
+
+    sprintf(tmp, "FSDevice%dDir", num);
+    resources_get_value(tmp, (resource_value_t *) &dir);
+    SetDlgItemText(hwnd, IDC_DIR, dir != NULL ? dir : "");
+
+    sprintf(tmp, "FSDevice%dConvertP00", num);
+    resources_get_value(tmp, (resource_value_t *) &n);
+    CheckDlgButton(hwnd, IDC_TOGGLE_READP00, n ? BST_CHECKED : BST_UNCHECKED);
+
+    sprintf(tmp, "FSDevice%dSaveP00", num);
+    resources_get_value(tmp, (resource_value_t *) &n);
+    CheckDlgButton(hwnd, IDC_TOGGLE_WRITEP00, n ? BST_CHECKED : BST_UNCHECKED);
+
+    sprintf(tmp, "FSDevice%dHideCBMFiles", num);
+    resources_get_value(tmp, (resource_value_t *) &n);
+    CheckDlgButton(hwnd, IDC_TOGGLE_HIDENONP00, n ? BST_CHECKED : BST_UNCHECKED);
+
+    if (disk_image != NULL) {
+        n = IDC_SELECTDISK;
+    } else if (dir != NULL) {
+        n = IDC_SELECTDIR;
+    } else {
+        n = IDC_SELECTNONE;
+    }
+    CheckRadioButton(hwnd, IDC_SELECTDISK, IDC_SELECTDIR, n);
+    enable_controls_for_disk_device_type(hwnd, n);
+}
+
+static BOOL CALLBACK dialog_proc(int num, HWND hwnd, UINT msg,
+                                 WPARAM wparam, LPARAM lparam)
+{
+    switch (msg) {
+      case WM_INITDIALOG:
+        init_dialog(hwnd, num);
+        return TRUE;
+      case WM_COMMAND:
+          switch (LOWORD(wparam)) {
+            case IDC_SELECTDIR:
+            case IDC_SELECTDISK:
+            case IDC_SELECTNONE:
+              enable_controls_for_disk_device_type(hwnd,
+                                                   LOWORD(wparam));
+              break;
+            case IDC_BROWSEDISK:
+              {
+                  char *s;
+                  if ((s = ui_select_file("Attach disk image",
+                      "Disk image files (*.d64;*.d71;*.d81;*.g64;*.g41;*.x64)\0*.d64;*.d71;*.d81;*.g64;*.g41;*.x64\0"
+                      "All files (*.*)\0*.*\0", hwnd)) != NULL) {
+                      SetDlgItemText(hwnd, IDC_DISKIMAGE, s);
+                      if (file_system_attach_disk(num, s) < 0)
+                          ui_error("Cannot attach specified file");
+                      free(s);
+                  }
+              }
+              break;
+            case IDC_AUTOSTART:
+              {
+                  char *s;
+                  if ((s = ui_select_file("Autostart disk image",
+                      "Disk image files (*.d64;*.d71;*.d81;*.g64;*.g41;*.x64)\0*.d64;*.d71;*.d81;*.g64;*.g41;*.x64\0"
+                      "All files (*.*)\0*.*\0", hwnd)) != NULL) {
+                      SetDlgItemText(hwnd, IDC_DISKIMAGE, s);
+                      if (autostart_autodetect(s, "*") < 0)
+                          ui_error("Cannot autostart specified file.");
+                      free(s);
+                  }
+              }
+              break;
+            case IDC_BROWSEDIR:
+              {
+                  char *s;
+                  /* FIXME: We must select a directory, not a file!  */
+                  if ((s = ui_select_file("Select file system directory",
+                      "All files (*.*)\0*.*\0", hwnd)) != NULL) {
+                      char tmp[256];
+                      SetDlgItemText(hwnd, IDC_DIR, s);
+                      sprintf(tmp, "FSDevice%dDir", num);
+                      resources_set_value(tmp, (resource_value_t) s);
+                      free(s);
+                  }
+              }
+              break;
+            case IDC_TOGGLE_READP00:
+              {
+                  int n;
+                  char tmp[256];
+                  sprintf(tmp, "FSDevice%dConvertP00", num);
+                  resources_get_value(tmp, (resource_value_t *) &n);
+                  resources_set_value(tmp, (resource_value_t) !n);
+                  break;
+              }
+            case IDC_TOGGLE_WRITEP00:
+              {
+                  int n;
+                  char tmp[256];
+                  sprintf(tmp, "FSDevice%dSaveP00", num);
+                  resources_get_value(tmp, (resource_value_t *) &n);
+                  resources_set_value(tmp, (resource_value_t) !n);
+                  break;
+              }
+            case IDC_TOGGLE_HIDENONP00:
+              {
+                  int n;
+                  char tmp[256];
+                  sprintf(tmp, "FSDevice%dHideCBMFiles", num);
+                  resources_get_value(tmp, (resource_value_t *) &n);
+                  resources_set_value(tmp, (resource_value_t) !n);
+                  break;
+              }
+          }
+          return TRUE;
+    }
+    return FALSE;
+}
+
+#define _CALLBACK(num)                                            \
+static BOOL CALLBACK callback_##num(HWND dialog, UINT msg,        \
+                                    WPARAM wparam, LPARAM lparam) \
+{                                                                 \
+    return dialog_proc(num, dialog, msg, wparam, lparam);         \
+}
+
+_CALLBACK(8)
+_CALLBACK(9)
+_CALLBACK(10)
+_CALLBACK(11)
+
+void ui_attach_dialog(HWND hwnd)
+{
+    PROPSHEETPAGE psp[4];
+    PROPSHEETHEADER psh;
+    int i;
+
+    for (i = 0; i < 4; i++) {
+        psp[i].dwSize = sizeof(PROPSHEETPAGE);
+        psp[i].dwFlags = PSP_USETITLE /*| PSP_HASHELP*/ ;
+        psp[i].hInstance = winmain_instance;
+        psp[i].u1.pszTemplate = MAKEINTRESOURCE(IDD_DISKDEVICE_DIALOG);
+        psp[i].u2.pszIcon = NULL;
+        psp[i].lParam = 0;
+        psp[i].pfnCallback = NULL;
+    }
+
+    psp[0].pfnDlgProc = callback_8;
+    psp[0].pszTitle = "Drive 8";
+    psp[1].pfnDlgProc = callback_9;
+    psp[1].pszTitle = "Drive 9";
+    psp[2].pfnDlgProc = callback_10;
+    psp[2].pszTitle = "Drive 10";
+    psp[3].pfnDlgProc = callback_11;
+    psp[3].pszTitle = "Drive 11";
+
+    psh.dwSize = sizeof(PROPSHEETHEADER);
+    psh.dwFlags = PSH_PROPSHEETPAGE | PSH_NOAPPLYNOW;
+    psh.hwndParent = hwnd;
+    psh.hInstance = winmain_instance;
+    psh.u1.pszIcon = NULL;
+    psh.pszCaption = "Device manager";
+    psh.nPages = 4;
+    psh.u2.nStartPage = 0;
+    psh.u3.ppsp = psp;
+    psh.pfnCallback = NULL;
+
+    PropertySheet(&psh);
+}
+
---
+++ tags/v0.16.1/v0.16.1.44/vice/src/interrupt.c
@@ -0,0 +1,201 @@
+/*
+ * interrupt.h - Implementation of 6510 interrupts and alarms.
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *  André Fachat (fachat@physik.tu-chemnitz.de)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#define _INTERRUPT_C
+
+#include "vice.h"
+
+#ifdef STDC_HEADERS
+#include <stdio.h>
+#endif
+
+#include "interrupt.h"
+
+#include "snapshot.h"
+
+/* ------------------------------------------------------------------------- */
+
+/* Initialization.  */
+void cpu_int_status_init(cpu_int_status_t *cs, int num_ints,
+                         opcode_info_t *last_opcode_info_ptr)
+{
+    memset(cs, 0, sizeof(cpu_int_status_t));
+    cs->num_ints = num_ints;
+    cs->last_opcode_info_ptr = last_opcode_info_ptr;
+    cs->num_last_stolen_cycles = 0;
+    cs->last_stolen_cycles_clk = (CLOCK)0;
+    cs->global_pending_int = IK_NONE;
+}
+
+/* This is used to avoid clock counter overflows.  Return the number of
+   cycles subtracted, which is always a multiple of `baseval'.  */
+CLOCK prevent_clk_overflow(cpu_int_status_t *cs, CLOCK *clk, CLOCK baseval)
+{
+#if 0
+    if (*clk > PREVENT_CLK_OVERFLOW_TICK) {
+	int i;
+        CLOCK prevent_clk_overflow_sub =
+            (((PREVENT_CLK_OVERFLOW_TICK & ~((CLOCK)0xffff))
+              / baseval - 1) * baseval);
+
+	*clk -= prevent_clk_overflow_sub;
+	cs->next_alarm_clk -= prevent_clk_overflow_sub;
+	cs->irq_clk -= prevent_clk_overflow_sub;
+	cs->nmi_clk -= prevent_clk_overflow_sub;
+	if (cs->last_stolen_cycles_clk > prevent_clk_overflow_sub)
+	    cs->last_stolen_cycles_clk -= prevent_clk_overflow_sub;
+	else
+	    cs->last_stolen_cycles_clk = (CLOCK) 0;
+	for (i = 0; i < cs->num_alarms; i++) {
+	    if (cs->alarm_clk[i] != CLOCK_MAX)
+		cs->alarm_clk[i] -= prevent_clk_overflow_sub;
+	}
+	return prevent_clk_overflow_sub;
+    } else
+	return 0;
+#else
+    /* FIXME FIXME FIXME */
+    return 0;
+#endif
+}
+
+/* ------------------------------------------------------------------------- */
+
+/* These functions are used by snaphots only: they update the IRQ/NMI line
+   value, but do not update the variables used to emulate the timing.  This
+   is necessary to allow the chip modules to dump their own IRQ/NMI status
+   information; the global timing status is stored in the CPU module (see
+   `interrupt_write_snapshot()' and `interrupt_read_snapshot()'). */
+
+void set_irq_noclk(cpu_int_status_t *cs, int int_num, int value)
+{
+    if (value) {
+	if (!(cs->pending_int[int_num] & IK_IRQ)) {
+	    cs->nirq++;
+	    cs->global_pending_int |= IK_IRQ;
+	    cs->pending_int[int_num] |= IK_IRQ;
+	}
+    } else {
+	if (cs->pending_int[int_num] & IK_IRQ) {
+	    if (cs->nirq > 0) {
+		cs->pending_int[int_num] &= ~IK_IRQ;
+ 		if (--cs->nirq == 0)
+		    cs->global_pending_int &= ~IK_IRQ;
+	    }
+	}
+    }
+}
+
+void set_nmi_noclk(cpu_int_status_t *cs, int int_num, int value)
+{
+    if (value) {
+	if (!(cs->pending_int[int_num] & IK_NMI)) {
+	    if (cs->nnmi == 0 && !(cs->global_pending_int & IK_NMI))
+		cs->global_pending_int |= IK_NMI;
+	    cs->nnmi++;
+	    cs->pending_int[int_num] |= IK_NMI;
+	}
+    } else {
+	if (cs->pending_int[int_num] & IK_NMI) {
+	    if (cs->nnmi > 0) {
+		cs->nnmi--;
+		cs->pending_int[int_num] &= ~IK_NMI;
+		if (clk == cs->nmi_clk)
+		    cs->global_pending_int &= ~IK_NMI;
+	    }
+	}
+    }
+}
+
+void set_int_noclk(cpu_int_status_t *cs, int int_num, enum cpu_int value)
+{
+    set_nmi(cs, int_num, value & IK_NMI, clk);
+    set_irq(cs, int_num, value & IK_IRQ, clk);
+}
+
+int get_irq(cpu_int_status_t *cs, int int_num)
+{
+    return cs->pending_int[int_num] & IK_IRQ;
+}
+
+int get_nmi(cpu_int_status_t *cs, int int_num)
+{
+    return cs->pending_int[int_num] & IK_NMI;
+}
+
+enum cpu_int get_int(cpu_int_status_t *cs, int int_num)
+{
+    return cs->pending_int[int_num];
+}
+
+/* ------------------------------------------------------------------------- */
+
+int interrupt_write_snapshot(cpu_int_status_t *cs, snapshot_module_t *m)
+{
+#if 0
+    int i;
+
+    for (i = 0; i < cs->num_ints; i++) {
+        if (snapshot_module_write_byte(m, cs->pending_int[i]) < 0)
+            return -1;
+    }
+#endif
+
+    /* FIXME: could we avoid some of this info?  */
+    if (snapshot_module_write_dword(m, cs->irq_clk) < 0
+        || snapshot_module_write_dword(m, cs->nmi_clk) < 0
+        || snapshot_module_write_dword(m, cs->num_last_stolen_cycles) < 0
+        || snapshot_module_write_dword(m, cs->last_stolen_cycles_clk) < 0)
+        return -1;
+
+    return 0;
+}
+
+int interrupt_read_snapshot(cpu_int_status_t *cs, snapshot_module_t *m)
+{
+    int i;
+    DWORD dw;
+
+    for (i = 0; i < cs->num_ints; i++)
+	cs->pending_int[i] = IK_NONE;
+    cs->global_pending_int = IK_NONE;
+    cs->nirq = cs->nnmi = cs->reset = cs->trap = 0;
+
+    if (snapshot_module_read_dword(m, &cs->irq_clk) < 0
+        || snapshot_module_read_dword(m, &cs->nmi_clk) < 0)
+        return -1;
+
+    if (snapshot_module_read_dword(m, &dw) < 0)
+        return -1;
+    cs->num_last_stolen_cycles = dw;
+
+    if (snapshot_module_read_dword(m, &dw) < 0)
+        return -1;
+    cs->last_stolen_cycles_clk = dw;
+
+    return 0;
+}
