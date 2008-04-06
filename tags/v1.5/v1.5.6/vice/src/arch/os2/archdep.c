dd673f66db046a6cd1bbcb68c8b8eee7a56e0fea
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 09:25:10 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.5.6'.

---
+++ tags/v1.5/v1.5.6/vice/src/arch/os2/archdep.c
@@ -0,0 +1,474 @@
+/*
+ * archdep.c - Miscellaneous system-specific stuff.
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Thomas Bretz <tbretz@gsi.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#define INCL_DOSQUEUES     /* Queue commands     */
+#define INCL_DOSSESMGR     /* DosStartSession    */
+#define INCL_DOSMEMMGR     /* DosFreeMem         */
+#define INCL_DOSPROFILE     // DosTmrQueryTime
+#define INCL_DOSPROCESS    /* DosGetInfoBlock    */
+#define INCL_DOSMODULEMGR  /* DosQueryModuleName */
+#define INCL_DOSSEMAPHORES /* Dos-*-MutexSem     */
+
+#define INCL_DOSMISC
+#define INCL_DOSFILEMGR
+
+#include <os2.h>
+
+#include <stdarg.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <process.h>
+#ifdef __IBMC__
+#include <io.h>
+#include <fcntl.h>
+#include <direct.h>
+#include <sys/stat.h>
+#endif
+
+#include "findpath.h"
+#include "archdep.h"
+#include "utils.h"
+
+#include "log.h"
+
+#include "ui.h"
+#include "signals.h"
+#include "vsyncarch.h"
+
+/* ---------------------- OS/2 specific ------------------ */
+
+static char *orig_workdir;
+static char argv0[CCHMAXPATH];
+
+static void restore_workdir(void)
+{
+    if (orig_workdir) chdir(orig_workdir);
+}
+
+HAB habMain;
+HMQ hmqMain;
+
+void PM_close(void)
+{
+    APIRET rc;
+
+    log_message(LOG_DEFAULT, "archdep.c: PM_close");
+
+    rc=WinDestroyMsgQueue(hmqMain);  // Destroy Msg Queue
+    if (!rc)
+        log_message(LOG_DEFAULT, "archdep.c: Error! Msg Queue not destroyed (rc=%li)",rc);
+
+    rc=WinTerminate(habMain);  // Release Anchor to PM
+    if (!rc)
+        log_message(LOG_DEFAULT, "archdep.c: Error! PM anchor release.");
+}
+
+void PM_open(void)
+{
+    APIRET rc;
+
+    habMain = WinInitialize(0);              // Initialize PM
+    hmqMain = WinCreateMsgQueue(habMain, 0); // Create Msg Queue
+
+    // this hould make sure, that the system doesn't hang because
+    // vice uses 100% CPU time
+    if (rc=DosSetPriority(PRTYS_THREAD, PRTYC_REGULAR, -1, 0))
+        log_debug("archdep.c: Error DosSetPriority (rc=%li)", rc);
+
+    atexit(PM_close);
+}
+
+#if !defined __C1541__ && !defined __PETCAT__
+/* ------------------------------------------------------------------------ */
+void archdep_create_mutex_sem(HMTX *hmtx, const char *pszName, int fState)
+{
+    APIRET rc;
+
+    char *sem = xmalloc(13+strlen(pszName)+5+1);
+
+    sprintf(sem, "\\SEM32\\VICE2\\%s_%04x", pszName, vsyncarch_gettime()&0xffff);
+
+    if (rc=DosCreateMutexSem(sem, hmtx, 0, fState))
+        log_message(LOG_DEFAULT, "archdep.c: DosCreateMutexSem '%s' (rc=%i)", pszName, rc);
+}
+
+HMTX hmtxSpawn;
+
+#endif
+
+int archdep_startup(int *argc, char **argv)
+{
+    /* This is right way to do this in OS/2 (not via argv[0]) */
+    TIB *pTib;
+    PIB *pPib;
+    DosGetInfoBlocks(&pTib, &pPib);
+    DosQueryModuleName(pPib->pib_hmte, CCHMAXPATH, argv0);
+
+    orig_workdir = (char*) getcwd(NULL, GET_PATH_MAX);
+    atexit(restore_workdir);
+
+    PM_open();
+#if !defined __C1541__ && !defined __PETCAT__
+    archdep_create_mutex_sem(&hmtxSpawn, "Spawn", FALSE);
+#endif
+    return 0;
+}
+
+const char *archdep_program_name(void)
+{
+    static char *name=NULL;
+
+    if (!name)
+    {
+        char drive[_MAX_DRIVE];
+        char dir  [_MAX_DIR];
+        char fname[_MAX_FNAME+_MAX_EXT];
+        char ext  [_MAX_EXT];
+        _splitpath(argv0, drive, dir, fname, ext);
+        name = concat(fname, ext, NULL);
+    }
+    return name;
+}
+
+const char *archdep_boot_path(void)
+{
+    static char *boot_path=NULL;
+
+    if (!boot_path)
+    {
+        char drive[_MAX_DRIVE+_MAX_DIR];
+        char dir  [_MAX_DIR];
+        char fname[_MAX_FNAME+_MAX_EXT];
+        char ext  [_MAX_EXT];
+        _splitpath(argv0, drive, dir, fname, ext);
+        if (strlen(dir))
+            *(dir+strlen(dir)-1) = '\0'; // cut last backslash
+        boot_path = concat(drive, dir, NULL);
+    }
+    return boot_path;
+}
+
+const char *archdep_default_sysfile_pathlist(const char *emu_id)
+{
+    static char *pathlist=NULL;
+
+    if (!pathlist)
+        pathlist=concat(emu_id, FINDPATH_SEPARATOR_STRING, "DRIVES", NULL);
+
+    return pathlist;
+}
+
+const char *archdep_default_save_resource_file_name(void)
+{
+    return archdep_default_resource_file_name();
+}
+
+const char *archdep_default_resource_file_name(void)
+{
+    static char *filename=NULL;
+
+    if (!filename)
+        filename = concat(archdep_boot_path(), "\\vice2.cfg", NULL);
+
+    return filename;
+}
+
+int archdep_default_logger(const char *level_string,
+                           const char *format, va_list ap)
+{
+    return 0;
+}
+
+FILE *archdep_open_default_log_file(void)
+{
+    char *fname = concat (archdep_boot_path(), "\\vice2.log", NULL);
+
+    FILE *f = fopen(fname, "w");
+    free(fname);
+
+    return f;
+}
+
+int archdep_num_text_lines(void)
+{
+#ifdef __IBMC__
+   return 25;
+#else
+   int dst[2];
+   _scrsize(dst);
+   return dst[1];
+#endif
+}
+
+int archdep_num_text_columns(void)
+{
+#ifdef __IBMC__
+   return 80;
+#else
+   int dst[2];
+   _scrsize(dst);
+   return dst[0];
+#endif
+}
+
+#if !defined __C1541__ && !defined __PETCAT__
+extern int trigger_shutdown;
+#endif
+
+static RETSIGTYPE break64(int sig)
+{
+    char *sigtxt;
+    sigtxt = xmsprintf("Received signal %d (%s). Vice will be closed.",
+                       sig, sys_siglist[sig]);
+    log_message(LOG_DEFAULT, sigtxt);
+#if !defined __C1541__ && !defined __PETCAT__
+    WinMessageBox(HWND_DESKTOP, HWND_DESKTOP,
+                  sigtxt, "VICE/2 Exception", 0, MB_OK);
+    trigger_shutdown = TRUE;
+
+#endif
+    free(sigtxt);
+    exit (-1);
+}
+
+void archdep_setup_signals(int do_core_dumps)
+{
+    // at the place where it's called at the moment it's only valid
+    // for the vice (simulation) thread
+
+    signal(SIGINT, SIG_IGN);
+
+    if (!do_core_dumps) {
+        signal(SIGSEGV,  break64);
+        signal(SIGILL,   break64);
+        signal(SIGFPE,   break64);
+        signal(SIGABRT,  break64);
+        // signal(SIGINT,   break64);
+        signal(SIGTERM,  break64);
+        // signal(SIGUSR1,  break64);
+        // signal(SIGUSR2,  break64);
+        signal(SIGBREAK, break64);
+#ifdef __IBMC__
+        // signal(SIGUSR3,  break64);
+#else
+        signal(SIGPIPE,  break64);
+        signal(SIGHUP,   break64);
+        signal(SIGQUIT,  break64);
+#endif
+    }
+}
+
+int archdep_path_is_relative(const char *path)
+{
+    return !(isalpha(path[0]) && path[1] == ':' &&
+            (path[2] == '/' || path[2] == '\\') ||
+            (path[0] == '/' || path[0] == '\\'));
+}
+
+/* Return a malloc'ed backup file name for file `fname'.  */
+char *archdep_make_backup_filename(const char *fname)
+{
+    return concat(fname, "~", NULL);
+}
+
+/* return malloced version of full pathname of filename */
+int archdep_expand_path(char **return_path, const char *filename)
+{
+    if (filename[0] == '\\' || filename[1] == ':')
+        *return_path = stralloc(filename);
+    else
+    {
+        char *cwd = get_current_dir();
+        *return_path = concat(cwd, "\\", filename, NULL);
+        free(cwd);
+    }
+    return 0;
+}
+
+int archdep_search_path(const char *name, char *pBuf, int lBuf)
+{
+    const int flags = SEARCH_CUR_DIRECTORY|SEARCH_IGNORENETERRS;
+    char *path      = "";        /* PATH environment variable */
+    char *pgmName   = concat(name, ".exe", NULL);
+
+    // Search the program in the path
+    if (DosScanEnv("PATH",&path))
+        log_message(LOG_DEFAULT, "archdep.c: Environment variable PATH not found.");
+    if (DosSearchPath(flags, path, pgmName, pBuf, lBuf))
+    {
+        log_message(LOG_DEFAULT, "archdep.c: File \"%s\" not found.", pgmName);
+        return -1;
+    }
+    free(pgmName);
+    return 0;
+}
+
+char *archdep_cmdline(const char *name, char **argv, const char *sout, const char *serr)
+{
+    char *res;
+    int length = 0;
+    int i      = 0;
+    while (argv[++i]) length += strlen(argv[i]);
+    length += i+strlen(name)+3+
+        (sout?strlen(sout)+5:0)+
+        (serr?strlen(serr)+6:0); // need space for the spaces
+    res = xcalloc(1,length);
+    i = 0;
+    strcat(strcpy(res,"/c "),name);
+    while (argv[++i]) strcat(strcat(res," "), argv[i]);
+    if (sout) strcat(strcat(strcat(res,  " > \""), sout), "\"");
+    if (serr) strcat(strcat(strcat(res, " 2> \""), serr), "\"");
+    return res;
+}
+
+/* Launch program `name' (searched via the PATH environment variable)
+   passing `argv' as the parameters, wait for it to exit and return its
+   exit status. If `stdout_redir' or `stderr_redir' are != NULL,
+   redirect stdout or stderr to the corresponding file.  */
+int archdep_spawn(const char *name, char **argv,
+                  const char *stdout_redir, const char *stderr_redir)
+{  // how to redirect stdout & stderr??
+    typedef struct _CHILDINFO {  /* Define a structure for the queue data */
+        USHORT usSessionID;
+        USHORT usReturn;
+    } CHILDINFO;
+
+    UCHAR       fqName[256] = ""; /* Result of PATH search             */
+    HQUEUE      hqQueue;          /* Queue handle                      */
+    REQUESTDATA rdRequest;        /* Request data for the queue        */
+    ULONG       ulSzData;         /* Size of the queue data            */
+    BYTE        bPriority;        /* For the queue                     */
+    PVOID       pvData;           /* Pointer to the queue data         */
+    STARTDATA   sd;               /* Start Data for DosStartSession    */
+    PID         pid;              /* PID for the started child session */
+    ULONG       ulSession;        /* Session ID for the child session  */
+    APIRET      rc;               /* Return code from API's            */
+    char       *cmdline;
+
+    if (archdep_search_path(name, fqName, sizeof(fqName)))
+        return -1;
+
+    // Make the needed command string
+    cmdline = archdep_cmdline(fqName, argv, stdout_redir, stderr_redir);
+#if !defined __C1541__ && !defined __PETCAT__
+    log_message(LOG_DEFAULT, "archdep.c: Spawning \"cmd.exe %s\"", cmdline);
+#endif
+
+    memset(&sd,0,sizeof(STARTDATA));
+    sd.Length     = sizeof(STARTDATA);
+    sd.FgBg       = SSF_FGBG_BACK;      /* Start session in background */
+    sd.Related    = SSF_RELATED_CHILD;  /* Start a child session       */
+    sd.PgmName    = "cmd.exe";
+    sd.PgmInputs  = cmdline;
+    sd.PgmControl = SSF_CONTROL_INVISIBLE;  // | SSF_CONTROL_NOAUTOCLOSE;
+    sd.TermQ      = "\\QUEUES\\VICE2\\CHILD.QUE";
+    sd.InheritOpt = SSF_INHERTOPT_SHELL;
+
+    /* Start a child process and return it's session ID.
+     Wait for the session to end and get it's session ID
+     from the termination queue */
+
+    // this prevents you from closing Vice while a child is running
+#if !defined __C1541__ && !defined __PETCAT__
+    if (DosRequestMutexSem(hmtxSpawn, SEM_INDEFINITE_WAIT))
+        return 0;
+#endif
+
+    if(rc=DosCreateQueue(&hqQueue, QUE_FIFO|QUE_CONVERT_ADDRESS, sd.TermQ))
+        log_message(LOG_DEFAULT,"archdep.c: Error in DosCreateQueue (rc=%li).",rc);
+    else
+    {
+        if(rc=DosStartSession(&sd, &ulSession, &pid))  /* Start the child session */
+            log_message(LOG_DEFAULT,"archdep.c: Error in DosStartSession (rc=%li).",rc);
+        else
+        {
+            if(rc=DosReadQueue(hqQueue, &rdRequest, &ulSzData,        /* Wait for the child session to end (you'll have to end it */
+                               &pvData, 0, DCWW_WAIT, &bPriority, 0)) /* in some other way) */
+                log_message(LOG_DEFAULT,"archdep.c: Error in DosReadQueue (rc=%li).",rc);
+            else
+            {
+                rc = ((CHILDINFO*)pvData)->usReturn;
+                DosFreeMem(pvData); /* Free the memory of the queue data element read */
+            }
+        }
+        DosCloseQueue(hqQueue);
+    }
+#if !defined __C1541__ && !defined __PETCAT__
+    DosReleaseMutexSem(hmtxSpawn);
+#endif
+    free(cmdline);
+    //    DosSleep(1000);
+    if (rc)
+        log_message(LOG_DEFAULT, "archdep.c: Return Code: rc = %li", rc);
+
+    return rc;
+}
+
+void archdep_startup_log_error(const char *format, ...)
+{
+    char *txt;
+    va_list ap;
+    va_start(ap, format);
+    txt = xmvsprintf(format, ap);
+#if !defined __C1541__ && !defined __PETCAT__
+    WinMessageBox(HWND_DESKTOP, HWND_DESKTOP,
+                  txt, "VICE/2 Startup Error", 0, MB_OK);
+#else
+    printf(txt);
+#endif
+    free(txt);
+}
+
+
+char *archdep_quote_parameter(const char *name)
+{
+    char *a = concat("\"", name, "\"", NULL);
+    return a;
+}
+
+
+char *archdep_filename_parameter(const char *name)
+{
+    char *exp;
+    char *a;
+    archdep_expand_path(&exp, name);
+    a = archdep_quote_parameter(exp);
+    free(exp);
+    return a;
+}
+
+
+char *archdep_tmpnam(void)
+{
+    return stralloc(tmpnam(NULL));
+}
+
---
+++ tags/v1.5/v1.5.6/vice/src/arch/unix/x11/gnome/uimenu.c
@@ -0,0 +1,471 @@
+/*
+ * uimenu.c - Simple and ugly cascaded pop-up menu implementation.
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Oliver Schaertel GTK+ port
+ *  Martin Pottendorfer Gnome port
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+/* Warning: this code sucks.  It does work, but it sucks.  */
+
+#include "vice.h"
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+#include <X11/Xlib.h>
+#include <X11/Intrinsic.h>
+#include <X11/Xaw/SimpleMenu.h>
+#include <X11/Xaw/SmeLine.h>
+#include <X11/Xaw/SmeBSB.h>
+
+#include "machine.h"
+#include "resources.h"
+#include "utils.h"
+#include "vsync.h"
+#include "x11menu.h"
+#ifdef GNOME_MENUS
+#include <gnome.h>
+#endif
+#include "uimenu.h"
+
+/* Separator item.  */
+ui_menu_entry_t ui_menu_separator[] = {
+    { "--" },
+    { NULL },
+};
+
+static int menu_popup = 0;
+
+#define MAX_SUBMENUS 1024
+static struct {
+    GtkWidget* widget;
+    GtkWidget* parent;
+    int level;
+} submenus[MAX_SUBMENUS];
+
+
+/* This keeps a list of the menus with a checkmark on the left.  Each time
+   some setting is changed, we have to update them. */
+#define MAX_UPDATE_MENU_LIST_SIZE 1024
+typedef struct {
+    char *name;
+#ifdef GNOME_MENUS
+    GnomeUIInfo *uiinfo;
+#else
+    GtkWidget *w;
+#endif
+    ui_callback_t cb;
+    ui_menu_cb_obj obj;
+    gint handlerid;
+} checkmark_t;
+
+static GList *checkmark_list = NULL;
+
+int num_checkmark_menu_items = 0; /* !static because vsidui needs it. ugly! */
+
+static int num_submenus = 0;
+/* ------------------------------------------------------------------------- */
+
+int ui_menu_init()
+{
+    ui_create_dynamic_menues();
+    return(0);
+}
+
+#ifdef GNOME_MENUS
+GnomeUIInfo* ui_menu_create(const char *menu_name, ...)
+{
+    static int level = 0;
+    unsigned int i;
+    ui_menu_entry_t *list;
+    va_list ap;
+    ui_menu_cb_obj *obj;
+    GnomeUIInfo *uiinfo;
+    int current = 0;
+    int num_menu_items = 0;
+
+    level++;
+    va_start(ap, menu_name);
+
+    /* Ugly, but I have to allocate the GnomeUIInfo array before, 
+       otherwhise the pointer in checkmark_menu_items is invalid */
+    while ((list = va_arg(ap, ui_menu_entry_t *)) != NULL) 
+        for (i = 0; list[i].string; i++)
+	    num_menu_items++;
+    uiinfo = g_new(GnomeUIInfo, num_menu_items + 1);
+
+    va_start(ap, menu_name);
+    while ((list = va_arg(ap, ui_menu_entry_t *)) != NULL) 
+    {
+        for (i = 0; list[i].string; i++) 
+	{
+            switch (*list[i].string) 
+	    {
+	    case '-':		/* line */
+		memset(&uiinfo[current], 0, sizeof(GnomeUIInfo));
+		uiinfo[current].type = GNOME_APP_UI_SEPARATOR;
+                break;
+	    case '*':		/* toggle */
+		if (list[i].callback) {
+		    uiinfo[current].type = GNOME_APP_UI_TOGGLEITEM;
+		    uiinfo[current].label = make_menu_label(&list[i]);
+		    uiinfo[current].hint = NULL;
+		    uiinfo[current].moreinfo = list[i].callback;
+		    uiinfo[current].unused_data = NULL;
+		    uiinfo[current].pixmap_type  = GNOME_APP_PIXMAP_NONE;
+		    uiinfo[current].accelerator_key = 0;
+		    uiinfo[current].widget = NULL;
+		    if (num_checkmark_menu_items < MAX_UPDATE_MENU_LIST_SIZE) {
+			obj = &checkmark_menu_items[num_checkmark_menu_items].obj;
+			checkmark_menu_items[num_checkmark_menu_items].uiinfo =
+			    &uiinfo[current];
+			checkmark_menu_items[num_checkmark_menu_items].cb =
+			    list[i].callback;
+			checkmark_menu_items[num_checkmark_menu_items].obj.value =
+			    (void*) list[i].callback_data;
+			checkmark_menu_items[num_checkmark_menu_items].obj.status =
+			    CB_NORMAL;
+			uiinfo[current].user_data = (gpointer) obj;
+			num_checkmark_menu_items++;
+			
+		    } else {
+			fprintf(stderr,
+				"Maximum number of menus reached!  "
+				"Please fix the code.\n");
+			exit(-1);
+		    }
+		} else {
+			fprintf(stderr,
+				"Checkbox Menu Item without callback: %s!  "
+				"Please fix the code.\n", list[i].string);
+			exit(-1);
+		}
+		break;
+	    default:
+	    {
+		if (list[i].sub_menu) {
+		    if (num_submenus > MAX_SUBMENUS) {
+			fprintf(stderr,
+				"Maximum number of sub menus reached! "
+				"Please fix the code.\n");
+			exit(-1);
+			    
+		    }
+		    uiinfo[current].type = GNOME_APP_UI_SUBTREE;
+		    uiinfo[current].moreinfo = ui_menu_create("SUB", list[i].sub_menu, NULL);
+		    uiinfo[current].user_data = NULL;
+		} 
+		else 
+		{
+		    uiinfo[current].type = GNOME_APP_UI_ITEM;
+		    uiinfo[current].moreinfo = list[i].callback;
+			
+		    if (list[i].callback) {
+			obj = (ui_menu_cb_obj*) xmalloc(sizeof(ui_menu_cb_obj));
+			obj->value = (void*) list[i].callback_data;
+			uiinfo[current].user_data = obj;
+		    }
+		}
+		uiinfo[current].label = make_menu_label(&list[i]);
+		uiinfo[current].hint = NULL;
+		uiinfo[current].unused_data = NULL;
+		uiinfo[current].pixmap_type = GNOME_APP_PIXMAP_NONE;
+		uiinfo[current].accelerator_key = 0;
+		uiinfo[current].widget = NULL;
+		
+		break;
+	    }
+	    }
+	    
+	    if (list[i].hotkey_keysym != (KeySym) 0
+		&& list[i].callback != NULL)
+		ui_hotkey_register(list[i].hotkey_modifier,
+				   list[i].hotkey_keysym,
+				   list[i].callback,
+				   obj);
+	    
+	    current++;
+/* 	    uiinfo = g_renew(GnomeUIInfo, uiinfo, current + 1); */
+        }
+    }
+    
+    memset(&uiinfo[current], 0, sizeof(GnomeUIInfo));
+    uiinfo[current].type = GNOME_APP_UI_ENDOFINFO;
+    
+    level--;
+    
+    va_end(ap);
+    return uiinfo;
+}
+
+#else  /* !GNOME_MENUS */
+
+static void delete_checkmark_cb(GtkWidget *w, gpointer data)
+{
+    checkmark_t *cm;
+    
+    cm = (checkmark_t *) data;
+    checkmark_list = g_list_remove(checkmark_list, data);
+    free(cm->name);
+    free(cm);
+}
+
+GtkWidget* ui_menu_create(const char *menu_name, ...)
+{
+    static int level = 0;
+    GtkWidget *w;
+    unsigned int i, j;
+    ui_menu_entry_t *list;
+    va_list ap;
+    ui_menu_cb_obj *obj = NULL;
+
+    level++;
+    va_start(ap, menu_name);
+
+    w = gtk_menu_new();
+
+    while ((list = va_arg(ap, ui_menu_entry_t *)) != NULL) {
+        for (i = j = 0; list[i].string; i++) {
+            GtkWidget *new_item;
+            char name[256];
+
+            sprintf(name, "MenuItem%d", j);	/* ugly... */
+            switch (*list[i].string) 
+	    {
+	    case '-':		/* line */
+		new_item  = gtk_menu_item_new();
+                break;
+	    case '*':		/* toggle */
+	    {
+		char *label = make_menu_label(&list[i]);
+		/* Add this item to the list of calls to perform to update the
+		   menu status. */
+		if (list[i].callback) 
+		{
+		    checkmark_t *cmt;
+		    new_item = gtk_check_menu_item_new_with_label(label+1);
+		    
+		    cmt = (checkmark_t *) xmalloc(sizeof(checkmark_t));
+		    cmt->name = stralloc(label+1);
+		    cmt->w = new_item;
+		    cmt->cb = list[i].callback;
+		    cmt->obj.value = (void*) list[i].callback_data;
+		    cmt->obj.status = CB_NORMAL;
+		    cmt->handlerid = 
+			gtk_signal_connect(GTK_OBJECT(new_item),"activate",
+					   GTK_SIGNAL_FUNC(list[i].callback),
+					   (gpointer) &(cmt->obj)); 
+		    gtk_signal_connect(GTK_OBJECT(new_item), "destroy",
+				       GTK_SIGNAL_FUNC(delete_checkmark_cb),
+				       (gpointer) cmt);
+		    checkmark_list = g_list_prepend(checkmark_list, cmt);
+		    obj = &cmt->obj;
+		} 
+		else 
+		    new_item = gtk_menu_item_new_with_label(label+1);
+
+		j++;
+		free(label);
+		break;
+	    }
+	    default:
+	    {
+		char *label = make_menu_label(&list[i]);
+		new_item = gtk_menu_item_new_with_label(label);
+		if (list[i].callback) {
+		    obj = (ui_menu_cb_obj*) xmalloc(sizeof(ui_menu_cb_obj));
+		    obj->value = (void*) list[i].callback_data;
+		    
+		    gtk_signal_connect(GTK_OBJECT(new_item),"activate",
+				       GTK_SIGNAL_FUNC(list[i].callback),
+				       (gpointer) obj); 
+		}
+		free(label);
+		j++;
+	    }
+            }
+
+	    gtk_menu_append(GTK_MENU(w),new_item);
+	    gtk_widget_show(new_item);
+
+            if (list[i].sub_menu) 
+	    {
+                GtkWidget *sub;
+		char subname[10];
+
+                if (num_submenus > MAX_SUBMENUS) {
+                    fprintf(stderr,
+                            "Maximum number of sub menus reached! "
+                            "Please fix the code.\n");
+                    exit(-1);
+                }
+		sprintf(subname, "SUB%d", num_submenus);
+		sub = ui_menu_create(list[i].string, 
+				     list[i].sub_menu, NULL);
+		gtk_menu_item_set_submenu(GTK_MENU_ITEM(new_item),sub);
+                submenus[num_submenus].widget = sub;
+                submenus[num_submenus].parent = new_item;
+                submenus[num_submenus].level = level;
+                num_submenus++;
+            } 
+	    else 
+	    {            /* no submenu */
+	        if (list[i].hotkey_keysym != (KeySym) 0
+		    && list[i].callback != NULL)
+		    ui_hotkey_register(list[i].hotkey_modifier,
+                                       list[i].hotkey_keysym,
+                                       list[i].callback,
+                                       obj);
+            }
+        }
+    }
+    
+    level--;
+
+    va_end(ap);
+    return w;
+}
+
+#endif /* !GNOME_MENUS */
+
+int ui_menu_any_open(void)
+{
+    return menu_popup;
+}
+
+static void menu_handle_block(gpointer data, gpointer user_data)
+{
+    checkmark_t *cm = (checkmark_t *)data;
+
+    if (user_data)
+	gtk_signal_handler_block(GTK_OBJECT(cm->w), cm->handlerid);
+    else
+	gtk_signal_handler_unblock(GTK_OBJECT(cm->w), cm->handlerid);
+}
+
+static void menu_update_checkmarks(gpointer data, gpointer user_data)
+{
+    checkmark_t *cm = (checkmark_t *) data;
+    
+    cm->obj.status = CB_REFRESH;
+    ((void*(*)(GtkWidget*, ui_callback_data_t))
+     cm->cb)(cm->w, (ui_callback_data_t) &cm->obj);
+    cm->obj.status = CB_NORMAL;
+}
+
+void ui_menu_update_all_GTK(void)
+{
+#ifndef GNOME_MENUS		/* Well, with GNOME_MENUS this won't
+				   won't work */
+    g_list_foreach(checkmark_list, menu_handle_block, (gpointer) 1);
+    g_list_foreach(checkmark_list, menu_update_checkmarks, NULL);
+    ui_dispatch_events();
+    g_list_foreach(checkmark_list, menu_handle_block, (gpointer) 0);
+#else
+    fprintf(stderr,
+	    "Gnome menus not supported."
+	    "Please fix the code.\n");
+    exit(-1);
+#endif
+    
+}
+
+void ui_menu_update_all(void)
+{
+}
+
+void ui_menu_set_tick(GtkWidget *w, int flag) {
+    if(GTK_IS_CHECK_MENU_ITEM(w))
+      gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(w), flag != 0);
+}
+
+void ui_menu_set_sensitive(GtkWidget *w, int flag)
+{
+    gtk_widget_set_sensitive(w,flag);
+}
+
+/* ------------------------------------------------------------------------- */
+
+/* These functions are called by radio and toggle menu items if the callback
+   functions are defined through `UI_MENU_DEFINE_TOGGLE()',
+   `UI_MENU_DEFINE_RADIO()' or `UI_MENU_DEFINE_STRING_RADIO()'.  */
+
+void _ui_menu_toggle_helper(GtkWidget *w,
+                            ui_callback_data_t event_data,
+                            const char *resource_name)
+{
+    int current_value;
+
+    if (resources_get_value(resource_name,
+                            (resource_value_t *) &current_value) < 0)
+        return;
+
+    if(!CHECK_MENUS) {
+        resources_set_value(resource_name, (resource_value_t) !current_value);
+	ui_update_menus();
+    } else {
+        ui_menu_set_tick(w, current_value);
+    }
+}
+
+void _ui_menu_radio_helper(GtkWidget *w,
+                           ui_callback_data_t event_data,
+                           const char *resource_name)
+{
+    int current_value;
+
+    resources_get_value(resource_name, (resource_value_t *) &current_value);
+
+    if (!CHECK_MENUS) {
+        if (current_value != (int) UI_MENU_CB_PARAM) {
+            resources_set_value(resource_name,
+				(resource_value_t) UI_MENU_CB_PARAM);
+            ui_update_menus();
+        }
+    } else {
+	ui_menu_set_tick(w, current_value == (int) UI_MENU_CB_PARAM);
+    }  
+}
+
+void _ui_menu_string_radio_helper(GtkWidget *w, 
+                                  ui_callback_data_t event_data,
+                                  const char *resource_name)
+{
+    resource_value_t current_value;
+
+    resources_get_value(resource_name, &current_value);
+
+    if( current_value == 0) return;
+
+    if (!CHECK_MENUS) {
+        if (strcmp((const char *) current_value, (const char *) UI_MENU_CB_PARAM) != 0) {
+	   resources_set_value(resource_name,(resource_value_t*) UI_MENU_CB_PARAM);
+	   ui_update_menus();
+	}
+    } else {
+        ui_menu_set_tick(w, strcmp((const char *) current_value,
+				   (const char *) UI_MENU_CB_PARAM) == 0);
+    }
+}
