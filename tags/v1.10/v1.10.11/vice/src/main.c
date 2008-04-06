f8790fcac769702327502bc3bff37256061ac757
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 12:20:48 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.10.11'.

---
+++ tags/v1.10/v1.10.11/vice/src/main.c
@@ -0,0 +1,210 @@
+/*
+ * main.c - VICE main startup entry.
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Teemu Rantanen <tvr@cs.hut.fi>
+ *  Vesa-Matti Puro <vmp@lut.fi>
+ *  Jarkko Sonninen <sonninen@lut.fi>
+ *  Jouko Valta <jopi@stekt.oulu.fi>
+ *  André Fachat <a.fachat@physik.tu-chemnitz.de>
+ *  Andreas Boose <viceteam@t-online.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef ENABLE_NLS
+#include <locale.h>
+#endif
+
+#include "archdep.h"
+#include "cmdline.h"
+#include "console.h"
+#include "debug.h"
+#include "drive.h"
+#include "fullscreen.h"
+#include "init.h"
+#include "initcmdline.h"
+#include "interrupt.h"
+#include "log.h"
+#include "machine.h"
+#include "maincpu.h"
+#include "main.h"
+#include "resources.h"
+#include "sysfile.h"
+#include "types.h"
+#include "ui.h"
+#include "utils.h"
+#include "version.h"
+#include "video.h"
+#include "fliplist.h"
+
+
+int vsid_mode = 0;
+#ifdef __OS2__
+const
+#endif
+int console_mode = 0;
+static int init_done;
+
+/* ------------------------------------------------------------------------- */
+
+/* This is the main program entry point.  When not compiling for Windows,
+   this is `main()'; on Windows we have to #define the name to something
+   different because the standard entry point is `WinMain()' there.  */
+
+int main_program(int argc, char **argv)
+{
+    int i;
+
+    /* Check for -console and -vsid before initializing the user interface.
+       -console => no user interface
+       -vsid    => user interface in separate process */
+    for (i = 0; i < argc; i++) {
+#ifndef __OS2__
+        if (strcmp(argv[i], "-console") == 0) {
+            console_mode = 1;
+        } else
+#endif
+        if (strcmp(argv[i], "-vsid") == 0) {
+            vsid_mode = 1;
+        }
+    }
+
+#ifdef ENABLE_NLS
+    /* gettext stuff, not needed in Gnome, but here I can
+       overrule the default locale path */
+    setlocale(LC_ALL, "");
+    bindtextdomain (PACKAGE, NLS_LOCALEDIR);
+    textdomain(PACKAGE);
+#endif
+
+    archdep_startup(&argc, argv);
+
+#ifndef __riscos
+    if (atexit(main_exit) < 0) {
+        perror("atexit");
+        return -1;
+    }
+#endif
+
+    drive_setup_context();
+
+    /* Initialize system file locator.  */
+    sysfile_init(machine_name);
+
+    if (init_resources() < 0 || init_cmdline_options() < 0)
+        return -1;
+
+    /* Set factory defaults.  */
+    if (resources_set_defaults() < 0) {
+        archdep_startup_log_error("Cannot set defaults.\n");
+        return -1;
+    }
+
+    /* Initialize the user interface.  `ui_init()' might need to handle the
+       command line somehow, so we call it before parsing the options.
+       (e.g. under X11, the `-display' option is handled independently).  */
+    if (!console_mode && ui_init(&argc, argv) < 0) {
+        archdep_startup_log_error("Cannot initialize the UI.\n");
+        return -1;
+    }
+
+    /* Load the user's default configuration file.  */
+    if (vsid_mode) {
+        resources_set_value("Drive8Type", (resource_value_t)0);
+        resources_set_value("Sound", (resource_value_t)1);
+#ifdef HAVE_RESID
+        resources_set_value("SidUseResid", (resource_value_t)1);
+#endif
+        resources_set_value("SidModel", (resource_value_t)0);
+        resources_set_value("SidFilters", (resource_value_t)1);
+        resources_set_value("SoundSampleRate", (resource_value_t)44100);
+        resources_set_value("SoundSpeedAdjustment", (resource_value_t)2);
+        resources_set_value("SoundBufferSize", (resource_value_t)1000);
+        resources_set_value("SoundSuspendTime", (resource_value_t)0);
+    } else {
+        int retval = resources_load(NULL);
+
+        if (retval < 0) {
+            /* The resource file might contain errors, and thus certain
+               resources might have been initialized anyway.  */
+            if (resources_set_defaults() < 0) {
+                archdep_startup_log_error("Cannot set defaults.\n");
+                return -1;
+            }
+        }
+	flip_load_list((unsigned int) -1, archdep_default_fliplist_file_name(),
+		       0);
+    }
+
+    if (initcmdline_check_args(argc, argv) < 0)
+        return -1;
+
+    if (log_init() < 0) {
+        archdep_startup_log_error("Cannot startup logging system.\n");
+    }
+
+    /* VICE boot sequence.  */
+    log_message(LOG_DEFAULT, "*** VICE Version %s ***", VERSION);
+    log_message(LOG_DEFAULT, " ");
+    log_message(LOG_DEFAULT, "Welcome to %s, the free portable Commodore %s Emulator.",
+                archdep_program_name(), machine_name);
+    log_message(LOG_DEFAULT, " ");
+    log_message(LOG_DEFAULT, "Written by");
+    log_message(LOG_DEFAULT, "D. Sladic, A. Boose, D. Lem, T. Biczo, A. Dehmel, T. Bretz,");
+    log_message(LOG_DEFAULT, "A. Matthies, M. Pottendorfer, M. Brenner, S. Trikaliotis.");
+    log_message(LOG_DEFAULT, " ");
+    log_message(LOG_DEFAULT, "This is free software with ABSOLUTELY NO WARRANTY.");
+    log_message(LOG_DEFAULT, "See the \"About VICE\" command for more info.");
+    log_message(LOG_DEFAULT, " ");
+
+    /* Complete the GUI initialization (after loading the resources and
+       parsing the command-line) if necessary.  */
+    if (!console_mode && ui_init_finish() < 0)
+        return -1;
+
+    if (!console_mode && video_init() < 0)
+        return -1;
+
+    if (initcmdline_check_psid() < 0)
+        return -1;
+
+    if (init_main() < 0)
+        return -1;
+
+    initcmdline_check_attach();
+
+    init_done = 1;
+
+    /* Let's go...  */
+    maincpu_trigger_reset();
+    maincpu_mainloop();
+
+    log_error(LOG_DEFAULT, "perkele!");
+
+    return 0;
+}
+
---
+++ tags/v1.10/v1.10.11/vice/src/vic20/vic20.c
@@ -0,0 +1,601 @@
+/*
+ * vic20.c
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "asm.h"
+#include "attach.h"
+#include "autostart.h"
+#include "cartridge.h"
+#include "ciatimer.h"
+#include "clkguard.h"
+#include "cmdline.h"
+#include "console.h"
+#include "datasette.h"
+#include "debug.h"
+#include "drive-cmdline-options.h"
+#include "drive-resources.h"
+#include "drive-snapshot.h"
+#include "drive.h"
+#include "drivecpu.h"
+#include "iecdrive.h"
+#include "interrupt.h"
+#include "ioutil.h"
+#include "kbdbuf.h"
+#include "keyboard.h"
+#include "log.h"
+#include "machine.h"
+#include "maincpu.h"
+#include "mon.h"
+#include "printer.h"
+#include "resources.h"
+#include "rs232drv.h"
+#include "rsuser.h"
+#include "screenshot.h"
+#include "serial.h"
+#include "snapshot.h"
+#include "sound.h"
+#include "tape.h"
+#include "traps.h"
+#include "types.h"
+#include "via.h"
+#include "vic.h"
+#include "vic20-cmdline-options.h"
+#include "vic20-resources.h"
+#include "vic20-snapshot.h"
+#include "vic20.h"
+#include "vic20ieeevia.h"
+#include "vic20mem.h"
+#include "vic20sound.h"
+#include "vic20rsuser.h"
+#include "vic20ui.h"
+#include "vic20via.h"
+#include "vsync.h"
+
+
+#define NUM_KEYBOARD_MAPPINGS 2
+
+const char *machine_keymap_res_name_list[NUM_KEYBOARD_MAPPINGS] = {
+    "KeymapSymFile", "KeymapPosFile"
+};
+
+char *machine_keymap_file_list[NUM_KEYBOARD_MAPPINGS] = {
+    NULL, NULL
+};
+
+const char machine_name[] = "VIC20";
+int machine_class = VICE_MACHINE_VIC20;
+
+static void machine_vsync_hook(void);
+
+/* ------------------------------------------------------------------------- */
+
+static trap_t vic20_serial_traps[] = {
+    {
+        "SerialListen",
+        0xEE2E,
+        0xEEB2,
+        { 0x20, 0xA0, 0xE4 },
+        serialattention,
+        rom_read,
+        rom_store
+    },
+    {
+        "SerialSaListen",
+        0xEE40,
+        0xEEB2,
+        { 0x20, 0x8D, 0xEF },
+        serialattention,
+        rom_read,
+        rom_store
+    },
+    {
+        "SerialSendByte",
+        0xEE49,
+        0xEEB2,
+        { 0x78, 0x20, 0xA0 },
+        serialsendbyte,
+        rom_read,
+        rom_store
+    },
+    {
+        "SerialReceiveByte",
+        0xEF19,
+        0xEEB2,
+        { 0x78, 0xA9, 0x00 },
+        serialreceivebyte,
+        rom_read,
+        rom_store
+    },
+    {
+        "SerialReady",
+        0xE4B2,
+        0xEEB2,
+        { 0xAD, 0x1F, 0x91 },
+        trap_serial_ready,
+        rom_read,
+        rom_store
+    },
+    {
+        NULL,
+        0,
+        0,
+        {0, 0, 0},
+        NULL,
+        NULL,
+        NULL
+    }
+};
+
+/* Tape traps.  */
+static trap_t vic20_tape_traps[] = {
+    {
+        "TapeFindHeader",
+        0xF7B2,
+        0xF7B5,
+        { 0x20, 0xC0, 0xF8 },
+        tape_find_header_trap,
+        rom_read,
+        rom_store
+    },
+    {
+        "TapeReceive",
+        0xF90B,
+        0xFCCF,
+        { 0x20, 0xFB, 0xFC },
+        tape_receive_trap,
+        rom_read,
+        rom_store
+    },
+    {
+        NULL,
+        0,
+        0,
+        { 0, 0, 0 },
+        NULL,
+        NULL,
+        NULL
+    }
+};
+
+static tape_init_t tapeinit = {
+    0xb2,
+    0x90,
+    0x93,
+    0x29f,
+    0,
+    0xc1,
+    0xae,
+    0x277,
+    0xc6,
+    vic20_tape_traps,
+    36 * 8,
+    54 * 8,
+    55 * 8,
+    73 * 8,
+    74 * 8,
+    100 * 8
+};
+
+static log_t vic20_log = LOG_ERR;
+static machine_timing_t machine_timing;
+
+/* ------------------------------------------------------------------------ */
+
+/* VIC20-specific resource initialization.  This is called before
+   initializing the machine itself with `machine_init()'.  */
+int machine_resources_init(void)
+{
+    if (traps_resources_init() < 0
+        || vsync_resources_init() < 0
+        || video_resources_init(VIDEO_RESOURCES_PAL) < 0
+        || vic20_resources_init() < 0
+        || vic_resources_init() < 0
+        || sound_resources_init() < 0
+        || rs232drv_resources_init() < 0
+        || rsuser_resources_init() < 0
+        || printer_resources_init() < 0
+#ifndef COMMON_KBD
+        || kbd_resources_init() < 0
+#endif
+        || drive_resources_init() < 0
+        || datasette_resources_init() < 0
+        || cartridge_resources_init() <0)
+        return -1;
+
+    return 0;
+}
+
+/* VIC20-specific command-line option initialization.  */
+int machine_cmdline_options_init(void)
+{
+    if (traps_cmdline_options_init() < 0
+        || vsync_cmdline_options_init() < 0
+        || video_init_cmdline_options() < 0
+        || vic20_cmdline_options_init() < 0
+        || vic_cmdline_options_init() < 0
+        || sound_cmdline_options_init() < 0
+        || rs232drv_cmdline_options_init() < 0
+        || rsuser_cmdline_options_init() < 0
+        || printer_cmdline_options_init() < 0
+#ifndef COMMON_KBD
+        || kbd_cmdline_options_init() < 0
+#endif
+        || drive_cmdline_options_init() < 0
+        || datasette_cmdline_options_init() < 0
+        || cartridge_cmdline_options_init() < 0)
+        return -1;
+
+    return 0;
+}
+
+void vic20_monitor_init(void)
+{
+    monitor_cpu_type_t asm6502;
+    monitor_cpu_type_t *asmarray[2];
+
+    asm6502_init(&asm6502);
+
+    asmarray[0] = &asm6502;
+    asmarray[1] = NULL;
+
+    /* Initialize the monitor.  */
+    monitor_init(&maincpu_monitor_interface, drive0_get_monitor_interface_ptr(),
+                 drive1_get_monitor_interface_ptr(), asmarray);
+}
+
+/* VIC20-specific initialization.  */
+int machine_init(void)
+{
+    vic20_log = log_open("VIC20");
+
+    if (mem_load() < 0)
+        return -1;
+
+    /* Setup trap handling.  */
+    traps_init();
+
+    /* Initialize serial traps.  If user does not want them, or if the
+       ``drive'' emulation is used, do not install them.  */
+    if (serial_init(vic20_serial_traps, 0xa4) < 0)
+        return -1;
+
+    /* Initialize drives. */
+    file_system_init();
+
+    /* Initialize RS232 handler.  */
+    rs232drv_init();
+    vic20_rsuser_init();
+
+    /* initialize print devices.  */
+    printer_init();
+
+    /* Initialize the tape emulation.  */
+    tape_init(&tapeinit);
+
+    /* Initialize the datasette emulation.  */
+    datasette_init();
+
+    /* Fire up the hardware-level drive emulation. */
+    drive_init(VIC20_PAL_CYCLES_PER_SEC, VIC20_NTSC_CYCLES_PER_SEC);
+
+    /* Initialize autostart.  */
+    autostart_init((CLOCK)
+                   (3 * VIC20_PAL_RFSH_PER_SEC * VIC20_PAL_CYCLES_PER_RFSH),
+                   1, 0xcc, 0xd1, 0xd3, 0xd5);
+
+    /* Initialize the VIC-I emulation.  */
+    if (vic_init() == NULL)
+        return -1;
+
+    /* needed for VC1571/1581 emulation */
+    ciat_init_table();
+
+    via1_init();
+    via2_init();
+
+    ieeevia1_init();
+    ieeevia2_init();
+
+#ifndef COMMON_KBD
+    /* Load the default keymap file.  */
+    if (vic20_kbd_init() < 0)
+        return -1;
+#endif
+
+    vic20_monitor_init();
+
+    /* Initialize vsync and register our hook function.  */
+    vsync_init(machine_vsync_hook);
+    vsync_set_machine_parameter(machine_timing.rfsh_per_sec,
+                                machine_timing.cycles_per_sec);
+
+    /* Initialize sound.  Notice that this does not really open the audio
+       device yet.  */
+    sound_init(machine_timing.cycles_per_sec, machine_timing.cycles_per_rfsh);
+
+    /* Initialize keyboard buffer.  */
+    kbd_buf_init(631, 198, 10, (CLOCK)(machine_timing.cycles_per_rfsh
+                 * machine_timing.rfsh_per_sec));
+
+    /* Initialize the VIC20-specific part of the UI.  */
+    vic20_ui_init();
+
+    iec_init();
+
+    return 0;
+}
+
+/* VIC20-specific reset sequence.  */
+void machine_specific_reset(void)
+{
+    serial_reset();
+
+    via1_reset();
+    via2_reset();
+    vic_reset();
+    vic_sound_reset();
+
+    ieeevia1_reset();
+    ieeevia2_reset();
+
+    rs232drv_reset();
+    rsuser_reset();
+
+    printer_reset();
+    drive_reset();
+    datasette_reset();
+}
+
+void machine_powerup(void)
+{
+    mem_powerup();
+    maincpu_trigger_reset();
+}
+
+void machine_shutdown(void)
+{
+    /* Detach all disks.  */
+    file_system_detach_disk_shutdown();
+
+    /* and the tape */
+    tape_image_detach(1);
+
+    console_close_all();
+
+    /* close the video chip(s) */
+    vic_free();
+}
+
+/* Return nonzero if `addr' is in the trappable address space.  */
+int rom_trap_allowed(ADDRESS addr)
+{
+    return 1; /* FIXME */
+}
+
+/* ------------------------------------------------------------------------- */
+
+/* This hook is called at the end of every frame.  */
+static void machine_vsync_hook(void)
+{
+    CLOCK sub;
+
+    drive_vsync_hook();
+
+    autostart_advance();
+
+    sub = clk_guard_prevent_overflow(&maincpu_clk_guard);
+
+    /* The drive has to deal both with our overflowing and its own one, so
+       it is called even when there is no overflowing in the main CPU.  */
+    /* FIXME: Do we have to check drive_enabled here?  */
+    drive_prevent_clk_overflow(sub, 0);
+    drive_prevent_clk_overflow(sub, 1);
+}
+
+int machine_set_restore_key(int v)
+{
+    via2_signal(VIA_SIG_CA1, v ? VIA_SIG_FALL : VIA_SIG_RISE);
+    return 1;
+}
+
+/* ------------------------------------------------------------------------- */
+
+long machine_get_cycles_per_second(void)
+{
+    return machine_timing.cycles_per_sec;
+}
+
+void machine_change_timing(int timeval)
+{
+    maincpu_trigger_reset();
+
+    switch (timeval) {
+      case MACHINE_SYNC_PAL:
+        machine_timing.cycles_per_sec = VIC20_PAL_CYCLES_PER_SEC;
+        machine_timing.cycles_per_rfsh = VIC20_PAL_CYCLES_PER_RFSH;
+        machine_timing.rfsh_per_sec = VIC20_PAL_RFSH_PER_SEC;
+        machine_timing.cycles_per_line = VIC20_PAL_CYCLES_PER_LINE;
+        machine_timing.screen_lines = VIC20_PAL_SCREEN_LINES;
+        break;
+      case MACHINE_SYNC_NTSC:
+        machine_timing.cycles_per_sec = VIC20_NTSC_CYCLES_PER_SEC;
+        machine_timing.cycles_per_rfsh = VIC20_NTSC_CYCLES_PER_RFSH;
+        machine_timing.rfsh_per_sec = VIC20_NTSC_RFSH_PER_SEC;
+        machine_timing.cycles_per_line = VIC20_NTSC_CYCLES_PER_LINE;
+        machine_timing.screen_lines = VIC20_NTSC_SCREEN_LINES;
+        break;
+      default:
+        log_error(vic20_log, "Unknown machine timing.");
+    }
+
+    vsync_set_machine_parameter(machine_timing.rfsh_per_sec,
+                                machine_timing.cycles_per_sec);
+    sound_set_machine_parameter(machine_timing.cycles_per_sec,
+                                machine_timing.cycles_per_rfsh);
+    debug_set_machine_parameter(machine_timing.cycles_per_line,
+                                machine_timing.screen_lines);
+    clk_guard_set_clk_base(&maincpu_clk_guard, machine_timing.cycles_per_rfsh);
+
+    vic_change_timing();
+
+    mem_patch_kernal();
+}
+
+/* ------------------------------------------------------------------------- */
+
+/* #define SNAP_MACHINE_NAME   "VIC20" */
+#define SNAP_MAJOR          0
+#define SNAP_MINOR          0
+
+int machine_write_snapshot(const char *name, int save_roms, int save_disks)
+{
+    snapshot_t *s;
+    int ieee488;
+
+    s = snapshot_create(name, ((BYTE)(SNAP_MAJOR)), ((BYTE)(SNAP_MINOR)),
+                        machine_name);
+    if (s == NULL) {
+        perror(name);
+        return -1;
+    }
+
+    /* FIXME: Missing sound.  */
+    if (maincpu_snapshot_write_module(s) < 0
+        || vic20_snapshot_write_module(s, save_roms) < 0
+        || vic_snapshot_write_module(s) < 0
+        || via1_snapshot_write_module(s) < 0
+        || via2_snapshot_write_module(s) < 0
+        || drive_snapshot_write_module(s, save_disks, save_roms) < 0) {
+        snapshot_close(s);
+        ioutil_remove(name);
+        return -1;
+    }
+
+    resources_get_value("IEEE488", (resource_value_t*) &ieee488);
+    if (ieee488) {
+        if (ieeevia1_snapshot_write_module(s) < 0
+            || ieeevia2_snapshot_write_module(s) < 0) {
+            snapshot_close(s);
+            ioutil_remove(name);
+            return 1;
+        }
+    }
+
+    snapshot_close(s);
+    return 0;
+}
+
+int machine_read_snapshot(const char *name)
+{
+    snapshot_t *s;
+    BYTE minor, major;
+
+    s = snapshot_open(name, &major, &minor, machine_name);
+    if (s == NULL)
+        return -1;
+
+    if (major != SNAP_MAJOR || minor != SNAP_MINOR) {
+        log_error(vic20_log,
+                  "Snapshot version (%d.%d) not valid: expecting %d.%d.",
+                  major, minor, SNAP_MAJOR, SNAP_MINOR);
+        goto fail;
+    }
+
+    /* FIXME: Missing sound.  */
+    if (maincpu_snapshot_read_module(s) < 0
+        || vic20_snapshot_read_module(s) < 0
+        || vic_snapshot_read_module(s) < 0
+        || via1_snapshot_read_module(s) < 0
+        || via2_snapshot_read_module(s) < 0
+        || drive_snapshot_read_module(s) < 0)
+        goto fail;
+
+    if (ieeevia1_snapshot_read_module(s) < 0
+        || ieeevia2_snapshot_read_module(s) < 0) {
+        /* IEEE488 module not undumped */
+        resources_set_value("IEEE488", (resource_value_t)0);
+    } else {
+        resources_set_value("IEEE488", (resource_value_t)1);
+    }
+
+    snapshot_close(s);
+    return 0;
+
+fail:
+    if (s != NULL)
+        snapshot_close(s);
+    maincpu_trigger_reset();
+    return -1;
+}
+
+
+/* ------------------------------------------------------------------------- */
+int machine_autodetect_psid(const char *name)
+{
+    return -1;
+}
+
+void machine_play_psid(int tune)
+{
+}
+
+int machine_screenshot(screenshot_t *screenshot, unsigned int wn)
+{
+    if (wn != 0)
+        return -1;
+
+    vic_screenshot(screenshot);
+    return 0;
+}
+
+int machine_canvas_screenshot(screenshot_t *screenshot,
+                              struct video_canvas_s *canvas)
+{
+    if (canvas != vic_get_canvas())
+        return -1;
+
+    vic_screenshot(screenshot);
+    return 0;
+}
+
+int machine_canvas_async_refresh(struct canvas_refresh_s *refresh,
+                                 struct video_canvas_s *canvas)
+{
+    if (canvas != vic_get_canvas())
+        return -1;
+
+    vic_async_refresh(refresh);
+    return 0;
+}
+
+unsigned int machine_num_keyboard_mappings(void)
+{
+    return NUM_KEYBOARD_MAPPINGS;
+}
+
