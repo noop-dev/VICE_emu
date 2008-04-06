d76e36bb3f1950071c16184673a6530747066df5
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 14:42:27 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.13.8'.

---
+++ v1.13.8/vice/src/arch/win32/petui.c
@@ -0,0 +1,70 @@
+/*
+ * petui.c - PET-specific user interface.
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+#include <windows.h>
+
+#include "petui.h"
+#include "res.h"
+#include "ui.h"
+#include "uidrivepetcbm2.h"
+#include "uipetset.h"
+#include "uivideo.h"
+#include "winmain.h"
+
+
+static const ui_menu_toggle pet_ui_menu_toggles[] = {
+    { "CrtcDoubleSize", IDM_TOGGLE_CRTCDOUBLESIZE },
+    { "CrtcDoubleScan", IDM_TOGGLE_CRTCDOUBLESCAN },
+    { "CrtcVideoCache", IDM_TOGGLE_CRTCVIDEOCACHE },
+    { NULL, 0 }
+};
+
+static void pet_ui_specific(WPARAM wparam, HWND hwnd)
+{
+    switch (wparam) {
+      case IDM_PET_SETTINGS:
+        ui_pet_settings_dialog(hwnd);
+		break;
+      case IDM_VIDEO_SETTINGS:
+        ui_video_settings_dialog(hwnd, UI_VIDEO_CHIP_CRTC, UI_VIDEO_CHIP_NONE);
+        break;
+      case IDM_DRIVE_SETTINGS:
+        uidrivepetcbm2_settings_dialog(hwnd);
+        break;
+    }
+}
+
+int pet_ui_init(void)
+{
+    ui_register_machine_specific(pet_ui_specific);
+    ui_register_menu_toggles(pet_ui_menu_toggles);
+    return 0;
+}
+
