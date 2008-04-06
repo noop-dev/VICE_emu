6199dcd05d70fb6759c9201af5780f20797ca846
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 10:31:18 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.7.51'.

---
+++ v1.7.51/vice/src/pet/petacia.h
@@ -0,0 +1,34 @@
+/*
+ *
+ * This file is part of Commodore 64 emulator.
+ * See README for copyright notice
+ *
+ * ACIA 6551 register number declarations.
+ * Interrupt signals are defined in interrupt.h.
+ *
+ * Written by
+ *    Andre Fachat <a.fachat@physik.tu-chemnitz.de>
+ *
+ */
+
+#ifndef _PETACIA_H
+#define _PETACIA_H
+
+#include "types.h"
+
+struct snapshot_s;
+
+extern void acia1_init(void);
+extern BYTE REGPARM1 acia1_read(ADDRESS a);
+extern BYTE REGPARM1 acia1_peek(ADDRESS a);
+extern void REGPARM2 acia1_store(ADDRESS a, BYTE b);
+extern void acia1_reset(void);
+
+extern int acia1_cmdline_options_init(void);
+extern int acia1_resources_init(void);
+
+extern int acia1_snapshot_read_module(struct snapshot_s *);
+extern int acia1_snapshot_write_module(struct snapshot_s *);
+
+#endif
+
