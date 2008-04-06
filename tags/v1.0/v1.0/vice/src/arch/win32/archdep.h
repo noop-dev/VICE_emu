f1a7440d571ae00f6bc2465409ea96f88ead3662
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 08:18:41 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.0'.

---
+++ tags/v1.0/v1.0/vice/src/arch/win32/archdep.h
@@ -0,0 +1,58 @@
+/*
+ * archdep.h - Architecture dependant defines.
+ *
+ * Written by
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _ARCHDEP_H
+#define _ARCHDEP_H
+
+/* Filesystem dependant operators.  */
+#define FSDEVICE_DEFAULT_DIR   "."
+#define FSDEV_DIR_SEP_STR      "\\"
+#define FSDEV_DIR_SEP_CHR      '\\'
+#define FSDEV_EXT_SEP_STR      "."
+#define FSDEV_EXT_SEP_CHR      '.'
+
+/* Modes for fopen().  */
+#define MODE_READ   "r"
+#define MODE_WRITE  "w"
+#define MODE_APPEND "w+"
+
+int archdep_startup(int *argc, char **argv);
+const char *archdep_program_name(void);
+const char *archdep_boot_path(void);
+const char *archdep_default_sysfile_pathlist(const char *emu_id);
+const char *archdep_default_resource_file_name(void);
+const char *archdep_default_save_resource_file_name(void);
+FILE *archdep_open_default_log_file(void);
+int archdep_num_text_lines(void);
+int archdep_num_text_columns(void);
+int archdep_default_logger(const char *level_string, const char *format,
+                           va_list ap);
+
+int archdep_path_is_relative(const char *path);
+void archdep_setup_signals(int do_core_dumps);
+
+#endif
+
