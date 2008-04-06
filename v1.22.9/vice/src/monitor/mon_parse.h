60f4ecec4230441af469e4521d915a293ef238ba
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 20:19:38 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.22.9'.

---
+++ v1.22.9/vice/src/monitor/mon_parse.h
@@ -0,0 +1,320 @@
+/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     H_NUMBER = 258,
+     D_NUMBER = 259,
+     O_NUMBER = 260,
+     B_NUMBER = 261,
+     CONVERT_OP = 262,
+     B_DATA = 263,
+     H_RANGE_GUESS = 264,
+     D_NUMBER_GUESS = 265,
+     O_NUMBER_GUESS = 266,
+     B_NUMBER_GUESS = 267,
+     BAD_CMD = 268,
+     MEM_OP = 269,
+     IF = 270,
+     MEM_COMP = 271,
+     MEM_DISK8 = 272,
+     MEM_DISK9 = 273,
+     MEM_DISK10 = 274,
+     MEM_DISK11 = 275,
+     REG_ASGN_SEP = 276,
+     EQUALS = 277,
+     TRAIL = 278,
+     CMD_SEP = 279,
+     CMD_SIDEFX = 280,
+     CMD_RETURN = 281,
+     CMD_BLOCK_READ = 282,
+     CMD_BLOCK_WRITE = 283,
+     CMD_UP = 284,
+     CMD_DOWN = 285,
+     CMD_LOAD = 286,
+     CMD_SAVE = 287,
+     CMD_VERIFY = 288,
+     CMD_IGNORE = 289,
+     CMD_HUNT = 290,
+     CMD_FILL = 291,
+     CMD_MOVE = 292,
+     CMD_GOTO = 293,
+     CMD_REGISTERS = 294,
+     CMD_READSPACE = 295,
+     CMD_WRITESPACE = 296,
+     CMD_RADIX = 297,
+     CMD_MEM_DISPLAY = 298,
+     CMD_BREAK = 299,
+     CMD_TRACE = 300,
+     CMD_IO = 301,
+     CMD_BRMON = 302,
+     CMD_COMPARE = 303,
+     CMD_DUMP = 304,
+     CMD_UNDUMP = 305,
+     CMD_EXIT = 306,
+     CMD_DELETE = 307,
+     CMD_CONDITION = 308,
+     CMD_COMMAND = 309,
+     CMD_ASSEMBLE = 310,
+     CMD_DISASSEMBLE = 311,
+     CMD_NEXT = 312,
+     CMD_STEP = 313,
+     CMD_PRINT = 314,
+     CMD_DEVICE = 315,
+     CMD_HELP = 316,
+     CMD_WATCH = 317,
+     CMD_DISK = 318,
+     CMD_SYSTEM = 319,
+     CMD_QUIT = 320,
+     CMD_CHDIR = 321,
+     CMD_BANK = 322,
+     CMD_LOAD_LABELS = 323,
+     CMD_SAVE_LABELS = 324,
+     CMD_ADD_LABEL = 325,
+     CMD_DEL_LABEL = 326,
+     CMD_SHOW_LABELS = 327,
+     CMD_RECORD = 328,
+     CMD_STOP = 329,
+     CMD_PLAYBACK = 330,
+     CMD_CHAR_DISPLAY = 331,
+     CMD_SPRITE_DISPLAY = 332,
+     CMD_TEXT_DISPLAY = 333,
+     CMD_SCREENCODE_DISPLAY = 334,
+     CMD_ENTER_DATA = 335,
+     CMD_ENTER_BIN_DATA = 336,
+     CMD_KEYBUF = 337,
+     CMD_BLOAD = 338,
+     CMD_BSAVE = 339,
+     CMD_SCREEN = 340,
+     CMD_UNTIL = 341,
+     CMD_CPU = 342,
+     L_PAREN = 343,
+     R_PAREN = 344,
+     ARG_IMMEDIATE = 345,
+     REG_A = 346,
+     REG_X = 347,
+     REG_Y = 348,
+     COMMA = 349,
+     INST_SEP = 350,
+     REG_B = 351,
+     REG_C = 352,
+     REG_D = 353,
+     REG_E = 354,
+     REG_H = 355,
+     REG_L = 356,
+     REG_AF = 357,
+     REG_BC = 358,
+     REG_DE = 359,
+     REG_HL = 360,
+     REG_IX = 361,
+     REG_IY = 362,
+     REG_SP = 363,
+     REG_IXH = 364,
+     REG_IXL = 365,
+     REG_IYH = 366,
+     REG_IYL = 367,
+     STRING = 368,
+     FILENAME = 369,
+     R_O_L = 370,
+     OPCODE = 371,
+     LABEL = 372,
+     BANKNAME = 373,
+     CPUTYPE = 374,
+     REGISTER = 375,
+     COMPARE_OP = 376,
+     RADIX_TYPE = 377,
+     INPUT_SPEC = 378,
+     CMD_CHECKPT_ON = 379,
+     CMD_CHECKPT_OFF = 380,
+     TOGGLE = 381
+   };
+#endif
+/* Tokens.  */
+#define H_NUMBER 258
+#define D_NUMBER 259
+#define O_NUMBER 260
+#define B_NUMBER 261
+#define CONVERT_OP 262
+#define B_DATA 263
+#define H_RANGE_GUESS 264
+#define D_NUMBER_GUESS 265
+#define O_NUMBER_GUESS 266
+#define B_NUMBER_GUESS 267
+#define BAD_CMD 268
+#define MEM_OP 269
+#define IF 270
+#define MEM_COMP 271
+#define MEM_DISK8 272
+#define MEM_DISK9 273
+#define MEM_DISK10 274
+#define MEM_DISK11 275
+#define REG_ASGN_SEP 276
+#define EQUALS 277
+#define TRAIL 278
+#define CMD_SEP 279
+#define CMD_SIDEFX 280
+#define CMD_RETURN 281
+#define CMD_BLOCK_READ 282
+#define CMD_BLOCK_WRITE 283
+#define CMD_UP 284
+#define CMD_DOWN 285
+#define CMD_LOAD 286
+#define CMD_SAVE 287
+#define CMD_VERIFY 288
+#define CMD_IGNORE 289
+#define CMD_HUNT 290
+#define CMD_FILL 291
+#define CMD_MOVE 292
+#define CMD_GOTO 293
+#define CMD_REGISTERS 294
+#define CMD_READSPACE 295
+#define CMD_WRITESPACE 296
+#define CMD_RADIX 297
+#define CMD_MEM_DISPLAY 298
+#define CMD_BREAK 299
+#define CMD_TRACE 300
+#define CMD_IO 301
+#define CMD_BRMON 302
+#define CMD_COMPARE 303
+#define CMD_DUMP 304
+#define CMD_UNDUMP 305
+#define CMD_EXIT 306
+#define CMD_DELETE 307
+#define CMD_CONDITION 308
+#define CMD_COMMAND 309
+#define CMD_ASSEMBLE 310
+#define CMD_DISASSEMBLE 311
+#define CMD_NEXT 312
+#define CMD_STEP 313
+#define CMD_PRINT 314
+#define CMD_DEVICE 315
+#define CMD_HELP 316
+#define CMD_WATCH 317
+#define CMD_DISK 318
+#define CMD_SYSTEM 319
+#define CMD_QUIT 320
+#define CMD_CHDIR 321
+#define CMD_BANK 322
+#define CMD_LOAD_LABELS 323
+#define CMD_SAVE_LABELS 324
+#define CMD_ADD_LABEL 325
+#define CMD_DEL_LABEL 326
+#define CMD_SHOW_LABELS 327
+#define CMD_RECORD 328
+#define CMD_STOP 329
+#define CMD_PLAYBACK 330
+#define CMD_CHAR_DISPLAY 331
+#define CMD_SPRITE_DISPLAY 332
+#define CMD_TEXT_DISPLAY 333
+#define CMD_SCREENCODE_DISPLAY 334
+#define CMD_ENTER_DATA 335
+#define CMD_ENTER_BIN_DATA 336
+#define CMD_KEYBUF 337
+#define CMD_BLOAD 338
+#define CMD_BSAVE 339
+#define CMD_SCREEN 340
+#define CMD_UNTIL 341
+#define CMD_CPU 342
+#define L_PAREN 343
+#define R_PAREN 344
+#define ARG_IMMEDIATE 345
+#define REG_A 346
+#define REG_X 347
+#define REG_Y 348
+#define COMMA 349
+#define INST_SEP 350
+#define REG_B 351
+#define REG_C 352
+#define REG_D 353
+#define REG_E 354
+#define REG_H 355
+#define REG_L 356
+#define REG_AF 357
+#define REG_BC 358
+#define REG_DE 359
+#define REG_HL 360
+#define REG_IX 361
+#define REG_IY 362
+#define REG_SP 363
+#define REG_IXH 364
+#define REG_IXL 365
+#define REG_IYH 366
+#define REG_IYL 367
+#define STRING 368
+#define FILENAME 369
+#define R_O_L 370
+#define OPCODE 371
+#define LABEL 372
+#define BANKNAME 373
+#define CPUTYPE 374
+#define REGISTER 375
+#define COMPARE_OP 376
+#define RADIX_TYPE 377
+#define INPUT_SPEC 378
+#define CMD_CHECKPT_ON 379
+#define CMD_CHECKPT_OFF 380
+#define TOGGLE 381
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+#line 116 "mon_parse.y"
+{
+    MON_ADDR a;
+    MON_ADDR range[2];
+    int i;
+    REG_ID reg;
+    CONDITIONAL cond_op;
+    cond_node_t *cond_node;
+    RADIXTYPE rt;
+    ACTION action;
+    char *str;
+}
+/* Line 1529 of yacc.c.  */
+#line 313 "mon_parse.h"
+	YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE yylval;
+
