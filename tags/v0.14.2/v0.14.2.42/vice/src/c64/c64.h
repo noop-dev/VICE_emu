04175bc28f8f0ef9e5df74c057bb7745be0e2ed5
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 07:47:18 +0000

    This commit was manufactured by cvs2svn to create tag 'v0.14.2.42'.

---
+++ tags/v0.14.2/v0.14.2.42/vice/src/c64/c64.h
@@ -0,0 +1,46 @@
+/*
+ * c64.h
+ *
+ * Written by
+ *  Ettore Perazzoli (ettore@comm2000.it)
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _C64_H
+#define _C64_H
+
+#define C64_PAL_CYCLES_PER_SEC	985248
+#define C64_PAL_CYCLES_PER_LINE 63
+#define C64_PAL_SCREEN_LINES    312
+#define C64_PAL_CYCLES_PER_RFSH (C64_PAL_SCREEN_LINES \
+                                 * C64_PAL_CYCLES_PER_LINE)
+#define C64_PAL_RFSH_PER_SEC    (1.0 / ((double)C64_PAL_CYCLES_PER_RFSH	\
+                                        / (double)C64_PAL_CYCLES_PER_SEC))
+
+#define C64_NTSC_CYCLES_PER_SEC  1022730
+#define C64_NTSC_CYCLES_PER_LINE 65
+#define C64_NTSC_SCREEN_LINES	 262
+#define C64_NTSC_CYCLES_PER_RFSH (C64_NTSC_SCREEN_LINES \
+                                  * C64_NTSC_CYCLES_PER_LINE)
+#define C64_NTSC_RFSH_PER_SEC    (1.0 / ((double)C64_NTSC_CYCLES_PER_RFSH   \
+                                        / (double)C64_NTSC_CYCLES_PER_SEC))
+
+#endif
