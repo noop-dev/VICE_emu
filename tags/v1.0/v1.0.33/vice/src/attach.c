36899c1a28ba5d0ca48f3804d2040e0178f2353d
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 08:29:00 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.0.33'.

---
+++ tags/v1.0/v1.0.33/vice/src/attach.c
@@ -0,0 +1,414 @@
+/*
+ * attach.c - File system attach management.
+ *
+ * Written by
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#ifdef STDC_HEADERS
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#endif
+
+#include "attach.h"
+#include "diskimage.h"
+#include "drive.h"
+#include "fdc.h"
+#include "fliplist.h"
+#include "resources.h"
+#include "serial.h"
+#include "ui.h"
+#include "utils.h"
+#include "vdrive.h"
+#include "wd1770.h"
+
+static int file_system_device_enabled[4];
+
+static int set_file_system_device8(resource_value_t v);
+static int set_file_system_device9(resource_value_t v);
+static int set_file_system_device10(resource_value_t v);
+static int set_file_system_device11(resource_value_t v);
+
+static resource_t resources[] = {
+    { "FileSystemDevice8", RES_INTEGER, (resource_value_t) 1,
+      (resource_value_t *) &file_system_device_enabled[0],
+      set_file_system_device8 },
+    { "FileSystemDevice9", RES_INTEGER, (resource_value_t) 1,
+      (resource_value_t *) &file_system_device_enabled[1],
+      set_file_system_device9 },
+    { "FileSystemDevice10", RES_INTEGER, (resource_value_t) 1,
+      (resource_value_t *) &file_system_device_enabled[2],
+      set_file_system_device10 },
+    { "FileSystemDevice11", RES_INTEGER, (resource_value_t) 1,
+      (resource_value_t *) &file_system_device_enabled[3],
+      set_file_system_device11 },
+    { NULL }
+};
+
+/* ------------------------------------------------------------------------- */
+
+int file_system_init_resources(void)
+{
+    return resources_register(resources);
+}
+
+void file_system_init(void)
+{
+    serial_t *p;
+    vdrive_t *vdrive;
+    int i;
+
+    for (i = 0; i < 4; i++) {
+        p = serial_get_device(i + 8);
+        p->info = xmalloc(sizeof(vdrive_t));
+        vdrive = (vdrive_t *)p->info;
+        vdrive->image = NULL;
+        vdrive_setup_device(vdrive, i + 8, ((file_system_device_enabled[0] 
+                            ? DT_FS : DT_DISK)));
+        p->image = xmalloc(sizeof(disk_image_t));    
+        p->image->name = NULL;
+    }
+}
+
+char *file_system_get_disk_name(int unit)
+{
+    serial_t *p;
+    p = serial_get_device(unit);
+
+    return p->image->name;
+}
+
+static int set_file_system_device8(resource_value_t v)
+{
+    serial_t *p;
+    vdrive_t *floppy;
+
+    file_system_device_enabled[0] = (int) v;
+
+    p = serial_get_device(8);
+    floppy = (vdrive_t *)p->info;
+    if (floppy != NULL) {
+	if (floppy->image == NULL) {
+	    p->inuse = 0;
+	    vdrive_setup_device(floppy, 8, (file_system_device_enabled[0]
+                            ? DT_FS : DT_DISK));
+	}
+    }
+    return 0;
+}
+
+static int set_file_system_device9(resource_value_t v)
+{
+    serial_t *p;
+    vdrive_t *floppy;
+
+    file_system_device_enabled[1] = (int) v;
+
+    p = serial_get_device(9);
+    floppy = (vdrive_t *)p->info;
+    if (floppy != NULL) {
+	if (floppy->image == NULL) {
+	    p->inuse = 0;
+	    vdrive_setup_device(floppy, 9, (file_system_device_enabled[1]
+                                ? DT_FS : DT_DISK));
+	}
+    }
+    return 0;
+}
+
+static int set_file_system_device10(resource_value_t v)
+{
+    serial_t *p;
+    vdrive_t *floppy;
+
+    file_system_device_enabled[2] = (int) v;
+
+    p = serial_get_device(10);
+    floppy = (vdrive_t *)p->info;
+    if (floppy != NULL) {
+	if (floppy->image == NULL) {
+	    p->inuse = 0;
+	    vdrive_setup_device(floppy, 10, (file_system_device_enabled[2]
+                                 ? DT_FS : DT_DISK));
+	}
+    }
+    return 0;
+}
+
+static int set_file_system_device11(resource_value_t v)
+{
+    serial_t *p;
+    vdrive_t *floppy;
+
+    file_system_device_enabled[3] = (int) v;
+
+    p = serial_get_device(11);
+    floppy = (vdrive_t *)p->info;
+    if (floppy != NULL) {
+	if (floppy->image == NULL) {
+	    p->inuse = 0;
+	    vdrive_setup_device(floppy, 11, (file_system_device_enabled[3]
+                                 ? DT_FS : DT_DISK));
+	}
+    }
+    return 0;
+}
+
+/* ------------------------------------------------------------------------- */
+
+int attach_disk_image(disk_image_t *image, vdrive_t *floppy,
+                      const char *filename, int unit)
+{
+    disk_image_t new_image;
+
+    if (!filename) {
+        log_error(LOG_ERR, "No name, cannot attach floppy image");
+        return -1;
+    }
+
+    new_image.name = stralloc(filename);
+
+    if (disk_image_open(&new_image) < 0) {
+        free(new_image.name);
+        log_error(LOG_ERR, "Cannot open file `%s'", filename);
+        return -1;
+    }
+
+    if (image->name != NULL) {
+        switch (unit) {
+          case 8:
+            wd1770_detach_image(image, 8);
+            fdc_detach_image(image, 8);
+            drive_detach_image(image, 8);
+            vdrive_detach_image(image, 8, floppy);
+            break;
+          case 9:
+            wd1770_detach_image(image, 9);
+            fdc_detach_image(image, 9);
+            drive_detach_image(image, 9);
+            vdrive_detach_image(image, 9, floppy);
+            break;
+          case 10:
+            vdrive_detach_image(image, 10, floppy);
+            break;
+          case 11:
+            vdrive_detach_image(image, 11, floppy);
+            break;
+        }
+        disk_image_close(image);
+    }
+
+    memcpy(image, &new_image, sizeof(disk_image_t));
+
+    switch (unit) {
+      case 8:
+        vdrive_attach_image(image, 8, floppy);
+        drive_attach_image(image, 8);
+        fdc_attach_image(image, 8);
+        wd1770_attach_image(image, 8);
+        break;
+      case 9:
+        vdrive_attach_image(image, 9, floppy);
+        drive_attach_image(image, 9);
+        fdc_attach_image(image, 9);
+        wd1770_attach_image(image, 9);
+        break;
+      case 10:
+        vdrive_attach_image(image, 10, floppy);
+        break;
+      case 11:
+        vdrive_attach_image(image, 11, floppy);
+        break;
+    }
+    return 0;
+}
+
+void detach_disk_image(disk_image_t *image, vdrive_t *floppy, int unit)
+{
+    if (image->name != NULL) {
+        switch (unit) {
+          case 8:
+            wd1770_detach_image(image, 8);
+            fdc_detach_image(image, 8);
+            drive_detach_image(image, 8);
+            vdrive_detach_image(image, 8, floppy);
+            break;
+          case 9:
+            wd1770_detach_image(image, 9);
+            fdc_detach_image(image, 9);
+            drive_detach_image(image, 9);
+            vdrive_detach_image(image, 9, floppy);
+            break;
+          case 10:
+            vdrive_detach_image(image, 10, floppy);
+            break;
+          case 11:
+            vdrive_detach_image(image, 11, floppy);
+            break;
+        }
+        disk_image_close(image);
+    }
+}
+
+/* ------------------------------------------------------------------------- */
+
+int file_system_attach_disk(int unit, const char *filename)
+{
+    serial_t *p;
+    vdrive_t *floppy;
+
+    p = serial_get_device(unit);
+    floppy = (vdrive_t *)p->info;
+    if ((floppy == NULL) || ((floppy->type & DT_FS) == DT_FS)) {
+        p->inuse = 0;
+        vdrive_setup_device(floppy, unit, DT_DISK);
+    }
+
+    if (attach_disk_image(p->image, (vdrive_t *)p->info, filename, unit) < 0) {
+        file_system_detach_disk(unit);
+        return -1;
+    } else {
+        flip_set_current(unit, filename);
+        ui_display_drive_current_image(unit - 8, filename);
+        return 0;
+    }
+    return 0;
+}
+
+void file_system_detach_disk(int unit)
+{
+    int i;
+    serial_t *p;
+    
+    if (unit < 0) {
+        p = serial_get_device(8);
+        detach_disk_image(p->image, (vdrive_t *)p->info, 8);
+        set_file_system_device8((resource_value_t)
+                                file_system_device_enabled[0]);
+        p = serial_get_device(9);
+        detach_disk_image(p->image, (vdrive_t *)p->info, 9);
+        set_file_system_device9((resource_value_t)
+                                file_system_device_enabled[1]);
+        p = serial_get_device(10);
+        detach_disk_image(p->image, (vdrive_t *)p->info, 10);
+        set_file_system_device10((resource_value_t)
+                                 file_system_device_enabled[2]);
+        p = serial_get_device(11);
+        detach_disk_image(p->image, (vdrive_t *)p->info, 11);
+        set_file_system_device11((resource_value_t)
+                                 file_system_device_enabled[3]);
+
+        /* XXX Fixme: Hardcoded 2 drives here! */
+        for (i = 8; i <= 9; i++)
+            ui_display_drive_current_image(i - 8, "");
+	    
+    } else {
+        p = serial_get_device(unit);
+        detach_disk_image(p->image, (vdrive_t *)p->info, unit);
+        switch(unit) {
+          case 8:
+            set_file_system_device8((resource_value_t)
+                                    file_system_device_enabled[0]);
+            ui_display_drive_current_image(0, "");
+            break;
+          case 9:
+            set_file_system_device9((resource_value_t)
+                                    file_system_device_enabled[1]);
+            ui_display_drive_current_image(1, "");
+            break;
+          case 10:
+            set_file_system_device10((resource_value_t)
+                                     file_system_device_enabled[2]);
+            break;
+          case 11:
+            set_file_system_device11((resource_value_t)
+                                     file_system_device_enabled[3]);
+            break;
+        }
+    }
+}
+
+/* ------------------------------------------------------------------------- */
+
+#define SNAP_MAJOR 1
+#define SNAP_MINOR 0
+
+int vdrive_write_snapshot_module(snapshot_t *s, int start)
+{
+    int i;
+    char snap_module_name[14];
+    snapshot_module_t *m;
+    serial_t *p;
+    vdrive_t *floppy;
+
+    for (i = start; i <= 11; i++) {
+
+        p = serial_get_device(i);
+        floppy = (vdrive_t *)p->info;
+        if (floppy->image != NULL) {
+            sprintf(snap_module_name, "VDRIVEIMAGE%i", i);
+            m = snapshot_module_create(s, snap_module_name, SNAP_MAJOR,
+                                       SNAP_MINOR);
+            if (m == NULL)
+                return -1;
+
+            if (snapshot_module_write_byte_array(m,
+                                                 (BYTE *)floppy->ActiveName,
+                                                 sizeof(floppy->ActiveName)) < 0) {
+                if (m != NULL)
+                    snapshot_module_close(m);
+                return -1;
+            }
+            snapshot_module_close(m);
+        }
+    }
+    return 0;
+}
+
+int vdrive_read_snapshot_module(snapshot_t *s, int start)
+{
+    BYTE major_version, minor_version;
+    int i;
+    snapshot_module_t *m;
+    char snap_module_name[14];
+
+    for (i = start; i <= 11; i++) {
+
+        sprintf(snap_module_name, "VDRIVEIMAGE%i", i);
+        m = snapshot_module_open(s, snap_module_name,
+                                 &major_version, &minor_version);
+        if (m == NULL)
+            return 0;
+
+        if (major_version > SNAP_MAJOR || minor_version > SNAP_MINOR) {
+            log_message(LOG_ERR,
+                        "Snapshot module version (%d.%d) newer than %d.%d.",
+                        major_version, minor_version, SNAP_MAJOR, SNAP_MINOR);
+        }
+        snapshot_module_close(m);
+    }
+    return 0;
+}
+
---
+++ tags/v1.0/v1.0.33/vice/src/attach.h
@@ -0,0 +1,44 @@
+
+/*
+ * attach.h - file system attach management
+ *
+ * Written by
+ *  Andreas Boose <boose@linux.rz.fh-hannover.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _ATTACH_H
+#define _ATTACH_H
+
+#include "types.h"
+#include "snapshot.h"
+
+extern int file_system_init_resources(void);
+extern void file_system_init(void);
+extern char *file_system_get_disk_name(int unit);
+extern int file_system_attach_disk(int unit, const char *filename);
+extern void file_system_detach_disk(int unit);
+
+extern int vdrive_write_snapshot_module(snapshot_t *s, int start);
+extern int vdrive_read_snapshot_module(snapshot_t *s, int start);
+
+#endif
+
