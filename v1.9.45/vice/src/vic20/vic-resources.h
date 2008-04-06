36d2c74ab081d950c90b00f9acd855c2d4a0bb7a
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 11:49:55 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.9.45'.

---
+++ v1.9.45/vice/src/vic20/vic-resources.h
@@ -0,0 +1,51 @@
+/*
+ * vic-resources.h - Resource handling for the VIC-I emulation.
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Andreas Boose <viceteam@t-online.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _VIC_RESOURCES_H
+#define _VIC_RESOURCES_H
+
+#include "vice.h"
+
+struct vic_resources_s
+{
+#ifdef USE_XF86_EXTENSIONS
+    /* Flag: Do we use double size?  */
+    int fullscreen_double_size_enabled;
+
+    /* Flag: Do we copy lines in double size mode?  */
+    int fullscreen_double_scan_enabled;
+#endif
+};
+typedef struct vic_resources_s vic_resources_t;
+
+extern vic_resources_t vic_resources;
+
+
+extern int vic_resources_init(void);
+
+#endif
+
