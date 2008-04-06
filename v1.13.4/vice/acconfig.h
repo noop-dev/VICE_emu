4583ecb0dc4fb93a5c7cb8f3df93a0c941839326
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 14:38:22 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.13.4'.

---
+++ v1.13.4/vice/acconfig.h
@@ -0,0 +1,22 @@
+/* The number of bytes in an unsigned long.  */
+#undef SIZEOF_UNSIGNED_LONG
+
+/* The number of bytes in an unsigned int.  */
+#undef SIZEOF_UNSIGNED_INT
+
+/* The number of bytes in an unsigned short.  */
+#undef SIZEOF_UNSIGNED_SHORT
+
+/* Where do we want to install the executable?  */
+#undef PREFIX
+
+/* gettext stuff */
+#undef ENABLE_NLS
+#undef HAVE_CATGETS
+#undef HAVE_GETTEXT
+#undef HAVE_LC_MESSAGES
+#undef HAVE_STPCPY
+#undef HAVE_LIBINTL_H
+#undef DATADIRNAME
+#undef NLS_LOCALEDIR
+
