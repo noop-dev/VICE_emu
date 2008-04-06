5a203b3d71286e9e345dbaaa25ffd3265b4011cc
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 11:19:20 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.9.11'.

---
+++ v1.9.11/vice/src/arch/unix/x11/vidmode.h
@@ -0,0 +1,41 @@
+/*
+ * vidmode.h
+ *
+ * Written by
+ *  Martin Pottendorfer <pottendo@utanet.at>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef __vidmode_h__
+#define __vidmode_h__
+
+#include <X11/Xlib.h>
+#include "resources.h"
+
+extern int vm_selected_videomode;
+extern int vm_is_enabled;
+
+int vidmode_init(Display *display, int screen);
+int vidmode_set_bestmode(resource_value_t v, void *p);
+int vidmode_set_mode(resource_value_t v, void *p);
+void vidmode_create_menus(void);
+
+#endif /* __vidmode_h__ */
