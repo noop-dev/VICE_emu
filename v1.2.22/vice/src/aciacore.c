decec564afb2dda1a3e4868253926faa12012812
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 09:00:56 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.2.22'.

---
+++ v1.2.22/vice/src/aciacore.c
@@ -0,0 +1,478 @@
+/*
+ * acia-tmpl.c - Template file for ACIA 6551 emulation.
+ *
+ * Written by
+ *  André Fachat <fachat@physik.tu-chemnitz.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "acia.h"
+#include "alarm.h"
+#include "clkguard.h"
+#include "cmdline.h"
+#include "interrupt.h"
+#include "log.h"
+#include "machine.h"
+#include "resources.h"
+#include "rs232.h"
+#include "snapshot.h"
+#include "types.h"
+
+#undef	DEBUG
+
+static alarm_t acia_alarm;
+
+static int acia_ticks = 21111;	/* number of clock ticks per char */
+static int fd = -1;
+static int intx = 0;	/* indicates that a transmit is currently ongoing */
+static int irq = 0;
+static BYTE cmd;
+static BYTE ctrl;
+static BYTE rxdata;	/* data that has been received last */
+static BYTE txdata;	/* data prepared to send */
+static BYTE status;
+static int alarm_active = 0;	/* if alarm is set or not */
+
+static log_t acia_log = LOG_ERR;
+
+static int int_acia(CLOCK offset);
+
+static BYTE acia_last_read = 0;  /* the byte read the last time (for RMW) */
+    
+/******************************************************************/
+
+/* rs232.h replacement functions if no rs232 device available */
+
+#ifndef HAVE_RS232
+
+int rs232_open(int device)
+{
+    return -1;
+}
+
+void rs232_close(int fd)
+{
+}
+
+int rs232_putc(int fd, BYTE b)
+{
+    return -1;
+}
+
+int rs232_getc(int fd, BYTE *b)
+{
+    return -1;
+}
+
+#endif
+
+/******************************************************************/
+
+static CLOCK acia_alarm_clk = 0;
+
+static int acia_device;
+static int acia_irq;
+static int acia_irq_res;
+
+static int acia_set_device(resource_value_t v) {
+
+    if (fd >= 0) {
+	log_error(acia_log,
+                  "Device open, change effective only after close!");
+    }
+    acia_device = (int) v;
+    return 0;
+}
+
+static int acia_set_irq(resource_value_t v) {
+    int new_irq_res = (int)v;
+    int new_irq;
+    static const int irq_tab[] = { IK_NONE, IK_IRQ, IK_NMI };
+
+    if (new_irq_res < 0 || new_irq_res > 2) return -1;
+
+    new_irq = irq_tab[new_irq_res];
+
+    if (acia_irq != new_irq) {
+	mycpu_set_int(I_MYACIA, IK_NONE);
+	if (irq) {
+	    mycpu_set_int(I_MYACIA, new_irq);
+	}
+    }
+    acia_irq = new_irq;
+    acia_irq_res = new_irq_res;
+
+    return 0;
+}
+
+static resource_t resources[] = {
+    { MYACIA "Dev", RES_INTEGER, (resource_value_t) MyDevice,
+      (resource_value_t *) & acia_device, acia_set_device },
+    { MYACIA "Irq", RES_INTEGER, (resource_value_t) MyIrq,
+      (resource_value_t *) & acia_irq_res, acia_set_irq },
+    { NULL }
+};
+
+int myacia_init_resources(void) {
+    return resources_register(resources);
+}
+
+static cmdline_option_t cmdline_options[] = {
+    { "-myaciadev", SET_RESOURCE, 1, NULL, NULL, MYACIA "Dev", NULL,
+	"<0-3>", "Specify RS232 device this ACIA should work on" },
+    { NULL }
+};
+
+int myacia_init_cmdline_options(void) {
+    return cmdline_register_options(cmdline_options);
+}
+
+/******************************************************************/
+
+/* note: the first value is bogus. It should be 16*external clock. */
+static double acia_baud_table[16] = {
+	10, 50, 75, 109.92, 134.58, 150, 300, 600, 1200, 1800,
+	2400, 3600, 4800, 7200, 9600, 19200
+};
+
+/******************************************************************/
+
+static void clk_overflow_callback(CLOCK sub, void *var)
+{
+    if(alarm_active)
+        acia_alarm_clk -= sub;
+}
+
+void myacia_init(void) {
+    alarm_init(&acia_alarm, &mycpu_alarm_context, MYACIA, int_acia);
+
+    clk_guard_add_callback(&mycpu_clk_guard, clk_overflow_callback, NULL);
+
+    if (acia_log == LOG_ERR)
+        acia_log = log_open(MYACIA);
+}
+
+void myacia_reset(void) {
+
+#ifdef DEBUG
+	log_message(acia_log, "reset_myacia");
+#endif
+
+	cmd = 0;
+	ctrl = 0;
+
+        acia_ticks = (int)(machine_get_cycles_per_second() 
+				/ acia_baud_table[ctrl & 0xf]);
+
+	status = 0x10;
+	intx = 0;
+
+	if (fd >= 0) rs232_close(fd);
+	fd = -1;
+
+	alarm_unset(&acia_alarm);
+	alarm_active = 0;
+
+	mycpu_set_int(I_MYACIA, 0);
+	irq = 0;
+}
+
+/* -------------------------------------------------------------------------- */
+
+/* The dump format has a module header and the data generated by the
+ * chip...
+ *
+ * The version of this dump description is 0/0
+ */
+
+#define ACIA_DUMP_VER_MAJOR      1
+#define ACIA_DUMP_VER_MINOR      0
+
+/*
+ * The dump data:
+ *
+ * UBYTE	TDR	Transmit Data Register
+ * UBYTE	RDR	Receiver Data Register
+ * UBYTE	SR	Status Register (includes state of IRQ line)
+ * UBYTE	CMD	Command Register
+ * UBYTE	CTRL	Control Register
+ *
+ * UBYTE	INTX	0 = no data to tx; 2 = TDR valid; 1 = in transmit
+ *
+ * DWORD	TICKS	ticks till the next TDR empty interrupt
+ */
+
+static const char module_name[] = MYACIA;
+
+/* FIXME!!!  Error check.  */
+/* FIXME!!!  If no connection, emulate carrier lost or so */
+int myacia_write_snapshot_module(snapshot_t * p)
+{
+    snapshot_module_t *m;
+
+    m = snapshot_module_create(p, module_name,
+                               (BYTE)ACIA_DUMP_VER_MAJOR, (BYTE)ACIA_DUMP_VER_MINOR);
+    if (m == NULL)
+        return -1;
+
+    snapshot_module_write_byte(m, txdata);
+    snapshot_module_write_byte(m, rxdata);
+    snapshot_module_write_byte(m, (BYTE)(status | (irq?0x80:0)));
+    snapshot_module_write_byte(m, cmd);
+    snapshot_module_write_byte(m, ctrl);
+    snapshot_module_write_byte(m, intx);
+
+    if(alarm_active) {
+        snapshot_module_write_dword(m, (acia_alarm_clk - myclk));
+    } else {
+        snapshot_module_write_dword(m, 0);
+    }
+
+    snapshot_module_close(m);
+
+    return 0;
+}
+
+int myacia_read_snapshot_module(snapshot_t * p)
+{
+    BYTE vmajor, vminor;
+    BYTE byte;
+    DWORD dword;
+    snapshot_module_t *m;
+
+    alarm_unset(&acia_alarm);   /* just in case we don't find module */
+    alarm_active = 0;
+
+    mycpu_set_int_noclk(I_MYACIA, 0);
+
+    m = snapshot_module_open(p, module_name, &vmajor, &vminor);
+    if (m == NULL)
+        return -1;
+
+    if (vmajor != ACIA_DUMP_VER_MAJOR) {
+        snapshot_module_close(m);
+        return -1;
+    }
+
+    snapshot_module_read_byte(m, &txdata);
+    snapshot_module_read_byte(m, &rxdata);
+
+    irq = 0;
+    snapshot_module_read_byte(m, &status);
+    if(status & 0x80) {
+	status &= 0x7f;
+	irq = 1;
+	mycpu_set_int_noclk(I_MYACIA, acia_irq);
+    } else {
+	mycpu_set_int_noclk(I_MYACIA, 0);
+    }
+
+    snapshot_module_read_byte(m, &cmd);
+    if((cmd & 1) && (fd < 0)) {
+        fd = rs232_open(acia_device);
+    } else
+        if((fd >= 0) && !(cmd&1)) {
+        rs232_close(fd);
+        fd = -1;
+    }
+
+    snapshot_module_read_byte(m, &ctrl);
+    acia_ticks = (int)(machine_get_cycles_per_second() 
+                                / acia_baud_table[ctrl & 0xf]);
+
+    snapshot_module_read_byte(m, &byte);
+    intx = byte;
+
+    snapshot_module_read_dword(m, &dword);
+    if (dword) {
+	acia_alarm_clk = myclk + dword;
+        alarm_set(&acia_alarm, acia_alarm_clk);
+        alarm_active = 1;
+    } else {
+        alarm_unset(&acia_alarm);
+        alarm_active = 0;
+    }
+
+    if (snapshot_module_close(m) < 0)
+        return -1;
+
+    return 0;
+}
+
+
+void REGPARM2 myacia_store(ADDRESS a, BYTE b) {
+
+#ifdef DEBUG
+	log_message(acia_log, "store_myacia(%04x,%02x)",a,b);
+#endif
+
+        if (mycpu_rmw_flag) {
+            myclk --;
+            mycpu_rmw_flag = 0;
+            myacia_store(a, acia_last_read);
+            myclk ++;
+        }
+
+	switch(a & 3) {
+	case ACIA_DR:
+		txdata = b;
+		if(cmd&1) {
+		  if(!intx) {
+	            acia_alarm_clk = myclk + 1;
+		    alarm_set(&acia_alarm, acia_alarm_clk);
+                    alarm_active = 1;
+		    intx = 2;
+		  } else
+		  if(intx==1) {
+		    intx++;
+		  }
+		  status &= 0xef;		/* clr TDRE */
+		}
+		break;
+	case ACIA_SR:
+		if(fd >= 0) rs232_close(fd);
+		fd = -1;
+		status &= ~4;
+		cmd &= 0xe0;
+		intx = 0;
+		mycpu_set_int(I_MYACIA, 0);
+		irq = 0;
+		alarm_unset(&acia_alarm);
+                alarm_active = 0;
+		break;
+	case ACIA_CTRL:
+		ctrl = b;
+                acia_ticks = (int)(machine_get_cycles_per_second() 
+                                / acia_baud_table[ctrl & 0xf]);
+		break;
+	case ACIA_CMD:
+		cmd = b;
+		if((cmd & 1) && (fd < 0)) {
+		  fd = rs232_open(acia_device);
+	          acia_alarm_clk = myclk + acia_ticks;
+		  alarm_set(&acia_alarm, acia_alarm_clk);
+                  alarm_active = 1;
+		} else
+		if((fd >= 0) && !(cmd&1)) {
+		  rs232_close(fd);
+		  alarm_unset(&acia_alarm);
+                  alarm_active = 0;
+		  fd = -1;
+		}
+		break;
+	}
+}
+
+BYTE REGPARM1 myacia_read(ADDRESS a) {
+#if 0 /* def DEBUG */
+	BYTE myacia_read_(ADDRESS);
+	BYTE b = myacia_read_(a);
+	static ADDRESS lasta = 0;
+	static BYTE lastb = 0;
+
+	if((a!=lasta) || (b!=lastb)) {
+	  log_message(acia_log, "read_myacia(%04x) -> %02x",a,b);
+	}
+	lasta = a; lastb = b;
+	return b;
+}
+BYTE myacia_read_(ADDRESS a) {
+#endif
+
+	switch(a & 3) {
+	case ACIA_DR:
+		status &= ~8;
+		acia_last_read = rxdata;
+		return rxdata;
+	case ACIA_SR:
+		{
+		  BYTE c = status | (irq?0x80:0);
+		  mycpu_set_int(I_MYACIA, 0);
+		  irq = 0;
+		  acia_last_read = c;
+		  return c;
+		}
+	case ACIA_CTRL:
+		acia_last_read = ctrl;
+		return ctrl;
+	case ACIA_CMD:
+		acia_last_read = cmd;
+		return cmd;
+	}
+	/* should never happen */
+	return 0;
+}
+
+BYTE myacia_peek(ADDRESS a) {
+
+	switch(a & 3) {
+	case ACIA_DR:
+		return rxdata;
+	case ACIA_SR:
+		{
+		  BYTE c = status | (irq?0x80:0);
+		  return c;
+		}
+	case ACIA_CTRL:
+		return ctrl;
+	case ACIA_CMD:
+		return cmd;
+	}
+	return 0;
+}
+
+static int int_acia(CLOCK offset) 
+{
+	int rxirq;
+
+#if 0 /* def DEBUG */
+	log_message(acia_log, "int_acia(offset=%ld, myclk=%d", offset, myclk);
+#endif
+	if((intx == 2) && (fd >= 0)) rs232_putc(fd,txdata);
+	if(intx) intx--;
+
+	rxirq = 0;
+        if( (fd >= 0) && (!(status&8)) && rs232_getc(fd, &rxdata)) {
+          status |= 8;
+	  rxirq = 1;
+        }
+
+	if ( (rxirq && (!(cmd & 0x02))) || ((cmd & 0x0c) == 0x04) ) {
+	    mycpu_set_int(I_MYACIA, acia_irq);
+	    irq = 1;
+	}
+
+	if(!(status&0x10)) {
+	  status |= 0x10;
+	}
+
+	acia_alarm_clk = myclk + acia_ticks;
+	alarm_set(&acia_alarm, acia_alarm_clk);
+        alarm_active = 1;
+
+	return 0;
+}
+
+
---
+++ v1.2.22/vice/src/vic20/vic.c
@@ -0,0 +1,374 @@
+/*
+ * vic.c - A line-based VIC-I emulation (under construction).
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *
+ * 16/24bpp support added by
+ *  Steven Tieu <stieu@physics.ubc.ca>
+ *  Teemu Rantanen <tvr@cs.hut.fi>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+/* Warning: this emulation is very incomplete and buggy.  */
+
+#include "vice.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "clkguard.h"
+#include "log.h"
+#include "mem.h"
+#include "machine.h"
+#include "maincpu.h"
+#include "palette.h"
+#include "raster-modes.h"
+#include "snapshot.h"
+#include "types.h"
+#include "utils.h"
+#include "vic.h"
+#include "vic-cmdline-options.h"
+#include "vic-draw.h"
+#include "vic-mem.h"
+#include "vic-resources.h"
+#include "vic-snapshot.h"
+#include "vsync.h"
+
+
+
+vic_t vic;
+
+
+
+static int raster_draw_alarm_handler (CLOCK offset);
+static void vic_exposure_handler (unsigned int width, unsigned int height);
+
+
+
+static void 
+vic_exposure_handler (unsigned int width, unsigned int height)
+{
+  raster_resize_viewport (&vic.raster, width, height);
+
+  /* FIXME: Needed?  Maybe this should be triggered by
+     `raster_resize_viewport()' automatically.  */
+  raster_force_repaint (&vic.raster);
+}
+
+/* Notice: The screen origin X register has a 4-pixel granularity, so our
+   write accesses are always aligned. */
+
+static int 
+raster_draw_alarm_handler (CLOCK offset)
+{
+  int in_visible_area;
+
+  alarm_set (&vic.raster_draw_alarm, clk + VIC_CYCLES_PER_LINE - offset);
+
+  in_visible_area = (vic.raster.current_line >= vic.raster.display_ystart
+                     && vic.raster.current_line <= vic.raster.display_ystop);
+
+  raster_emulate_line (&vic.raster);
+
+  if (in_visible_area)
+    {
+      /* FIXME: I don't think this is exact emulation.  */
+      if (vic.raster.ycounter == vic.char_height - 1)
+        {
+          vic.raster.ycounter = 0;
+          vic.memptr += vic.text_cols;
+        }
+      else
+        vic.raster.ycounter++;
+    }
+
+  if (vic.raster.current_line == 0)
+    {
+      raster_skip_frame (&vic.raster, do_vsync (vic.raster.skip_frame));
+      vic.raster.blank_enabled = 1;
+      vic.raster.ycounter = 0;
+      vic.memptr = 0;
+    }
+
+  return 0;
+}
+
+
+
+static int
+init_raster (void)
+{
+  raster_t *raster;
+  unsigned int width, height;
+  char *title;
+
+  raster = &vic.raster;
+
+  raster_init (raster, VIC_NUM_VMODES, VIC_NUM_SPRITES);
+  raster_modes_set_idle_mode (raster->modes, VIC_IDLE_MODE);
+  raster_set_exposure_handler (raster, vic_exposure_handler);
+  raster_enable_cache (raster, vic_resources.video_cache_enabled);
+  raster_enable_double_scan (raster, vic_resources.double_scan_enabled);
+  raster_set_canvas_refresh(raster, 1);
+
+  raster_set_geometry (raster,
+                       VIC_SCREEN_WIDTH, VIC_SCREEN_HEIGHT,
+                       VIC_SCREEN_WIDTH, 1,
+                       0, 0,
+                       0, 4 * 8, /* Border shift.  */
+                       1,
+                       VIC_FIRST_DISPLAYED_LINE,
+                       VIC_LAST_DISPLAYED_LINE,
+                       0);
+
+  width = VIC_SCREEN_WIDTH * VIC_PIXEL_WIDTH;
+  height = VIC_LAST_DISPLAYED_LINE - VIC_FIRST_DISPLAYED_LINE + 1;
+  if (vic_resources.double_size_enabled)
+    {
+      width *= 2;
+      height *= 2;
+      raster_set_pixel_size (raster, VIC_PIXEL_WIDTH * 2, 2);
+    }
+  else
+    raster_set_pixel_size (raster, VIC_PIXEL_WIDTH, 1);
+
+  raster_resize_viewport (raster, width, height);
+
+  if (vic_load_palette (vic_resources.palette_file_name) < 0) {
+    log_error (vic.log, "Cannot load palette.");
+    return -1;
+  }
+
+  title = concat ("VICE: ", machine_name, " emulator", NULL);
+  raster_set_title (raster, title);
+  free (title);
+
+  raster_realize (raster);
+
+  raster->display_ystart = VIC_FIRST_DISPLAYED_LINE;
+  raster->display_ystop = VIC_FIRST_DISPLAYED_LINE + 1;
+  raster->display_xstart = 0;
+  raster->display_xstop = 1; 
+
+  return 0;
+}
+
+/* Initialization. */
+canvas_t 
+vic_init (void)
+{
+  vic.log = log_open ("VIC");
+
+  alarm_init (&vic.raster_draw_alarm, &maincpu_alarm_context,
+              "VicIRasterDraw", raster_draw_alarm_handler);
+
+  if (init_raster() < 0)
+    return NULL;
+
+  vic.color_ptr = ram;
+  vic.screen_ptr = ram;
+  vic.chargen_ptr = chargen_rom + 0x400;
+
+  /* FIXME */
+  vic.char_height = 8;
+  vic.text_cols = 22;
+  vic.text_lines = 23;
+
+  vic_reset ();
+
+  vic_draw_init ();
+  vic_draw_set_double_size (vic_resources.double_size_enabled);
+
+  vic_update_memory_ptrs ();
+
+  vic.initialized = 1;
+
+  if (clk_guard_get_clk_base (&maincpu_clk_guard) == 0)
+    clk_guard_set_clk_base (&maincpu_clk_guard, VIC20_PAL_CYCLES_PER_RFSH);
+  else
+    /* Safety measure.  */
+    log_error (vic.log, "Trying to override clk base!?  Code is broken.");
+
+  return vic.raster.viewport.canvas;
+}
+
+/* Reset the VIC-I chip. */
+void 
+vic_reset (void)
+{
+  raster_reset (&vic.raster);
+  alarm_set (&vic.raster_draw_alarm, VIC_CYCLES_PER_LINE);
+
+  vic.memptr = 0;
+}
+
+
+
+/* WARNING: This does not change the resource value.  External modules are
+   expected to set the resource value to change the VIC-II palette instead of
+   calling this function directly.  */
+int
+vic_load_palette (const char *name)
+{
+  static const char *color_names[] =
+    {
+      "Black", "White", "Red", "Cyan", "Purple", "Green", "Blue",
+      "Yellow", "Orange", "Light Orange", "Pink", "Light Cyan",
+      "Light Purple", "Light Green", "Light Blue", "Light Yellow"
+    };
+  palette_t *palette;
+
+  palette = palette_create (VIC_NUM_COLORS, color_names);
+  if (palette == NULL)
+    return -1;
+
+  if (palette_load (name, palette) < 0)
+    {
+      log_message (vic.log, "Cannot load palette file `%s'.", name);
+      return -1;
+    }
+
+  raster_set_palette (&vic.raster, palette);
+  return 0;
+}
+
+
+
+/* This hook is called whenever the screen parameters (eg. window size) are
+   changed.  */
+void 
+vic_resize (void)
+{
+  if (! vic.initialized)
+    return;
+
+  if (vic_resources.double_size_enabled)
+    {
+      if (vic.raster.viewport.pixel_size.width == VIC_PIXEL_WIDTH
+          && vic.raster.viewport.canvas != NULL) {
+          raster_set_pixel_size (&vic.raster, VIC_PIXEL_WIDTH * 2, 2);
+        raster_resize_viewport (&vic.raster,
+                                vic.raster.viewport.width * 2,
+                                vic.raster.viewport.height * 2);
+      } else {
+          raster_set_pixel_size (&vic.raster, VIC_PIXEL_WIDTH * 2, 2);
+      }
+
+      vic_draw_set_double_size (1);
+    }
+  else
+    {
+      if (vic.raster.viewport.pixel_size.width == VIC_PIXEL_WIDTH * 2
+          && vic.raster.viewport.canvas != NULL) {
+          raster_set_pixel_size (&vic.raster, VIC_PIXEL_WIDTH, 1);
+        raster_resize_viewport (&vic.raster,
+                                vic.raster.viewport.width / 2,
+                                vic.raster.viewport.height / 2);
+      } else {
+          raster_set_pixel_size (&vic.raster, VIC_PIXEL_WIDTH, 1);
+      }
+
+      vic_draw_set_double_size (0);
+    }
+}
+
+
+
+/* Set the memory pointers according to the values stored in the VIC
+   registers. */
+void 
+vic_update_memory_ptrs (void)
+{
+  ADDRESS char_addr;
+  int tmp;
+
+  tmp = vic.regs[0x5] & 0xf;
+  char_addr = (tmp & 0x8) ? 0x0000 : 0x8000;
+  char_addr += (tmp & 0x7) * 0x400;
+
+  if (char_addr >= 0x8000 && char_addr < 0x9000)
+    {
+      vic.chargen_ptr = chargen_rom + 0x400 + (char_addr & 0xfff);
+      VIC_DEBUG_REGISTER (("Character memory at $%04X "
+                           "(character ROM + $%04X).",
+                           char_addr,
+                           char_addr & 0xfff));
+    }
+  else
+    {
+      if (char_addr == 0x1c00)
+        vic.chargen_ptr = chargen_rom;    /* handle wraparound */
+      else
+        vic.chargen_ptr = ram + char_addr;
+      VIC_DEBUG_REGISTER (("Character memory at $%04X.", char_addr));
+    }
+
+  vic.color_ptr = ram + 0x9400 + (vic.regs[0x2] & 0x80 ? 0x200 : 0x0);
+  vic.screen_ptr = ram + (((vic.regs[0x2] & 0x80) << 2)
+                          | ((vic.regs[0x5] & 0x70) << 6));
+
+  VIC_DEBUG_REGISTER (("Color memory at $%04X.", vic.color_ptr - ram));
+  VIC_DEBUG_REGISTER (("Screen memory at $%04X.", vic.screen_ptr - ram));
+}
+
+
+
+int
+vic_init_resources (void)
+{
+  return vic_resources_init ();
+}
+
+int
+vic_init_cmdline_options (void)
+{
+  return vic_cmdline_options_init ();
+}
+
+
+
+int
+vic_write_snapshot_module (snapshot_t *s)
+{
+  return vic_snapshot_write_module (s);
+}
+
+int
+vic_read_snapshot_module (snapshot_t *s)
+{
+  return vic_snapshot_read_module (s);
+}
+
+
+
+/* FIXME: Just a dummy.  */
+void 
+video_setfullscreen (int v, int width, int height)
+{
+}
+
+/* Free the allocated frame buffer.  FIXME: Not incapsulated.  */
+void 
+video_free (void)
+{
+  video_frame_buffer_free (&vic.raster.frame_buffer);
+}
