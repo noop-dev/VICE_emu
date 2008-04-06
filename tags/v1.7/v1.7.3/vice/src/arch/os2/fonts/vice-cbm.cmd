2624f5e87a2fb2bfb382ae7419ea66e798fabb75
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 10:02:30 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.7.3'.

---
+++ tags/v1.7/v1.7.3/vice/src/arch/os2/fonts/vice-cbm.cmd
@@ -0,0 +1,29 @@
+@Echo Off
+
+REM /*                        */
+REM /* Set Variables          */
+REM /*                        */
+
+call e:\toolkit\tksetenv
+
+REM /*                        */
+REM /* Convert Unix Font File */
+REM /*                        */
+chargen2fnt ..\..\..\..\data\C64\chargen   C64
+chargen2fnt ..\..\..\..\data\C128\chargen  C128
+chargen2fnt ..\..\..\..\data\PET\chargen   PET
+chargen2fnt ..\..\..\..\data\VIC20\chargen VIC20
+
+REM /*                        */
+REM /* Create font-dll        */
+REM /*                        */
+alp -Mb vice2.asm
+link386 vice2,,,,vice2.def /NOLOGO
+rc -n vice2.rc vice2.dll
+
+REM /*                        */
+REM /* 'Rename' font-dll      */
+REM /*                        */
+move vice2.dll vice2.fon > NUL
+
+del chargen*.fnt vice-*.fnt *.res *.obj *.map > NUL
