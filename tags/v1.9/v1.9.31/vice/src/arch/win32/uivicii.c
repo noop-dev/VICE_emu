0ebaa568920325675807efe620e6a2ed7af5c251
trikalio <trikalio@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-04-06 11:36:57 +0000

    This commit was manufactured by cvs2svn to create tag 'v1.9.31'.

---
+++ tags/v1.9/v1.9.31/vice/src/arch/win32/uivicii.c
@@ -0,0 +1,117 @@
+/*
+ * uivicii.c - Implementation of VIC-II settings dialog box.
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Tibor Biczo <crown@mail.matav.hu>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <windows.h>
+#include <prsht.h>
+
+#ifdef HAVE_SHLOBJ_H
+#include <shlobj.h>
+#endif
+
+#ifndef DUMMYUNIONNAME
+#define DUMMYUNIONNAME  u1
+#endif
+
+#include "res.h"
+#include "resources.h"
+#include "ui.h"
+#include "uilib.h"
+#include "utils.h"
+#include "winmain.h"
+
+static void enable_controls_for_vicii_settings(HWND hwnd, int type)
+{
+}
+
+static void init_dialog(HWND hwnd)
+{
+    int n;
+
+    resources_get_value("CheckSsColl", (resource_value_t *) &n);
+    CheckDlgButton(hwnd, IDC_TOGGLE_VICII_SSC, n ? BST_CHECKED : BST_UNCHECKED);
+
+    resources_get_value("CheckSbColl", (resource_value_t *) &n);
+    CheckDlgButton(hwnd, IDC_TOGGLE_VICII_SBC, n ? BST_CHECKED : BST_UNCHECKED);
+
+    resources_get_value("NewLuminances", (resource_value_t *) &n);
+    CheckDlgButton(hwnd, IDC_TOGGLE_VICII_NEWLUM, n ? BST_CHECKED : BST_UNCHECKED);
+}
+
+static BOOL CALLBACK dialog_proc(HWND hwnd, UINT msg,
+                                        WPARAM wparam, LPARAM lparam)
+{
+    int type;
+
+    switch (msg) {
+        case WM_CLOSE:
+            EndDialog(hwnd,0);
+            return TRUE;
+        case WM_INITDIALOG:
+            init_dialog(hwnd);
+            return TRUE;
+        case WM_COMMAND:
+            type = LOWORD(wparam);
+            switch (type) {
+                case IDC_TOGGLE_VICII_SSC:
+                    break;
+                case IDC_TOGGLE_VICII_SBC:
+                    break;
+                case IDC_TOGGLE_VICII_NEWLUM:
+                    break;
+                case IDOK:
+                    resources_set_value("CheckSsColl", (resource_value_t)
+                        (IsDlgButtonChecked
+                            (hwnd,IDC_TOGGLE_VICII_SSC)==BST_CHECKED ?
+                            1 : 0 ));
+
+                    resources_set_value("CheckSbColl", (resource_value_t)
+                        (IsDlgButtonChecked
+                            (hwnd,IDC_TOGGLE_VICII_SBC)==BST_CHECKED ?
+                            1 : 0 ));
+
+                    resources_set_value("NewLuminances", (resource_value_t)
+                        (IsDlgButtonChecked
+                            (hwnd,IDC_TOGGLE_VICII_NEWLUM)==BST_CHECKED ?
+                            1 : 0 ));
+
+                case IDCANCEL:
+                    EndDialog(hwnd,0);
+                    return TRUE;
+            }
+            return TRUE;
+    }
+    return FALSE;
+}
+
+void ui_vicii_settings_dialog(HWND hwnd)
+{
+    DialogBox(winmain_instance,(LPCTSTR)IDD_VICII_DIALOG,hwnd,dialog_proc);
+}
+
