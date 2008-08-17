205bcd921e11cc90006e014c82b2b3638a40898d
nojoopa <nojoopa@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-08-17 21:37:14 +0000

    SDL port improvements

--- v2.0.1-sdl/vice/src/arch/sdl/kbd.h
+++ v2.0.1-sdl/vice/src/arch/sdl/kbd.h
@@ -27,33 +27,35 @@
 #ifndef _KBD_H
 #define _KBD_H
 
+#include <SDL/SDL.h>
+
 extern void kbd_arch_init(void);
 
 extern signed long kbd_arch_keyname_to_keynum(char *keyname);
 extern const char *kbd_arch_keynum_to_keyname(signed long keynum);
 extern void kbd_initialize_numpad_joykeys(int *joykeys);
 
-#define KBD_C64_SYM_US  "x11_sym.vkm"
-#define KBD_C64_SYM_DE  "x11_sym.vkm"
-#define KBD_C64_POS     "x11_pos.vkm"
-#define KBD_C128_SYM    "x11_sym.vkm"
-#define KBD_C128_POS    "x11_pos.vkm"
-#define KBD_VIC20_SYM   "x11_sym.vkm"
-#define KBD_VIC20_POS   "x11_pos.vkm"
-#define KBD_PET_SYM_UK  "x11_buks.vkm"
-#define KBD_PET_POS_UK  "x11_bukp.vkm"
-#define KBD_PET_SYM_DE  "x11_bdes.vkm"
-#define KBD_PET_POS_DE  "x11_bdep.vkm"
-#define KBD_PET_SYM_GR  "x11_bgrs.vkm"
-#define KBD_PET_POS_GR  "x11_bgrp.vkm"
-#define KBD_PLUS4_SYM   "x11_sym.vkm"
-#define KBD_PLUS4_POS   "x11_pos.vkm"
-#define KBD_CBM2_SYM_UK "x11_buks.vkm"
-#define KBD_CBM2_POS_UK "x11_bukp.vkm"
-#define KBD_CBM2_SYM_DE "x11_bdes.vkm"
-#define KBD_CBM2_POS_DE "x11_bdep.vkm"
-#define KBD_CBM2_SYM_GR "x11_bgrs.vkm"
-#define KBD_CBM2_POS_GR "x11_bgrp.vkm"
+#define KBD_C64_SYM_US  "sdl_sym.vkm"
+#define KBD_C64_SYM_DE  "sdl_sym.vkm"
+#define KBD_C64_POS     "sdl_pos.vkm"
+#define KBD_C128_SYM    "sdl_sym.vkm"
+#define KBD_C128_POS    "sdl_pos.vkm"
+#define KBD_VIC20_SYM   "sdl_sym.vkm"
+#define KBD_VIC20_POS   "sdl_pos.vkm"
+#define KBD_PET_SYM_UK  "sdl_buks.vkm"
+#define KBD_PET_POS_UK  "sdl_bukp.vkm"
+#define KBD_PET_SYM_DE  "sdl_bdes.vkm"
+#define KBD_PET_POS_DE  "sdl_bdep.vkm"
+#define KBD_PET_SYM_GR  "sdl_bgrs.vkm"
+#define KBD_PET_POS_GR  "sdl_bgrp.vkm"
+#define KBD_PLUS4_SYM   "sdl_sym.vkm"
+#define KBD_PLUS4_POS   "sdl_pos.vkm"
+#define KBD_CBM2_SYM_UK "sdl_buks.vkm"
+#define KBD_CBM2_POS_UK "sdl_bukp.vkm"
+#define KBD_CBM2_SYM_DE "sdl_bdes.vkm"
+#define KBD_CBM2_POS_DE "sdl_bdep.vkm"
+#define KBD_CBM2_SYM_GR "sdl_bgrs.vkm"
+#define KBD_CBM2_POS_GR "sdl_bgrp.vkm"
 
 #define KBD_INDEX_C64_DEFAULT   KBD_INDEX_C64_SYM
 #define KBD_INDEX_C128_DEFAULT  KBD_INDEX_C128_SYM
@@ -62,8 +64,8 @@ extern void kbd_initialize_numpad_joykeys(int *joykeys);
 #define KBD_INDEX_PLUS4_DEFAULT KBD_INDEX_PLUS4_SYM
 #define KBD_INDEX_CBM2_DEFAULT  KBD_INDEX_CBM2_BUKS
 
-extern void sdlkbd_press(signed long key);
-extern void sdlkbd_release(signed long key);
+extern void sdlkbd_press(SDLKey key, SDLMod mod);
+extern void sdlkbd_release(SDLKey key, SDLMod mod);
 extern void kbd_enter_leave(void);
 extern void kbd_focus_change(void);
 
--- v2.0.1-sdl/vice/src/arch/sdl/signals.c
+++ v2.0.1-sdl/vice/src/arch/sdl/signals.c
@@ -26,71 +26,21 @@
 
 #include "vice.h"
 
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#ifdef OPENSERVER6_COMPILE
-#include <sys/signal.h>
-#endif
-
-#include "log.h"
-#include "monitor.h"
 #include "signals.h"
-
-#ifdef OPENSERVER5_COMPILE
-static RETSIGTYPE ignore64(int sig)
-{
-}
-#endif
-
-static RETSIGTYPE break64(int sig)
-{
-#ifdef SYS_SIGLIST_DECLARED
-    log_message(LOG_DEFAULT, _("Received signal %d (%s)."),
-                sig, sys_siglist[sig]);
-#else
-    log_message(LOG_DEFAULT, _("Received signal %d."), sig);
-#endif
-
-    exit (-1);
-}
 
 void signals_init(int do_core_dumps)
 {
-    signal(SIGINT, break64);
-    signal(SIGTERM, break64);
-
-#ifdef OPENSERVER5_COMPILE
-    signal(SIGALRM, ignore64);
-#endif
-
-    if (!do_core_dumps) {
-        signal(SIGSEGV,  break64);
-        signal(SIGILL,   break64);
-        signal(SIGPIPE,  break64);
-        signal(SIGHUP,   break64);
-        signal(SIGQUIT,  break64);
-    }
 }
-
-typedef void (*signal_handler_t)(int);
-
-static signal_handler_t old_handler;
 
 static void handle_abort(int signo)
 {
-    monitor_abort();
-    signal(SIGINT, (signal_handler_t)handle_abort);
 }
 
 void signals_abort_set(void)
 {
-    old_handler = signal(SIGINT, handle_abort);
 }
 
 void signals_abort_unset(void)
 {
-    signal(SIGINT, old_handler);
 }
 
