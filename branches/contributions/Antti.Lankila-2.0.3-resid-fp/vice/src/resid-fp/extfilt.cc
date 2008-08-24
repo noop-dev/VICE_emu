3e36efdb080924ee5b3fd76ecb70a21b119ff51e
blackystardust <blackystardust@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-08-24 19:54:32 +0000

    Duplicated the resid directory and applied the new resid engine patch to it.

---
+++ branches/contributions/Antti.Lankila-2.0.3-resid-fp/vice/src/resid-fp/extfilt.cc
@@ -0,0 +1,94 @@
+//  ---------------------------------------------------------------------------
+//  This file is part of reSID, a MOS6581 SID emulator engine.
+//  Copyright (C) 2004  Dag Lem <resid@nimrod.no>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//  ---------------------------------------------------------------------------
+
+#define __EXTFILT_CC__
+#include "extfilt.h"
+
+// ----------------------------------------------------------------------------
+// Constructor.
+// ----------------------------------------------------------------------------
+ExternalFilter::ExternalFilter()
+{
+  reset();
+  enable_filter(true);
+  set_chip_model(MOS6581);
+  set_clock_frequency(1e6f);
+  set_sampling_parameter(15915.6f);
+}
+
+
+// ----------------------------------------------------------------------------
+// Enable filter.
+// ----------------------------------------------------------------------------
+void ExternalFilter::enable_filter(bool enable)
+{
+  enabled = enable;
+}
+
+// ----------------------------------------------------------------------------
+// Setup of the external filter sampling parameters.
+// ----------------------------------------------------------------------------
+void ExternalFilter::set_clock_frequency(float clock)
+{
+  clock_frequency = clock;
+  _set_sampling_parameter();
+}
+
+void ExternalFilter::set_sampling_parameter(float freq)
+{
+  pass_frequency = freq;
+  _set_sampling_parameter();
+}
+
+void ExternalFilter::_set_sampling_parameter()
+{
+  // Low-pass:  R = 10kOhm, C = 1000pF; w0l = 1/RC = 1/(1e4*1e-9) = 100000
+  // High-pass: R =  1kOhm, C =   10uF; w0h = 1/RC = 1/(1e3*1e-5) =    100
+  w0hp = 100.f / clock_frequency;
+  w0lp = pass_frequency * 2.f * M_PI / clock_frequency;
+}
+
+// ----------------------------------------------------------------------------
+// Set chip model.
+// ----------------------------------------------------------------------------
+void ExternalFilter::set_chip_model(chip_model model)
+{
+  if (model == MOS6581) {
+    // Approximate the DC output level to be removed if the external
+    // filter is turned off. (0x800 - wave_zero + voice DC) * maxenv * voices
+    //  - extin offset...
+    mixer_DC = (-0x600 + 0x800) * 0xff * 3 - 0x20000;
+  }
+  else {
+    // No DC offsets in the MOS8580.
+    mixer_DC = 0;
+  }
+}
+
+
+// ----------------------------------------------------------------------------
+// SID reset.
+// ----------------------------------------------------------------------------
+void ExternalFilter::reset()
+{
+  // State of filter.
+  Vlp = 0;
+  Vhp = 0;
+  Vo = 0;
+}
