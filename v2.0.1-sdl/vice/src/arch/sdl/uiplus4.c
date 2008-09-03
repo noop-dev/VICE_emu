9ae6d6e9593a8610dca8edbc73184d86f8678a0f
blackystardust <blackystardust@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-09-03 20:55:15 +0000

    extended sdl ui to support xplus4.

--- v2.0.1-sdl/vice/src/arch/sdl/uiplus4.c
+++ v2.0.1-sdl/vice/src/arch/sdl/uiplus4.c
@@ -28,14 +28,62 @@
 #include "vice.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "plus4memrom.h"
+#include "resources.h"
 #include "ui.h"
+#include "uimenu.h"
+
+UI_MENU_DEFINE_TOGGLE(CrtcDoubleSize)
+
+static UI_MENU_CALLBACK(quit_callback)
+{
+    exit(0);
+    return 0;
+}
+
+static ui_menu_entry_t xplus4_main_menu[] = {
+    { "Attach disk",
+      MENU_ENTRY_SUBMENU,
+      NULL, /* disk_attach_dialog */
+      NULL,
+      NULL },
+    { "Doublesize",
+      MENU_ENTRY_RESOURCE_TOGGLE,
+      toggle_CrtcDoubleSize_callback,
+      NULL,
+      NULL },
+    { "-", MENU_ENTRY_SEPARATOR, NULL, NULL, NULL },
+    { "Quit",
+      MENU_ENTRY_OTHER,
+      quit_callback,
+      NULL,
+      NULL },
+    { NULL }
+};
+
+BYTE plus4_font[8*256];
 
 int plus4ui_init(void)
 {
+    int i, j;
+
 fprintf(stderr,"%s\n",__func__);
 
     sdl_register_vcachename("TEDVideoCache");
+    sdl_ui_set_main_menu(xplus4_main_menu);
+
+    for (i=0; i<128; i++)
+    {
+        for (j=0; j<8; j++)
+        {
+            plus4_font[(i*8)+j]=plus4memrom_kernal_rom[(i*8)+(128*8)+j+0x1000];
+            plus4_font[(i*8)+(128*8)+j]=plus4memrom_kernal_rom[(i*8)+j+0x1000];
+        }
+    }
+
+    sdl_ui_set_menu_font(plus4_font, NULL, 0, 8, 8);
     return 0;
 }
 
