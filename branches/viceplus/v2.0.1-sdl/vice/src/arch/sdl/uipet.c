3c90a732e1b3f115b6111db58f1bd2a474b71daa
blackystardust <blackystardust@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2008-09-03 20:07:33 +0000

    extended sdl ui to support xpet.

--- branches/viceplus/v2.0.1-sdl/vice/src/arch/sdl/uipet.c
+++ branches/viceplus/v2.0.1-sdl/vice/src/arch/sdl/uipet.c
@@ -28,13 +28,60 @@
 #include "vice.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "petmem.h"
+#include "resources.h"
 #include "ui.h"
+#include "uimenu.h"
+
+UI_MENU_DEFINE_TOGGLE(CrtcDoubleSize)
+
+static UI_MENU_CALLBACK(quit_callback)
+{
+    exit(0);
+    return 0;
+}
+
+static ui_menu_entry_t xpet_main_menu[] = {
+    { "Attach disk",
+      MENU_ENTRY_SUBMENU,
+      NULL, /* disk_attach_dialog */
+      NULL,
+      NULL },
+    { "Doublesize",
+      MENU_ENTRY_RESOURCE_TOGGLE,
+      toggle_CrtcDoubleSize_callback,
+      NULL,
+      NULL },
+    { "-", MENU_ENTRY_SEPARATOR, NULL, NULL, NULL },
+    { "Quit",
+      MENU_ENTRY_OTHER,
+      quit_callback,
+      NULL,
+      NULL },
+    { NULL }
+};
+
+BYTE pet_font[14*256];
 
 int petui_init(void)
 {
+    int i, j;
+
 fprintf(stderr,"%s\n",__func__);
 
+    sdl_ui_set_main_menu(xpet_main_menu);
+
+    for (i=0; i<128; i++)
+    {
+        for (j=0; j<8; j++)
+        {
+            pet_font[(i*8)+j]=mem_chargen_rom[(i*16)+(256*16)+j];
+            pet_font[(i*8)+(128*8)+j]=mem_chargen_rom[(i*16)+j];
+        }
+    }
+    sdl_ui_set_menu_font(pet_font, NULL, 0, 8, 8);
     sdl_register_vcachename("CrtcVideoCache");
     return 0;
 }
