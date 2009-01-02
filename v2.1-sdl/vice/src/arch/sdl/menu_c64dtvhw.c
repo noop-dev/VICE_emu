3fc5dd9246ca4bbd4618cf78ba7e5d32fd53b73e
nojoopa <nojoopa@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2009-01-02 17:19:45 +0000

    SDL port improvements

--- v2.1-sdl/vice/src/arch/sdl/menu_c64dtvhw.c
+++ v2.1-sdl/vice/src/arch/sdl/menu_c64dtvhw.c
@@ -27,12 +27,56 @@
 #include "vice.h"
 #include "types.h"
 
+#include "c64dtv-resources.h"
 #include "lib.h"
 #include "menu_common.h"
+#include "menu_joystick.h"
 #include "menu_sid.h"
 #include "resources.h"
 #include "uifilereq.h"
 #include "uimenu.h"
+
+UI_MENU_DEFINE_RADIO(HummerUserportDevice)
+UI_MENU_DEFINE_RADIO(HummerUserportJoyPort)
+UI_MENU_DEFINE_TOGGLE(ps2mouse)
+UI_MENU_DEFINE_TOGGLE(Mouse)
+
+static const ui_menu_entry_t c64dtv_userport_menu[] = {
+    SDL_MENU_ITEM_TITLE("Hummer userport device"),
+    { "None",
+      MENU_ENTRY_RESOURCE_RADIO,
+      radio_HummerUserportDevice_callback,
+      (ui_callback_data_t)HUMMER_USERPORT_NONE },
+    { "ADC",
+      MENU_ENTRY_RESOURCE_RADIO,
+      radio_HummerUserportDevice_callback,
+      (ui_callback_data_t)HUMMER_USERPORT_ADC },
+    { "Joystick",
+      MENU_ENTRY_RESOURCE_RADIO,
+      radio_HummerUserportDevice_callback,
+      (ui_callback_data_t)HUMMER_USERPORT_JOY },
+    SDL_MENU_ITEM_SEPARATOR,
+    SDL_MENU_ITEM_TITLE("Hummer joystick port mapped to userport"),
+    { "Joy1",
+      MENU_ENTRY_RESOURCE_RADIO,
+      radio_HummerUserportJoyPort_callback,
+      (ui_callback_data_t)1 },
+    { "Joy2",
+      MENU_ENTRY_RESOURCE_RADIO,
+      radio_HummerUserportJoyPort_callback,
+      (ui_callback_data_t)2 },
+    SDL_MENU_ITEM_SEPARATOR,
+    SDL_MENU_ITEM_TITLE("PS/2 mouse on userport"),
+    { "Enable PS/2 mouse",
+      MENU_ENTRY_RESOURCE_TOGGLE,
+      toggle_ps2mouse_callback,
+      NULL },
+    { "Grab mouse events",
+      MENU_ENTRY_RESOURCE_TOGGLE,
+      toggle_Mouse_callback,
+      NULL },
+    { NULL }
+};
 
 static UI_MENU_CALLBACK(c64dtvromimage_callback)
 {
@@ -51,12 +95,12 @@ static UI_MENU_CALLBACK(c64dtvromimage_callback)
 UI_MENU_DEFINE_TOGGLE(c64dtvromrw)
 UI_MENU_DEFINE_TOGGLE(FlashTrueFS)
 UI_MENU_DEFINE_RADIO(DtvRevision)
-UI_MENU_DEFINE_RADIO(HummerUserportDevice)
-UI_MENU_DEFINE_RADIO(HummerUserportJoyPort)
-UI_MENU_DEFINE_TOGGLE(ps2mouse)
-UI_MENU_DEFINE_TOGGLE(Mouse)
 
 const ui_menu_entry_t c64dtv_hardware_menu[] = {
+    { "Joystick settings",
+      MENU_ENTRY_SUBMENU,
+      submenu_callback,
+      (ui_callback_data_t)joystick_menu },
     { "SID settings",
       MENU_ENTRY_SUBMENU,
       submenu_callback,
@@ -86,38 +130,9 @@ const ui_menu_entry_t c64dtv_hardware_menu[] = {
       radio_DtvRevision_callback,
       (ui_callback_data_t)3 },
     SDL_MENU_ITEM_SEPARATOR,
-    SDL_MENU_ITEM_TITLE("Hummer userport device"),
-    { "None",
-      MENU_ENTRY_RESOURCE_RADIO,
-      radio_HummerUserportDevice_callback,
-      (ui_callback_data_t)0 },
-    { "ADC",
-      MENU_ENTRY_RESOURCE_RADIO,
-      radio_HummerUserportDevice_callback,
-      (ui_callback_data_t)1 },
-    { "Joystick",
-      MENU_ENTRY_RESOURCE_RADIO,
-      radio_HummerUserportDevice_callback,
-      (ui_callback_data_t)2 },
-    SDL_MENU_ITEM_SEPARATOR,
-    SDL_MENU_ITEM_TITLE("Hummer joystick port mapped to userport"),
-    { "Joy1",
-      MENU_ENTRY_RESOURCE_RADIO,
-      radio_HummerUserportJoyPort_callback,
-      (ui_callback_data_t)1 },
-    { "Joy2",
-      MENU_ENTRY_RESOURCE_RADIO,
-      radio_HummerUserportJoyPort_callback,
-      (ui_callback_data_t)2 },
-    SDL_MENU_ITEM_SEPARATOR,
-    SDL_MENU_ITEM_TITLE("PS/2 mouse on userport"),
-    { "Enable PS/2 mouse",
-      MENU_ENTRY_RESOURCE_TOGGLE,
-      toggle_ps2mouse_callback,
-      NULL },
-    { "Grab mouse events",
-      MENU_ENTRY_RESOURCE_TOGGLE,
-      toggle_Mouse_callback,
-      NULL },
+    { "Userport settings",
+      MENU_ENTRY_SUBMENU,
+      submenu_callback,
+      (ui_callback_data_t)c64dtv_userport_menu },
     { NULL }
 };
