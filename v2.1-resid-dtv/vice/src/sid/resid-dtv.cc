3c54b5265bdeca72e37312c7c6d867f430e5bdc8
nojoopa <nojoopa@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2009-01-09 15:16:04 +0000

    Forked resid-dtv/ from resid/

---
+++ v2.1-resid-dtv/vice/src/sid/resid-dtv.cc
@@ -0,0 +1,244 @@
+/*
+ * resid-dtv.cc - reSID-DTV interface code.
+ *
+ * Written by
+ *  Teemu Rantanen <tvr@cs.hut.fi>
+ *  Dag Lem <resid@nimrod.no>
+ *  Andreas Boose <viceteam@t-online.de>
+ * C64 DTV modifications written by
+ *  Daniel Kahlin <daniel@kahlin.net>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+/* resid itself is always compiled with C64DTV support */
+#include "resid-dtv/sid.h"
+
+extern "C" {
+
+/* QNX has problems with const and inline definitions
+   in its string.h file when using g++ */
+
+#ifndef __QNX__
+#include <string.h>
+#endif
+
+#include "sid/sid.h" /* sid_engine_t */
+#include "lib.h"
+#include "log.h"
+#include "resid.h"
+#include "resources.h"
+#include "sid-snapshot.h"
+#include "types.h"
+
+struct sound_s
+{
+    /* resid sid implementation */
+    SIDDTV *sid;
+};
+
+typedef struct sound_s sound_t;
+
+static sound_t *residdtv_open(BYTE *sidstate)
+{
+    sound_t *psid;
+    int	i;
+
+    psid = new sound_t;
+    psid->sid = new SIDDTV;
+
+    for (i = 0x00; i <= 0x18; i++) {
+        psid->sid->write(i, sidstate[i]);
+    }
+
+    return psid;
+}
+
+static int residdtv_init(sound_t *psid, int speed, int cycles_per_sec)
+{
+    sampling_method method;
+    char method_text[100];
+    double passband, gain;
+    int filters_enabled, model, sampling, passband_percentage, gain_percentage;
+
+    if (resources_get_int("SidFilters", &filters_enabled) < 0)
+        return 0;
+
+    if (resources_get_int("SidModel", &model) < 0)
+        return 0;
+
+    if (resources_get_int("SidResidSampling", &sampling) < 0)
+        return 0;
+
+    if (resources_get_int("SidResidPassband", &passband_percentage) < 0)
+        return 0;
+
+    if (resources_get_int("SidResidGain", &gain_percentage) < 0)
+        return 0;
+
+    passband = speed * passband_percentage / 200.0;
+    gain = gain_percentage / 100.0;
+ 
+    psid->sid->set_chip_model(DTVSID);
+    psid->sid->input(0);
+
+    psid->sid->enable_filter(false);
+    psid->sid->enable_external_filter(filters_enabled ? true : false);
+
+    switch (sampling) {
+      default:
+      case 0:
+        method = SAMPLE_FAST;
+	strcpy(method_text, "fast");
+	break;
+      case 1:
+        method = SAMPLE_INTERPOLATE;
+	strcpy(method_text, "interpolating");
+	break;
+      case 2:
+        method = SAMPLE_RESAMPLE_INTERPOLATE;
+	sprintf(method_text, "resampling, pass to %dHz", (int)passband);
+	break;
+      case 3:
+        method = SAMPLE_RESAMPLE_FAST;
+	sprintf(method_text, "resampling, pass to %dHz", (int)passband);
+	break;
+    }
+
+    if (!psid->sid->set_sampling_parameters(cycles_per_sec, method,
+					   speed, passband, gain)) {
+        log_warning(LOG_DEFAULT,
+                    "reSID-DTV: Out of spec, increase sampling rate or decrease maximum speed");
+        return 0;
+    }
+
+    log_message(LOG_DEFAULT, "reSID-DTV: DTVSID, filter %s, sampling rate %dHz - %s",
+                filters_enabled ? "on" : "off",
+                speed, method_text);
+
+    return 1;
+}
+
+static void residdtv_close(sound_t *psid)
+{
+    delete psid->sid;
+    delete psid;
+}
+
+static BYTE residdtv_read(sound_t *psid, WORD addr)
+{
+    return psid->sid->read(addr);
+}
+
+static void residdtv_store(sound_t *psid, WORD addr, BYTE byte)
+{
+    psid->sid->write(addr, byte);
+}
+
+static void residdtv_reset(sound_t *psid, CLOCK cpu_clk)
+{
+    psid->sid->reset();
+}
+
+static int residdtv_calculate_samples(sound_t *psid, SWORD *pbuf, int nr,
+                                      int interleave, int *delta_t)
+{
+    return psid->sid->clock(*delta_t, pbuf, nr, interleave);
+}
+
+static void residdtv_prevent_clk_overflow(sound_t *psid, CLOCK sub)
+{
+}
+
+static char *residdtv_dump_state(sound_t *psid)
+{
+    return lib_stralloc("");
+}
+
+static void residdtv_state_read(sound_t *psid, sid_snapshot_state_t *sid_state)
+{
+    SIDDTV::State state;
+    unsigned int i;
+
+    state = psid->sid->read_state();
+
+    for (i = 0; i < 0x20; i++) {
+        sid_state->sid_register[i] = (BYTE)state.sid_register[i];
+    }
+
+    sid_state->bus_value = (BYTE)state.bus_value;
+    sid_state->bus_value_ttl = (DWORD)state.bus_value_ttl;
+    for (i = 0; i < 3; i++) {
+        sid_state->accumulator[i] = (DWORD)state.accumulator[i];
+        sid_state->shift_register[i] = (DWORD)state.shift_register[i];
+        sid_state->rate_counter[i] = (WORD)state.rate_counter[i];
+        sid_state->rate_counter_period[i] = (WORD)state.rate_counter_period[i];
+        sid_state->exponential_counter[i] = (WORD)state.exponential_counter[i];
+        sid_state->exponential_counter_period[i] = (WORD)state.exponential_counter_period[i];
+        sid_state->envelope_counter[i] = (BYTE)state.envelope_counter[i];
+        sid_state->envelope_state[i] = (BYTE)state.envelope_state[i];
+        sid_state->hold_zero[i] = (BYTE)state.hold_zero[i];
+    }
+}
+
+static void residdtv_state_write(sound_t *psid, sid_snapshot_state_t *sid_state)
+{
+    SIDDTV::State state;
+    unsigned int i;
+
+    for (i = 0; i < 0x20; i++) {
+        state.sid_register[i] = (char)sid_state->sid_register[i];
+    }
+
+    state.bus_value = (reg8)sid_state->bus_value;
+    state.bus_value_ttl = (cycle_count)sid_state->bus_value_ttl;
+    for (i = 0; i < 3; i++) {
+        state.accumulator[i] = (reg24)sid_state->accumulator[i];
+        state.shift_register[i] = (reg24)sid_state->shift_register[i];
+        state.rate_counter[i] = (reg16)sid_state->rate_counter[i];
+	if (sid_state->rate_counter_period[i])
+            state.rate_counter_period[i] = (reg16)sid_state->rate_counter_period[i];
+        state.exponential_counter[i] = (reg16)sid_state->exponential_counter[i];
+	if (sid_state->exponential_counter_period[i])
+            state.exponential_counter_period[i] = (reg16)sid_state->exponential_counter_period[i];
+        state.envelope_counter[i] = (reg8)sid_state->envelope_counter[i];
+        state.envelope_state[i] = (EnvelopeGeneratorDTV::State)sid_state->envelope_state[i];
+        state.hold_zero[i] = (sid_state->hold_zero[i] != 0);
+    }
+
+    psid->sid->write_state((const SIDDTV::State)state);
+}
+
+sid_engine_t residdtv_hooks =
+{
+    residdtv_open,
+    residdtv_init,
+    residdtv_close,
+    residdtv_read,
+    residdtv_store,
+    residdtv_reset,
+    residdtv_calculate_samples,
+    residdtv_prevent_clk_overflow,
+    residdtv_dump_state,
+    residdtv_state_read,
+    residdtv_state_write
+};
+
+} // extern "C"
