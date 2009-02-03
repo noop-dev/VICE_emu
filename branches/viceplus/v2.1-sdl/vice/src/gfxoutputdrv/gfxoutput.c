1bc4570ecde344ba298936de1ad9c9115db78a44
blackystardust <blackystardust@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2009-02-03 07:12:23 +0000

    Improved the SDL_main header/lib checking and added checking/support for dx sound under win32 sdl.

--- branches/viceplus/v2.1-sdl/vice/src/gfxoutputdrv/gfxoutput.c
+++ branches/viceplus/v2.1-sdl/vice/src/gfxoutputdrv/gfxoutput.c
@@ -42,7 +42,7 @@
 #include "pngdrv.h"
 #endif
 
-#if defined(HAVE_GIF) || defined(WIN32)
+#if defined(HAVE_GIF) || (defined(WIN32) && !defined(USE_SDLUI))
 #include "gifdrv.h"
 #endif
 
@@ -99,7 +99,7 @@ int gfxoutput_early_init(void)
     gfxoutputdrv_list->next = NULL;
 
     gfxoutput_init_bmp();
-#if defined(HAVE_GIF) || defined(WIN32)
+#if defined(HAVE_GIF) || (defined(WIN32) && !defined(USE_SDLUI))
     gfxoutput_init_gif();
 #endif
     gfxoutput_init_iff();
