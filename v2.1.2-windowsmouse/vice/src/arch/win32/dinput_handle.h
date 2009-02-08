8dfe421db3c7ab19d3883a4a6f8310d46308cf1e
fabbo <fabbo@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2009-02-08 13:00:16 +0000

    Fix to make DirectInput mouse work

--- v2.1.2-windowsmouse/vice/src/arch/win32/dinput_handle.h
+++ v2.1.2-windowsmouse/vice/src/arch/win32/dinput_handle.h
@@ -7,6 +7,11 @@
 #define DIRECTINPUT_VERSION     0x0500
 #include <dinput.h>
 
+/* not defined in DirectInput headers prior to 8 */
+#ifndef DIDFT_OPTIONAL
+#define DIDFT_OPTIONAL          0x80000000
+#endif
+
 LPDIRECTINPUT get_directinput_handle();
 #endif
 
