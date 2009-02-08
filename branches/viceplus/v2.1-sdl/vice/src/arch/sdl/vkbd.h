6231eb3d1f92d160171369e17c70e9e398e647b7
nojoopa <nojoopa@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2009-02-08 20:38:11 +0000

    SDL port improvements

--- branches/viceplus/v2.1-sdl/vice/src/arch/sdl/vkbd.h
+++ branches/viceplus/v2.1-sdl/vice/src/arch/sdl/vkbd.h
@@ -45,6 +45,7 @@ extern void sdl_vkbd_set_vkbd(const vkbd_t *machine_vkbd);
 
 extern vkbd_t vkbd_c64;
 extern vkbd_t vkbd_vic20;
+extern vkbd_t vkbd_c128;
 
 #define SDL_VKBD_ACTIVE  (1<<0)
 #define SDL_VKBD_REPAINT (1<<1)
