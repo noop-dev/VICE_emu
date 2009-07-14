7486d0ecab9727c2f43f8bb0ba665e6f1e4ab4bc
tlr <tlr@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2009-07-14 20:20:42 +0000

    Added smart attach to win32 menus.

--- branches/tlr/v2.1.14-xvic-cartridge/vice/src/arch/win32/vic20ui.c
+++ branches/tlr/v2.1.14-xvic-cartridge/vice/src/arch/win32/vic20ui.c
@@ -282,13 +282,14 @@ ui_menu_translation_table_t vic20ui_menu_translation_table[] = {
     { IDM_SIDCART_SETTINGS, IDS_MI_SIDCART_SETTINGS },
     { IDM_MIDI_SETTINGS, IDS_MI_MIDI_SETTINGS },
     { IDM_TOGGLE_OEM_JOY, IDS_MI_TOGGLE_OEM_JOY },
+    { IDM_CART_VIC20_GENERIC, IDS_MI_CART_VIC20_GENERIC },
+    { IDM_CART_VIC20_MEGACART, IDS_MI_CART_VIC20_MEGACART },
+    { IDM_CART_VIC20_SMART_ATTACH, IDS_MI_CART_VIC20_SMART_ATTACH },
     { IDM_CART_VIC20_8KB_2000, IDS_MI_CART_VIC20_8KB_2000 },
     { IDM_CART_VIC20_16KB_4000, IDS_MI_CART_VIC20_16KB_4000 },
     { IDM_CART_VIC20_8KB_6000, IDS_MI_CART_VIC20_8KB_6000 },
     { IDM_CART_VIC20_8KB_A000, IDS_MI_CART_VIC20_8KB_A000 },
     { IDM_CART_VIC20_4KB_B000, IDS_MI_CART_VIC20_4KB_B000 },
-    { IDM_CART_VIC20_GENERIC, IDS_MI_CART_VIC20_GENERIC },
-    { IDM_CART_VIC20_MEGACART, IDS_MI_CART_VIC20_MEGACART },
     { IDM_TOGGLE_CART_RESET, IDS_MI_TOGGLE_CART_RESET },
     { IDM_VIC_SETTINGS, IDS_MI_VIC_SETTINGS },
     { 0, 0 }
@@ -324,13 +325,6 @@ ui_popup_translation_table_t vic20ui_popup_translation_table[] = {
 static void vic20_ui_specific(WPARAM wparam, HWND hwnd)
 {
     switch (wparam) {
-      case IDM_CART_VIC20_8KB_2000:
-      case IDM_CART_VIC20_16KB_4000:
-      case IDM_CART_VIC20_8KB_6000:
-      case IDM_CART_VIC20_8KB_A000:
-      case IDM_CART_VIC20_4KB_B000:
-        uicart_attach(wparam, hwnd, vic20_ui_cartridges);
-        break;
       case IDM_CART_VIC20_GENERIC:
         uicart_attach_special(hwnd, translate_text(IDS_SELECT_GENERIC),
                               UILIB_FILTER_ALL, CARTRIDGE_VIC20_GENERIC);
@@ -338,6 +332,17 @@ static void vic20_ui_specific(WPARAM wparam, HWND hwnd)
       case IDM_CART_VIC20_MEGACART:
         uicart_attach_special(hwnd, translate_text(IDS_SELECT_MEGACART),
                               UILIB_FILTER_ALL, CARTRIDGE_VIC20_MEGACART);
+        break;
+      case IDM_CART_VIC20_SMART_ATTACH:
+        uicart_attach_special(hwnd, translate_text(IDS_SELECT_CARTRIDGE_IMAGE),
+                              UILIB_FILTER_ALL, CARTRIDGE_VIC20_DETECT);
+        break;
+      case IDM_CART_VIC20_8KB_2000:
+      case IDM_CART_VIC20_16KB_4000:
+      case IDM_CART_VIC20_8KB_6000:
+      case IDM_CART_VIC20_8KB_A000:
+      case IDM_CART_VIC20_4KB_B000:
+        uicart_attach(wparam, hwnd, vic20_ui_cartridges);
         break;
       case IDM_CART_SET_DEFAULT:
         cartridge_set_default();
