b00111dbf35bb5c3e293870e2fd9739cebc6e9bf
strik <strik@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2009-09-21 18:24:41 +0000

    Tagged version v2.1.17.

---
+++ tags/v2.1/v2.1.17/vice/build/macosx/build-vice-release.sh
@@ -0,0 +1,256 @@
+#!/bin/bash
+#
+# build snapshot binaries for x11 gtk and cocoa from the SVN 
+#
+# written by Christian Vogelgsang <chris@vogelgsang.org>
+#
+
+echo "--- build VICE release from SVN repository ---"
+
+# default parameter
+SNAPSHOT=0
+LINK_SRC=0
+ONLY_BIN=0
+DEL_TARGET=0
+EXTLIB="extlib"
+BUILD_DIR=""
+ARCH="ub"
+UI="sdl x11 gtk cocoa cocoa+10.5"
+DEBUG=0
+DIR_DIST=0
+
+usage() {
+  cat <<EOF
+
+  Usage:
+    $0 [options] <vice-svn-repository>
+
+  Options:                                                                 Default:
+    -s                       do snapshot build                             [release build]
+    -l                       link repository directory and use it directly [fresh svn export]
+    -b                       only binaries and no source package           [with source package]
+    -f                       force delete target directory if it exists    [abort if exists]
+    -e <path to extlib>      set path of external library directory        [extlib]
+    -o <target dir>          set target directory                          [BUILD-snapshot/release]
+    -a <arch>                set arch type, e.g. i386 ppc ub               [$ARCH]
+    -u <uis>                 set uis <ui>[+<sdk>]                          [$UI]
+    -d                       build debug version                           [release]
+    -i                       create distribution in directory              [create DMG]
+    -D                       quick debug preset: -slbfdi -a i386 -u cocoa+10.5
+EOF
+  exit 2
+}
+
+# parse arguments
+while getopts "slbfe:o:a:u:diD" i ; do
+  case "$i" in
+    s) SNAPSHOT=1;;
+    l) LINK_SRC=1;;
+    b) ONLY_BIN=1;;
+    f) DEL_TARGET=1;;
+    e) EXTLIB="$OPTARG";;
+    o) BUILD_DIR="$OPTARG";;
+    a) ARCH="$OPTARG";;
+    u) UI="$OPTARG";;
+    d) DEBUG=1;;
+    i) DIR_DIST=1;;
+    D) DEBUG=1
+       SNAPSHOT=1
+       LINK_SRC=1
+       ONLY_BIN=1
+       DEL_TARGET=1
+       DIR_DIST=1
+       UI="cocoa+10.5"
+       ARCH="i386";;
+    ?) usage;;
+  esac
+done
+shift $(($OPTIND-1))
+
+# mode
+if [ $SNAPSHOT = 1 ]; then
+  echo -n "mode:           snapshot"
+else
+  echo -n "mode:           RELASE"
+fi
+if [ $DEBUG = 1 ]; then
+  echo " (DEBUG)"
+else
+  echo
+fi
+
+# check repository directory
+SVN_REPO="$1"
+if [ "x$SVN_REPO" = "x" ]; then
+  echo "ERROR: no repository given!"
+  usage
+fi
+if [ ! -d "$SVN_REPO" ]; then
+  echo "ERROR: SVN repository '$SVN_REPO' not found!"
+  exit 1
+fi
+if [ ! -s "$SVN_REPO/.svn" ]; then
+  echo "ERROR: No SVN repository '$SVN_REPO'!"
+  exit 1
+fi
+if [ ! -x "$SVN_REPO/autogen.sh" ]; then
+  echo "ERROR: SVN repository '$SVN_REPO' contains no VICE!"
+  exit 1
+fi
+SVN_REPO="`(cd \"$SVN_REPO\" && pwd -P)`"
+echo "SVN repository: $SVN_REPO"
+
+# get revision of build
+REVISION=`(cd "$SVN_REPO" && svn info | grep Revision | awk '{ print $2 }')`
+echo "SVN revision:   $REVISION"
+
+# check extlib directory
+if [ ! -d "$EXTLIB" ]; then
+  echo "ERROR: external libs '$EXTLIB' not found!"
+  exit 1
+fi
+EXTLIB="`(cd \"$EXTLIB\" && pwd)`"
+echo "external libs:  $EXTLIB"
+
+# check target dir
+if [ "x$BUILD_DIR" = x ]; then
+  if [ $SNAPSHOT = 1 ]; then
+    BUILD_DIR=BUILD-snapshot
+  else
+    BUILD_DIR=BUILD-release
+  fi
+fi
+if [ -d "$BUILD_DIR" ]; then
+  if [ $DEL_TARGET = 0 ]; then
+    echo "ERROR: $BUILD_DIR already exists!"
+    exit 1
+  else
+    rm -rf "$BUILD_DIR"
+  fi
+fi
+BUILD_DIR="`pwd`/$BUILD_DIR"
+echo "build dir:      $BUILD_DIR"
+
+# create build src dir
+mkdir -p "$BUILD_DIR"
+if [ ! -d "$BUILD_DIR" ]; then
+  echo "ERROR: can't creat dir!"
+  exit 1
+fi
+
+SRC_DIR="$BUILD_DIR/src"
+if [ $LINK_SRC = 1 ]; then
+  # link existing source
+  echo "linking src:    $SRC_DIR"
+  ln -sf "$SVN_REPO" "$SRC_DIR"
+else
+  # export fresh source
+  echo "exporting src:  $SRC_DIR"
+  svn export "$SVN_REPO" "$SRC_DIR"
+  if [ $? != 0 ]; then
+    echo "ERROR: export faild!"
+    exit 1
+  fi
+
+  # patch BUILD version if doing snapshot
+  if [ $SNAPSHOT = 1 ]; then
+    # tag
+    DATE=`date '+%Y%m%d'`
+    TAG="-r${REVISION}_$DATE"
+
+    # patch VICE_VERSION_BUILD
+    echo "patching configure.in: $TAG"
+    PATCH_VVB="$SRC_DIR/configure.in"
+    perl -pi -e "s/(VICE_VERSION_BUILD=\d+).*\$/\$1$TAG/" $PATCH_VVB
+    if [ $? != 0 ]; then
+      echo "ERROR: patching..."
+      exit 1
+    fi
+  fi
+fi
+
+# configure snapshot
+echo "--- configuring source tree ---"
+(cd "$SRC_DIR" && ./autogen.sh)
+if [ $? != 0 ]; then
+  echo "ERROR: configuring VICE..."
+  exit 1
+fi
+
+# build dists
+if [ "$UI" != "none" ]; then
+  echo "--- binaries for $UI ---"
+  for dist in $UI ; do
+    
+    # extract ui and sdk
+    UI_TYPE="${dist%+*}"
+    if [ "$UI_TYPE" != "$dist" ]; then
+      SDK="${dist#*+}"
+    else
+      SDK="10.4"
+    fi
+        
+    # set output log
+    LOG="$BUILD_DIR/build-$dist.log"
+    
+    # set dist type
+    if [ $DIR_DIST = 1 ]; then
+      DIST_TYPE="dir"
+    else
+      DIST_TYPE="dmg"
+    fi
+    
+    # do build
+    echo "-- building binaries for $UI_TYPE $ARCH $SDK --"
+    (cd "$SRC_DIR" && $BASH build/macosx/build-vice-dist.sh $ARCH "$UI_TYPE" "$DIST_TYPE" "$EXTLIB" "$BUILD_DIR" "$SDK" $DEBUG) >"$LOG" 2>&1 
+
+    # check generated files
+    if [ $DIR_DIST = 1 ]; then
+      FILES="$(ls -d $BUILD_DIR/$UI_TYPE-$SDK/$ARCH/vice-macosx-* 2>/dev/null)"
+    else
+      FILES="$(ls $BUILD_DIR/$UI_TYPE-$SDK/$ARCH/vice-macosx-*.dmg 2>/dev/null)"
+    fi
+    echo "generated output: $FILES"
+    if [ "x$FILES" = "x" ]; then
+      echo "no file found!"
+      exit 1
+    fi
+
+    # show warnings
+    fgrep +++ "$LOG"
+    echo " -warnings begin-"
+    fgrep warning: "$LOG" | sort | uniq
+    echo " -warnings end-"
+
+    # size and move files
+    du -sh "$FILES"
+    mv "$FILES" "$BUILD_DIR"
+  done
+fi
+  
+# packing source
+if [ $ONLY_BIN = 0 ]; then
+  echo "--- source ---"
+  echo "packing source"
+  DEST_DIR="$BUILD_DIR/tarball"
+  mkdir "$DEST_DIR"
+  if [ ! -d "$DEST_DIR" ]; then
+    echo "ERROR: creating dir"
+    exit 1
+  fi
+  LOG="$BUILD_DIR/build-tarball.log"
+  (cd "$DEST_DIR" && $SRC_DIR/configure && make dist) >"$LOG" 2>&1
+  # move generated files to top level
+  FILES="$(ls $DEST_DIR/*.tar.gz 2>/dev/null)"
+  if [ "x$FILES" = "x" ]; then
+    echo "FAILED!"
+    tail -10 $LOG
+    exit 1
+  fi
+  echo "generated: $FILES"
+  du -sh "$FILES"
+  mv "$FILES" "$BUILD_DIR"
+fi
+
+echo "--- ready ---"
+exit 0
---
+++ tags/v2.1/v2.1.17/vice/src/arch/platform.h
@@ -0,0 +1,308 @@
+/*
+ * platform.h - port/platform specific discovery macros.
+ *
+ * Written by
+ *  Marco van den Heuvel <blackystardust68@yahoo.com>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef VICE_PLATFORM_H
+#define VICE_PLATFORM_H
+
+#include "vice.h"
+
+/* AIX discovery */
+
+#ifdef _AIX
+
+/* find out which compiler is being used */
+#ifdef __TOS_AIX__
+#  define PLATFORM_COMPILER "xLC"
+#else
+#  define PLATFORM COMPILER "GCC"
+#endif
+
+/* Get AIX version */
+#include "platform_aix_version.h"
+
+#endif /* AIX */
+
+
+/* AmigaOS 4.x discovery */
+#ifdef AMIGA_OS4
+#define PLATFORM_OS "AmigaOS 4.x"
+#define PLATFORM_COMPILER "GCC"
+#define PLATFORM_CPU "PPC"
+#endif
+
+
+/* AROS discovery */
+#ifdef AMIGA_AROS
+#define PLATFORM_OS "AROS"
+#define PLATFORM_COMPILER "GCC"
+#endif
+
+
+/* MorphOS discovery */
+#ifdef AMIGA_MORPHOS
+#define PLATFORM_OS "MorphOS"
+#define PLATFORM_COMPILER "GCC"
+#endif
+
+
+/* BeOS discovery */
+#ifdef __BEOS__
+
+#ifdef WORDS_BIGENDIAN
+#define PLATFORM_CPU "PPC"
+#define PLATFORM_COMPILER "MetroWerks"
+#else
+#define FIND_X86_CPU
+#define PLATFORM_COMPILER "GCC"
+#endif
+
+#define PLATFORM_OS "BeOS"
+
+#endif /* __BEOS__ */
+
+
+/* BSDI discovery */
+#ifdef __bsdi__
+#define PLATFORM_OS "BSDi"
+#define PLATFORM_COMPILER "GCC"
+#define FIND_X86_CPU
+#endif
+
+
+/* DragonFly BSD discovery */
+#ifdef __DragonFly__
+#define PLATFORM_OS "DragonFly BSD"
+#define PLATFORM_COMPILER "GCC"
+#define FIND_X86_CPU
+#endif
+
+
+/* FreeBSD discovery */
+#ifdef __FreeBSD__
+
+/* Get FreeBSD version */
+#include "platform_freebsd_version.h"
+
+#endif
+
+
+/* NetBSD discovery */
+#ifdef __NetBSD__
+
+/* Get NetBSD version */
+#include "platform_netbsd_version.h"
+
+#endif
+
+/* OpenBSD discovery */
+#ifdef __OpenBSD__
+
+/* Get OpenBSD version */
+#include "platform_openbsd_version.h"
+
+#endif
+
+/* QNX 4.x discovery */
+#if defined(__QNX__) && !defined(__QNXNTO__)
+#define PLATFORM_OS "QNX 4.x"
+#define PLATFORM_COMPILER "Watcom"
+#define FIND_X86_CPU
+#endif
+
+
+/* QNX 6.x discovery */
+#ifdef __QNXNTO__
+
+#define PLATFORM_COMPILER "GCC"
+
+/* Get QNX version */
+#include "platform_qnx6_version.h"
+
+#ifdef __arm__
+#define PLATFORM_CPU "ARMLE"
+#endif
+
+#ifdef __mips__
+#define PLATFORM_CPU "MIPSLE"
+#endif
+
+#ifdef __sh__
+#define PLATFORM_CPU "SHLE"
+#endif
+
+#if defined(__powerpc__) || defined(__ppc__)
+#define PLATFORM_CPU "PPCBE"
+#endif
+
+#ifndef PLATFORM_CPU
+#define FIND_X86_CPU
+#endif
+
+#endif
+
+
+/* HPUX discovery */
+#ifdef _hpux
+#define PLATFORM_OS "HPUX"
+#define PLATFORM_COMPILER "HP UPC"
+#endif
+
+#if defined(__hpux) && !defined(_hpux)
+#define PLATFORM_OS "HPUX"
+#define PLATFORM_COMPILER "GCC"
+#endif
+
+
+/* IRIX discovery */
+#ifdef __sgi
+#define PLATFORM_OS "IRIX"
+#define PLATFORM_COMPILER "GCC"
+#endif
+
+
+/* OpenServer 5.x discovery */
+#ifdef OPENSERVER5_COMPILE
+#define PLATFORM_OS "OpenServer 5.x"
+#define PLATFORM_COMPILER "GCC"
+#define FIND_X86_CPU
+#endif
+
+
+/* OpenServer 6.x discovery */
+#ifdef OPENSERVER6_COMPILE
+#define PLATFORM_OS "OpenServer 6.x"
+#define PLATFORM_COMPILER "GCC"
+#define FIND_X86_CPU
+#endif
+
+
+/* UnixWare 7.x discovery */
+#ifdef _UNIXWARE7
+#define PLATFORM_OS "UnixWare 7.x"
+#define PLATFORM_COMPILER "GCC"
+#define FIND_X86_CPU
+#endif
+
+
+/* SunOS and Solaris discovery */
+#if defined(sun) || defined(__sun)
+#  if defined(__SVR4) || defined(__svr4__)
+#    define PLATFORM_OS "Solaris"
+# else
+#    define PLATFORM_OS "SunOS"
+#  endif
+#endif
+
+/* UWIN discovery */
+#ifdef _UWIN
+#define PLATFORM_OS "UWIN"
+#define PLATFORM_COMPILER "GCC"
+#define FIND_X86_CPU
+#endif
+
+
+/* Generic cpu discovery */
+#include "platform_cpu_type.h"
+
+
+/* Generic m68k cpu discovery */
+#ifdef FIND_M68K_CPU
+
+#ifdef __mc68060__
+#define PLATFORM_CPU "68060"
+#endif
+
+#if !defined(PLATFORM_CPU) && defined(__mc68040__)
+#define PLATFORM_CPU "68040"
+#endif
+
+#if !defined(PLATFORM_CPU) && defined(__mc68030__)
+#define PLATFORM_CPU "68030"
+#endif
+
+#if !defined(PLATFORM_CPU) && defined(__mc68020__)
+#define PLATFORM_CPU "68020"
+#endif
+
+#if !defined(PLATFORM_CPU) && defined(__mc68010__)
+#define PLATFORM_CPU "68010"
+#endif
+
+#if !defined(PLATFORM_CPU) && defined(__mc68000__)
+#define PLATFORM_CPU "68000"
+#endif
+
+#ifndef PLATFORM_CPU
+#define PLATFORM_CPU "M68K"
+#endif
+
+#endif /* FIND_M68K_CPU */
+
+
+/* Generic alpha cpu discovery */
+#ifdef FIND_ALPHA_CPU
+
+#ifdef __alpha_ev6__
+#define PLATFORM_CPU "Alpha EV6"
+#endif
+
+#if !defined(PLATFORM_CPU) && defined(__alpha_ev5__)
+#define PLATFORM_CPU "Alpha EV5"
+#endif
+
+#if !defined(PLATFORM_CPU) && defined(__alpha_ev4__)
+#define PLATFORM_CPU "Alpha EV4"
+#endif
+
+#ifndef PLATFORM_CPU
+#define PLATFORM_CPU "Alpha"
+#endif
+
+#endif /* FIND_ALPHA_CPU */
+
+
+/* Generic x86 cpu discovery */
+#ifdef FIND_X86_CPU
+
+#ifdef __i686__
+#define PLATFORM_CPU "Pentium Pro"
+#endif
+
+#if !defined(PLATFORM_CPU) && defined(__i586__)
+#define PLATFORM_CPU "Pentium"
+#endif
+
+#if !defined(PLATFORM_CPU) && defined(__i486__)
+#define PLATFORM_CPU "80486"
+#endif
+
+#if !defined(PLATFORM_CPU) && defined(__i386__)
+#define PLATFORM_CPU "80386"
+#endif
+
+#endif /* FIND_X86_CPU */
+
+#endif
