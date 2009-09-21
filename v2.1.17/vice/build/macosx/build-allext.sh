c1d94610ea7702a9983ec7bd5a6693bff55bc5f5
strik <strik@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2009-09-21 18:24:41 +0000

    Tagged version v2.1.17.

---
+++ v2.1.17/vice/build/macosx/build-allext.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# ----- build-allext.sh -----
+# build the allext on ppc+i386 macs for VICE
+#
+# Usage:     build-allext.sh <build-dir> <arch:ppc|i386>
+#
+# written by Christian Vogelgsang <chris@vogelgsang.org>
+#
+
+# get build tools
+SCRIPT_DIR="`dirname \"$0\"`"
+
+# hidutils
+$SHELL "$SCRIPT_DIR/build-hidutils.sh" "$1" ppc
+$SHELL "$SCRIPT_DIR/build-hidutils.sh" "$1" i386
+# pcaplibnet
+$SHELL "$SCRIPT_DIR/build-pcaplibnet.sh" "$1" ppc
+$SHELL "$SCRIPT_DIR/build-pcaplibnet.sh" "$1" i386
+# ffmpeg
+$SHELL "$SCRIPT_DIR/build-ffmpeglame.sh" "$1" ppc
+$SHELL "$SCRIPT_DIR/build-ffmpeglame.sh" "$1" i386
+# gtk
+$SHELL "$SCRIPT_DIR/build-gtk.sh" "$1" ppc
+$SHELL "$SCRIPT_DIR/build-gtk.sh" "$1" i386
+echo "========== build-allext: ready =========="
