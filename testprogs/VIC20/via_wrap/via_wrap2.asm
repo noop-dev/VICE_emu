e5ccbb25e185ee739a4923ae6f78d40741023865
tlr <tlr@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2009-10-21 16:59:52 +0000

    Improved via_wrap2.prg to test for LSB and MSB in both single shot and continuous mode.

--- testprogs/VIC20/via_wrap/via_wrap2.asm
+++ testprogs/VIC20/via_wrap/via_wrap2.asm
@@ -15,6 +15,7 @@ SCR_BASE	equ	$1e00
 COL_BASE	equ	$9600
 
 tmp_zp	equ	$fb
+ptr_zp	equ	$fc
 
 	seg	code
 	org	$1001
@@ -40,74 +41,95 @@ startofcode:
 	lda	#<greet_msg
 	ldy	#>greet_msg
 	jsr	$cb1e
-	sei
 
 	ldx	#0
-soc_lp00:
-	ldy	#10
-soc_lp0:
-	tya
-	sta	SCR_BASE+22*7,x
-	inx
-	dey
-	cpy	#254
-	beq	soc_lp00
-	cpx	#22
-	bne	soc_lp0
-	
-
-	ldx	#22
-	lda	#6
+	lda	646
 soc_lp1:
-	sta	COL_BASE+22*4-1,x
-	sta	COL_BASE+22*7-1,x
-	dex
+	sta	COL_BASE,x
+	sta	COL_BASE+$0100,x
+	inx
 	bne	soc_lp1
 
-	ldx	#0
+	ldx	#22*2
 soc_lp2:
+	lda	reference-1,x
+	sta	SCR_BASE+22*3-1,x
+	dex
+	bne	soc_lp2
+	
+	lda	#$24
+	ldx	#%00000000	; single
+	jsr	perform_test
+	lda	#$25
+	ldx	#%00000000	; single
+	jsr	perform_test
+	
+	lda	#<continuous_msg
+	ldy	#>continuous_msg
+	jsr	$cb1e
+
+	lda	#$24
+	ldx	#%01000000	; continuous
+	jsr	perform_test
+	lda	#$25
+	ldx	#%01000000	; continuous
+	jsr	perform_test
+
+soc_lp4:
+	jmp	soc_lp4
+
+
+perform_test:
+	sei
+	sta	pt_sm2+1
+	ldy	#0
+pt_lp1:
 	lda	#$7f
 	sta	$912e
 	sta	$912d
-	lda	#%11000000
-	sta	$912e
-	lda	#%01000000
-	sta	$912b
-	lda	#10
+	stx	$912b
+	lda	#18
 	sta	$9124
-	lda	#0
-	sta	$9125	;load T1
-	stx	tmp_zp
+	sty	tmp_zp
 	lda	#BR_LEN-1
 	sec
 	sbc	tmp_zp	
-	sta	soc_sm1+1
+	sta	pt_sm1+1
 	clc
-soc_sm1:
-	bcc	soc_branch
-soc_branch:
+	lda	#0
+	sta	$9125	;load T1
+pt_sm1:
+	bcc	pt_sm1
+pt_branch:
 	ds.b	20,$a9
 	dc.b	$24,$ea
-BR_LEN	equ	. - soc_branch
+BR_LEN	equ	. - pt_branch
 
+pt_sm2:
 	lda	$9124
-	sta	SCR_BASE+22*4,x
-	inx
-	cpx	#BR_LEN
-	bne	soc_lp2
+	sta	($d1),y
+	iny
+	cpy	#BR_LEN
+	bne	pt_lp1
 
-	lda	#$7f
-	sta	$912e
-	sta	$912d
-lp:
-	jmp	lp
-	rts
-
+	cli
+	lda	#13
+	jmp	$ffd2
+;	rts
 
 greet_msg:
-	dc.b	147,"VIA WRAP2 (CONTINUOUS)",13,13
-	dc.b	"MEASURED:",13,13,13
-	dc.b	"REF:",0
+	dc.b	147,"VIA WRAP2 / TLR",13,13
+	dc.b	"REFERENCE:",13,13,13,13
+	dc.b	"MEASURED (SINGLE):",13
+	dc.b	0
+continuous_msg:
+	dc.b	13
+	dc.b	"MEASURED (CONT):",13
+	dc.b	0
+
+reference:
+	dc.b	10,9,8,7,6,5,4,3,2,1,0,255,18,17,16,15,14,13,12,11,10,9		
+	dc.b	0,0,0,0,0,0,0,0,0,0,0,255,0,0,0,0,0,0,0,0,0,0
 ; eof
 
 	
