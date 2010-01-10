9b3f2140e115e09c63361c58f0e738224cd43bf9
amatthies <amatthies@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2010-01-10 23:06:24 +0000

    Some fixes for sprite y-expand and mcbase. Quite ad-hoc but starion intro works now (except for the idle-sprite-fetch).

--- branches/viceplus/v2.2-x64/vice/src/viciinew/vicii-mem.c
+++ branches/viceplus/v2.2-x64/vice/src/viciinew/vicii-mem.c
@@ -167,6 +167,31 @@ inline static void d016_store(const BYTE value)
 
     vicii_update_video_mode(cycle);
 }
+const int vicii_sprites_crunch_table[64] =
+{
+    1,   4,   3, /* 0 */
+    4,   1,   0, /* 3 */
+    -1,  0,   1, /* 6 */
+    4,   3,   4, /* 9 */
+    1,   8,   7, /* 12 */
+    8,   1,   4, /* 15 */
+    3,   4,   1, /* 18 */
+    0,  -1,   0, /* 21 */
+    1,   4,   3, /* 24 */
+    4,   1,  -8, /* 27 */
+    -9, -8,   1, /* 30 */
+    4,   3,   4, /* 33 */
+    1,   0,  -1, /* 36 */
+    0,   1,   4, /* 39 */
+    3,   4,   1, /* 42 */
+    8,   7,   8, /* 45 */
+    1,   4,   3, /* 48 */
+    4,   1,   0, /* 51 */
+    -1,  0,   1, /* 54 */
+    4,   3,   4, /* 57 */
+    1, -40, -41, /* 60 */
+    0
+};
 
 inline static void d017_store(const BYTE value)
 {
@@ -185,7 +210,8 @@ inline static void d017_store(const BYTE value)
         if (!(value & b)) {
             /* sprite crunch */
             if (vicii.regs[0x17] & b && vicii.sprite[i].exp_flop && cycle == 15) {
-                mcbase = vicii.sprite[i].mcbase;
+#if 0
+                mcbase = vicii.sprite[i].mcbase - 2;
                 if (mcbase != 0x3f && (mcbase & 0x0f) > 0x0c) {
                     if ((mcbase & 0x30) == 0x30) {
                         mcbase &= ~0x20;
@@ -200,9 +226,22 @@ inline static void d017_store(const BYTE value)
                 mcbase |= tmp << 1;
                 mcbase = mcbase - 4 & 0x3f;
                 vicii.sprite[i].mcbase = mcbase;
+#else
+                /* undo the progress made in cycle 14 in sprite_exp_inc() */
+                vicii.sprite[i].mcbase -= 2;
+                /* apply the magic crunch */
+                vicii.sprite[i].mcbase += vicii_sprites_crunch_table[vicii.sprite[i].mcbase];
+                /* undo the progress that will follow in cycle 15 in sprite_exp_inc() */
+                vicii.sprite[i].mcbase--;
+#endif
             }
 
             vicii.sprite[i].exp_flop = 1;
+#ifdef DEBUG
+            if (debug.maincpu_traceflg) {
+                log_debug("D017_store. exp_flop=1, New mcbase for sprite %d: %d",i, vicii.sprite[i].mcbase);
+            }
+#endif
         }
     }
 
