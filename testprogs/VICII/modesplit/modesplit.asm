c930e6c2e19a74b8ea33238d3310dc434b7bcc1d
tlr <tlr@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2010-09-11 18:12:33 +0000

    Prepared modesplit for PAL/NTSC/NTSCOLD operation.  Currently only PAL works. Functionally equivalent with the previous version.

--- testprogs/VICII/modesplit/modesplit.asm
+++ testprogs/VICII/modesplit/modesplit.asm
@@ -3,13 +3,11 @@
 ;* FILE  modesplit.asm
 ;* Copyright (c) 2010 Daniel Kahlin <daniel@kahlin.net>
 ;* Written by Daniel Kahlin <daniel@kahlin.net>
-;* $Id: intro.asm,v 1.101 2009-06-02 20:49:58 tlr Exp $
 ;*
 ;* DESCRIPTION
 ;*
 ;******
 	processor 6502
-
 
 LINE		equ	56
 
@@ -53,13 +51,23 @@ sa_lp1:
 	dex
 	bne	sa_lp1
 
-	jsr	prepare_test
-
-	jsr	wait_vb
-
 	lda	#$7f
 	sta	$dc0d
 	lda	$dc0d
+	jsr	check_time
+	sec
+	sbc	#63
+	bcc	sa_fl1		;<63, fail
+	cmp	#66-63		;>=66, fail
+	bcs	sa_fl1
+	tax
+	lda	time1,x
+	sta	is_sm1+1
+
+	jsr	prepare_test
+	
+	jsr	wait_vb
+
 	lda	#$1b | (>LINE << 7)
 	sta	$d011
 	lda	#<LINE
@@ -71,7 +79,7 @@ sa_lp1:
 	cli
 	
 sa_lp2:
-	if	0
+	if	1
 	inx
 	bpl	sa_lp2
 	inc	$4080,x
@@ -82,12 +90,16 @@ sa_lp2:
 vectors:
 	dc.w	nmi_entry,0,irq_stable
 
+sa_fl1:
 nmi_entry:
 	sei
 	lda	#$37
 	sta	$01
 	jmp	$fe66
-	
+
+time1:
+	dc.b	irq_stable2_pal, irq_stable2_ntscold, irq_stable2_ntsc
+
 ;**************************************************************************
 ;*
 ;* NAME  wait_vb
@@ -102,6 +114,34 @@ wv_lp2:
 	bmi	wv_lp2
 	rts
 
+
+;**************************************************************************
+;*
+;* NAME  check_time
+;*   
+;* DESCRIPTION
+;*   Determine number of cycles per raster line.
+;*   
+;******
+check_time:
+	lda	#0
+	sta	$dc0e
+	jsr	wait_vb
+;--- raster line 0
+	lda	#$fe
+	sta	$dc04
+	sta	$dc05		; load timer with $fefe
+	lda	#%00010001
+	sta	$dc0e		; start one shot timer
+ct_lp3:
+	bit	$d011
+	bpl	ct_lp3
+;--- raster line 256
+	lda	$dc05		; timer msb
+	eor	#$ff		; invert
+twelve:
+	rts
+	
 ;**************************************************************************
 ;*
 ;* NAME  irq_stable, irq_stable2
@@ -114,21 +154,31 @@ irq_stable:
 	inc	$d019		; 6
 	inc	$d012		; 6
 is_sm1:
-	lda	#<irq_stable2	; 2
+	lda	#<irq_stable2_pal	; 2
 	sta	$fffe		; 4
 	tsx			; 2
 	cli			; 2
-	ds.b	13,$ea		; 24
-	if	0
+;10 * nop for PAL (63)
+;11 * nop for NTSC (65)
+	ds.b	11,$ea		; 22
+; guard
 is_lp1:
 	sei
 	inc	$d020
 	jmp	is_lp1	
-irq_stable2:
-	ds.b	13,$ea		; 26
-	else
-irq_stable2	equ	.-13
-	endif
+
+;28 cycles for NTSC (65)
+;27 cycles for NTSCOLD (64)
+;26 cycles for PAL (63)
+irq_stable2_ntsc:
+	dc.b	$2c		; bit <abs>
+irq_stable2_ntscold:
+	dc.b	$24		; bit <zp>
+irq_stable2_pal:
+	dc.b	$ea
+	jsr	twelve		; 12
+	jsr	twelve		; 12
+;---
 	txs			; 2
 	dec	$d019		; 6
 	dec	$d012		; 6
@@ -150,7 +200,11 @@ ystore	equ	.+1
 	rti
 
 
-
+;**************************************************************************
+;*
+;* NAME  prepare_test
+;*   
+;******
 label:
 	dc.b	"0123456789012345678901234567890123456789"
 
