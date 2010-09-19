3a2de4e8188463c3e2eb3f7a374980306c0bb45a
strik <strik@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2010-09-19 14:55:54 +0000

    Tagged version v2.2.7.

---
+++ v2.2.7/vice/src/arch/unix/gui/uivdc.c
@@ -0,0 +1,206 @@
+/*
+ * uivdc.c
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+
+#include "fullscreenarch.h"
+#include "resources.h"
+#include "uiapi.h"
+#include "uimenu.h"
+#include "uipalemu.h"
+#include "uipalette.h"
+#include "uirenderer.h"
+#include "uivdc.h"
+#include "uifullscreen-menu.h"
+#ifdef HAVE_OPENGL_SYNC
+#include <stdlib.h>             /* strtol() */
+#include "openGL_sync.h"
+#include "lib.h"
+#endif
+
+UI_FULLSCREEN(VDC, KEYSYM_f)
+
+static UI_CALLBACK(radio_VDCPaletteFile)
+{
+    ui_select_palette(w, CHECK_MENUS, UI_MENU_CB_PARAM, "VDC");
+}
+
+static ui_menu_entry_t vdc_palette_submenu[] = {
+    { N_("*internal"), (ui_callback_t)radio_VDCPaletteFile,
+      NULL, NULL },
+    { "--" },
+    { N_("*Default"), (ui_callback_t)radio_VDCPaletteFile,
+      (ui_callback_data_t)"vdc_deft", NULL },
+    { N_("*Composite"), (ui_callback_t)radio_VDCPaletteFile,
+      (ui_callback_data_t)"vdc_comp", NULL },
+    { "--" },
+    { N_("Load custom"), (ui_callback_t)ui_load_palette,
+      (ui_callback_data_t)"VDC", NULL },
+    { NULL }
+};
+
+static UI_CALLBACK(radio_renderer)
+{
+    ui_select_renderer(w, CHECK_MENUS, vice_ptr_to_int(UI_MENU_CB_PARAM), "VDC");
+}
+
+static ui_menu_entry_t renderer_submenu[] = {
+    { N_("*unfiltered"), (ui_callback_t)radio_renderer,
+      (ui_callback_data_t)0, NULL },
+    { N_("*CRT Emulation"), (ui_callback_t)radio_renderer,
+      (ui_callback_data_t)1, NULL },
+#if 0
+    { N_("*Scale 2x"), (ui_callback_t)radio_renderer,
+      (ui_callback_data_t)2, NULL },
+#endif
+    { NULL }
+};
+
+UI_MENU_DEFINE_RADIO(VDCRevision)
+
+static ui_menu_entry_t set_vdc_revison_submenu[] = {
+    { N_("*Revison 0"), (ui_callback_t)radio_VDCRevision,
+      (ui_callback_data_t)0, NULL },
+    { N_("*Revison 1"), (ui_callback_t)radio_VDCRevision,
+      (ui_callback_data_t)1, NULL },
+    { N_("*Revison 2"), (ui_callback_t)radio_VDCRevision,
+      (ui_callback_data_t)2, NULL },
+    { NULL }
+};
+
+#define NOTHING(x) x
+
+UI_MENU_DEFINE_TOGGLE(VDCDoubleSize)
+UI_MENU_DEFINE_TOGGLE(VDCDoubleScan)
+UI_MENU_DEFINE_TOGGLE(VDCVideoCache)
+
+#ifdef HAVE_HWSCALE
+UI_MENU_DEFINE_TOGGLE_COND(VDCHwScale, HwScalePossible, NOTHING)
+#endif
+
+#ifndef USE_GNOMEUI
+UI_MENU_DEFINE_TOGGLE(UseXSync)
+#endif
+
+UI_MENU_DEFINE_TOGGLE(VDC64KB)
+
+#ifdef HAVE_HWSCALE
+#ifdef USE_GNOMEUI
+UI_MENU_DEFINE_TOGGLE(KeepAspectRatio)
+UI_MENU_DEFINE_TOGGLE(TrueAspectRatio)
+#endif
+#endif
+
+#ifdef HAVE_OPENGL_SYNC
+UI_MENU_DEFINE_TOGGLE_COND(openGL_sync, openGL_no_sync, openGL_available)
+
+static UI_CALLBACK(openGL_set_desktoprefresh)
+{
+    if (!CHECK_MENUS) {
+        float f;
+        char *buf = lib_calloc(sizeof(char), 10);
+
+        sprintf(buf, "%.0f", openGL_get_canvas_refreshrate());
+        ui_input_string(_("Refreshrate: "), _("Enter Refreshrate (Hz): "), buf, 10);
+        f = (float) strtol(buf, NULL, 10);
+        openGL_set_canvas_refreshrate(f);
+        lib_free(buf);
+    } else {
+        if (openGL_available(0) && openGL_sync_enabled()) {
+            ui_menu_set_sensitive(w, 1);
+        } else {
+            ui_menu_set_sensitive(w, 0);
+        }
+    }
+}
+#endif
+
+ui_menu_entry_t set_vdcmodel_submenu[] = {
+    { N_("*64KB display memory"),
+      (ui_callback_t)toggle_VDC64KB, NULL, NULL },
+    { N_("Revision"),
+      NULL, NULL, set_vdc_revison_submenu },
+    { NULL }
+};
+
+ui_menu_entry_t vdc_submenu[] = {
+    { N_("*Double size"),
+      (ui_callback_t)toggle_VDCDoubleSize, NULL, NULL },
+    { N_("*Double scan"),
+      (ui_callback_t)toggle_VDCDoubleScan, NULL, NULL },
+    { N_("*Video cache"),
+      (ui_callback_t)toggle_VDCVideoCache, NULL, NULL },
+    { "--" },
+    { N_("Color set"),
+      NULL, NULL, vdc_palette_submenu },
+    { "--" },
+    { N_("Renderer"),
+      NULL, NULL, renderer_submenu },
+#ifndef USE_GNOMEUI
+    { N_("CRT Emulation Settings"),
+      NULL, NULL, PALMode_submenu },
+#endif
+#ifdef HAVE_HWSCALE
+    { "--" },
+    { N_("*Hardware scaling"),
+      (ui_callback_t)toggle_VDCHwScale, NULL, NULL },
+#ifdef USE_GNOMEUI
+    { N_("*Keep aspect ratio"),
+      (ui_callback_t)toggle_KeepAspectRatio, NULL, NULL },
+    { N_("*True aspect ratio"),
+      (ui_callback_t)toggle_TrueAspectRatio, NULL, NULL },
+#endif
+#endif
+#ifdef HAVE_OPENGL_SYNC
+    { "--" },
+    { N_("*OpenGL Rastersynchronization"),
+      (ui_callback_t)toggle_openGL_sync, NULL, NULL },
+    { N_("Desktop Refreshrate..."),
+      (ui_callback_t)openGL_set_desktoprefresh, NULL, NULL },
+#endif
+#ifdef HAVE_FULLSCREEN
+    { "--" },
+    { N_("*Fullscreen settings"), NULL, NULL, fullscreen_menuVDC },
+#endif
+#ifndef USE_GNOMEUI
+    { "--" },
+    { N_("*Use XSync()"),
+      (ui_callback_t)toggle_UseXSync, NULL, NULL },
+#endif
+    { NULL }
+};
+
+void uivdc_menu_create(void)
+{
+    UI_FULLSCREEN_MENU_CREATE(VDC)
+}
+
+void uivdc_menu_shutdown(void)
+{
+    UI_FULLSCREEN_MENU_SHUTDOWN(VDC)
+}
---
+++ v2.2.7/vice/src/c64/cart/ocean.c
@@ -0,0 +1,146 @@
+/*
+ * ocean.c - Cartridge handling, Ocean cart.
+ *
+ * Written by
+ *  Marco van den Heuvel <blackystardust68@yahoo.com>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "c64cart.h"
+#include "c64cartmem.h"
+#include "c64export.h"
+#include "c64io.h"
+#include "c64mem.h"
+#include "cartridge.h"
+#include "types.h"
+#include "util.h"
+
+
+/*
+    This type of cart comes in 4 sizes: 32Kb, 128Kb, 256Kb, 512Kb.
+
+    The 32Kb type of cart has 4 banks, banked in at $8000-$9FFF.
+
+    The 128Kb type of cart has 16 banks, banked in at $8000-$9FFF.
+
+    The 256Kb type of cart has 32 banks, 16 banked in at $8000-$9FFF,
+    and 16 banked in at $A000-$BFFF.
+
+    The 512Kb type of cart has 64 banks, banked in at $8000-$9FFF.
+
+    Bank switching is done by writing to $DE00. The lower six bits give the
+    bank number (ranging from 0-63). Bit 8 in this selection word is always
+    set.
+ */
+
+static void REGPARM2 ocean_io1_store(WORD addr, BYTE value)
+{
+    /* FIXME */
+    cartridge_romhbank_set(value & 0x3f);
+    cartridge_romlbank_set(value & 0x3f);
+    export.game = export.exrom = 1;
+    mem_pla_config_changed();
+    export.ultimax_phi1 = 0;
+    export.ultimax_phi2 = 0;
+}
+
+/* ---------------------------------------------------------------------*/
+
+static io_source_t ocean_device = {
+    "Ocean",
+    IO_DETACH_CART,
+    NULL,
+    0xde00, 0xdeff, 0xff,
+    0,
+    ocean_io1_store,
+    NULL,
+    NULL,
+    NULL,
+    CARTRIDGE_OCEAN
+};
+
+static io_source_list_t *ocean_list_item = NULL;
+
+static const c64export_resource_t export_res = {
+    "Ocean", 1, 1, &ocean_device, NULL, CARTRIDGE_OCEAN
+};
+
+/* ---------------------------------------------------------------------*/
+
+BYTE REGPARM1 ocean_romh_read(WORD addr)
+{
+    /* 256 kB OCEAN carts may access memory either at $8000 or $a000 */
+    return roml_banks[(addr & 0x1fff) + (romh_bank << 13)];
+}
+
+void ocean_config_init(void)
+{
+    cartridge_config_changed(1, 1, CMODE_READ);
+    ocean_io1_store((WORD)0xde00, 0);
+}
+
+void ocean_config_setup(BYTE *rawcart)
+{
+    memcpy(roml_banks, rawcart, 0x2000 * 64);
+    memcpy(romh_banks, &rawcart[0x2000 * 16], 0x2000 * 16);
+
+    /* Hack: using 16kB configuration, but some carts are 8kB only */
+    cartridge_config_changed(1, 1, CMODE_READ);
+}
+
+/* ---------------------------------------------------------------------*/
+
+int ocean_crt_attach(FILE *fd, BYTE *rawcart)
+{
+    BYTE chipheader[0x10];
+
+    while (1) {
+        if (fread(chipheader, 0x10, 1, fd) < 1) {
+            break;
+        }
+        if (chipheader[0xb] >= 64 || (chipheader[0xc] != 0x80 && chipheader[0xc] != 0xa0)) {
+            return -1;
+        }
+        if (fread(&rawcart[chipheader[0xb] << 13], 0x2000, 1, fd) < 1) {
+            return -1;
+        }
+    }
+
+    if (c64export_add(&export_res) < 0) {
+        return -1;
+    }
+
+    ocean_list_item = c64io_register(&ocean_device);
+
+    return 0;
+}
+
+void ocean_detach(void)
+{
+    c64export_remove(&export_res);
+    c64io_unregister(ocean_list_item);
+    ocean_list_item = NULL;
+}
