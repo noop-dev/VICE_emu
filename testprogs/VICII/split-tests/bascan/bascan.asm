cdfe32e1d9a115257a64cde150d7142b2c7e9b9a
tlr <tlr@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2010-10-26 17:46:03 +0000

    Made bascan use common presentation and save code.

--- testprogs/VICII/split-tests/bascan/bascan.asm
+++ testprogs/VICII/split-tests/bascan/bascan.asm
@@ -9,6 +9,8 @@
 ;******
 	processor 6502
 
+TEST_NAME	eqm	"BASCAN"
+TEST_REVISION	eqm	"R??"
 LINE	equ	48+8*16+6
 
 	seg.u	zp
@@ -34,6 +36,7 @@ HAVE_TEST_RESULT	equ	1
 HAVE_STABILITY_GUARD	equ	1
 	include	"../common/startup.asm"
 
+	include	"../common/scandump.asm"
 
 ;**************************************************************************
 ;*
@@ -41,38 +44,15 @@ HAVE_STABILITY_GUARD	equ	1
 ;*
 ;******
 test_present:
-	lda	#14
-	sta	646
-	sta	$d020
-	lda	#6
-	sta	$d021
+	jsr	show_info
 
-	lda	#<timing_msg
-	ldy	#>timing_msg
+	lda	#<measure_msg
+	ldy	#>measure_msg
 	jsr	$ab1e
-	lda	#0
-	ldx	cycles_per_line
-	jsr	$bdcd
-	lda	#<cycles_line_msg
-	ldy	#>cycles_line_msg
-	jsr	$ab1e
-	lda	#1
-	ldx	num_lines
-	jsr	$bdcd
-	lda	#<lines_msg
-	ldy	#>lines_msg
-	jsr	$ab1e
-	
 	rts
 
-timing_msg:
-	dc.b	147,"BASCAN R?? / TLR",13,13
-	dc.b	"TIMING: ",0
-cycles_line_msg:
-	dc.b	" CYCLES/LINE, ",0
-lines_msg:
-	dc.b	" LINES",13,13
-	dc.b	"MEASURING $DC04 AT CYCLE $00...",0
+measure_msg:
+	dc.b	13,"MEASURING $DC04 AT CYCLE $00...",0
 
 show_params:
 	lda	$d3
@@ -91,25 +71,6 @@ show_params:
 	lda	cycle_zp
 	jmp	update_hex
 ;	rts
-	
-update_hex:
-	pha
-	lsr
-	lsr
-	lsr
-	lsr
-	jsr	uh_common
-	pla
-	and	#$0f
-uh_common:
-	tax
-	lda	htab,x
-	sta	($d1),y
-	iny
-	rts
-
-htab:
-	dc.b	"0123456789",1,2,3,4,5,6
 
 ;**************************************************************************
 ;*
@@ -126,45 +87,12 @@ test_result:
 	jsr	$ab1e
 
 
-; set device num to 8 if less than 8.
-	lda	#8
-	cmp	$ba
-	bcc	tr_skp1
-	sta	$ba
-tr_skp1:
-
-
-	lda	#<save_msg
-	ldy	#>save_msg
-	jsr	$ab1e
-tr_lp1:
-	jsr	$ffe4
-	cmp	#"Y"
-	bne	tr_lp1
-
-	lda	#$80
-	sta	$9d
-
-	lda	#1
-	ldx	$ba
-	tay
-	jsr	$ffba
 	ldx	#<filename
 	ldy	#>filename
 	lda	#FILENAME_LEN
 	jsr	$ffbd
-	ldx	#<BUFFER
-	ldy	#>BUFFER
-	stx	$fb
-	sty	$fc
-	lda	#$fb
-	ldx	#<BUFFER_END
-	ldy	#>BUFFER_END
-	jsr	$ffd8
+	jsr	save_file
 
-	lda	#<ok_msg
-	ldy	#>ok_msg
-	jsr	$ab1e
 	rts
 
 done_msg:
@@ -172,11 +100,6 @@ done_msg:
 
 result_msg:
 	dc.b	13,13,"(RESULT AT $4000-$4900)",0
-
-save_msg:
-	dc.b	13,13,"SAVE TO DISK? ",13,0
-ok_msg:
-	dc.b	13,"OK",13,0
 
 filename:
 	dc.b	"BARESULT"
