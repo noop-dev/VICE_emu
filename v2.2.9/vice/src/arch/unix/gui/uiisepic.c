a586fd3f00dfd823754501b868b0ebe28395a10d
strik <strik@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2010-12-10 18:16:53 +0000

    Tagged version v2.2.9.

---
+++ v2.2.9/vice/src/arch/unix/gui/uiisepic.c
@@ -0,0 +1,53 @@
+/*
+ * uiisepic.c
+ *
+ * Written by
+ *  Marco van den Heuvel <blackystardust68@yahoo.com>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+
+#include "cartridge.h"
+#include "ui.h"
+#include "uicartridge.h"
+#include "uilib.h"
+#include "uimenu.h"
+#include "uiisepic.h"
+
+static UI_CALLBACK(isepic_save_cartridge)
+{
+    ui_cartridge_save_dialog(CARTRIDGE_ISEPIC);
+}
+
+UI_MENU_DEFINE_TOGGLE(IsepicCartridgeEnabled)
+UI_MENU_DEFINE_TOGGLE(IsepicSwitch)
+
+ui_menu_entry_t isepic_submenu[] = {
+    { N_("Enable ISEPIC"), UI_MENU_TYPE_TICK,
+      (ui_callback_t)toggle_IsepicCartridgeEnabled, NULL, NULL },
+    { N_("Enable ISEPIC switch"), UI_MENU_TYPE_TICK,
+      (ui_callback_t)toggle_IsepicSwitch, NULL, NULL },
+    { "--", UI_MENU_TYPE_SEPARATOR },
+    { N_("Save ISEPIC image..."), UI_MENU_TYPE_NORMAL,
+      (ui_callback_t)isepic_save_cartridge, NULL, NULL },
+    { NULL }
+};
