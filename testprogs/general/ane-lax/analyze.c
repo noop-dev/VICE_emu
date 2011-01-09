44147858933875fb2ff7db50d3d302f5af5ebbe4
tlr <tlr@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2011-01-09 00:53:52 +0000

    Added fuzzy matching.

--- testprogs/general/ane-lax/analyze.c
+++ testprogs/general/ane-lax/analyze.c
@@ -42,7 +42,7 @@ int match_ane(uint8_t *tab, uint8_t op, uint8_t c)
 {
     int i;
     uint8_t acc, xr;
-    int match = 1;
+    int match = 0;
 
     acc = 0;
     xr = 0;
@@ -50,8 +50,8 @@ int match_ane(uint8_t *tab, uint8_t op, uint8_t c)
 	uint8_t acc_r;
 
 	acc_r = (acc | c) & (xr & op);
-	if (acc_r != tab[i])
-	    match = 0;
+	if (acc_r == tab[i])
+	    match++;
 
 	acc += ACC_STRIDE;
 	xr += XR_STRIDE;
@@ -65,7 +65,7 @@ int match_lax(uint8_t *tab, uint8_t op, uint8_t c)
 {
     int i;
     uint8_t acc, xr;
-    int match = 1;
+    int match = 0;
 
     acc = 0;
     xr = 0;
@@ -73,8 +73,8 @@ int match_lax(uint8_t *tab, uint8_t op, uint8_t c)
 	uint8_t acc_r;
 
 	acc_r = (acc | c) & op;
-	if (acc_r != tab[i])
-	    match = 0;
+	if (acc_r == tab[i])
+	    match++;
 
 	acc += ACC_STRIDE;
 	xr += XR_STRIDE;
@@ -88,48 +88,70 @@ uint8_t ops[] = {
     0xff, 0x00, 0x5a, 0xa5
 };
 
+struct Bin {
+    uint8_t b;
+    int count;
+};
+
+static int cmpfunc(const void *ap, const void *bp)
+{
+    struct Bin *a = (struct Bin *)ap;
+    struct Bin *b = (struct Bin *)bp;
+
+    if (a->count < b->count) 
+	return 1;
+    if (b->count > a->count) 
+	return -1;
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     read_dump(argv[1]);
 
     int c;
     int i;
-    int match, found;
-    found = 0;
+    struct Bin match[256];
+
+    for (i = 0; i < 256; i++) {
+	match[i].b = i;
+	match[i].count = 0;
+    }
+
     for (c = 0x00; c <= 0xff; c++) {
 	for (i = 0; i < 4; i++) {
-	    match = match_ane(ane_buf + SCAN_LEN * i, ops[i], c);
-	    if (!match)
-		break;
-	}
-	if (match) {
-	    found = 1;
-	    break;
+	    match[c].count += match_ane(ane_buf + SCAN_LEN * i, ops[i], c);
 	}
     }
-    if (found) {
-	printf("ANE ($8B) matches: Acc = (Acc | $%02X) & X & #<imm>\n", c);
+
+    qsort(match, 256, sizeof(struct Bin), cmpfunc);
+
+    if (match[0].count == SCAN_LEN * 4) {
+	printf("ANE ($8B) matches: Acc = (Acc | $%02X) & X & #<imm>\n", match[0].b);
     } else {
 	printf("ANE ($8B) does not match: Acc = (Acc | CONST) & X & #<imm>\n");
+	printf("(closest CONST = $%02X, %d errors)\n", match[0].b, SCAN_LEN*4-match[0].count);
     }
 
 
-    found = 0;
+    for (i = 0; i < 256; i++) {
+	match[i].b = i;
+	match[i].count = 0;
+    }
+
     for (c = 0x00; c <= 0xff; c++) {
 	for (i = 0; i < 4; i++) {
-	    match = match_lax(lax_buf + SCAN_LEN * i, ops[i], c);
-	    if (!match)
-		break;
-	}
-	if (match) {
-	    found = 1;
-	    break;
+	    match[c].count += match_lax(lax_buf + SCAN_LEN * i, ops[i], c);
 	}
     }
-    if (found) {
-	printf("LAX ($AB) matches: Acc = (Acc | $%02X) & #<imm>\n", c);
+
+    qsort(match, 256, sizeof(struct Bin), cmpfunc);
+
+    if (match[0].count == SCAN_LEN * 4) {
+	printf("LAX ($AB) matches: Acc = (Acc | $%02X) & #<imm>\n", match[0].b);
     } else {
 	printf("LAX ($AB) does not match: Acc = (Acc | CONST) & #<imm>\n");
+	printf("(closest CONST= $%02X, %d errors)\n", match[0].b, SCAN_LEN*4-match[0].count);
     }
 
     exit(0);
