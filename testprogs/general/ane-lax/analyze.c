142bb24ea27cc7a5ac754d56e538c3eb0f114874
tlr <tlr@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2011-01-10 19:39:17 +0000

    Implemented analysis of which bits changed during errors.

--- testprogs/general/ane-lax/analyze.c
+++ testprogs/general/ane-lax/analyze.c
@@ -38,59 +38,25 @@ void read_dump(const char *name)
     fclose(fp);
 }
 
-int match_ane(uint8_t *tab, uint8_t op, uint8_t c)
+static char *binstr(uint8_t b, const char ch[2])
 {
+    static char str[9];
     int i;
-    uint8_t acc, xr;
-    int match = 0;
 
-    acc = 0;
-    xr = 0;
-    for (i = 0; i < SCAN_LEN; i++) {
-	uint8_t acc_r;
-
-	acc_r = (acc | c) & (xr & op);
-	if (acc_r == tab[i])
-	    match++;
-
-	acc += ACC_STRIDE;
-	xr += XR_STRIDE;
+    for (i = 0; i < 8; i++) {
+	str[i] = ((b >> (7-i)) & 1) ? ch[1] : ch[0];
     }
-
-    return match;
+	
+    str[8] = 0;
+    return str;
 }
-
-
-int match_lax(uint8_t *tab, uint8_t op, uint8_t c)
-{
-    int i;
-    uint8_t acc, xr;
-    int match = 0;
-
-    acc = 0;
-    xr = 0;
-    for (i = 0; i < SCAN_LEN; i++) {
-	uint8_t acc_r;
-
-	acc_r = (acc | c) & op;
-	if (acc_r == tab[i])
-	    match++;
-
-	acc += ACC_STRIDE;
-	xr += XR_STRIDE;
-    }
-
-    return match;
-}
-
-
-uint8_t ops[] = {
-    0xff, 0x00, 0x5a, 0xa5
-};
 
 struct Bin {
     uint8_t b;
     int count;
+    uint8_t fault0;
+    uint8_t fault1;
+    uint8_t fault_abn;
 };
 
 static int cmpfunc(const void *ap, const void *bp)
@@ -105,6 +71,85 @@ static int cmpfunc(const void *ap, const void *bp)
     return 0;
 }
 
+static void match_ane(struct Bin *p, uint8_t *tab, uint8_t op, uint8_t c)
+{
+    int i;
+    uint8_t acc, xr;
+
+    acc = 0;
+    xr = 0;
+    for (i = 0; i < SCAN_LEN; i++) {
+	uint8_t acc_r;
+	uint8_t acc_e, acc_b;
+
+	/* measured result */
+	acc_r = tab[i];
+
+	/* ANE #<imm> model (expected result) */
+	acc_e = (acc | c) & (xr & op);
+	acc_b = (xr & op);
+
+	/* evaluate result */
+	if (acc_r == acc_e) {
+	    p->count++;
+	} else {
+	    /* cleared bits */
+	    p->fault0 |= (~acc_r) & acc_e;
+	    /* set bits */
+	    p->fault1 |= acc_r & (~acc_e);
+	    /* abnormal bits */
+	    p->fault_abn |= acc_r & (~acc_b);
+	}
+
+	acc += ACC_STRIDE;
+	xr += XR_STRIDE;
+    }
+
+}
+
+
+static void match_lax(struct Bin *p, uint8_t *tab, uint8_t op, uint8_t c)
+{
+    int i;
+    uint8_t acc, xr;
+
+    acc = 0;
+    xr = 0;
+    for (i = 0; i < SCAN_LEN; i++) {
+	uint8_t acc_r;
+	uint8_t acc_e, acc_b;
+
+	/* measured result */
+	acc_r = tab[i];
+
+	/* LAX #<imm> model (expected result) */
+	acc_e = (acc | c) & op;
+	acc_b = op;
+
+	/* evaluate result */
+	if (acc_r == acc_e) {
+	    p->count++;
+	} else {
+	    /* cleared bits */
+	    p->fault0 |= (~acc_r) & acc_e;
+	    /* set bits */
+	    p->fault1 |= acc_r & (~acc_e);
+	    /* abnormal bits */
+	    p->fault_abn |= acc_r & (~acc_b);
+	}
+
+	acc += ACC_STRIDE;
+	xr += XR_STRIDE;
+    }
+
+}
+
+
+uint8_t ops[] = {
+    0xff, 0x00, 0x5a, 0xa5
+};
+
+
 int main(int argc, char *argv[])
 {
     read_dump(argv[1]);
@@ -116,11 +161,14 @@ int main(int argc, char *argv[])
     for (i = 0; i < 256; i++) {
 	match[i].b = i;
 	match[i].count = 0;
+	match[i].fault0 = 0;
+	match[i].fault1 = 0;
+	match[i].fault_abn = 0;
     }
 
     for (c = 0x00; c <= 0xff; c++) {
 	for (i = 0; i < 4; i++) {
-	    match[c].count += match_ane(ane_buf + SCAN_LEN * i, ops[i], c);
+	    match_ane(&match[c], ane_buf + SCAN_LEN * i, ops[i], c);
 	}
     }
 
@@ -130,18 +178,25 @@ int main(int argc, char *argv[])
 	printf("ANE ($8B) matches: Acc = (Acc | $%02X) & X & #<imm>\n", match[0].b);
     } else {
 	printf("ANE ($8B) does not match: Acc = (Acc | CONST) & X & #<imm>\n");
-	printf("(closest CONST = $%02X, %d errors)\n", match[0].b, SCAN_LEN*4-match[0].count);
+	printf("  closest CONST = $%02X, %d errors\n", match[0].b, SCAN_LEN*4-match[0].count);
+	printf("       CONST: %s\n", binstr(match[0].b, "01") );
+	printf("     cleared: %s\n", binstr(match[0].fault0, "-0") );
+	printf("         set: %s\n", binstr(match[0].fault1, "-1") );
+	printf("    abnormal: %s\n", binstr(match[0].fault_abn, "-!") );
     }
 
 
     for (i = 0; i < 256; i++) {
 	match[i].b = i;
 	match[i].count = 0;
+	match[i].fault0 = 0;
+	match[i].fault1 = 0;
+	match[i].fault_abn = 0;
     }
 
     for (c = 0x00; c <= 0xff; c++) {
 	for (i = 0; i < 4; i++) {
-	    match[c].count += match_lax(lax_buf + SCAN_LEN * i, ops[i], c);
+	    match_lax(&match[c], lax_buf + SCAN_LEN * i, ops[i], c);
 	}
     }
 
@@ -151,7 +206,11 @@ int main(int argc, char *argv[])
 	printf("LAX ($AB) matches: Acc = (Acc | $%02X) & #<imm>\n", match[0].b);
     } else {
 	printf("LAX ($AB) does not match: Acc = (Acc | CONST) & #<imm>\n");
-	printf("(closest CONST= $%02X, %d errors)\n", match[0].b, SCAN_LEN*4-match[0].count);
+	printf("  closest CONST = $%02X, %d errors\n", match[0].b, SCAN_LEN*4-match[0].count);
+	printf("       CONST: %s\n", binstr(match[0].b, "01") );
+	printf("     cleared: %s\n", binstr(match[0].fault0, "-0") );
+	printf("         set: %s\n", binstr(match[0].fault1, "-1") );
+	printf("    abnormal: %s\n", binstr(match[0].fault_abn, "-!") );
     }
 
     exit(0);
