f4d671e93d18cb97018c7e722350a1d4d5349236
blackystardust <blackystardust@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2013-01-15 23:07:54 +0000

    Fixed linkage of libvice.so, next step is fixing the java part.

--- branches/marco/android-port/android/AnVICE/jni/locnet/Android.mk
+++ branches/marco/android-port/android/AnVICE/jni/locnet/Android.mk
@@ -29,46 +29,10 @@ LOCAL_CPP_EXTENSION := .cpp
 
 LOCAL_SRC_FILES := $(foreach F, $(CG_SUBDIRS), $(addprefix $(F)/,$(notdir $(wildcard $(LOCAL_PATH)/$(F)/*.cpp))))
 
-LOCAL_STATIC_LIBRARIES := locnet_al vice_main vice_x64 vice_common vice_driver vice_temp
+LOCAL_STATIC_LIBRARIES := locnet_al vice_main vice_x64 vice_common vice_driver vice_vicii vice_main vice_common vice_driver
 #LOCAL_LDLIBS := -ljnigraphics
 LOCAL_LDLIBS := -lz -llog
 
-
-MY_PATH2 := C:/Projects/Eclipse/C64/AnVICE/jni/application/vice/vice
-
-LOCAL_LDFLAGS := \
-	-LC:/Projects/Eclipse/C64/AnVICE/obj/local/armeabi	\
-	-L$(MY_PATH2)/src/arch/sdl	\
-	-L$(MY_PATH2)/src/fsdevice \
-	-L$(MY_PATH2)/src/tape \
-	-L$(MY_PATH2)/src/imagecontents \
-	-L$(MY_PATH2)/src/fileio \
-	-L$(MY_PATH2)/src/serial \
-	-L$(MY_PATH2)/src/core \
-	-L$(MY_PATH2)/src/sounddrv \
-	-L$(MY_PATH2)/src/gfxoutputdrv \
-	-L$(MY_PATH2)/src/printerdrv \
-	-L$(MY_PATH2)/src/rs232drv \
-	-L$(MY_PATH2)/src/diskimage \
-	-L$(MY_PATH2)/src/vicii \
-	-L$(MY_PATH2)/src/raster \
-	-L$(MY_PATH2)/src/rtc \
-	-L$(MY_PATH2)/src/video \
-	-L$(MY_PATH2)/src/c64 \
-	-L$(MY_PATH2)/src/c64/cart \
-	-L$(MY_PATH2)/src/drive/iec \
-	-L$(MY_PATH2)/src/drive/iecieee \
-	-L$(MY_PATH2)/src/drive/iec/c64exp \
-	-L$(MY_PATH2)/src/drive/ieee \
-	-L$(MY_PATH2)/src/drive \
-	-L$(MY_PATH2)/src/iecbus \
-	-L$(MY_PATH2)/src/parallel \
-	-L$(MY_PATH2)/src/vdrive \
-	-L$(MY_PATH2)/src/sid \
-	-L$(MY_PATH2)/src/monitor \
-	-Wl,--start-group -lvice_main -lvice_x64 -lvice_common -lvice_driver \
-	-lvice_temp \
-	-Wl,--end-group 
 
 LOCAL_ARM_MODE := arm
 
