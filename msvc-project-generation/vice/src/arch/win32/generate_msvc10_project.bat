88a4565153166c071ed5b68dd5ce0efad9443d3a
blackystardust <blackystardust@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2013-07-20 16:00:03 +0000

    Fixed.

--- msvc-project-generation/vice/src/arch/win32/generate_msvc10_project.bat
+++ msvc-project-generation/vice/src/arch/win32/generate_msvc10_project.bat
@@ -1,25 +1,25 @@
 @echo off
 if not exist vs_tmpl\mkmsvc.exe goto missingmkmsvc
-echo generating MSVC9 project as vs9
-if exist vs9 goto removedir
+echo generating MSVC10 project as vs10
+if exist vs10 goto removedir
 :makemsvcdir
-mkdir vs9
+mkdir vs10
 cd vs_tmpl
-mkmsvc.exe -native -9 vice
+mkmsvc.exe -native -10 vice
 cd ..
-copy msvc\make-msvc-bindist-native.bat.proto vs9\make-msvc-bindist.bat
-copy msvc\winid*.bat vs9
-echo MSVC9 project files generated in vs9
+copy msvc\make-msvc-bindist-native.bat.proto vs10\make-msvc-bindist.bat
+copy msvc\winid*.bat vs10
+echo MSVC10 project files generated in vs10
 goto end
 :missingmkmsvc
 echo vs_tmpl\mkmsvc.exe is missing
 goto end
 :removedir
-echo y | del vs9\*.* /S
-rd vs9 /s /q
-if exist vs9 goto cannotremovedir
+echo y | del vs10\*.* /S
+rd vs10 /s /q
+if exist vs10 goto cannotremovedir
 goto makemsvcdir
 :cannotremovedir
-echo can't delete the vs9 directory, please remove vs9 manually and run this batch file again.
+echo can't delete the vs10 directory, please remove vs10 manually and run this batch file again.
 :end
 pause
