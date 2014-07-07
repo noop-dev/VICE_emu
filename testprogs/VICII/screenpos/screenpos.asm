4b1316612433e5252fcec59b6a9d9b21d21eb0d2
gpz <gpz@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2014-07-07 15:29:43 +0000

    added precompiled ntsc versions

--- testprogs/VICII/screenpos/screenpos.asm
+++ testprogs/VICII/screenpos/screenpos.asm
@@ -14,7 +14,7 @@
 ; Select the video timing (processor clock cycles per raster line)
 ;CYCLES = 65     ; 6567R8 and above, NTSC-M
 ;CYCLES = 64    ; 6567R5 6A, NTSC-M
-CYCLES = 63    ; 6569 (all revisions), PAL-B
+;CYCLES = 63    ; 6569 (all revisions), PAL-B
 
 cinv = $314
 cnmi = $318
