a7e225cdb86e37e05cf9754788abd7cb959dbdf8
gpz <gpz@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2014-07-07 23:03:13 +0000

    added precompiled ntsc versions

--- testprogs/VICII/sprite0move/sprite0move.asm
+++ testprogs/VICII/sprite0move/sprite0move.asm
@@ -21,7 +21,7 @@
 ; Select the video timing (processor clock cycles per raster line)
 ;CYCLES = 65     ; 6567R8 and above, NTSC-M
 ;CYCLES = 64    ; 6567R5 6A, NTSC-M
-CYCLES = 63    ; 6569 (all revisions), PAL-B
+;CYCLES = 63    ; 6569 (all revisions), PAL-B
 
 raster = 46          ; start of raster interrupt
 spritey = 51         ; sprite 0 Y
