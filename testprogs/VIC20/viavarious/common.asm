097111e665bb5d42cda3dd828a50c7fdd8117198
gpz <gpz@379a1393-f5fb-40a0-bcee-ef074d9b53f7>
2014-08-01 00:10:25 +0000

    ported viavarious programs to vic20, VIA10-13 still need some work

---
+++ testprogs/VIC20/viavarious/common.asm
@@ -0,0 +1,489 @@
+        !convtab pet
+        !cpu 6510
+
+expanded=1
+
+;-------------------------------------------------------------------------------
+
+viabase = $9110
+;viabase = $9120
+
+
+charsperline = 22+2
+numrows = 23+2
+
+!if (expanded=1) {
+basicstart = $1201      ; expanded
+screenmem = $1000
+colormem = $9400
+} else {
+basicstart = $1001      ; unexpanded
+screenmem = $1e00
+colormem = $9600
+}
+
+refdataoffs = (charsperline * (($100 / charsperline) + 2))
+
+
+                * = basicstart
+                !word basicstart + $0c
+                !byte $0a, $00
+                !byte $9e
+!if (expanded=1) {
+                !byte $34, $36, $32, $32
+} else {
+                !byte $34, $31, $31, $30
+}
+                !byte 0,0,0
+
+                * = basicstart + $0d
+start:
+        jmp start2
+                * = basicstart + $0100
+;-------------------------------------------------------------------------------
+start2:
+;        jsr $e518       ; init I/O
+
+        jsr screen_enable
+        jsr clrscr
+
+        lda #6
+        ldx #$ff
+-
+        dex
+        sta ERRBUF,x
+        bne -
+        
+        lda #5
+        sta ERRBUF+$ff
+
+        !if (1) {
+        ; run all tests once
+        ldx #0
+clp1b
+        txa
+        sta tmp
+        pha
+;        jsr $e518       ; init I/O
+        jsr calcaddr
+        jsr displayprepare
+        jsr dotest
+        pla
+        tax
+        inx
+        cpx #NUMTESTS
+        bne clp1b
+        }
+        !if (0) {
+        ; run only first test
+        lda #0
+        sta tmp
+;        jsr $e518       ; init I/O
+        jsr calcaddr
+        jsr displayprepare
+        jsr dotest
+        }
+
+loop
+;        jsr calcaddr
+;        jsr displayresults
+
+        ; wait for keypress
+wkey
+        cli
+        jsr $ffe4
+        cmp #"a"
+        bcc wkey
+        cmp #"a"+NUMTESTS
+        bcs wkey
+
+        tax
+        sec
+        sbc #"a"
+        sta tmp
+        txa
+        and #$3f
+        sta screenmem+((numrows-1)*charsperline)+(charsperline-1)
+
+        lda #$00
+        sta viabase+$0
+-       lda viabase+$1
+        cmp #$ff
+        bne -
+        lda #$ff
+        sta viabase+$0
+
+        jsr calcaddr
+        jsr displayprepare
+        jsr dotest
+;        jsr calcaddr
+;        jsr displayresults
+        jmp loop
+
+;-------------------------------------------------------------------------------
+
+dotest
+        inc $900F
+
+        lda #$20
+        ldx #0
+clp1    sta screenmem,x
+        inx
+        bne clp1
+
+        lda #0
+        sta addr+1
+        lda tmp
+        asl
+        rol addr+1
+        asl
+        rol addr+1
+        asl
+        rol addr+1
+        asl
+        rol addr+1
+        asl
+        rol addr+1
+!if (TESTLEN >= $40) {
+        asl
+        rol addr+1
+}
+        sta addr
+        sta jumpaddr+1
+        lda #>TESTSLOC        ; testloc hi
+        clc
+        adc addr+1
+        sta addr+1
+        sta jumpaddr+2
+
+;        ldy #0
+;clp2:
+;        lda (addr),y
+;        sta drivecode_dotest,y
+;        iny
+;        cpy #TESTLEN
+;        bne clp2
+
+;        lda #<drivecode
+;        ldy #>drivecode
+;        ldx #((drivecode_end - drivecode) + $1f) / $20 ; upload x * $20 bytes to 1541
+;        jsr upload_code
+
+;        lda #<drivecode_exec
+;        ldy #>drivecode_exec
+;        jsr start_code
+test:
+        sei
+;        jsr rcv_init
+
+;        dec $900F
+
+;        ldx #$a0
+;-
+
+        jsr screen_disable
+
+        lda #250 >> 1
+        cmp $9004
+        bne *-3
+        cmp $9004
+        beq *-3
+
+;        dex
+;        bne -
+
+
+        inc $900F
+        jsr dotest2
+
+        dec $900F
+
+        jsr screen_enable
+
+;jmp test
+;        jsr rcv_wait
+
+        ; recieve the result data
+;        jsr calcaddr
+;        ldy #$00
+;-
+;        jsr rcv_1byte
+;        lda (addr),y
+;        sta screenmem,y
+;        iny
+;        bne -
+
+        ;jsr $fda3
+        ;cli
+
+        jsr calcaddr
+
+        ; copy data from screen to TMP
+        ldy #0
+-
+        lda screenmem,y
+        sta (addr),y
+        iny
+        bne -
+
+ ;       dec $900F
+
+        jsr displayresults
+
+;        inc $900F
+
+        ; delay here long enough so the floppy is done resetting itself
+;        ldx #$80
+;-
+;        lda #$0f
+;        cmp $9004
+;        bne *-3
+;        cmp $9004
+;        beq *-3
+
+;        dex
+;        bne -
+
+;        dec $900F
+;        cli
+        rts
+
+calcaddr:
+        lda tmp
+        clc
+        adc #>TMP
+        sta addr+1
+        lda #<TMP
+        sta addr
+
+        lda tmp
+        clc
+        adc #>DATA
+        sta add2+1
+        lda #<DATA
+        sta add2
+        rts
+
+displayprepare:
+        lda #0
+        sta $900f
+
+        ldy #0
+-
+        lda #$20
+        sta screenmem,y
+;        sta screenmem+$100,y
+        lda (add2),y
+        sta screenmem+refdataoffs,y
+        iny
+        bne -
+
+        ldx tmp
+        lda #1
+        sta ERRBUF,x
+        
+        jsr .displaytestid
+        jsr .displayerrlog
+
+        rts
+
+.displaytestid:
+        ; test id in bottom right corner
+        ldx tmp
+        txa
+        clc
+        adc #"a"
+        and #$3f
+        sta screenmem+((numrows-1)*charsperline)+(charsperline-1)
+        rts
+
+
+.displayerrlog:
+        ; show errors
+        ldy #0
+clp1a
+        tya
+        clc
+        adc #"a"
+        and #$3f
+        sta screenmem+((numrows-1)*charsperline),y
+        lda ERRBUF,y
+        sta colormem+((numrows-1)*charsperline),y
+        iny
+        cpy #NUMTESTS
+        bne clp1a
+        rts
+
+displayresults:
+
+        jsr .displaytestid
+
+        ldx tmp
+        lda #5
+        sta ERRBUF,x
+
+        ; check TMP for errors, display screen
+        lda #$05        ; green
+        ldy #0
+lla     sta colormem,y
+        sta colormem+refdataoffs,y
+        iny
+        bne lla
+
+        ldy #0
+ll      lda (addr),y
+        sta screenmem,y
+        ;lda screenmem,y
+        ;sta (addr),y
+        cmp (add2),y
+        beq rot
+        lda #$02        ; red
+        sta colormem,y
+        sta colormem+refdataoffs,y
+
+        sta ERRBUF,x
+        sta ERRBUF+$ff
+rot
+        lda (add2),y
+        sta screenmem+refdataoffs,y
+;        eor (addr),y
+;        sta screenmem+(16*charsperline),y
+        iny
+        bne ll
+
+        jsr .displayerrlog
+
+        lda ERRBUF+$ff
+;        sta $900F
+        lda #0
+;        sta $d021
+
+        rts
+
+;-------------------------------------------------------------------------------
+
+dotest2:
+        sei
+        jsr .setdefaults
+
+        ldy #$00
+-
+        iny
+        bne -
+
+        ; call actual test
+jumpaddr:
+        jsr $dead
+
+        sei
+        jsr .setdefaults
+
+        ; (more or less) reset VIA regs and drive
+;        sei
+        rts
+
+.setdefaults:
+!if (1) {
+        ; serial port: disabled
+        ; timer A: count clk, continuous, Timed Interrupt when Timer 1 is loaded, no PB7
+        ; timer B: count PB6 pulses (=stop)
+        ; port A latching disabled
+        ; port B latching disabled
+        lda #%00100000
+        sta viabase+$b
+        ; CB2 Control: Input negative active edge 
+        ; CB1 Interrupt Control: Negative active edge
+        ; CA2 Control: Input negative active edge 
+        ; CA1 Interrupt Control: Negative active edge
+        lda #0
+        sta viabase+$c
+
+        lda #$a5
+        sta viabase+$a ; serial shift register
+
+        ; disable IRQs
+        lda #$7f
+        sta viabase+$e
+
+        ; acknowledge pending IRQs
+        lda viabase+$d
+        sta viabase+$d
+
+        ; DDR output
+        lda #$ff
+        sta viabase+$2
+        sta viabase+$3
+
+        lda #$ff
+        sta viabase+$0
+        sta viabase+$1
+
+        ; DDR input
+        lda #0
+        sta viabase+$2
+        sta viabase+$3
+
+        ; init timers
+        ; after this all timer values will be = $0000
+        ldy #0
+        tya
+t1a     sta viabase+$5  ; Timer A lo
+        sta viabase+$4
+        sta viabase+$7
+        sta viabase+$6
+        sta viabase+$9
+        sta viabase+$8
+        dey
+        bne t1a
+        rts
+}
+
+;-------------------------------------------------------------------------------
+
+clrscr:
+        ldx #0
+        stx $900F
+clp:
+        lda #$20
+        sta screenmem,x
+        sta screenmem+$0100,x
+        sta screenmem+$0200,x
+        lda #1
+        sta colormem,x
+        sta colormem+$0100,x
+        sta colormem+$0200,x
+        inx
+        bne clp
+        rts
+
+screen_disable:
+        lda #250 >> 1
+        cmp $9004
+        bne *-3
+        cmp $9004
+        beq *-3
+
+        lda #0
+        sta $9002
+        sta $9003
+        rts
+screen_enable:
+        lda #250 >> 1
+        cmp $9004
+        bne *-3
+        cmp $9004
+        beq *-3
+
+        lda #$00 | charsperline
+        sta $9002
+        lda #numrows << 1
+        sta $9003
+
+        lda #12 - 2
+        sta $9000
+        lda #38 - 2
+        sta $9001
+        rts
+
+hextab:
+        !byte $30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$01,$02,$03,$04,$05,$06
---
+++ testprogs/VIC20/viavarious/readme.txt
@@ -0,0 +1,99 @@
+This directory contains various VIA tests, ported from Andre Fachats 
+"ciavarious" programs.
+
+The idea is that Andres programs cover a bunch of things that likely matter for
+the VIA too. The respective tests have been reworked to match what the original
+tests want to do as close as possible. Consequently tests for cascaded timers,
+and TOD clock tests, have been removed, as VIAs dont have these features.
+
+the tests should be run on a VIC20 with (at least) 8k RAM expansion (block1)
+
+when run, each program will cycle through all sub tests once and then display
+the results. red/green characters at bottom left indicate success/failure of
+the sub tests. red/green border indicates success/failure of the complete test
+(shows red if one of the sub tests failed). at the top of the screen you can
+see the measured data, at the bottom the reference data.
+
+--------------------------------------------------------------------------------
+
+      (r28197)
+
+VIA1:   works   Timer A / B
+VIA2:   works   Timer A / B
+
+VIA3:   fail    Timer A / B IRQ Flags
+VIA3a:  fail    Timer A IRQ Flags
+
+VIA4:   fail    Timer A (toggle timer A timed/continous irq)
+VIA4a:  fail    Timer A (toggle timer A timed/continous irq)
+
+VIA5:   fail    Timer A / B IRQ Flags
+VIA5a:  fail    Timer A / B IRQ Flags
+
+VIA9:   fail    Timer B (toggle timer B counts PB6/Clock)
+
+TODO: these need proper reference data:
+
+VIA10:  fail    Port B (output timer at PB7 and read back PB)
+VIA11:  fail    Port B (output timer at PB7 and read back PB)
+VIA12:  fail    Port B (output timer at PB7 and read back PB)
+VIA13:  fail    Port B (output timer at PB7 and read back PB)
+
+(gpz) reference data comes from the respective 1541 tests, VIA1-VIA9 are ok on
+      my actual VIC20, VIA10-13 need reference data fixed!
+
+TODO:
+
+- add reading and/or writing Timer A latches to all tests
+- test input latching
+- test serial shift register
+
+--------------------------------------------------------------------------------
+Following is a brief overview of how certain CIA features are related to the
+respective VIA features (in reality it can be assumed that CIA was actually
+developed by using the VIA masks and extending them - simply because that would
+save a lot of time and eventually very expensive test runs).
+
+CIA      VIA
+
+$dc00 -> $9111  Port A Data
+n/a      $911f  Port A Data (no handshake)
+$dc01 -> $9110  Port B Data
+$dc02 -> $9113  Port A DDR
+$dc03 -> $9112  Port B DDR
+
+$dc04 -> $9114  Timer A lo
+$dc05 -> $9115  Timer A hi
+n/a      $9116  Timer A Latch lo
+n/a      $9117  Timer A Latch hi
+$dc06 -> $9118  Timer B lo
+$dc07 -> $9119  Timer B hi
+
+$dc08 -> n/a    TOD 10th sec
+$dc09 -> n/a    TOD sec
+$dc0a -> n/a    TOD min
+$dc0b -> n/a    TOD hour
+
+$dc0c -> $911a  Synchronous Serial I/O Data Buffer
+
+$dc0d -> $911d  IRQ CTRL  (w:Enable Mask / r:Acknowledge)
+         $911e  IRQ flags
+
+$dc0e ->        CTRL A (TimerA)
+$dc0f ->        CTRL B (TimerB)
+
+         $911b  Aux Control (TimerA, TimerB)
+         $911c  Peripherial Control
+
+- no cascade mode for timers
+- timers run always
+  - VIA1 timer B can be stopped by configuring it to count PB6 transitions. 
+    since the drive-number jumpers are wired there, it will not count.
+- only first timer can be output at port B
+- no TOD clock
++ seperate register for the Timer A latch
+
+http://www.zimmers.net/anonftp/pub/cbm/documents/chipdata/6522-VIA.txt
+http://www.zimmers.net/anonftp/pub/cbm/schematics/computers/vic20/251027l2.gif
+
+--------------------------------------------------------------------------------
